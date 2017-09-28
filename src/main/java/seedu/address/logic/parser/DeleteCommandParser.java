0188fb2885e23945163836442991fa69640d270d
17navasaw <17navasaw@gmail.com>
2017-09-28 17:25:50 +0800

    Edited DeleteCommandParser to parse more than one index

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,6 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.ArrayList;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DeleteCommand;
@@ -19,8 +21,16 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      */
     public DeleteCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            String trimmedArgs = args.trim();
+            String[] indicesInString = trimmedArgs.split("\\s+");
+
+            ArrayList<Index> indices = new ArrayList<>();
+            for (int i=0;i<indicesInString.length;i++) {
+                Index index = ParserUtil.parseIndex(indicesInString[i]);
+                indices.add(index);
+            }
+
+            return new DeleteCommand(indices);
         } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
