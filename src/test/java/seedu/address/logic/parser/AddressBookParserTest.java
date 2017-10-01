bcd1c857dffec3e49b2f33f2c6ab2fb612adc334
Xiang Hailin <895227442@qq.com>
2017-10-01 15:39:26 +0800

    edit JUnit method names

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -186,7 +186,7 @@ public class AddressBookParserTest {
     }
 
     @Test
-    public void parseCommand_redoCommandWord_returnsRedoCommand_alias() throws Exception {
+    public void parseCommand_redoCommandAlias_returnsRedoCommand() throws Exception {
         assertTrue(parser.parseCommand(RedoCommand.COMMAND_ALIAS) instanceof RedoCommand);
         assertTrue(parser.parseCommand("redo 1") instanceof RedoCommand);
     }
@@ -198,7 +198,7 @@ public class AddressBookParserTest {
     }
 
     @Test
-    public void parseCommand_undoCommandWord_returnsUndoCommand_alias() throws Exception {
+    public void parseCommand_undoCommandAlias_returnsUndoCommand() throws Exception {
         assertTrue(parser.parseCommand(UndoCommand.COMMAND_ALIAS) instanceof UndoCommand);
         assertTrue(parser.parseCommand("undo 3") instanceof UndoCommand);
     }
