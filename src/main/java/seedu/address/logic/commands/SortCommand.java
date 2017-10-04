27454278b47d80f18c7f04c450bf900a4d79d55e
Emil Koh <e0014667@u.nus.edu>
2017-10-04 19:08:58 +0800

    new SortCommand class

---
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD //need to edit, ripped from DeleteCommand
+            + ": Sorts Address Book contacts according to specified field.\n"
+            + "Parameters: TYPE (must be 'name', 'phone', or 'email')\n"
+            + "Example: " + COMMAND_WORD + " name";
+
+    private String sortType;
+
+    public String MESSAGE_SUCCESS = "Sorted all persons by ";
+
+    public SortCommand(String type){
+        this.sortType = type;
+    }
+
+    @Override
+    public CommandResult execute() {
+        //sort method calls goes here
+        switch (sortType){
+            case "name":
+                //insert sort method call here
+                break;
+
+            case "phone":
+                //insert sort method call here
+                break;
+
+            case "email":
+                //insert sort method call here
+                break;
+        }
+
+        if(sortType.equals("phone")) {sortType = sortType + " number"; }
+        //lists all contacts after sorting
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS + sortType);
+    }
+}
