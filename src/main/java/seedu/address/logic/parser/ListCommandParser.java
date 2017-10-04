7f805151d48f268799bf192806824debc8547724
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-10-04 14:12:17 +0800

    ListCommandParser.java: add import for ListCommand ListCommandParser.java: add import for MESSAGE_INVALID ListCommandParser.java: add import for ParseException ListCommandParser.java: add parse method

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -1,4 +1,29 @@
 package seedu.address.logic.parser;
 
-public class ListCommandParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import
+
+/**
+ * Parses input arguments and creates a new ListCommand object
+ */
+public class ListCommandParser implements Parser<ListCommand>{
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListCommand
+     * and returns an ListCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ListCommand parse(String args) throws ParseException {
+        try {
+            String trimmedArgs = args.trim();
+            return new ListCommand(trimmedArgs);
+        } catch (IllegalArgumentException iae) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+        }
+    }
+
 }
