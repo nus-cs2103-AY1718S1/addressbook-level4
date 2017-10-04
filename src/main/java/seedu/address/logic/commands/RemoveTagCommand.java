125d7309d3cf7143e3546acbf439d7df0bb947c7
Jackie Tan <free-soup@hotmail.com>
2017-10-05 02:09:11 +0800

    Clean up RemoveTagCommand class.

--- src/main/java/seedu/address/logic/commands/RemoveTagCommand.java
+++ src/main/java/seedu/address/logic/commands/RemoveTagCommand.java
@@ -2,12 +2,15 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
 
 
 /**
- * Adds a person to the address book.
+ * Removes a tag from the address book.
  */
 public class RemoveTagCommand extends UndoableCommand {
 
@@ -17,12 +20,12 @@ public class RemoveTagCommand extends UndoableCommand {
 
     public static final String MESSAGE_SUCCESS = "Tag removed";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
-
+    public static final String MESSAGE_NOT_DELETED = "Tag not deleted";
 
     private final String toRemove;
 
     /**
-     * Creates an RemoveTagCommand to add the specified {@code ReadOnlyPerson}
+     * Creates an RemoveTagCommand to remove the specified {@code Tag}
      */
     public RemoveTagCommand (String tag) {
         toRemove = tag.trim();
@@ -34,8 +37,12 @@ public class RemoveTagCommand extends UndoableCommand {
         try {
             model.removeTag(new Tag(toRemove));
             return new CommandResult(String.format(MESSAGE_SUCCESS));
-        } catch (Exception e) {
+        } catch (DuplicatePersonException e) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        } catch (PersonNotFoundException e) {
+            throw new CommandException(MESSAGE_NOT_DELETED);
+        } catch (IllegalValueException e) {
+            throw new CommandException(MESSAGE_NOT_DELETED);
         }
     }
 }
