51c4f5ec987cafb7547af8799a4eef4b5bedb3ba
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-10-04 21:29:23 +0800

    ListCommandParser.java: add import for PREFIX_TAG ListCommandParser.java: add import for Collection,Set ListCommandParser.java: add import for Tag ListCommandParser.java: add condition for empty argument

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -1,10 +1,15 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import
+
+import java.util.Collection;
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new ListCommand object
@@ -19,7 +24,15 @@ public class ListCommandParser implements Parser<ListCommand>{
     public ListCommand parse(String args) throws ParseException {
         try {
             String trimmedArgs = args.trim();
+            if(trimmedArgs.isEmpty()) {
+               return new ListCommand(); 
+            }
+
+            //TODO: convert the string to collection to pass to parseTags
+            
+            Set<Tag> inputTag = ParserUtil.parseTags();
             return new ListCommand(trimmedArgs);
+            
         } catch (IllegalArgumentException iae) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
