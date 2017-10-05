9bcc4fc85b2c1b9208947bad052c8c0e84eca49b
Srivatsa <p.psrivatsa@gmail.com>
2017-10-05 17:59:18 +0800

    Update Model Interface with deleteTag method

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -6,7 +6,7 @@ import javafx.collections.ObservableList;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
-
+import seedu.address.model.tag.Tag;
 /**
  * The API of the Model component.
  */
@@ -22,6 +22,9 @@ public interface Model {
 
     /** Deletes the given person. */
     void deletePerson(ReadOnlyPerson target) throws PersonNotFoundException;
+
+    /** Deletes given tag from everyone in the addressbook */
+    void deleteTag(Tag tag) throws PersonNotFoundException, DuplicatePersonException;
 
     /** Adds the given person */
     void addPerson(ReadOnlyPerson person) throws DuplicatePersonException;
