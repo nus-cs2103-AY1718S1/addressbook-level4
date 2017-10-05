e0e41517b122fccfee54250718d1f0b7197a3c86
Vanessa Lim <31247542+vlimshimin@users.noreply.github.com>
2017-10-05 11:20:28 +0800

    Edit style and format in Storage

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -61,11 +61,6 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         saveAddressBook(addressBook, filePath);
     }
 
-    @Override
-    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-        saveAddressBook(addressBook, filePath + "-backup.xml");
-    }
-
     /**
      * Similar to {@link #saveAddressBook(ReadOnlyAddressBook)}
      * @param filePath location of the data. Cannot be null
@@ -77,6 +72,11 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         File file = new File(filePath);
         FileUtil.createIfMissing(file);
         XmlFileStorage.saveDataToFile(file, new XmlSerializableAddressBook(addressBook));
+    }
+
+    @Override
+    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
+        saveAddressBook(addressBook, filePath + "-backup.xml");
     }
 
 }
