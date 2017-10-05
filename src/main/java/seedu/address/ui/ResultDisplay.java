94b394b586935673061d07b6beb83dd8d2e3fb6c
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-10-06 04:08:46 +0800

    Fix trailing white space

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -18,9 +18,7 @@ import seedu.address.commons.events.ui.NewResultAvailableEvent;
  * A ui for the status bar that is displayed at the header of the application.
  */
 public class ResultDisplay extends UiPart<Region> {
-
     public static final String ERROR_STYLE_CLASS = "error";
-
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "ResultDisplay.fxml";
 
@@ -48,17 +46,17 @@ public class ResultDisplay extends UiPart<Region> {
 
     }
 
-        private void setStyleToDefault() {
-            resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
-        }
-
-        private void setStyleToIndicateCommandFailure() {
-            ObservableList<String> styleClass = resultDisplay.getStyleClass();
-
-            if (styleClass.contains(ERROR_STYLE_CLASS)) {
-                return;
-            }
-
-            styleClass.add(ERROR_STYLE_CLASS);
-        }
+    private void setStyleToDefault() {
+        resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
     }
+
+    private void setStyleToIndicateCommandFailure() {
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if (styleClass.contains(ERROR_STYLE_CLASS)) {
+            return;
+        }
+
+        styleClass.add(ERROR_STYLE_CLASS);
+    }
+}
