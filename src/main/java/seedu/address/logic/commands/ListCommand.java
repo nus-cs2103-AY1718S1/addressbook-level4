75ff4acc9c7095817f0ca2e66fd9351d078592ec
Wang Junming <e0134140@u.nus.edu>
2017-10-06 11:05:26 +0800

    update ListCommand to deal with two conditions

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,5 +1,12 @@
 package seedu.address.logic.commands;
 
+import javafx.collections.ObservableList;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.UniqueAddressPredicate;
+
+import java.util.HashSet;
+import java.util.List;
+
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
@@ -9,12 +16,47 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": List all persons or all attributes(if specified) and "
+            + "displays them as a list with index numbers.\n"
+            + "Parameters: ATTRIBUTE NAME\n"
+            + "Example: " + COMMAND_WORD + " address";
 
+    public static final String MESSAGE_SUCCESS = "Listed all %1$s";
+
+    public static final String DEFAULT_LISTING_ELEMENT = "Persons";
+
+    private final String attName;
+
+    public ListCommand(String attributeName) { this.attName = attributeName; }
+
+    public ListCommand() { this.attName = DEFAULT_LISTING_ELEMENT; };
+
+    private boolean hasAttribute() {
+        return attName.equals(DEFAULT_LISTING_ELEMENT);
+    }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (hasAttribute()) {
+            UniqueAddressPredicate predicate = new UniqueAddressPredicate(getUniqueAdPersonSet());
+            model.updateFilteredPersonList(predicate);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, attName));
+        } else {
+            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, attName));
+        }
+    }
+
+    private HashSet<ReadOnlyPerson> getUniqueAdPersonSet() {
+        HashSet<ReadOnlyPerson> set = new HashSet<>();
+
+        ObservableList<ReadOnlyPerson> personLst = model.getFilteredPersonList();
+        for(ReadOnlyPerson p : personLst) {
+            if (!set.contains(p)) {
+                set.add(p);
+            }
+        }
+        return set;
+
     }
 }
