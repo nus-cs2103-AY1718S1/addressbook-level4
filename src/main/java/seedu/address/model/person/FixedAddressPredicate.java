a551a63a8defccaa942f8a84f0a7a8f884991420
Wang Junming <e0134140@u.nus.edu>
2017-10-06 18:36:20 +0800

    Remove trailing whitespace

--- src/main/java/seedu/address/model/person/FixedAddressPredicate.java
+++ src/main/java/seedu/address/model/person/FixedAddressPredicate.java
@@ -18,7 +18,7 @@ public class FixedAddressPredicate implements Predicate<ReadOnlyPerson> {
             return true;
         } else {
             return false;
-        } 
+        }
     }
 
     @Override
