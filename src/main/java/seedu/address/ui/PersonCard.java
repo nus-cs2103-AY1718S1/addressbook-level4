720674efd2b90b718e1ef08ebb6f42c21079b6a6
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-10-06 21:11:13 +0800

    Travis CI : Fix 4

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -53,7 +53,8 @@ public class PersonCard extends UiPart<Region> {
         initTags(person);
         bindListeners(person);
     }
-    private static String getColorForTag(String tagValue){
+
+    private static String getColorForTag(String tagValue) {
         if (!tagColors.containsKey(tagValue)) {
             tagColors.put(tagValue, colors[random.nextInt(colors.length)]);
         }
