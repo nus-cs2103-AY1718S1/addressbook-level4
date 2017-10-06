b987a0c55446b21cd8b0603cfb64d031598a0ccd
Wang Junming <e0134140@u.nus.edu>
2017-10-06 17:21:06 +0800

    let addressbookparser accept view command but just throw exception

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 
 public class ViewCommand extends Command{
+
+    public static final String COMMAND_WORD = "view";
+
     @Override
     public CommandResult execute() throws CommandException {
         return null;
