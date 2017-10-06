379b0221475b6a40fabc49d7d4d3788ba412662c
Wang Junming <e0134140@u.nus.edu>
2017-10-06 17:36:07 +0800

    implement view command

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -1,13 +1,62 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
+import java.util.List;
 
-public class ViewCommand extends Command{
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.ListingUnit;
+import seedu.address.model.person.ReadOnlyPerson;
+
+import static seedu.address.model.ListingUnit.PERSON;
+
+/**
+ * Views all persons with the selected listing unit from the address book.
+ */
+public class ViewCommand extends Command {
 
     public static final String COMMAND_WORD = "view";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Views all persons with the selected listing attribute from the address book.\n"
+            + " It will simply listing the person of select index if the panel is currently listing all persons.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "%1$s person founded";
+
+    private final Index targetIndex;
+
+    public ViewCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
     @Override
     public CommandResult execute() throws CommandException {
-        return null;
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+        ListingUnit currentUnit = ListingUnit.getCurrentListingUnit();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        if (currentUnit.equals(PERSON)) {
+            ReadOnlyPerson personToView = lastShownList.get(targetIndex.getZeroBased());
+        } else {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, targetIndex.getOneBased()));
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ViewCommand // instanceof handles nulls
+                && this.targetIndex.equals(((ViewCommand) other).targetIndex)); // state check
     }
 }
