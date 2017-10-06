129dba699deea63d7e3eaac21c5956e5df1ad9eb
marvinchin <chinkunsong.marvin@gmail.com>
2017-10-06 14:44:55 +0800

    Add parser to parse export command args

---
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.ExportCommand;
+
+public class ExportCommandParserTest {
+
+    private ExportCommandParser parser = new ExportCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsExportCommand() {
+        String args = " some/path/somewhere  ";
+        assertParseSuccess(parser, args, new ExportCommand("some/path/somewhere"));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        String args = "";
+        assertParseFailure(parser, args, String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE));
+    }
+}
