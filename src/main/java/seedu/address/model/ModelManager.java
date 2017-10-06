c0ba2f6e9178265aa0e35a27c379a18388eadd1d
DESKTOP-7SUIEBU\Jasmine <jgreenteaj@gmail.com>
2017-10-06 19:23:52 +0800

    ModelManager: changed removeTags() method

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
@@ -92,7 +93,7 @@ public class ModelManager extends ComponentManager implements Model {
             ReadOnlyPerson oldPerson = addressBook.getPersonList().get(i);
 
             Person newPerson = new Person(oldPerson);
-            Set<Tag> newTags = newPerson.getTags();
+            Set<Tag> newTags = new HashSet<Tag>(newPerson.getTags());
             newTags.remove(tag);
             newPerson.setTags(newTags);
             addressBook.updatePerson(oldPerson, newPerson);
