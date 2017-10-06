2dd9d3158030bca5e4afe65c1c0249883f516a95
jo-lyn <e0007247@u.nus.edu>
2017-10-06 22:36:58 +0800

    Add unit test to removeTag

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
@@ -89,9 +90,9 @@ public class ModelManager extends ComponentManager implements Model {
         for (ReadOnlyPerson person: addressBook.getPersonList()) {
             Person newPerson = new Person(person);
 
-            Set<Tag> newTags = newPerson.getTags();
+            Set<Tag> newTags = new HashSet<>(newPerson.getTags());
             newTags.remove(tag);
-            newPerson.setTags(newTags);
+                      newPerson.setTags(newTags);
 
             addressBook.updatePerson(person, newPerson);
         }
