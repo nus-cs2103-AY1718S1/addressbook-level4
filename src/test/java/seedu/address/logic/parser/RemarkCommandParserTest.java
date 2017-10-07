25f2fe1aedbec66f8c746b4b142d349e279988a3
heiseish <dtrnggiang@gmail.com>
2017-10-07 09:37:36 +0800

    fix checkStyleTest

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -2,8 +2,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.REMARK_DESC_ANY;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.REMARK_DESC_ANY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
