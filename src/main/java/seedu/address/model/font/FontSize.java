063dc811223c9b2789a5503839c19135c82dd5a9
Daniel Chew <cctdaniel@outlook.com>
2017-10-07 21:47:52 +0800

    Added a new Customise Command to change font size and created new class FontSize

---
+++ src/main/java/seedu/address/model/font/FontSize.java
@@ -0,0 +1,48 @@
+package seedu.address.model.font;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents the font size of the AddressBook.
+ * Guarantees: immutable; is valid as declared in {@link #isValidFontSize(String)}
+ */
+public class FontSize {
+
+    public static final String MESSAGE_FONT_SIZE_CONSTRAINTS =
+            "Font size can only be either \"xsmall\", \"small\", \"normal\", \"large\",  or \"xlarge\" (case-sensitive).";
+    public static final String[] FONT_SIZE_VALIDATION = {"xsmall", "small", "normal", "large", "xlarge"};
+    public final String value;
+
+    /**
+     * Validates given font size.
+     *
+     * @throws IllegalValueException if given font size is invalid.
+     */
+    public FontSize(String fontSize) throws IllegalValueException {
+        requireNonNull(fontSize);
+        if (!isValidFontSize(fontSize)) {
+            throw new IllegalValueException(MESSAGE_FONT_SIZE_CONSTRAINTS);
+        }
+        this.value = fontSize;
+    }
+
+    /**
+     * Returns true if a given string is a valid font size.
+     */
+    public static boolean isValidFontSize(String test) {
+        for (String s : FONT_SIZE_VALIDATION) {
+            if (test.equalsIgnoreCase(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
