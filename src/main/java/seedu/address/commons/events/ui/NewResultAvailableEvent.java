60a828bf4c79eac35df6bd7a9b7ff16f021c5855
blaqkrow <joshnfsmw@hotmail.com>
2017-10-08 01:47:33 +0800

    added a check for valid commands
    if invalid, text would be red in colour

--- src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
+++ src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
@@ -8,14 +8,29 @@ import seedu.address.commons.events.BaseEvent;
 public class NewResultAvailableEvent extends BaseEvent {
 
     public final String message;
+    public final boolean unknownCommandEntered;
 
     public NewResultAvailableEvent(String message) {
         this.message = message;
+        if(message.equals("Unknown command")) {
+            unknownCommandEntered = true;
+        }
+        else {
+            unknownCommandEntered = false;
+        }
+        
     }
 
     @Override
     public String toString() {
         return this.getClass().getSimpleName();
+    }
+
+    /**
+     * returns if command entered is valid
+     */
+    public boolean isUnknownCommandEntered() {
+        return unknownCommandEntered;
     }
 
 }
