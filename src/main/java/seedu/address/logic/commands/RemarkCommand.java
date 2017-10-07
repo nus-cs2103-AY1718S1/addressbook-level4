f11dd88372ad79ddb1f18966a5c9593e73c28793
Joshua <joshua.nkj@gmail.com>
2017-10-07 17:45:14 +0800

    Model: Added Remark class
    . Add Remark to model component (copy from Address, remove the regex and
    rename accordingly).
    . Add test for Remark (to test the equals method).
    . Modify RemarkCommand to now take in a Remark instead of a String.

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.Remark;
 
 public class RemarkCommand extends UndoableCommand {
 
@@ -21,7 +22,7 @@ public class RemarkCommand extends UndoableCommand {
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
 
     private final Index index;
-    private final String remark;
+    private final Remark remark;
 
     /**
      *
@@ -29,7 +30,7 @@ public class RemarkCommand extends UndoableCommand {
      * @param remark of the person
      */
 
-    public RemarkCommand (Index index, String remark) {
+    public RemarkCommand (Index index, Remark remark) {
         requireNonNull(index);
         requireNonNull(remark);
 
--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -33,6 +33,7 @@ import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
@@ -126,11 +127,11 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_remarkCommandWord_returnsRemarkCommand() throws Exception {
-        final String remark = "Some remark.";
+        final Remark remark = new Remark("Some remark.");
         RemarkCommand command = (RemarkCommand) parser.parseCommand(RemarkCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_PERSON.getOneBased()
                 + " " + PREFIX_REMARK
-                + " " + remark);
+                + " " + remark.value);
         assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, remark), command);
     }
 
