645858fd7da8a476280ac1454d9dc53f3e66e3c3
blaqkrow <joshnfsmw@hotmail.com>
2017-10-08 01:49:49 +0800

    fixed white space

--- src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
+++ src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
@@ -18,7 +18,6 @@ public class NewResultAvailableEvent extends BaseEvent {
         else {
             unknownCommandEntered = false;
         }
-        
     }
 
     @Override
@@ -32,5 +31,4 @@ public class NewResultAvailableEvent extends BaseEvent {
     public boolean isUnknownCommandEntered() {
         return unknownCommandEntered;
     }
-
 }
