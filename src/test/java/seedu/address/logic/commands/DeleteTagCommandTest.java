3f90f65197ec167536d84a267f24842cdcc1f587
Alexander Lee <alexleegs@gmail.com>
2017-10-08 19:53:59 +0800

    style 3

--- src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Test;
