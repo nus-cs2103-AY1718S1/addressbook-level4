13f0854f6e21f36987cf6e97c54275b4f0584e6f
Alexander Lee <alexleegs@gmail.com>
2017-10-08 19:17:15 +0800

    Style update

--- src/main/java/seedu/address/logic/commands/DeleteTagCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTagCommand.java
@@ -1,17 +1,16 @@
 package seedu.address.logic.commands;
 
 import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.person.exceptions.TagNotFoundException;
 import seedu.address.model.tag.Tag;
 
-import java.util.List;
-
+/**
+ * Deletes a tag identified using its name from the address book.
+ */
 public class DeleteTagCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "deletetag";
     public static final String COMMAND_ALIAS = "dt";
@@ -36,7 +35,7 @@ public class DeleteTagCommand extends UndoableCommand {
         try {
             model.deleteTag(targetTag);
         } catch (DuplicatePersonException | PersonNotFoundException ex) {
-            assert false: "Not creating a new person";
+            assert false : "Not creating a new person";
         } catch (TagNotFoundException tnfe) {
             throw new CommandException(Messages.MESSAGE_INVALID_TAG_DISPLAYED);
         }
