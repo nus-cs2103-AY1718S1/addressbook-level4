8fe379d5a4d61515f60c97f09166bd2e72c8c559
marvinchin <chinkunsong.marvin@gmail.com>
2017-10-08 16:23:19 +0800

    Move model stubs to separate package to be shared among tests

---
+++ src/test/java/seedu/address/testutil/modelstubs/ModelStubAcceptingPersonAdded.java
@@ -0,0 +1,34 @@
+package seedu.address.testutil.modelstubs;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+
+/**
+ * A Model stub that always accept the person being added.
+ */
+public class ModelStubAcceptingPersonAdded extends ModelStub {
+    public final ArrayList<Person> personsAdded = new ArrayList<>();
+
+    @Override
+    public void addPerson(ReadOnlyPerson person) throws DuplicatePersonException {
+        personsAdded.add(new Person(person));
+    }
+
+    @Override
+    public void addPersons(Collection<ReadOnlyPerson> persons) {
+        for (ReadOnlyPerson person : persons) {
+            personsAdded.add(new Person(person));
+        }
+    }
+
+    @Override
+    public ReadOnlyAddressBook getAddressBook() {
+        return new AddressBook();
+    }
+}
---
+++ src/test/java/seedu/address/testutil/modelstubs/ModelStubThrowingDuplicatePersonException.java
@@ -0,0 +1,21 @@
+package seedu.address.testutil.modelstubs;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+
+/**
+ * A Model stub that always throw a DuplicatePersonException when trying to add a person.
+ */
+public class ModelStubThrowingDuplicatePersonException extends ModelStub {
+    @Override
+    public void addPerson(ReadOnlyPerson person) throws DuplicatePersonException {
+        throw new DuplicatePersonException();
+    }
+
+    @Override
+    public ReadOnlyAddressBook getAddressBook() {
+        return new AddressBook();
+    }
+}
