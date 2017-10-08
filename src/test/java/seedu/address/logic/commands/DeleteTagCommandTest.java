a13214ac6dee0ad6e1cb73ffc51872f6499dd670
Alexander Lee <alexleegs@gmail.com>
2017-10-08 19:46:42 +0800

    Style 2

--- src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
@@ -4,8 +4,8 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.*;
-
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Test;
