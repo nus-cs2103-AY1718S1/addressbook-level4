e0d3d099abc54d7c40b78e75f83ee95ef569fa2c
Jeremy Long Shi Wei <E0014824@u.nus.edu>
2017-10-10 00:37:19 +0800

    Travis Checkstyle

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -14,7 +14,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-
 import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
