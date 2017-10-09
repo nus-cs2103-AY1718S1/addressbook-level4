307811be912ca33d1ea8c9d0206cfcc41a023daa
khooroko <khoowp25@hotmail.com>
2017-10-09 15:46:12 +0800

    BrowserPanel.java: fix coding style

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -1,19 +1,13 @@
 package seedu.address.ui;
 
-import java.net.URL;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
 
-import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
-import javafx.scene.web.WebView;
-import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -22,10 +16,6 @@ import seedu.address.model.person.ReadOnlyPerson;
  * The Browser Panel of the App.
  */
 public class BrowserPanel extends UiPart<Region> {
-
-    public static final String DEFAULT_PAGE = "default.html";
-    public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
-    public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
 
     private static final String FXML = "BrowserPanel.fxml";
 
@@ -45,14 +35,15 @@ public class BrowserPanel extends UiPart<Region> {
     public BrowserPanel() {
         super(FXML);
 
-        // To prevent triggering events for typing inside the loaded Web page.
-        getRoot().setOnKeyPressed(Event::consume);
-
         loadDefaultPage();
         registerAsAnEventHandler(this);
     }
 
-    private void loadPersonPage(ReadOnlyPerson person) {
+    /**
+     * Loads the full info of the person
+     * @param person the selected person to display the full info of.
+     */
+    private void loadPersonInfo(ReadOnlyPerson person) {
         bindListeners(person);
     }
 
@@ -69,7 +60,7 @@ public class BrowserPanel extends UiPart<Region> {
     }
 
     /**
-     * Loads a default HTML file with a background that matches the general theme.
+     * Sets all info fields to not display anything.
      */
     private void loadDefaultPage() {
         name.setText("");
@@ -82,6 +73,6 @@ public class BrowserPanel extends UiPart<Region> {
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        loadPersonPage(event.getNewSelection().person);
+        loadPersonInfo(event.getNewSelection().person);
     }
 }
