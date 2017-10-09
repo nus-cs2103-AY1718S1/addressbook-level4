ae10e3afdf45ce243b2cf83fa5d8acdf56622c17
LawMan <lawrenciok@gmail.com>
2017-10-09 11:21:36 +0800

    Add Debt field to Person

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DEBT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -15,6 +16,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Debt;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -36,10 +38,10 @@ public class AddCommandParser implements Parser<AddCommand> {
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
-                        PREFIX_POSTAL_CODE, PREFIX_TAG);
+                        PREFIX_POSTAL_CODE, PREFIX_DEBT, PREFIX_TAG);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_POSTAL_CODE,
-                PREFIX_EMAIL)) {
+                PREFIX_EMAIL, PREFIX_DEBT)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
@@ -49,9 +51,10 @@ public class AddCommandParser implements Parser<AddCommand> {
             Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL)).get();
             Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS)).get();
             PostalCode postalCode = ParserUtil.parsePostalCode(argMultimap.getValue(PREFIX_POSTAL_CODE)).get();
+            Debt debt = ParserUtil.parseDebt(argMultimap.getValue(PREFIX_DEBT)).get();
             Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-            ReadOnlyPerson person = new Person(name, phone, email, address, postalCode, tagList);
+            ReadOnlyPerson person = new Person(name, phone, email, address, postalCode, debt, tagList);
 
             return new AddCommand(person);
         } catch (IllegalValueException ive) {
---
+++ src/main/java/seedu/address/model/person/Debt.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+//@@author lawwman
+/**
+ * Represents a Person's debt or amount owed in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDebt(String)}
+ */
+public class Debt {
+
+    public static final String MESSAGE_DEBT_CONSTRAINTS =
+            "Debt can only contain numbers, and should have 1 or more digits";
+    public static final String DEBT_VALIDATION_REGEX = "\\d+";
+    public final String value;
+
+    /**
+     * Validates given debt.
+     *
+     * @throws IllegalValueException if given phone string is invalid.
+     */
+    public Debt(String debt) throws IllegalValueException {
+        requireNonNull(debt);
+        String trimmedDebt = debt.trim();
+        if (!isValidDebt(trimmedDebt)) {
+            throw new IllegalValueException(MESSAGE_DEBT_CONSTRAINTS);
+        }
+        this.value = trimmedDebt;
+    }
+
+    /**
+     * Returns true if a given string is a valid person phone number.
+     */
+    public static boolean isValidDebt(String test) {
+        return test.matches(DEBT_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Debt // instanceof handles nulls
+                && this.value.equals(((Debt) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
