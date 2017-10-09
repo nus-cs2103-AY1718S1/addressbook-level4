e39bd097c50725bfa2cb779680d2af5e7d8b9d00
Eric Loh <ericlohyg@gmail.com>
2017-10-09 23:51:52 +0800

    Add tests for UniqueTagListTest.java

--- src/test/java/seedu/address/model/UniqueTagListTest.java
+++ src/test/java/seedu/address/model/UniqueTagListTest.java
@@ -4,9 +4,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
+import seedu.address.testutil.TypicalPersons;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UniqueTagListTest {
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
@@ -15,5 +22,22 @@ public class UniqueTagListTest {
         UniqueTagList uniqueTagList = new UniqueTagList();
         thrown.expect(UnsupportedOperationException.class);
         uniqueTagList.asObservableList().remove(0);
+    }
+
+    @Test
+    public void TagsTests() throws IllegalValueException {
+        UniqueTagList uniqueTagList = new UniqueTagList();
+        uniqueTagList.setTags(TypicalPersons.ALICE.getTags());
+        uniqueTagList.setTags(TypicalPersons.BENSON.getTags());
+        uniqueTagList.setTags(TypicalPersons.CARL.getTags());
+
+        assertTrue(uniqueTagList.contains(new Tag("friends")));
+        assertFalse(uniqueTagList.contains(new Tag("aaaaaaaa")));
+
+        for(Tag tag : uniqueTagList.asObservableList()){
+            assertTrue(tag.getTagColor().equals("#dcdcdc"));
+        }
+
+
     }
 }
