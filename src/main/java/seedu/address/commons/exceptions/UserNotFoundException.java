7a36c31e308407829beba45d0088d134e5c4d469
Jelena <neohuilingjelena@gmail.com>
2017-10-09 23:54:53 +0800

    add UserNotFoundException class

---
+++ src/main/java/seedu/address/commons/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package seedu.address.commons.exceptions;
+
+//@@author jelneo
+/**
+ * Signals that the user does not exist
+ */
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException() {
+        super();
+    }
+}
