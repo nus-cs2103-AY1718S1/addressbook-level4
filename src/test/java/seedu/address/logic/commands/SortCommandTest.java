e758e0b44c0ab67c098f787e8fa76104416239f1
DarrenCzen <zen_darren@hotmail.com>
2017-10-10 00:26:11 +0800

    Updated JUnits4 Testing for SortCommand with one line indentations

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
