d46d4f948bb5950e8ffe76ba4ee676728e1e1fa7
Khor Shao Liang <stephenksl@gmail.com>
2017-10-09 17:19:13 +0800

    AddMultipleCommandParser.java: update to catch file exceptions

--- src/main/java/seedu/address/logic/parser/AddMultipleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddMultipleCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
@@ -35,58 +36,51 @@ public class AddMultipleCommandParser implements Parser<AddMultipleCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the AddMultipleCommand
-     * then read data from file name given {@param args} if it exists
+     * then parse data from file name given {@param args} if it exists
      * and returns an AddMultipleCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddMultipleCommand parse(String args) throws ParseException {
-        ArrayList<ReadOnlyPerson> personsList = new ArrayList<>();
         String fileName = args.trim();
         if (fileName.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddMultipleCommand.MESSAGE_USAGE));
         }
+
+        ArrayList<ReadOnlyPerson> personsList = new ArrayList<>();
+        File fileToRead = new File(AddMultipleCommand.DEFAULT_FOLDER_PATH, fileName);
+        String line;
         
-        File fileToRead = new File("./data", fileName);
-        
-        if (fileToRead.exists()) {
-            String line;
-            
-            try {
-                // FileReader reads text files in the default encoding.
-                FileReader fileReader = new FileReader(fileToRead);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-                while ((line = bufferedReader.readLine()) != null) {
-                    String toAdd = " " + line;
-                    ArgumentMultimap argMultimap =
-                            ArgumentTokenizer.tokenize(toAdd, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
-
-                    if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
-                        throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddMultipleCommand.MESSAGE_USAGE));
-                    }
-
-                    try {
-                        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME)).get();
-                        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE)).get();
-                        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL)).get();
-                        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS)).get();
-                        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
-
-                        ReadOnlyPerson person = new Person(name, phone, email, address, tagList);
-                        
-                        personsList.add(person);
-                    } catch (IllegalValueException ive) {
-                        throw new ParseException(ive.getMessage(), ive);
-                    }
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileToRead);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                String toAdd = " " + line;
+                ArgumentMultimap argMultimap =
+                        ArgumentTokenizer.tokenize(toAdd, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
+                    throw new ParseException(String.format(MESSAGE_INVALID_PERSON_FORMAT, AddMultipleCommand.MESSAGE_PERSON_FORMAT));
                 }
-                
-                bufferedReader.close();
-            } catch (FileNotFoundException ex) {
-                System.out.println("Unable to open file '" + fileName + "'");
-            } catch (IOException ex) {
-                System.out.println("Error reading file '" + fileName + "'");
+                try {
+                    Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME)).get();
+                    Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE)).get();
+                    Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL)).get();
+                    Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS)).get();
+                    Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+                    ReadOnlyPerson person = new Person(name, phone, email, address, tagList);
+                    
+                    personsList.add(person);
+                } catch (IllegalValueException ive) {
+                    throw new ParseException(ive.getMessage(), ive);
+                }
             }
+            
+            bufferedReader.close();
+        } catch (FileNotFoundException fnfe) {
+            throw new ParseException("Unable to open file '" + fileName + "'");
+        } catch (IOException ioe) {
+            throw new ParseException("Error reading file '" + fileName + "'");
         }
 
         return new AddMultipleCommand(personsList);
