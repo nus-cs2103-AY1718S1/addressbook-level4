183d57a4a7f11f215e891b49b39a5e160dc961cc
user\Junyang <junyangooooo@gmail.com>
2017-10-10 03:08:10 +0800

    created EventTime class

---
+++ src/main/java/seedu/address/model/event/EventTime.java
@@ -0,0 +1,54 @@
+package seedu.address.model.event;
+
+import static java.util.Objects.requireNonNull;
+import seedu.address.commons.exceptions.IllegalValueException;
+
+public class EventTime {
+
+    public static final String MESSAGE_TIME_CONSTRAINTS =
+            "Event time must be numbers followed by pm/am";
+
+
+    public static final String TIME_VALIDATION_REGEX = "\\b((1[0-2]|0?[1-9])([0-5][0-9])([AaPp][Mm]))";
+
+    public final String value;
+
+    /**
+     * Validates given address.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public EventTime(String time) throws IllegalValueException {
+        requireNonNull(time);
+        if (!isValidTime(time)) {
+            throw new IllegalValueException(MESSAGE_TIME_CONSTRAINTS);
+        }
+        this.value = time;
+    }
+
+    /**
+     * Returns true if a given string is a valid person email.
+     */
+    public static boolean isValidTime(String test) {
+        return test.matches(TIME_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof EventTime // instanceof handles nulls
+                && this.value.equals(((EventTime) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
+
