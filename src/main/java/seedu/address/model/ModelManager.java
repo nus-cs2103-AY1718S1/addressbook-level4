adfb991355b4f4244c40b6ff84a402dd6342c2c0
shunyong <e0002695@u.nus.edu>
2017-10-09 21:14:10 +0800

    implement sorting logic

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -58,6 +58,11 @@ public class ModelManager extends ComponentManager implements Model {
         return addressBook;
     }
 
+    @Override
+    public void sortContact() {
+        addressBook.sort();
+    }
+
     /**
      * Raises an event to indicate the model has changed
      */
