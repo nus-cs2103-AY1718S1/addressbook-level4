00f28b659d9a243b551df7b3ab1af4c37b418d26
Jelena <neohuilingjelena@gmail.com>
2017-10-11 02:04:35 +0800

    AddressBookGuiTest.java: fix FX thread issues

--- src/test/java/guitests/AddressBookGuiTest.java
+++ src/test/java/guitests/AddressBookGuiTest.java
@@ -1,5 +1,7 @@
 package guitests;
 
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import java.util.concurrent.TimeoutException;
 
 import org.junit.After;
@@ -19,7 +21,17 @@ import guitests.guihandles.StatusBarFooterHandle;
 import javafx.stage.Stage;
 import seedu.address.TestApp;
 import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.Password;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.Username;
+import seedu.address.logic.commands.LoginCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.testutil.TypicalPersons;
 
 /**
@@ -27,6 +39,8 @@ import seedu.address.testutil.TypicalPersons;
  */
 public abstract class AddressBookGuiTest {
 
+    private static final String ADMIN_USERNAME = "loanShark97";
+    private static final String ADMIN_PASSWORD = "hitMeUp123";
     /* The TestName Rule makes the current test name available inside test methods */
     @Rule
     public TestName name = new TestName();
@@ -36,6 +50,24 @@ public abstract class AddressBookGuiTest {
     protected Stage stage;
 
     protected MainWindowHandle mainWindowHandle;
+    private static ModelManager modelManager;
+    private static Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+
+    private static void login() {
+        modelManager = new ModelManager();
+        try {
+            Username username = new Username(ADMIN_USERNAME);
+            Password password = new Password(ADMIN_PASSWORD);
+            LoginCommand loginCommand = new LoginCommand(username, password);
+            loginCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+            loginCommand.execute();
+        } catch(IllegalValueException ive) {
+            ive.printStackTrace();
+        } catch (CommandException ce) {
+            ce.printStackTrace();
+        }
+    }
 
     @BeforeClass
     public static void setupOnce() {
@@ -49,6 +81,7 @@ public abstract class AddressBookGuiTest {
 
     @Before
     public void setup() throws Exception {
+        login();
         FxToolkit.setupStage((stage) -> {
             this.stage = stage;
         });
