62386fa7f17496dfff08fe791202e3f70122a15d
Jiashu Tao <Jiashu@Jiashus-MacBook-Pro.local>
2017-10-10 18:10:42 +0800

    Created `Favourite Command`

---
+++ src/main/java/seedu/address/logic/commands/FavouriteCommand.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+public class FavouriteCommand extends UndoableCommand{
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+
+        return null;
+    }
+
+}
