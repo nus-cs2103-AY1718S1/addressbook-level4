8711a249e3cc177e1dc7d6fa14f638714c905389
Jackie Tan <free-soup@hotmail.com>
2017-10-11 03:18:07 +0800

    Style Fix

--- src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -12,14 +11,14 @@ import seedu.address.logic.parser.RemoveTagParser;
 
 public class RemoveTagCommandTest {
 
-    public RemoveTagParser parser = new RemoveTagParser();
-
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveTagCommand.MESSAGE_USAGE));
+        RemoveTagParser parser = new RemoveTagParser();
+        assertParseFailure(parser, "     ", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, RemoveTagCommand.MESSAGE_USAGE));
     }
 
 }
