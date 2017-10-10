2c853d09c6ba3d1fc9ea560fa73213f066ce9671
qihao <wuqihao07@gmail.com>
2017-10-10 20:44:21 +0800

    Update Junit tests

---
+++ src/test/java/seedu/address/logic/parser/DeleteAltCommandParserTest.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.DeleteAltCommand;
+import seedu.address.logic.commands.DeleteCommand;
+
+import org.junit.Test;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalNames.NAME_FIRST_PERSON;
+
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the DeleteAltCommand code. For example, inputs "abc" and "abc 1" take the
+ * same path through the DeleteAltCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
+public class DeleteAltCommandParserTest {
+
+    private DeleteAltCommandParser parser = new DeleteAltCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteAltCommand() {
+        assertParseSuccess(parser, "Alice Pauline", new DeleteAltCommand(NAME_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAltCommand.MESSAGE_USAGE));
+    }
+}
