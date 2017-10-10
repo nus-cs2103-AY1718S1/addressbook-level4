2f08ce61e01f20127cb6c7e7e0733ea6461c75cf
khooroko <khoowp25@hotmail.com>
2017-10-10 23:39:07 +0800

    Person.java: add displayPostalCode to hashCode()

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -172,7 +172,7 @@ public class Person implements ReadOnlyPerson {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, postalCode, debt, tags);
+        return Objects.hash(name, phone, email, address, postalCode, displayPostalCode, debt, tags);
     }
 
     @Override
