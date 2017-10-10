ef674c946fb07bfad6fe55ab634c3c6e365de5db
APPLETEA\Huijia <hjc2304@gmail.com>
2017-10-10 14:48:01 +0800

    import modelmanager

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.ModelManager;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 
