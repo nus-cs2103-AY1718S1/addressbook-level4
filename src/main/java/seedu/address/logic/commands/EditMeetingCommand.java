32b7d5a3c45514041e456b8c7ebf3b705b5c4575
DESKTOP-LNICKT9\Ky <kyngyi95@gmail.com>
2017-10-10 22:49:14 +0800

    Added Classes: "EditMeetingCommand" and "EditMeetCommandParser"

---
+++ src/main/java/seedu/address/logic/commands/EditMeetingCommand.java
@@ -0,0 +1,195 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.meeting.Meeting;
+import seedu.address.model.meeting.Place;
+import seedu.address.model.meeting.Name;
+import seedu.address.model.meeting.ReadOnlyMeeting;
+import seedu.address.model.tag.Tag;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+
+public class EditMeetingCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "editmeeting";
+    public static final String COMMAND_ALIAS = "em";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the meeting identified "
+            + "by the index number used in the last meeting listing. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_DATE + "DATE] "
+            + "[" + PREFIX_TIME + "TIME] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_DATE + "01012017"
+            + PREFIX_ADDRESS + "Clementi MRT";
+
+    public static final String MESSAGE_EDIT_MEETING_SUCCESS = "Edited Meeting: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_MEETING = "This meeting already exists in the address book.";
+
+    private final Index index;
+    private final EditMeetingDescriptor editMeetingDescriptor;
+
+    /**
+     * @param index of the meeting in the filtered Meeting list to edit
+     * @param editMeetingDescriptor details to edit the meeting with
+     */
+    public EditMeetingCommand(Index index, EditMeetingDescriptor editMeetinbgDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editMeetingDescriptor);
+
+        this.index = index;
+        this.editMeetingDescriptor = new EditMeetingDescriptor(editMeetingDescriptor);
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        List<ReadOnlyMeeting> lastShownList = model.getFilteredMeetingList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_MEETING_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyMeeting meetingToEdit = lastShownList.get(index.getZeroBased());
+        Meeting editedMeeting = createEditedMeeting(meetingToEdit, editMeetingDescriptor);
+
+        try {
+            model.updateMeeting(meetingToEdit, editedMeeting);
+        } catch (DuplicateMeetingException dpe) {
+            throw new CommandException(MESSAGE_DUPLICATE_MEETING);
+        } catch (MeetingNotFoundException pnfe) {
+            throw new AssertionError("The target meeting cannot be missing");
+        }
+        model.updateFilteredMeetingList(PREDICATE_SHOW_ALL_MEETINGS);
+        return new CommandResult(String.format(MESSAGE_EDIT_MEETING_SUCCESS, editedMeeting));
+    }
+
+    /**
+     * Creates and returns a {@code Meeting} with the details of {@code meetingToEdit}
+     * edited with {@code editMeetingDescriptor}.
+     */
+    private static Meeting createEditedMeeting(ReadOnlyMeeting meetingToEdit,
+                                              EditMeetingDescriptor editMeetingDescriptor) {
+        assert meetingToEdit != null;
+
+        Name updatedName = editMeetingDescriptor.getName().orElse(meetingToEdit.getName());
+        Date updatedDate = editMeetingDescriptor.getDate().orElse(meetingToEdit.getDate());
+        Place updatedPlace = editMeetingDescriptor.getPlace().orElse(meetingToEdit.getPlace());
+        Set<Tag> updatedTags = editMeetingDescriptor.getTags().orElse(meetingToEdit.getTags());
+
+        return new Meeting(updatedName, updatedDate, updatedPlace,  updatedTags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditMeetingCommand)) {
+            return false;
+        }
+
+        // state check
+        EditMeetingCommand e = (EditMeetingCommand) other;
+        return index.equals(e.index)
+                && editMeetingDescriptor.equals(e.editMeetingDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the Meeting with. Each non-empty field value will replace the
+     * corresponding field value of the meeting.
+     */
+    public static class EditMeetingDescriptor {
+        private Name name;
+        private Date date;
+        private Place place;
+        private Set<Tag> tags;
+
+        public EditMeetingDescriptor() {
+        }
+
+        public EditMeetingDescriptor(EditMeetingCommand.EditMeetingDescriptor toCopy) {
+            this.name = toCopy.name;
+            this.date = toCopy.date;
+            this.place = toCopy.place;
+            this.tags = toCopy.tags;
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(this.name, this.date, this.place, this.tags);
+        }
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setDate(Date date) { this.date = date; }
+
+        public Optional<Date> getDate() {
+            return Optional.ofNullable(date);
+        }
+
+        public void setPlace(Place place) {
+            this.place = place;
+        }
+
+        public Optional<Place> getPlace() {
+            return Optional.ofNullable(place);
+        }
+
+        public void setTags(Set<Tag> tags) {
+            this.tags = tags;
+        }
+
+        public Optional<Set<Tag>> getTags() {
+            return Optional.ofNullable(tags);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditMeetingDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditMeetingDescriptor e = (EditMeetingDescriptor) other;
+
+            return getName().equals(e.getName())
+                    && getDate().equals(e.getDate())
+                    && getplace().equals(e.getplace())
+                    && getTags().equals(e.getTags());
+        }
+    }
+}
