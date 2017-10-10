9ef6bc88f6f7729595ff6806197a959e4e4a0ccd
Jelena <neohuilingjelena@gmail.com>
2017-10-11 00:15:57 +0800

    LoginCommandTest.java: fix NullPointerException error when tests are called

--- src/test/java/seedu/address/logic/commands/LoginCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LoginCommandTest.java
@@ -3,15 +3,24 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.logic.commands.LoginCommand.MESSAGE_LOGIN_ACKNOWLEDGEMENT;
 import static seedu.address.logic.commands.LoginCommand.MESSAGE_LOGIN_UNSUCCESSFUL;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.Arrays;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import seedu.address.commons.events.ui.LoginAppRequestEvent;
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.CommandHistory;
 import seedu.address.logic.Password;
+import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.Username;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
 //@@author jelneo
@@ -22,13 +31,18 @@ public class LoginCommandTest {
 
     private final boolean hasLoggedIn = true;
     private LoginAppRequestEvent event;
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private static final String TEST_USERNAME = "TESTloanShark97";
+    private static final String TEST_PASSWORD = "TESThitMeUp123";
 
     @Test
     public void execute_login_success() {
         try {
-            Username username = new Username("TESTloanShark97");
-            Password password = new Password("TESThitMeUp123");
-            CommandResult result = new LoginCommand(username, password).execute();
+            Username username = new Username(TEST_USERNAME);
+            Password password = new Password(TEST_PASSWORD);
+            LoginCommand loginCommand = new LoginCommand(username, password);
+            loginCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+            CommandResult result = loginCommand.execute();
             assertEquals(hasLoggedIn, event.getLoginStatus());
             assertEquals(MESSAGE_LOGIN_ACKNOWLEDGEMENT, result.feedbackToUser);
 
@@ -42,9 +56,11 @@ public class LoginCommandTest {
     @Test
     public void execute_login_failure() {
         try {
-            Username username = new Username("iDontExist");
-            Password password = new Password("thisPwdDontExist");
-            CommandResult result = new LoginCommand(username, password).execute();
+            Username username = new Username(TEST_USERNAME);
+            Password password = new Password(TEST_PASSWORD);
+            LoginCommand loginCommand = new LoginCommand(username, password);
+            loginCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+            CommandResult result = loginCommand.execute();
             assertEquals(!hasLoggedIn, event.getLoginStatus());
             assertEquals(MESSAGE_LOGIN_UNSUCCESSFUL, result.feedbackToUser);
 
