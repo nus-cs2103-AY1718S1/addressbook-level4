41049b083098f15fb857752de64b9c547126b154
Eldric <eldriclim93@gmail.com>
2017-10-11 00:02:07 +0800

    Remove DateAdded.java, to be implemented on separate branch

--- src/main/java/seedu/address/model/person/DateAdded.java
+++
@@ -1,8 +0,0 @@
-package seedu.address.model.person;
-
-/**
- * Represents a Person's timestamp when first added into the address book.
- * Guarantees: correct format; generated from Java Date class
- */
-public class DateAdded {
-}
