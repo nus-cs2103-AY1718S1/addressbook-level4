6e82fb6738b55a83f8c7870d444e09aaa4fff0fc
SECRET\Zhang H <E0053345@u.nus.edu.sg>
2017-10-10 23:20:18 +0800

    TagAddCommandParser: Modify for coding style.

--- src/main/java/seedu/address/logic/parser/TagAddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TagAddCommandParser.java
@@ -3,13 +3,12 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.Optional;
+import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
