d8627260a06990a9e5118539566b4aab83c04402
Kennard Ng <Kennard_ng@outlook.com>
2017-10-10 14:38:33 +0800

    update sample data for addresses that uses new regex

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -22,22 +22,22 @@ public class SampleDataUtil {
         try {
             return new Person[] {
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                    new Address("Blk 30 Geylang Street 29, #06-40"),
+                    new Address("Blk 29 Lor 30 Geylang, #06-40 s398362"),
                     getTagSet("friends")),
                 new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                    new Address("Blk 326 Serangoon Ave 3, #07-18 S550326"),
                     getTagSet("colleagues", "friends")),
                 new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    new Address("Blk 512 Ang Mo Kio Ave 8, #11-04 s560512"),
                     getTagSet("neighbours")),
                 new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43 s558675"),
                     getTagSet("family")),
                 new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"),
+                    new Address("Blk 47 Tampines Street 20, #17-35 s535070"),
                     getTagSet("classmates")),
                 new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"),
+                    new Address("Blk 45 Aljunied Street 85, #11-31 S389825"),
                     getTagSet("colleagues"))
             };
         } catch (IllegalValueException e) {
