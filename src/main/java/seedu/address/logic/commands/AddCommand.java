20d0c6202da6d48b26b8a909717331e8a3d8e173
Kennard Ng <Kennard_ng@outlook.com>
2017-10-10 23:41:28 +0800

    update documentation

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -30,7 +30,7 @@ public class AddCommand extends UndoableCommand {
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 S123123 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
