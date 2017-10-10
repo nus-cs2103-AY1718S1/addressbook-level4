061ff357ec6ebd83189ea4367a9f865db0b38875
Kennard Ng <Kennard_ng@outlook.com>
2017-10-10 17:58:29 +0800

    update styling in PostalCodeTest and PostalCode to meet style guidelines

--- src/main/java/seedu/address/model/person/PostalCode.java
+++ src/main/java/seedu/address/model/person/PostalCode.java
@@ -9,54 +9,54 @@ import seedu.address.commons.exceptions.IllegalValueException;
  */
 public class PostalCode {
 
-	public static final String MESSAGE_POSTAL_CODE_CONSTRAINTS =
-			"Person postal code should contain only a string of 6 digits";
+    public static final String MESSAGE_POSTAL_CODE_CONSTRAINTS =
+            "Person postal code should contain only a string of 6 digits";
 
-	/*
+    /*
      * Person names should only contain alphanumeric characters and spaces, and it should not be blank
      * otherwise " " (a blank string) becomes a valid input.
      */
-	public static final String POSTAL_CODE_VALIDATION_REGEX = "^[0-9]{6}$";
+    public static final String POSTAL_CODE_VALIDATION_REGEX = "^[0-9]{6}$";
 
-	public final String postalCode;
+    public final String postalCode;
 
-	/**
-	 * Validates given name.
-	 *
-	 * @throws IllegalValueException if given name string is invalid.
-	 */
-	public PostalCode(String postalCode) throws IllegalValueException {
-		requireNonNull(postalCode);
-		String trimmedPostalCode = postalCode.trim();
-		if (!isValidPostalCode(trimmedPostalCode)) {
-			throw new IllegalValueException(MESSAGE_POSTAL_CODE_CONSTRAINTS);
-		}
-		this.postalCode = trimmedPostalCode;
-	}
+    /**
+     * Validates given name.
+     *
+     * @throws IllegalValueException if given name string is invalid.
+     */
+    public PostalCode(String postalCode) throws IllegalValueException {
+        requireNonNull(postalCode);
+        String trimmedPostalCode = postalCode.trim();
+        if (!isValidPostalCode(trimmedPostalCode)) {
+            throw new IllegalValueException(MESSAGE_POSTAL_CODE_CONSTRAINTS);
+        }
+        this.postalCode = trimmedPostalCode;
+    }
 
-	/**
-	 * Returns true if a given string is a valid person name.
-	 */
-	public static boolean isValidPostalCode(String test) {
-		return test.matches(POSTAL_CODE_VALIDATION_REGEX);
-	}
+    /**
+     * Returns true if a given string is a valid person name.
+     */
+    public static boolean isValidPostalCode(String test) {
+        return test.matches(POSTAL_CODE_VALIDATION_REGEX);
+    }
 
 
-	@Override
-	public String toString() {
-		return postalCode;
-	}
+    @Override
+    public String toString() {
+        return postalCode;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		return other == this // short circuit if same object
-				|| (other instanceof PostalCode // instanceof handles nulls
-				&& this.postalCode.equals(((PostalCode) other).postalCode)); // state check
-	}
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PostalCode // instanceof handles nulls
+                && this.postalCode.equals(((PostalCode) other).postalCode)); // state check
+    }
 
-	@Override
-	public int hashCode() {
-		return postalCode.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return postalCode.hashCode();
+    }
 
 }
--- src/test/java/seedu/address/model/person/PostalCodeTest.java
+++ src/test/java/seedu/address/model/person/PostalCodeTest.java
@@ -7,18 +7,18 @@ import org.junit.Test;
 
 public class PostalCodeTest {
 
-	@Test
-	public void isValidPostalCode() {
-		// invalid name
-		assertFalse(PostalCode.isValidPostalCode("")); // empty string
-		assertFalse(PostalCode.isValidPostalCode(" ")); // spaces only
-		assertFalse(PostalCode.isValidPostalCode("AD")); // only numeric characters
-		assertFalse(PostalCode.isValidPostalCode("00000")); // shorter than 6 digits
-		assertFalse(PostalCode.isValidPostalCode("0000000")); // longer than 6 digits
+    @Test
+    public void isValidPostalCode() {
+        // invalid name
+        assertFalse(PostalCode.isValidPostalCode("")); // empty string
+        assertFalse(PostalCode.isValidPostalCode(" ")); // spaces only
+        assertFalse(PostalCode.isValidPostalCode("AD")); // only numeric characters
+        assertFalse(PostalCode.isValidPostalCode("00000")); // shorter than 6 digits
+        assertFalse(PostalCode.isValidPostalCode("0000000")); // longer than 6 digits
 
-		// valid name
-		assertTrue(PostalCode.isValidPostalCode("000000")); // alphabets only
-		assertTrue(PostalCode.isValidPostalCode("123456")); // numbers only
-		assertTrue(PostalCode.isValidPostalCode("000845")); // alphanumeric characters
-	}
+        // valid name
+        assertTrue(PostalCode.isValidPostalCode("000000")); // alphabets only
+        assertTrue(PostalCode.isValidPostalCode("123456")); // numbers only
+        assertTrue(PostalCode.isValidPostalCode("000845")); // alphanumeric characters
+    }
 }
