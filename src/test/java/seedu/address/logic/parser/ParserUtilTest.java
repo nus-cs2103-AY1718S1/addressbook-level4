fd0be640a74cb2792a0c4209238095d910212391
Kennard Ng <Kennard_ng@outlook.com>
2017-10-10 17:58:11 +0800

    update test for PostalCode implementation

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -33,7 +33,7 @@ public class ParserUtilTest {
 
     private static final String VALID_NAME = "Rachel Walker";
     private static final String VALID_PHONE = "123456";
-    private static final String VALID_ADDRESS = "123 Main Street #0505";
+    private static final String VALID_ADDRESS = "505 Beach Road, Golden Mile Food Centre, #02-193/194, S199583";
     private static final String VALID_EMAIL = "rachel@example.com";
     private static final String VALID_TAG_1 = "friend";
     private static final String VALID_TAG_2 = "neighbour";
