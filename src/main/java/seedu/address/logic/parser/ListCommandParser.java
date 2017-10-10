5f4db39536add51189cf0c7d8692ffc5a3578bba
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-10-10 15:27:13 +0800

    ListCommandParser: remove import for Collection ListCommandParser: add import for Set ListCommandParser: add import for IllegalValueException
    ListCommandParser: add argMultimap for finding PREFIX_TAG
    ListCommandParser: add condition for isPrefixPresent
    
    ListCommandParser: add isPrefixPresent method

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -5,9 +5,10 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.commons.exceptions.IllegalValueException;
 
-import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import seedu.address.model.tag.Tag;
 
@@ -22,21 +23,29 @@ public class ListCommandParser implements Parser<ListCommand>{
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
         try {
-            String trimmedArgs = args.trim();
-            if(trimmedArgs.isEmpty()) {
-               return new ListCommand(); 
+            if(!isPrefixPresent(argMultimap, PREFIX_TAG)) {
+               return new ListCommand();
             }
 
             //TODO: convert the string to collection to pass to parseTags
-            
-            Set<Tag> inputTag = ParserUtil.parseTags();
-            return new ListCommand(trimmedArgs);
-            
-        } catch (IllegalArgumentException iae) {
+            Set<Tag> inputTag = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            return new ListCommand(inputTag);
+
+        } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
         }
+    }
+
+    /**
+     * Returns true if none of the tag prefix contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean isPrefixPresent(ArgumentMultimap argumentMultimap, Prefix tagPrefix) {
+        return Stream.of(tagPrefix).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
