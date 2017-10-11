46df0230342a655c8e6c40ffccb3accfb9b44f0e
Han Siang Phua <hansiang93@gmail.com>
2017-10-11 22:30:30 +0800

    Added dummy filter command based on 'find'

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.person.TagContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case sensitive.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+    public static final String COMMAND_ALIAS = "ft";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+    private final TagContainsKeywordsPredicate predicate;
+    
+    public FilterCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && this.predicate.equals(((FilterCommand) other).predicate)); // state check
+    }
+}
