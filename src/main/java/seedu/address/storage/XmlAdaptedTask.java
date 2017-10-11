5269e31b291035a7a765a837261f8cb7a0b76a37
DongShaocong <qq423785549@gmail.com>
2017-10-11 14:08:52 +0800

    Add XML adapted task

---
+++ src/main/java/seedu/address/storage/XmlAdaptedTask.java
@@ -0,0 +1,71 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlElement;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.Task;
+
+/**
+ * JAXB-friendly version of the Task.
+ */
+public class XmlAdaptedTask {
+
+    @XmlElement(required = true)
+    private String taskName;
+    @XmlElement(required = true)
+    private String taskDescription;
+    @XmlElement(required = true)
+    private String startDateTime;
+    @XmlElement(required = true)
+    private String endDateTime;
+    @XmlElement
+    private List<XmlAdaptedTag> tagged = new ArrayList<>();
+
+    /**
+     * Constructs an XmlAdaptedPerson.
+     * This is the no-arg constructor that is required by JAXB.
+     */
+    public XmlAdaptedTask() {}
+
+
+    /**
+     * Converts a given Task into this class for JAXB use.
+     *
+     * @param source future changes to this will not affect the created XmlAdaptedPerson
+     */
+    public XmlAdaptedTask(ReadOnlyTask source) {
+        taskName = source.getName();
+        taskDescription = source.getDescription();
+        startDateTime = source.getStartDateTime();
+        endDateTime = source.getEndDateTime();
+        tagged = new ArrayList<>();
+        for (Tag tag : source.getTags()) {
+            tagged.add(new XmlAdaptedTag(tag));
+        }
+    }
+
+    /**
+     * Converts this jaxb-friendly adapted task object into the model's Task object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person
+     */
+    public Task toModelType() throws IllegalValueException {
+        final List<Tag> personTags = new ArrayList<>();
+        for (XmlAdaptedTag tag : tagged) {
+            personTags.add(tag.toModelType());
+        }
+        final String taskName = this.taskName;
+        final String taskDescription = this.taskDescription;
+        final String startDateTime = this.startDateTime;
+        final String endDateTime = this.endDateTime;
+        final Set<Tag> tags = new HashSet<>(personTags);
+        return new Task(taskName, taskDescription, startDateTime, endDateTime, tags);
+    }
+}
