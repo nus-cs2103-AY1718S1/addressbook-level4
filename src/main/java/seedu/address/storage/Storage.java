979bc75680d9ca05e929ee2c7eec234c4ebe7241
DongShaocong <qq423785549@gmail.com>
2017-10-11 13:51:16 +0800

    Add TaskBook Changed event class to events model

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -7,12 +7,13 @@ import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyTaskBook;
 import seedu.address.model.UserPrefs;
 
 /**
  * API of the Storage component
  */
-public interface Storage extends AddressBookStorage, UserPrefsStorage {
+public interface Storage extends AddressBookStorage, UserPrefsStorage, TaskBookStorage {
 
     @Override
     Optional<UserPrefs> readUserPrefs() throws DataConversionException, IOException;
@@ -22,6 +23,15 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage {
 
     @Override
     String getAddressBookFilePath();
+
+    @Override
+    String getTaskBookFilePath();
+
+    @Override
+    Optional<ReadOnlyTaskBook> readTaskBook() throws DataConversionException, IOException;
+
+    @Override
+    void saveTaskBook(ReadOnlyTaskBook addressBook) throws IOException;
 
     @Override
     Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException, IOException;
@@ -35,4 +45,11 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage {
      * Raises {@link DataSavingExceptionEvent} if there was an error during saving.
      */
     void handleAddressBookChangedEvent(AddressBookChangedEvent abce);
+
+    /**
+     * Saves the current version of the Address Book to the hard disk.
+     *   Creates the data file if it is missing.
+     * Raises {@link DataSavingExceptionEvent} if there was an error during saving.
+     */
+    void handleTaskBookChangedEvent(AddressBookChangedEvent abce);
 }
