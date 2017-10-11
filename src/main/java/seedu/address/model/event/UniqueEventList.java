ee88de1385ef69f262338ba949182d7feb99a4fd
user\Junyang <junyangooooo@gmail.com>
2017-10-12 00:16:39 +0800

    added UniqueEventList

---
+++ src/main/java/seedu/address/model/event/UniqueEventList.java
@@ -0,0 +1,116 @@
+package seedu.address.model.event;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.fxmisc.easybind.EasyBind;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.person.exceptions.DuplicateEventException;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.EventNotFoundException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
+public class UniqueEventList implements Iterable<Event> {
+
+
+    private final ObservableList<Event> internalList = FXCollections.observableArrayList();
+    // used by asObservableList()
+    private final ObservableList<ReadOnlyEvent> mappedList = EasyBind.map(internalList, (event) -> event);
+
+    /**
+     * Returns true if the list contains an equivalent person as the given argument.
+     */
+    public boolean contains(ReadOnlyEvent toCheck) {
+        requireNonNull(toCheck);
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds a person to the list.
+     *
+     * @throws DuplicateEventException if the person to add is a duplicate of an existing person in the list.
+     */
+    public void add(ReadOnlyEvent toAdd) throws DuplicateEventException {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateEventException();
+        }
+        internalList.add(new Event(toAdd));
+    }
+    /**
+     * Replaces the person {@code target} in the list with {@code editedEvent}.
+     *
+     * @throws DuplicatePersonException if the replacement is equivalent to another existing person in the list.
+     * @throws PersonNotFoundException  if {@code target} could not be found in the list.
+     */
+    public void setPerson(ReadOnlyEvent target, ReadOnlyEvent editedEvent) throws DuplicatePersonException, PersonNotFoundException {
+        requireNonNull(editedEvent);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new PersonNotFoundException();
+        }
+
+        if (!target.equals(editedEvent) && internalList.contains(editedEvent)) {
+            throw new DuplicatePersonException();
+        }
+
+        internalList.set(index, new Event(editedEvent));
+    }
+
+    /**
+     * Removes the equivalent person from the list.
+     *
+     * @throws PersonNotFoundException if no such person could be found in the list.
+     */
+    public boolean remove(ReadOnlyEvent toRemove) throws EventNotFoundException {
+        requireNonNull(toRemove);
+        final boolean eventFoundAndDeleted = internalList.remove(toRemove);
+        if (!eventFoundAndDeleted) {
+            throw new EventNotFoundException();
+        }
+        return eventFoundAndDeleted;
+    }
+
+    public void setEvents(UniqueEventList replacement) {
+        this.internalList.setAll(replacement.internalList);
+    }
+
+    public void setEvents(List<? extends ReadOnlyEvent> events) throws DuplicateEventException {
+        final UniqueEventList replacement = new UniqueEventList();
+        for (final ReadOnlyEvent event : events) {
+            replacement.add(new Event(event));
+        }
+        setEvents(replacement);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<ReadOnlyEvent> asObservableList() {
+        return FXCollections.unmodifiableObservableList(mappedList);
+    }
+
+    @Override
+    public Iterator<Event> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UniqueEventList // instanceof handles nulls
+                && this.internalList.equals(((UniqueEventList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+}
+
+
