2752c0d7d8f3ddc3ff50f0db25a00f613b021723
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-10-11 21:20:08 +0800

    Add find tag feature, but not yet test

---
+++ src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class FindTagCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    
+
+}
