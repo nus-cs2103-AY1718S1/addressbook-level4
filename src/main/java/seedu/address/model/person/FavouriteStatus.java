36eb8c141ce94317e196118ab720bd7150a1737a
Houjisan <hongjing10188@gmail.com>
2017-10-12 03:52:40 +0900

    Add FavouriteStatus class

---
+++ src/main/java/seedu/address/model/person/FavouriteStatus.java
@@ -0,0 +1,25 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's favourite status in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class FavouriteStatus {
+
+    public boolean isFavourite;
+    
+    public FavouriteStatus(boolean isFavourite) {
+        requireNonNull(isFavourite);
+        this.isFavourite = isFavourite;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FavouriteStatus // instanceof handles nulls
+                && this.isFavourite == ((FavouriteStatus) other).isFavourite); // state check
+    }
+
+}
