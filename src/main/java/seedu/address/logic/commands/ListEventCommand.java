60117d5bcde034628a85c9755b39c5e1f710971f
user\Junyang <junyangooooo@gmail.com>
2017-10-12 00:14:55 +0800

    added addevent, listevent and deleteevent commands

---
+++ src/main/java/seedu/address/logic/commands/ListEventCommand.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
+public class ListEventCommand extends Command {
+
+    public static final String COMMAND_WORD = "listE";
+    public static final String COMMAND_ALIAS = "lE";
+
+    public static final String MESSAGE_EVENT_SUCCESS = "Listed all events";
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredEventsList(PREDICATE_SHOW_ALL_EVENTS);
+        return new CommandResult(MESSAGE_EVENT_SUCCESS);
+    }
+}
+
