bbb1340222ae1763814162f118667ccada059bc3
Lenald <lenaldnwj@gmail.com>
2017-10-11 20:49:58 +0800

    Include more test cases for phone validity

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -15,6 +15,8 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
+        assertFalse(Phone.isValidPhone("HIP: 9011041")); // Incorrect phone type
+        assertFalse(Phone.isValidPhone("HOP: 9011041 Oll: 9893898 Hm: 971897")); // Multiple incorrect phone type
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
@@ -24,5 +26,6 @@ public class PhoneTest {
         assertTrue(Phone.isValidPhone("HP: 928782711")); // hand phone number
         assertTrue(Phone.isValidPhone("O: 9134343431")); // office number
         assertTrue(Phone.isValidPhone("H: 911")); // house number
+        assertTrue(Phone.isValidPhone("O: 911 H: 972727 HP: 9234817")); // office number
     }
 }
