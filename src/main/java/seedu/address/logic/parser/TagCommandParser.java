c44b07e54e0ad88da162f0c855f85c58044df020
Raisa Kabir <raisa.kabir201@gmail.com>
2017-10-12 01:09:36 +0800

    edited TagCommandParser to parse multiple indices

--- src/main/java/seedu/address/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -1,14 +1,46 @@
-/*package seedu.address.logic.parser;
+package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
 
 public class TagCommandParser implements Parser<TagCommand> {
     
+    public static final String TAG_SEPARATOR_REGEX = ","; 
+    
     public TagCommand parse(String args) throws ParseException {
         requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        Index[] parsedIndices;
+
+        try {
+            String[] splitIndices = splitIndices(argMultimap);
+            int numberOfIndices = splitIndices.length;
+            parsedIndices = new Index[numberOfIndices];
+            for (int i = 0; i < numberOfIndices; i++) {
+                parsedIndices[i] = ParserUtil.parseIndex(splitIndices[i]);
+            }
+            Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
+        }
+        
+        return new TagCommand();
+    }
+        
+    private String[] splitIndices(ArgumentMultimap argMultimap) {
+        return argMultimap.getPreamble().split(TAG_SEPARATOR_REGEX);
+    }    
+    
         
     }
-}*/
