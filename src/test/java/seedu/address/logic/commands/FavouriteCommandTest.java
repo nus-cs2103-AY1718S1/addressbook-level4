42ff25e0219d1abda990727f5331171533af6894
Houjisan <hongjing10188@gmail.com>
2017-10-12 01:23:39 +0900

    Add FavouriteCommandTest

---
+++ src/test/java/seedu/address/logic/commands/FavouriteCommandTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.FavouriteCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+        
+import org.junit.Test;
+        
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+        
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ */
+public class FavouriteCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute() throws Exception {
+        assertCommandFailure(prepareCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+
+    /**
+      * Returns an {@code FavouriteCommand}.
+      */
+    private FavouriteCommand prepareCommand() {
+        FavouriteCommand favouriteCommand = new FavouriteCommand(INDEX_FIRST_PERSON);
+        favouriteCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return favouriteCommand;
+    }
+}
