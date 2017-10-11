23ce7c6f11ee2d21aad5363864ad666963b14213
DongShaocong <qq423785549@gmail.com>
2017-10-11 14:01:17 +0800

    Update Storage and storage manager with tasks

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 import seedu.address.commons.events.model.AddressBookChangedEvent;
+import seedu.address.commons.events.model.TaskBookChangedEvent;
 import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyAddressBook;
@@ -47,9 +48,9 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage, TaskBookS
     void handleAddressBookChangedEvent(AddressBookChangedEvent abce);
 
     /**
-     * Saves the current version of the Address Book to the hard disk.
+     * Saves the current version of the Task Book to the hard disk.
      *   Creates the data file if it is missing.
      * Raises {@link DataSavingExceptionEvent} if there was an error during saving.
      */
-    void handleTaskBookChangedEvent(AddressBookChangedEvent abce);
+    void handleTaskBookChangedEvent(TaskBookChangedEvent tbce);
 }
