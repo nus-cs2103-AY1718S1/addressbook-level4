c79c4163bde14c88fba8ff3858d0b44b90d2801b
Jackie Tan <free-soup@hotmail.com>
2017-10-11 10:13:10 +0800

    Added more test cases to RemoveTagCommand

--- src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
@@ -1,15 +1,27 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_TAG_REMOVED;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.parser.RemoveTagParser;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
 
 public class RemoveTagCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -19,6 +31,25 @@ public class RemoveTagCommandTest {
         RemoveTagParser parser = new RemoveTagParser();
         assertParseFailure(parser, "     ", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, RemoveTagCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void execute_remove_one_tag() throws IllegalValueException, PersonNotFoundException {
+        String expectedMessage = MESSAGE_TAG_REMOVED;
+
+        RemoveTagCommand command = prepareCommand("friends");
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.removeTag(new Tag("friends"));
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    /**
+     * Generates a new {@code RemoveTagCommand} which upon execution, removes tag in {@code model}.
+     */
+    private RemoveTagCommand prepareCommand(String tag) {
+        RemoveTagCommand command = new RemoveTagCommand(tag);
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
     }
 
 }
