d2dd92e70b1c662917d89e58764c08dce8639cd3
Lenald <lenaldnwj@gmail.com>
2017-10-11 08:55:56 +0800

    Phone : Update PHONE_VALIDATION_REGEX to allow multiple numbers

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -13,7 +13,7 @@ public class Phone {
 
     public static final String MESSAGE_PHONE_CONSTRAINTS =
             "Phone numbers can only contain numbers, and should be at least 3 digits long";
-    public static final String PHONE_VALIDATION_REGEX = "\\d{3,}";
+    public static final String PHONE_VALIDATION_REGEX = "(?=(?:.*\\d){3})[0-9 ]+";
     public final String value;
 
     /**
