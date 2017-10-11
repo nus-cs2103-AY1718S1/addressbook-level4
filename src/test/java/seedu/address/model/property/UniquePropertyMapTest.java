5948dd599d4a8039ead3f657cc04d0bce42084d4
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-11 22:59:24 +0800

    Add more test case for UniquePropertyMap

--- src/test/java/seedu/address/model/property/UniquePropertyMapTest.java
+++ src/test/java/seedu/address/model/property/UniquePropertyMapTest.java
@@ -2,8 +2,8 @@ package seedu.address.model.property;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -35,11 +35,37 @@ public class UniquePropertyMapTest {
 
     @Test
     public void createPropertyMap_validNonEmptyInput_successfullyCreated() throws Exception {
+        UniquePropertyMap propertyMap = createSampleMap();
+
+        assertEquals(2, propertyMap.toSet().size());
+    }
+
+    @Test
+    public void addProperty_checkCorrectness() throws Exception {
+        Property newProperty = new Property("e", "google@microsoft.com");
+        UniquePropertyMap propertyMap = createSampleMap();
+        propertyMap.add(newProperty);
+
+        assertEquals(3, propertyMap.toSet().size());
+    }
+
+    @Test
+    public void containsProperty_checkCorrectness() throws Exception {
+        Property newProperty = new Property("e", "google@microsoft.com");
+        UniquePropertyMap propertyMap = createSampleMap();
+        propertyMap.add(newProperty);
+
+        assertTrue(propertyMap.containsProperty("e"));
+        assertTrue(propertyMap.containsProperty(newProperty));
+    }
+
+    /**
+     * Util for creating a sample {@link UniquePropertyMap}.
+     */
+    private UniquePropertyMap createSampleMap() throws Exception {
         Set<Property> mySet = new HashSet<>();
         mySet.add(new Property("a", "some address"));
         mySet.add(new Property("p", "12345678"));
-        UniquePropertyMap propertyMap = new UniquePropertyMap(mySet);
-
-        assertEquals(2, propertyMap.toSet().size());
+        return new UniquePropertyMap(mySet);
     }
 }
