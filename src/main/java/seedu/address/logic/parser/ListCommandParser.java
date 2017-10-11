be5b170842250a5887672d1f317432d449868c59
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-10-11 15:03:00 +0800

    ListCommandParser.java: Introduce space

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -18,7 +18,7 @@ import seedu.address.model.person.PersonContainsKeywordsPredicate;
 /**
  * Parses input arguments and creates a new ListCommand object
  */
-public class ListCommandParser implements Parser<ListCommand>{
+public class ListCommandParser implements Parser<ListCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the ListCommand
