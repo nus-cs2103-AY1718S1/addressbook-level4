748cdeeb9fed8d08dd6b1dc26deff1c860cbd087
itsdickson <itsklein@hotmail.com>
2017-10-11 23:12:09 +0800

    added SwitchThemeCommand.java and its code to allow switching of theme by user (incomplete)

---
+++ src/main/java/seedu/address/logic/commands/SwitchThemeCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.ui.MainWindow;
+import seedu.address.ui.UiManager;
+
+/**
+ * Switches the current theme to a theme identified using it's index from the themes list in address book.
+ */
+public class SwitchThemeCommand extends Command {
+
+    public static final String COMMAND_WORD = "switchtheme";
+    public static final String COMMAND_ALIAS = "st";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Switches the current theme to the theme identified by the index number in the themes list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SWITCH_THEME_SUCCESS = "Switched Theme: %1$s";
+
+    private final Index targetIndex;
+    private MainWindow mainWindow;
+
+    public SwitchThemeCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+
+    @Override
+    public CommandResult execute() throws CommandException {
+
+        ArrayList<String> themesList = model.getThemesList();
+
+        if (targetIndex.getZeroBased() >= themesList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_DISPLAYED_INDEX);
+        }
+
+        String themeToChange = themesList.get(targetIndex.getZeroBased());
+
+        return new CommandResult(String.format(MESSAGE_SWITCH_THEME_SUCCESS, themeToChange));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SwitchThemeCommand // instanceof handles nulls
+                && this.targetIndex.equals(((SwitchThemeCommand) other).targetIndex)); // state check
+    }
+}
