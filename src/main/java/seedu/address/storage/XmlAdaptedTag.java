3dbddcffbd2c045b76a17b20dfe3e8dd53163bc8
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-11 10:00:25 +0800

    Add XmlAdaptedProperty class

--- src/main/java/seedu/address/storage/XmlAdaptedTag.java
+++ src/main/java/seedu/address/storage/XmlAdaptedTag.java
@@ -9,7 +9,6 @@ import seedu.address.model.tag.Tag;
  * JAXB-friendly adapted version of the Tag.
  */
 public class XmlAdaptedTag {
-
     @XmlValue
     private String tagName;
 
@@ -31,7 +30,7 @@ public class XmlAdaptedTag {
     /**
      * Converts this jaxb-friendly adapted tag object into the model's Tag object.
      *
-     * @throws IllegalValueException if there were any data constraints violated in the adapted person
+     * @throws IllegalValueException if there were any data constraints violated in the adapted tag.
      */
     public Tag toModelType() throws IllegalValueException {
         return new Tag(tagName);
