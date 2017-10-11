1637c3feef1a3d7db7a0e6759fbfae3e3a888f5a
DongShaocong <qq423785549@gmail.com>
2017-10-11 09:31:35 +0800

    Add task with only one time point

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -1,4 +1,57 @@
 package seedu.address.model.task;
 
+/**
+ * This is a task class with only a name
+ */
 public class Task {
+    private static Integer indexTask;
+
+    private String taskName;
+    private String taskDescription;
+    private int taskIndex;
+
+
+    /**
+     * Default constructor may not be used
+     */
+    public Task () {
+        if (indexTask == null) {
+            indexTask = 1;
+        }
+        taskIndex = indexTask;
+    }
+
+    /**
+     * Constructor with a name only to be passed in
+     * @param name, the name of this task
+     */
+    public Task (String name, String description) {
+        this();
+        this.taskName = name;
+        this.taskDescription = description;
+    }
+
+    /**
+     * get index from this task
+     * @return index
+     */
+    public int getIndex () {
+        return taskIndex;
+    }
+
+    /**
+     * get name from this task
+     * @return name
+     */
+    public String getName () {
+        return taskName;
+    }
+
+    /**
+     * get description from this task
+     * @return description
+     */
+    public String getTaskDescription () {
+        return taskDescription;
+    }
 }
