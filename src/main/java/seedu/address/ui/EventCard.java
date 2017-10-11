4cb0fae76f1d2013e05fbd42f817b496bf0bf8fe
Shuang Yang <david.yangshuang@icloud.com>
2017-10-12 06:23:59 +0800

    Add EventCard.java

---
+++ src/main/java/seedu/address/ui/EventCard.java
@@ -0,0 +1,89 @@
+package seedu.address.ui;
+
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.event.ReadOnlyEvent;
+
+/**
+ * An UI component that displays information of a {@code Event}.
+ */
+public class EventCard extends UiPart<Region> {
+
+  private static final String FXML = "EventListCard.fxml";
+
+  /**
+   * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+   * As a consequence, UI elements' variable titles cannot be set to such keywords
+   * or an exception will be thrown by JavaFX during runtime.
+   *
+   * @see
+   * <a href="https://github.com/se-edu/addressbook-level4/issues/336">The
+   * issue on AddressBook level 4</a>
+   */
+
+  public final ReadOnlyEvent event;
+
+  @FXML
+  private HBox cardPane;
+  @FXML
+  private Label title;
+  @FXML
+  private Label id;
+  @FXML
+  private Label timing;
+  @FXML
+  private Label description;
+  @FXML
+  private FlowPane tags;
+
+  public EventCard(ReadOnlyEvent event, int displayedIndex) {
+    super(FXML);
+    this.event = event;
+    id.setText(displayedIndex + ". ");
+    initTags(event);
+    bindListeners(event);
+  }
+
+  /**
+   * Binds the individual UI elements to observe their respective {@code Event}
+   * properties
+   * so that they will be notified of any changes.
+   */
+  private void bindListeners(ReadOnlyEvent event) {
+    title.textProperty().bind(Bindings.convert(event.titleProperty()));
+    timing.textProperty().bind(Bindings.convert(event.timingProperty()));
+    description.textProperty().bind(Bindings.convert(event.descriptionProperty
+        ()));
+    event.tagProperty().addListener((observable, oldValue, newValue) -> {
+      tags.getChildren().clear();
+      event.getTags().forEach(tag -> tags.getChildren().add(new Label(tag
+          .tagName)));
+    });
+  }
+
+  private void initTags(ReadOnlyEvent event) {
+    event.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    // short circuit if same object
+    if (other == this) {
+      return true;
+    }
+
+    // instanceof handles nulls
+    if (!(other instanceof EventCard)) {
+      return false;
+    }
+
+    // state check
+    EventCard card = (EventCard) other;
+    return id.getText().equals(card.id.getText())
+        && event.equals(card.event);
+  }
+}
