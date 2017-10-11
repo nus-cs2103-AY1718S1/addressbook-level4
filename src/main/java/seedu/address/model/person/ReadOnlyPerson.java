1817ba533239c04ba2d3a1b252e4cd60be3b97c3
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-11 19:35:52 +0800

    Change ReadOnlyPerson::isSameStateAs

--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -40,7 +40,8 @@ public interface ReadOnlyPerson {
                 && other.getName().equals(this.getName()) // state checks here onwards
                 && other.getPhone().equals(this.getPhone())
                 && other.getEmail().equals(this.getEmail())
-                && other.getAddress().equals(this.getAddress()));
+                && other.getAddress().equals(this.getAddress()))
+                && other.getProperties().equals(this.getProperties());
     }
 
     /**
