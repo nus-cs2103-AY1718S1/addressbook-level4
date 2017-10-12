6df476eb281c111e72a9e677accf8566f9051704
Jeremy Long Shi Wei <E0014824@u.nus.edu>
2017-10-12 15:21:24 +0800

    Test Coverage+ : PersonTest.java

---
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -0,0 +1,40 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.testutil.PersonBuilder;
+
+public class PersonTest {
+
+    @Test
+    public void testTagProperty() {
+        Person personOne = new PersonBuilder().withName("Tester").withTags("Friend").build();
+        Person personTwo = new PersonBuilder().withName("Jane").withTags("Family").build();
+
+        //Same UniqueTagList -> Returns True
+        assertTrue(personOne.tagProperty().equals(personOne.tagProperty()));
+
+        //Different UniqueTagList -> Returns False
+        assertFalse(personOne.tagProperty().equals(personTwo.tagProperty()));
+    }
+
+    @Test
+    public void testPersonHash() {
+        Person personOne = new PersonBuilder().withName("Tester").build();
+        Person personTwo = new PersonBuilder().withName("Jane").build();
+
+        int firstPersonHash = personOne.hashCode();
+        int secondPersonHash = personTwo.hashCode();
+
+        // Same person object has same hash
+        assertTrue(firstPersonHash == firstPersonHash);
+
+        // Different person object has different hash
+        assertFalse(firstPersonHash == secondPersonHash);
+    }
+
+
+}
