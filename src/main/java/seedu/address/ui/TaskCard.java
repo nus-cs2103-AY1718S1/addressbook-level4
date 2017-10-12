693563406e7cc6d955b0b0c95f1d97663534fe42
CuiXiaohan <cuixiaohan@me.com>
2017-10-12 14:45:28 +0800

    fix bug on unable to show some tag colour

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -19,7 +19,7 @@ public class TaskCard extends UiPart<Region> {
 
     private static final String FXML = "TaskListCard.fxml";
     private static ArrayList<String> colors = new ArrayList<String>(
-            Arrays.asList("Tomato", "Orange", "DodgerBlue", "MediumSeaGreen", "SlateBlue", "Violate", "Maroon"));
+            Arrays.asList("Tomato", "Orange", "DodgerBlue", "MediumSeaGreen", "SlateBlue", "Violet", "Maroon"));
     private static HashMap<String, String> tagColors = new HashMap<String, String>();
 
     /**
