e7d72538b080328a038067d32e5d34a6b0747e4b
peiquan <peiquan@hiei.local>
2017-10-12 21:14:20 +0800

    add DetagCommand and update relevant parsers

---
+++ src/main/java/seedu/address/logic/commands/DetagCommand.java
@@ -0,0 +1,70 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+/**
+ * Remove same tags from a list of people
+ */
+public class DetagCommand extends UndoableCommand{
+
+    public static final String COMMAND_WORD = "detag";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Remove tags from multiple people.";
+    public static final String MESSAGE_DETAG_PERSONS_SUCCESS = "Old tags removed.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+    public static final String MESSAGE_MISSING_TAG = "One or more person(s) don't has this tag";
+    private final Index[] indices;
+    private final Tag tag;
+
+    /**
+     * Create a DetagCommand to delete the specific
+     * @param indices
+     * @param tag
+     */
+    public DetagCommand(Index[] indices, Tag tag) {
+        requireNonNull(indices);
+        requireNonNull(tag);
+        this.indices = indices;
+        this.tag = tag;
+    }
+
+    @Override
+    protected CommandResult executeUndoableCommand() throws CommandException {
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        for(Index targetIndex: indices) {
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+        }
+
+        try {
+            model.deleteTag(indices, tag);
+        } catch (DuplicatePersonException dpe) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        } catch (PersonNotFoundException pnfe) {
+            throw new AssertionError("The target person cannot be missing");
+        }
+
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_DETAG_PERSONS_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DetagCommand // instanceof handles nulls
+                && tag.equals(((DetagCommand) other).tag)); // state check
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/DetagCommandParser.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.DetagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+/**
+ * Parse the given input and creates a DetagCommand object
+ */
+public class DetagCommandParser implements Parser<DetagCommand>{
+
+    public static final String INDEX_SEPARATOR_REGEX = ",";
+
+    /**
+     * Parse the given {@code String} of arguments in the context of the DetagCommand
+     * @param userInput
+     * @return DetagCommand object for execution
+     * @throws ParseException
+     */
+    @Override
+    public DetagCommand parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(userInput, PREFIX_TAG);
+
+        Index[] parsedIndices;
+        Set<Tag> tagList;
+        Tag tag;
+
+        try {
+            String[] splitIndices = splitIndices(argMultimap);
+            int numberOfIndices = splitIndices.length;
+            parsedIndices = new Index[numberOfIndices];
+            for (int i = 0; i < numberOfIndices; i++) {
+                parsedIndices[i] = ParserUtil.parseIndex(splitIndices[i]);
+            }
+            tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            tag = tagList.iterator().next();
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DetagCommand.MESSAGE_USAGE));
+        }
+
+        return new DetagCommand(parsedIndices, tag);
+    }
+
+    private String[] splitIndices(ArgumentMultimap argMultimap) {
+        return argMultimap.getPreamble().split(INDEX_SEPARATOR_REGEX);
+    }
+}
