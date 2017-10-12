650d3d61ccf85160e3c43f4c213c61cae6c53be2
fongwz <schifx95@gmail.com>
2017-10-12 22:42:06 +0800

    fixing travis errors

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -8,14 +8,11 @@ import org.fxmisc.easybind.EasyBind;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
