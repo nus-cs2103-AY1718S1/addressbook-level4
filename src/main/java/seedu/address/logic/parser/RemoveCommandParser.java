e9be3e325f0d0b2876178fc6cb053117234ef3c4
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-10-13 00:29:53 +0800

    implement RemoveCommand and RemoveCommandParser class

--- src/main/java/seedu/address/logic/parser/RemoveCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemoveCommandParser.java
@@ -1,4 +1,26 @@
 package seedu.address.logic.parser;
 
-public class RemoveCommandParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemoveCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+
+
+public class RemoveCommandParser implements Parser<RemoveCommand> {
+
+    public RemoveCommand parse(String args) throws ParseException {
+        Tag toRemove;
+
+        try{
+            toRemove = new Tag(args);
+            return new RemoveCommand(toRemove);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveCommand.MESSAGE_USAGE));
+        }
+
+    }
+
 }
