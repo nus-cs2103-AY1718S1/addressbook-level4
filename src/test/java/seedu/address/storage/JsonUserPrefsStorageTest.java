9288428f7edba6f0a799d50e3056ab1c807c6861
Jeremy Long Shi Wei <E0014824@u.nus.edu>
2017-10-12 14:41:16 +0800

    Test Coverage+ : JsonUserPrefsStorageTest.java

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -2,6 +2,7 @@ package seedu.address.storage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -77,6 +78,49 @@ public class JsonUserPrefsStorageTest {
         UserPrefs actual = readUserPrefs("ExtraValuesUserPref.json").get();
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testUserPrefsGetAddressBookName() {
+        UserPrefs upUnderTest = getTypicalUserPrefs();
+        assertTrue("TypicalAddressBookName".equals(upUnderTest.getAddressBookName()));
+    }
+
+    @Test
+    public void testUserPrefsEquals() {
+        UserPrefs upUnderTest = getTypicalUserPrefs();
+        UserPrefs copyOfUpUnderTest = getTypicalUserPrefs();
+
+        //Return true if same instance
+        assertTrue(upUnderTest.equals(copyOfUpUnderTest));
+
+        // different types -> returns false
+        assertFalse(upUnderTest.equals(1));
+
+        // null -> returns false
+        assertFalse(upUnderTest.equals(null));
+
+    }
+
+    @Test
+    public void testUserPrefsHashCode() {
+        UserPrefs upUnderTest = getTypicalUserPrefs();
+        int firstHashCode = upUnderTest.hashCode();
+
+        UserPrefs upUnderTestTwo = new UserPrefs();
+        upUnderTestTwo.setGuiSettings(5000, 300, 200, 100);
+        upUnderTestTwo.setAddressBookFilePath("addressbookDIFF.xml");
+        upUnderTestTwo.setAddressBookName("TypicalAddressBookNameDIFF");
+        int secondHashCode = upUnderTestTwo.hashCode();
+
+        UserPrefs upUnderTestThree = getTypicalUserPrefs();
+        int thirdHashCode = upUnderTestThree.hashCode();
+
+        // Different user pref generates different hash
+        assertFalse(firstHashCode == secondHashCode);
+
+        // Same user pref generates same hash
+        assertTrue(firstHashCode == thirdHashCode);
     }
 
     private UserPrefs getTypicalUserPrefs() {
