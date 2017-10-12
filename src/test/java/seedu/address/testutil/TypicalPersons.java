30db596af4d68625c467823ea81f3e83464e1d01
Jeremy <chanjem@gmail.com>
2017-10-12 21:09:08 +0800

    Added website test case for add command

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -29,36 +29,36 @@ public class TypicalPersons {
 
     public static final ReadOnlyPerson ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("85355255").withWebsite(Website.WEBSITE_NULL)
+            .withPhone("85355255").withWebsite(Website.WEBSITE_EXAMPLE)
             .withTags("friends").build();
     public static final ReadOnlyPerson BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
-            .withWebsite(Website.WEBSITE_NULL)
+            .withWebsite(Website.WEBSITE_EXAMPLE)
             .withTags("owesMoney", "friends").build();
     public static final ReadOnlyPerson CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
             .withEmail("heinz@example.com").withAddress("wall street")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
     public static final ReadOnlyPerson DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withAddress("10th street")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
     public static final ReadOnlyPerson ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com").withAddress("michegan ave")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
     public static final ReadOnlyPerson FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
             .withEmail("lydia@example.com").withAddress("little tokyo")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
     public static final ReadOnlyPerson GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
 
     // Manually added
     public static final ReadOnlyPerson HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
             .withEmail("stefan@example.com").withAddress("little india")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
     public static final ReadOnlyPerson IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
             .withEmail("hans@example.com").withAddress("chicago ave")
-            .withWebsite(Website.WEBSITE_NULL).build();
+            .withWebsite(Website.WEBSITE_EXAMPLE).build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final ReadOnlyPerson AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
