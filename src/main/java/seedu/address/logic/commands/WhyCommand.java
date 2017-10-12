73d914d232d8bb59a66466268e7a0c072502af14
Arnol <arnol.ice@gmail.com>
2017-10-13 00:30:24 +0800

    Complete WhyCommand class

--- src/main/java/seedu/address/logic/commands/WhyCommand.java
+++ src/main/java/seedu/address/logic/commands/WhyCommand.java
@@ -1,4 +1,53 @@
 package seedu.address.logic.commands;
 
-public class WhyCommand {
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.ReadOnlyPerson;
+
+
+
+/**
+ * Format full help instructions for every command for display.
+ */
+public class WhyCommand extends Command {
+
+    public static final String[] COMMAND_WORDS = {"why"};
+    public static final String COMMAND_WORD = "why";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Tells you why.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_WHY_REMARK_SUCCESS = "Added remark to Person: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+
+    public static final String SHOWING_WHY_MESSAGE = "Because %1$s %2$s";
+
+    private final Index targetIndex;
+
+    public WhyCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
+
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute() {
+        //EventsCenter.getInstance().post(new ShowHelpRequestEvent());
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            //throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyPerson personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        Name name = personToDelete.getName();
+        Address address = personToDelete.getAddress();
+        return new CommandResult(String.format(SHOWING_WHY_MESSAGE, name, address));
+    }
 }
