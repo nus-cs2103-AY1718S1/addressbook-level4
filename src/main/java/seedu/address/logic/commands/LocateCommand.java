bfe85cb052e91f96fe4633448636f9146b44f7f8
Yuchen He <yuchenhe@Yuchens-MBP.workgroup>
2017-10-13 00:21:59 +0800

    Send the event so that the addressbook can do the search

--- src/main/java/seedu/address/logic/commands/LocateCommand.java
+++ src/main/java/seedu/address/logic/commands/LocateCommand.java
@@ -1,7 +1,10 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.LocateCommandEvent;
+import seedu.address.commons.events.ui.ShowHelpRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
@@ -29,8 +32,17 @@ public class LocateCommand extends Command{
     }
 
     @Override
-    public CommandResult execute() {
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+    public CommandResult execute() throws CommandException{
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyPerson personToSearchAddress = lastShownList.get(targetIndex.getZeroBased());
+
+        EventsCenter.getInstance().post(new LocateCommandEvent(personToSearchAddress));
         return new CommandResult(MESSAGE_LOCATE_PERSON_SUCCESS);
     }
 }
