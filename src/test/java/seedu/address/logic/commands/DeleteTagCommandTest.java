aa7093b4f184b89f65eeca8ca059449f25954823
jiasheng <jiashengan@gmail.com>
2017-10-12 15:31:29 +0800

    Style change

--- src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
@@ -1,26 +1,16 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Test;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.UserPrefs;
-
+import seedu.address.model.tag.Tag;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for {@code DeleteTagCommand}.
