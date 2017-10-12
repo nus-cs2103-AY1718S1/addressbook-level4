de36d428bc6b186822903623dbde0434d83582ba
Yuchen He <yuchenhe@Yuchens-MBP.workgroup>
2017-10-13 00:22:38 +0800

    add the handleLocateCommandEvent in Browser to do the search

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.LocateCommandEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -74,5 +75,11 @@ public class BrowserPanel extends UiPart<Region> {
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         loadPersonPage(event.getNewSelection().person);
+    }
+
+    @Subscribe
+    private void handleLocateCommandEvent(LocateCommandEvent event){
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        loadLocationPage(event.getPerson());
     }
 }
