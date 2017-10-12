39148166a3caf52b9b6c8e741d8dd26fc58d5e09
Han Siang Phua <hansiang93@gmail.com>
2017-10-12 16:19:51 +0800

    Update FilterCommandParser.java

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new FilterCommand object
  */
 public class FilterCommandParser implements Parser<FilterCommand> {
 
