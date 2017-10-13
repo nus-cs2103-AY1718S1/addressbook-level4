5a32d7af12fcc07ac5b4aaee529570d3bbee4baa
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-13 15:15:23 +0800

    Add ConfigCommand class

---
+++ src/main/java/seedu/address/logic/commands/ConfigCommand.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Customizes the configuration of the application.
+ */
+public class ConfigCommand extends Command {
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        return null;
+    }
+}
