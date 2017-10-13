478e988c8c0fae32f62a9d135fffe13f99442112
jo-lyn <e0007247@u.nus.edu>
2017-10-13 22:10:23 +0800

    Add feedback for when user is typing in keyboard icon

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.animation.PauseTransition;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -9,6 +10,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
+import javafx.util.Duration;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
 import seedu.address.logic.ListElementPointer;
@@ -24,12 +26,16 @@ public class CommandBox extends UiPart<Region> {
 
     public static final String ERROR_STYLE_CLASS = "error";
     private static final String FXML = "CommandBox.fxml";
+    private static final int TIME_SINCE_TYPING = 300;
 
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private final Logic logic;
     private ListElementPointer historySnapshot;
-    private Image keyboardDefault;
+    private Image keyboardIdle;
+    private Image keyboardTyping;
     private Image keyboardError;
+    private PauseTransition pause;
+
 
     @FXML
     private TextField commandTextField;
@@ -40,10 +46,11 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(Logic logic) {
         super(FXML);
         this.logic = logic;
+        loadKeyboardIcons();
+        keyboardIcon.setImage(keyboardIdle);
+        pause = new PauseTransition(Duration.millis(TIME_SINCE_TYPING));
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
-        loadKeyboardIcons();
-        keyboardIcon.setImage(keyboardDefault);
         historySnapshot = logic.getHistorySnapshot();
     }
 
@@ -55,7 +62,8 @@ public class CommandBox extends UiPart<Region> {
      * Load images for keyboard icons in the command box.
      */
     private void loadKeyboardIcons() {
-        keyboardDefault = new Image(getClass().getResourceAsStream("/images/keyboard.png"));
+        keyboardIdle = new Image(getClass().getResourceAsStream("/images/keyboard.png"));
+        keyboardTyping = new Image(getClass().getResourceAsStream("/images/keyboardTyping.png"));
         keyboardError = new Image(getClass().getResourceAsStream("/images/keyboardError.png"));
     }
 
@@ -151,10 +159,14 @@ public class CommandBox extends UiPart<Region> {
 
     /**
      * Sets the command box style to use the default style.
+     * {@code keyboardTyping} icon changes to {@code keyboardIdle} when there is no change
+     * to text field after some time.
      */
     private void setStyleToDefault() {
         commandTextField.getStyleClass().remove(ERROR_STYLE_CLASS);
-        keyboardIcon.setImage(keyboardDefault);
+        keyboardIcon.setImage(keyboardTyping);
+        pause.setOnFinished(event -> keyboardIcon.setImage(keyboardIdle));
+        pause.playFromStart();
     }
 
     /**
