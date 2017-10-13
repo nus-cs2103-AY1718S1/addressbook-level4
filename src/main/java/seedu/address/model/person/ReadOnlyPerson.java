d03a3a91379a33c543fc4575d28c87dc519995dc
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-10-14 07:14:50 +0800

    include tag consideration for equals method

--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -41,7 +41,8 @@ public interface ReadOnlyPerson {
                 && other.getEmail().equals(this.getEmail())
                 && other.getAddress().equals(this.getAddress())
                 && other.getRemark().equals(this.getRemark())
-                && other.getFavouriteStatus().equals(this.getFavouriteStatus()));
+                && other.getFavouriteStatus().equals(this.getFavouriteStatus())
+                && other.getTags().equals(this.getTags()));
     }
 
     /**
