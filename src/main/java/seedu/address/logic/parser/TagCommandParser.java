d7b875c3800515d2a316d40dffbd82e29f9b94ce
Raisa Kabir <raisa.kabir201@gmail.com>
2017-10-13 17:26:18 +0800

    edited TagCommand and added Javadoc for multiple classes

--- src/main/java/seedu/address/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -4,8 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
@@ -40,7 +38,7 @@ public class TagCommandParser implements Parser<TagCommand> {
             for (int i = 0; i < numberOfIndices; i++) {
                 parsedIndices[i] = ParserUtil.parseIndex(splitIndices[i]);
             }
-            tagList = parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG));
+            tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
         }
@@ -55,19 +53,4 @@ public class TagCommandParser implements Parser<TagCommand> {
     private String[] splitIndices(ArgumentMultimap argMultimap) {
         return argMultimap.getPreamble().split(SEPARATOR_REGEX);
     }
-
-    /**
-     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
-     * If {@code tags} contain only one element which is an empty string, an exception will be thrown.
-     */
-    private Set<Tag> parseTagsForEdit(Collection<String> tags) throws IllegalValueException {
-        assert tags != null;
-
-        if (tags.isEmpty()) {
-            throw new IllegalValueException(MESSAGE_INVALID_COMMAND_FORMAT);
-        }
-        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
-        return ParserUtil.parseTags(tagSet);
-    }
-
 }
--- src/main/java/seedu/address/ui/Animation.java
+++ src/main/java/seedu/address/ui/Animation.java
@@ -5,15 +5,19 @@ import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
+/**
+ * Adds animation to the UI
+ */
 public class Animation {
 
-    public Animation() {
-    }
-
-    public static void fadeIn(ObservableList<Node> tags) {
-        for (Node tag : tags) {
-            tag.setVisible(true);
-            FadeTransition tagFadeTransition = new FadeTransition(Duration.millis(3000), tag);
+    /**
+     * Fades in a given list of nodes.
+     * @param nodes a list of nodes to fade into the UI.
+     */
+    public static void fadeIn(ObservableList<Node> nodes) {
+        for (Node node : nodes) {
+            node.setVisible(true);
+            FadeTransition tagFadeTransition = new FadeTransition(Duration.millis(3000), node);
             tagFadeTransition.setFromValue(0);
             tagFadeTransition.setToValue(1.0);
 
