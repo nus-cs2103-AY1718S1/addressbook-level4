748ce613fe6563da96a1ce865cc789a8a8c2d23a
khooroko <khoowp25@hotmail.com>
2017-10-13 13:40:07 +0800

    y

--- src/main/java/seedu/address/ui/InfoPanel.java
+++ src/main/java/seedu/address/ui/InfoPanel.java
@@ -120,13 +120,13 @@ public class InfoPanel extends UiPart<Region> {
      */
     private void loadDefaultPage() {
         for (Node node : pane.getChildren()) {
-            if (node instanceof Label) { (
+                if (node instanceof Label) { (
                     (Label) node).setText("");
             } else if (node instanceof TextFlow) {
                 for (Node subNode : ((TextFlow) node).getChildren()) {
-                    if (subNode instanceof Text) { (
+                        if (subNode instanceof Text) { (
                             (Text) subNode).setText("");
-                    } else if (subNode instanceof Label) { (
+                        } else if (subNode instanceof Label) { (
                             (Label) subNode).setText("");
                     }
                 }
