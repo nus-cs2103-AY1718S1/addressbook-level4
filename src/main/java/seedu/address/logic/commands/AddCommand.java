0a4fec1db9fabe66ef063c35a623daa4ee4b97a5
chernghann <chernghann.chia@gmail.com>
2017-10-13 12:29:12 +0800

    Small Minor edit in AddCommand.java, white space

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -33,7 +33,7 @@ public class AddCommand extends UndoableCommand {
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_SCH_EMAIL + "e0001111@u.nus.edu"
+            + PREFIX_SCH_EMAIL + "e0001111@u.nus.edu "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
