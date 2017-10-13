f2c23e3d5b3151d9d047a6a3e3a6595115fb5734
Sky Ang Tian Lan <tianlanwork44444@gmail.com>
2017-10-13 15:01:47 +0800

    add AllAttributesContainsKeywordsPredicate

---
+++ src/main/java/seedu/address/model/person/predicates/AllAttributesContainsKeywordsPredicate.java
@@ -0,0 +1,38 @@
+package seedu.address.model.person.predicates;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.person.ReadOnlyPerson;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ */
+public class AllAttributesContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
+    private final List<String> keywords;
+
+    public AllAttributesContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword)) ||
+                keywords.stream()
+                        .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword)) ||
+                keywords.stream()
+                        .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword)) ||
+                keywords.stream()
+                        .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AllAttributesContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((AllAttributesContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
