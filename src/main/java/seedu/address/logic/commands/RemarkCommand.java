9bdbc3ccb210513e06c243399d28a1dc6d1ee849
chilipadiboy <E0032076@u.nus.edu>
2017-10-15 04:59:51 +0800

    Update RemarkCommand.java

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,10 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
-
-import java.util.List;
-
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -14,6 +9,11 @@ import seedu.address.model.person.Remark;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
 /**
  * Changes the remark of an existing person in the address book.
  */
@@ -22,12 +22,12 @@ public class RemarkCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "remark";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
-        + "by the index number used in the last person listing. "
-        + "Existing remark will be overwritten by the input.\n"
-        + "Parameters: INDEX (must be a positive integer) "
-        + PREFIX_REMARK + "[REMARK]\n"
-        + "Example: " + COMMAND_WORD + " 1 "
-        + PREFIX_REMARK + "Likes to swim.";
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_REMARK + "[REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_REMARK + "Likes to swim.";
 
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
@@ -37,7 +37,7 @@ public class RemarkCommand extends UndoableCommand {
     private final Remark remark;
 
     /**
-     * @param index  of the person in the filtered person list to edit the remark
+     * @param index of the person in the filtered person list to edit the remark
      * @param remark of the person
      */
     public RemarkCommand(Index index, Remark remark) {
@@ -58,7 +58,7 @@ public class RemarkCommand extends UndoableCommand {
 
         ReadOnlyPerson personToEdit = lastShownList.get(index.getZeroBased());
         Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
-            personToEdit.getAddress(), personToEdit.getBirthday(), remark, personToEdit.getWebsite(), personToEdit.getTags());
+                personToEdit.getAddress(), personToEdit.getBirthday(), remark, personToEdit.getTags());
 
         try {
             model.updatePerson(personToEdit, editedPerson);
@@ -74,7 +74,6 @@ public class RemarkCommand extends UndoableCommand {
 
     /**
      * Generate success message based on the person edited
-     *
      * @param personToEdit
      * @return message whether remark is added or deleted
      */
@@ -101,6 +100,6 @@ public class RemarkCommand extends UndoableCommand {
         // state check
         RemarkCommand e = (RemarkCommand) other;
         return index.equals(e.index)
-            && remark.equals(e.remark);
+                && remark.equals(e.remark);
     }
 }
