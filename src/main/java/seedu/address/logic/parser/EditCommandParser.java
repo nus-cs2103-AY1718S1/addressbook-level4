c14767d95691f307e8b8bc2483fde0c5d2d0e365
Kennard Ng <Kennard_ng@outlook.com>
2017-10-14 22:28:08 +0800

    add ArticleNumber to EditCommandParser

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ARTICLE_NUMBER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -33,7 +34,8 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_ARTICLE_NUMBER, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
+                        PREFIX_ADDRESS, PREFIX_TAG);
 
         Index index;
 
@@ -45,6 +47,8 @@ public class EditCommandParser implements Parser<EditCommand> {
 
         EditParcelDescriptor editParcelDescriptor = new EditParcelDescriptor();
         try {
+            ParserUtil.parseArticleNumber(argMultimap.getValue(PREFIX_ARTICLE_NUMBER))
+                    .ifPresent(editParcelDescriptor::setArticleNumber);
             ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME)).ifPresent(editParcelDescriptor::setName);
             ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE)).ifPresent(editParcelDescriptor::setPhone);
             ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL)).ifPresent(editParcelDescriptor::setEmail);
