6e19aab382d8e7f7130a13eb71d1c48c07a83cd6
APPLETEA\Huijia <hjc2304@gmail.com>
2017-10-14 15:50:43 +0800

    junit test case for sort command

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,53 +1,46 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Test;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.testutil.TypicalPersons;
 
 public class SortCommandTest {
-    private Model model = new ModelManager(new AddressBook(), new UserPrefs());
+    private Model model;
 
     @Test
-    public void execute_validIndexUnfilteredList_success() throws Exception {
+    public void execute_success() throws Exception {
         //setting up the model to for entries in reverse order
-        model.addPerson(TypicalPersons.GEORGE);
-        model.addPerson(TypicalPersons.FIONA);
-        model.addPerson(TypicalPersons.ELLE);
-        model.addPerson(TypicalPersons.DANIEL);
-        model.addPerson(TypicalPersons.CARL);
-        model.addPerson(TypicalPersons.BENSON);
-        //model.addPerson(TypicalPersons.ALICE);
+        AddressBook abNIO = new AddressBook();
+        abNIO.addPerson(TypicalPersons.ELLE);
+        abNIO.addPerson(TypicalPersons.DANIEL);
+        abNIO.addPerson(TypicalPersons.CARL);
+        model = new ModelManager(abNIO, new UserPrefs());
 
         //create expectations
-//        SortCommand sortCommand = new SortCommand();
-        ModelManager expectedModel = new ModelManager(new AddressBook(), new UserPrefs());
-        expectedModel.addPerson(TypicalPersons.BENSON);
-        expectedModel.addPerson(TypicalPersons.CARL);
-        expectedModel.addPerson(TypicalPersons.DANIEL);
-        expectedModel.addPerson(TypicalPersons.ELLE);
-        expectedModel.addPerson(TypicalPersons.FIONA);
-        expectedModel.addPerson(TypicalPersons.GEORGE);
+        AddressBook abIO = new AddressBook();
+        abIO.addPerson(TypicalPersons.CARL);
+        abIO.addPerson(TypicalPersons.DANIEL);
+        abIO.addPerson(TypicalPersons.ELLE);
+        ModelManager expectedModel = new ModelManager(abIO, new UserPrefs());
 
+        //command and return result
+        SortCommand sortCommand = prepareCommand();
         String expectedMessage = SortCommand.MESSAGE_SUCCESS;
 
-        assertCommandSuccess(new SortCommand(), model, expectedMessage, expectedModel);
+        assertCommandSuccess(sortCommand, model, expectedMessage, expectedModel);
+    }
+
+    private SortCommand prepareCommand() {
+        SortCommand sortCommand = new SortCommand();
+        sortCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return sortCommand;
     }
 }
