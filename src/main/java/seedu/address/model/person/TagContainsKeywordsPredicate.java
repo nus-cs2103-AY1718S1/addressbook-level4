c8a268f86652b51910bc5b7b51f126321b599b35
DESKTOP-ISL6DTP\Adil Eyzekmoon <adil.eyzekmoon@gmail.com>
2017-10-15 01:06:57 +0800

    Implemented FindTagCommand

---
+++ src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
@@ -0,0 +1,42 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.StringJoiner;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ */
+public class TagContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
+    private final List<String> keywords;
+
+    public TagContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        Set<Tag> set = person.getTags();
+        Tag[] array = set.toArray(new Tag[0]);
+        StringJoiner stringjoiner = new StringJoiner(" ");
+        for (int i = 0; i < array.length; i++ ) {
+            stringjoiner.add(array[i].toString());
+        }
+        String finalString = stringjoiner.toString();
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(finalString, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TagContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((TagContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
