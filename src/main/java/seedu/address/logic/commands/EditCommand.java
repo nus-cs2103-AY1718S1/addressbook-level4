52010730aa2b60fb7860e732911ebefc1cb807ba
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-10-14 10:52:36 +0800

    amended message display

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -39,8 +39,7 @@ public class EditCommand extends UndoableCommand {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
             + "by the index number used in the last person listing. "
-            + "Existing values excluding tags will be overwritten by the input values.\n"
-            + "Input tag values will be added to existing tag values.\n"
+            + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + PREFIX_NAME + " [NAME] "
             + PREFIX_PHONE + " [PHONE] "
