7bde3bbf0dfb3e73a643a5c45256aa174c88f1a3
Wang Junming <e0134140@u.nus.edu>
2017-10-14 18:25:44 +0800

    change to lower case

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,12 +7,12 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_MODULE_CODE = new Prefix("M/");
-    public static final Prefix PREFIX_CLASS_TYPE = new Prefix("CT/");
-    public static final Prefix PREFIX_VENUE = new Prefix("V/");
-    public static final Prefix PREFIX_GROUP = new Prefix("GP/");
-    public static final Prefix PREFIX_TIME_SLOT = new Prefix("TS/");
-    public static final Prefix PREFIX_LECTURER = new Prefix("L/");
+    public static final Prefix PREFIX_MODULE_CODE = new Prefix("m/");
+    public static final Prefix PREFIX_CLASS_TYPE = new Prefix("ct/");
+    public static final Prefix PREFIX_VENUE = new Prefix("v/");
+    public static final Prefix PREFIX_GROUP = new Prefix("gp/");
+    public static final Prefix PREFIX_TIME_SLOT = new Prefix("ts/");
+    public static final Prefix PREFIX_LECTURER = new Prefix("l/");
     public static final Prefix PREFIX_FONT_SIZE = new Prefix("fs/");
 
 }
