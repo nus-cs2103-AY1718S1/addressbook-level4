f9f9ef65f29f1243e7bcb617635a45917fee95e3
Kennard Ng <Kennard_ng@outlook.com>
2017-10-14 23:26:20 +0800

    Add ArticleNumber to TypicalParcels

--- src/test/java/seedu/address/testutil/TypicalParcels.java
+++ src/test/java/seedu/address/testutil/TypicalParcels.java
@@ -24,30 +24,35 @@ import seedu.address.model.parcel.exceptions.DuplicateParcelException;
  */
 public class TypicalParcels {
 
-    public static final ReadOnlyParcel ALICE = new ParcelBuilder().withName("Alice Pauline")
-            .withAddress("6, Jurong West Ave 1, #08-111 S649520").withEmail("alice@example.com")
-            .withPhone("85355255")
-            .withTags("friends").build();
-    public static final ReadOnlyParcel BENSON = new ParcelBuilder().withName("Benson Meier")
-            .withAddress("336, Clementi Ave 2, #02-25 s120336")
-            .withEmail("johnd@example.com").withPhone("98765432")
-            .withTags("owesMoney", "friends").build();
-    public static final ReadOnlyParcel CARL = new ParcelBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("18 Marina Blvd, S018980").build();
-    public static final ReadOnlyParcel DANIEL = new ParcelBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("59 Namly Garden S267387").build();
-    public static final ReadOnlyParcel ELLE = new ParcelBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("2 Finlayson Green, S049247").build();
-    public static final ReadOnlyParcel FIONA = new ParcelBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("48 Upper Dickson Rd S207502").build();
-    public static final ReadOnlyParcel GEORGE = new ParcelBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("Block 532 HDB Upper Cross Street s050532").build();
+    public static final ReadOnlyParcel ALICE = new ParcelBuilder().withArticleNumber("RR000111000SG")
+            .withName("Alice Pauline").withAddress("6, Jurong West Ave 1, #08-111 S649520")
+            .withEmail("alice@example.com").withPhone("85355255").withTags("friends").build();
+    public static final ReadOnlyParcel BENSON = new ParcelBuilder().withArticleNumber("RR111000111SG")
+            .withName("Benson Meier").withAddress("336, Clementi Ave 2, #02-25 s120336").withEmail("johnd@example.com")
+            .withPhone("98765432").withTags("owesMoney", "friends").build();
+    public static final ReadOnlyParcel CARL = new ParcelBuilder().withArticleNumber("RR222000111SG")
+            .withName("Carl Kurz").withPhone("95352563").withEmail("heinz@example.com")
+            .withAddress("18 Marina Blvd, S018980").build();
+    public static final ReadOnlyParcel DANIEL = new ParcelBuilder().withArticleNumber("RR111321123SG")
+            .withName("Daniel Meier").withPhone("87652533").withEmail("cornelia@example.com")
+            .withAddress("59 Namly Garden S267387").build();
+    public static final ReadOnlyParcel ELLE = new ParcelBuilder().withArticleNumber("RR111321124SG")
+            .withName("Elle Meyer").withPhone("9482224").withEmail("werner@example.com")
+            .withAddress("2 Finlayson Green, S049247").build();
+    public static final ReadOnlyParcel FIONA = new ParcelBuilder().withArticleNumber("RR999123555SG")
+            .withName("Fiona Kunz").withPhone("9482427").withEmail("lydia@example.com")
+            .withAddress("48 Upper Dickson Rd S207502").build();
+    public static final ReadOnlyParcel GEORGE = new ParcelBuilder().withArticleNumber("RR696969696SG")
+            .withName("George Best").withPhone("9482442").withEmail("anna@example.com")
+            .withAddress("Block 532 HDB Upper Cross Street s050532").build();
 
     // Manually added
-    public static final ReadOnlyParcel HOON = new ParcelBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("522 Hougang Ave 6 s530522").build();
-    public static final ReadOnlyParcel IDA = new ParcelBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("3 River Valley Rd, S179024").build();
+    public static final ReadOnlyParcel HOON = new ParcelBuilder().withArticleNumber("RR121212124SG")
+            .withName("Hoon Meier").withPhone("8482424").withEmail("stefan@example.com")
+            .withAddress("522 Hougang Ave 6 s530522").build();
+    public static final ReadOnlyParcel IDA = new ParcelBuilder().withArticleNumber("RR111333888SG")
+            .withName("Ida Mueller").withPhone("8482131").withEmail("hans@example.com")
+            .withAddress("3 River Valley Rd, S179024").build();
 
     // Manually added - Parcel's details found in {@code CommandTestUtil}
     public static final ReadOnlyParcel AMY = new ParcelBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
