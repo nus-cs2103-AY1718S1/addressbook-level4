504dbfb3bf3db00f4f371f4cee03c4dcc4f86534
Eldon\eldonng <eldonng95@gmail.com>
2017-10-15 01:50:48 +0800

    add pin icon

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -3,10 +3,13 @@ package seedu.address.ui;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.ReadOnlyPerson;
+
 
 /**
  * An UI component that displays information of a {@code Person}.
@@ -14,6 +17,8 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
+    private static final String PIN_ICON = "/images/pinned_icon.png";
+    private static final String UNPIN_ICON = "/images/unpinned_icon.png";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -27,6 +32,8 @@ public class PersonCard extends UiPart<Region> {
 
     @FXML
     private HBox cardPane;
+    @FXML
+    private ImageView pinIcon;
     @FXML
     private Label name;
     @FXML
@@ -57,6 +64,7 @@ public class PersonCard extends UiPart<Region> {
         phone.textProperty().bind(Bindings.convert(person.phoneProperty()));
         address.textProperty().bind(Bindings.convert(person.addressProperty()));
         email.textProperty().bind(Bindings.convert(person.emailProperty()));
+        pinIcon.setImage(setPinIcon(person));
         person.tagProperty().addListener((observable, oldValue, newValue) -> {
             tags.getChildren().clear();
             person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
@@ -83,5 +91,15 @@ public class PersonCard extends UiPart<Region> {
         PersonCard card = (PersonCard) other;
         return id.getText().equals(card.id.getText())
                 && person.equals(card.person);
+    }
+
+    private Image setPinIcon(ReadOnlyPerson person) {
+        Image notPinned = new Image(UNPIN_ICON);
+        Image pinned = new Image(PIN_ICON);
+        if (person.isPinned()) {
+            return pinned;
+        } else {
+            return null;
+        }
     }
 }
