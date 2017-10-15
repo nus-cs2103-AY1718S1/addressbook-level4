d074530cb1eb3ca5718ddf2f306535d72d8f6bbe
Tao Jiashu <henrytjsh@gmail.com>
2017-10-15 15:00:13 +0800

    Break down long sentence into two lines

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -39,7 +39,8 @@ public class TypicalPersons {
     public static final ReadOnlyPerson ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com").withAddress("michegan ave").withTags("friend", "classmate").build();
     public static final ReadOnlyPerson FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").withFavourite("True").withTags("colleague", "friend").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo").withFavourite("True")
+            .withTags("colleague", "friend").build();
     public static final ReadOnlyPerson GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").build();
 
