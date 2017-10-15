11d674ce5354af2e4bce7a44e936c94f83a7fffb
Tao Jiashu <henrytjsh@gmail.com>
2017-10-15 23:07:10 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -91,7 +91,8 @@ public class AddressBookParserTest {
     public void parseCommand_favourite() throws Exception {
         FavouriteCommand command = (FavouriteCommand) parser.parseCommand(FavouriteCommand.COMMAND_WORD_1 + " "
                 + INDEX_FIRST_PERSON.getOneBased());
-        FavouriteCommand abbreviatedCommand = (FavouriteCommand) parser.parseCommand(FavouriteCommand.COMMAND_WORD_2 + " "
+        FavouriteCommand abbreviatedCommand =
+                (FavouriteCommand) parser.parseCommand(FavouriteCommand.COMMAND_WORD_2 + " "
                 + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new FavouriteCommand(INDEX_FIRST_PERSON), command);
         assertEquals(new FavouriteCommand(INDEX_FIRST_PERSON), abbreviatedCommand);
