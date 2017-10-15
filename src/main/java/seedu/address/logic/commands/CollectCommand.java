9ea7be3e632f3c8b9446e65f3ddf8f00b784aeb2
Wang Junming <e0134140@u.nus.edu>
2017-10-15 20:37:05 +0800

    refactor collectCommand

--- src/main/java/seedu/address/logic/commands/CollectCommand.java
+++ src/main/java/seedu/address/logic/commands/CollectCommand.java
@@ -1,24 +1,15 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
-import java.util.ArrayList;
 import java.util.List;
 
-import javafx.collections.ObservableList;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ListingUnit;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.model.person.exceptions.PersonNotFoundException;
-import seedu.address.model.person.predicates.UniqueAddressPredicate;
-import seedu.address.model.person.predicates.UniqueEmailPredicate;
-import seedu.address.model.person.predicates.UniquePhonePredicate;
+
+import static seedu.address.model.ListingUnit.PERSON;
 
 /**
  * Collects a person identified using it's last displayed index from the address book into the favor list.
@@ -33,6 +24,7 @@ public class CollectCommand extends UndoableCommand {
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_COLLECT_PERSON_SUCCESS = "Collected Person: %1$s";
+    public static final String MESSAGE_WRONG_LISTING_UNIT_FAILURE = "You can only add person into favor list";
 
     private final Index targetIndex;
 
@@ -52,27 +44,17 @@ public class CollectCommand extends UndoableCommand {
 
         ReadOnlyPerson personToCollect = lastShownList.get(targetIndex.getZeroBased());
 
-        switch (ListingUnit.getCurrentListingUnit()) {
-
-            case ADDRESS:
-                throw new CommandException("Cannot collect address");
-
-            case PHONE:
-                throw new CommandException("Cannot collect phone");
-
-            case EMAIL:
-                throw new CommandException("Cannot collect email");
-
-            default:
+        if (ListingUnit.getCurrentListingUnit().equals(PERSON)) {
                 try {
                     model.collectPerson(personToCollect);
                 } catch (DuplicatePersonException pnfe) {
                    throw new CommandException(pnfe.getMessage());
                 }
             return new CommandResult(String.format(MESSAGE_COLLECT_PERSON_SUCCESS, personToCollect));
+        } else {
+            throw  new CommandException(MESSAGE_WRONG_LISTING_UNIT_FAILURE);
         }
     }
-
 
 
     @Override
