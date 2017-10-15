93d7cca584a0b6f8b1be9792a133031e207ab6b3
Khor Shao Liang <stephenksl@gmail.com>
2017-10-16 00:00:18 +0800

    NameContainsKeywordsPredicate.java: update comment

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -10,7 +10,7 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} or {@code Tag} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
     private final HashMap<String, List<String>> keywords;
