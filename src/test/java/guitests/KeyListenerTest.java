e6db6b16665c279c15ed7772133883196a868f3f
jo-lyn <e0007247@u.nus.edu>
2017-10-16 01:11:54 +0800

    Add test cases for KeyListenerTest

--- src/test/java/guitests/KeyListenerTest.java
+++ src/test/java/guitests/KeyListenerTest.java
@@ -1,36 +1,80 @@
 package guitests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.UndoCommand;
 
 public class KeyListenerTest extends AddressBookGuiTest {
 
     @Test
-    public void handleKeyListeners() {
+    public void executeKeyEventForNonCommandEvents() {
         // Check that command box is accessible with assigned key presses
         guiRobot.push(KeyCode.ENTER);
-        assertTrue(mainWindowHandle.getCommandBox().isFocused());
+        assertTrue(getCommandBox().isFocused());
         guiRobot.push(KeyCode.A);
-        assertTrue(mainWindowHandle.getCommandBox().isFocused());
+        assertTrue(getCommandBox().isFocused());
         // Remove focus
         guiRobot.push(KeyCode.ESCAPE);
-        assertFalse(mainWindowHandle.getCommandBox().isFocused());
+        assertFalse(getCommandBox().isFocused());
 
         // Check that person list panel is accessible with assigned key presses
-        assertTrue(mainWindowHandle.getPersonListPanel().isFocused());
+        assertTrue(getPersonListPanel().isFocused());
         guiRobot.push(KeyCode.ESCAPE);
-        assertTrue(mainWindowHandle.getPersonListPanel().isFocused());
+        assertTrue(getPersonListPanel().isFocused());
         // Check scrolling
         guiRobot.push(KeyCode.UP);
-        assertTrue(mainWindowHandle.getPersonListPanel().isFocused());
+        assertTrue(getPersonListPanel().isFocused());
         guiRobot.push(KeyCode.DOWN);
-        assertTrue(mainWindowHandle.getPersonListPanel().isFocused());
+        assertTrue(getPersonListPanel().isFocused());
         // Remove focus
         guiRobot.push(KeyCode.ENTER);
-        assertFalse(mainWindowHandle.getPersonListPanel().isFocused());
+        assertFalse(getPersonListPanel().isFocused());
+    }
+
+    @Test
+    public void executeKeyEventForUndoCommand() {
+        KeyCodeCombination undoKeyCode = (KeyCodeCombination) KeyCombination.valueOf("Ctrl+Z");
+
+        guiRobot.push(undoKeyCode);
+        assertEquals(UndoCommand.MESSAGE_FAILURE, getResultDisplay().getText());
+
+        getCommandBox().run("delete 1");
+        guiRobot.push(undoKeyCode);
+        assertEquals(UndoCommand.MESSAGE_SUCCESS, getResultDisplay().getText());
+    }
+
+    @Test
+    public void executeKeyEventForListCommand() {
+        KeyCodeCombination listKeyCode = (KeyCodeCombination) KeyCombination.valueOf("Ctrl+L");
+
+        guiRobot.push(listKeyCode);
+        assertEquals(ListCommand.MESSAGE_SUCCESS, getResultDisplay().getText());
+    }
+
+
+    @Test
+    public void executeKeyEventForHistoryCommand() {
+        KeyCodeCombination viewHistoryKeyCode = (KeyCodeCombination) KeyCombination.valueOf("Ctrl+H");
+
+        guiRobot.push(viewHistoryKeyCode);
+        assertEquals(HistoryCommand.MESSAGE_NO_HISTORY, getResultDisplay().getText());
+
+        String command1 = "clear";
+        getCommandBox().run(command1);
+        guiRobot.push(viewHistoryKeyCode);
+
+        String expectedMessage = String.format(HistoryCommand.MESSAGE_SUCCESS,
+                String.join("\n", command1, "history"));
+
+        assertEquals(expectedMessage, getResultDisplay().getText());
     }
 }
