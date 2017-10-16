262f88254063129bed84bd3aabbc5a17ebb03400
Unknown <vivekscl5@gmail.com>
2017-10-16 15:34:43 +0800

    RemoveTagCommandParserTest: Add more tests

--- src/test/java/seedu/address/logic/parser/RemoveTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemoveTagCommandParserTest.java
@@ -4,6 +4,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
@@ -21,7 +22,7 @@ public class RemoveTagCommandParserTest {
     private RemoveTagCommandParser parser = new RemoveTagCommandParser();
 
     @Test
-    public void parse_indexSpecified_failure() throws Exception {
+    public void parse_indexAndTagSpecified_success() throws Exception {
         ArrayList<Index> indexes = new ArrayList<Index>();
         indexes.add(INDEX_FIRST_PERSON);
         indexes.add(INDEX_SECOND_PERSON);
@@ -43,9 +44,31 @@ public class RemoveTagCommandParserTest {
                 + nonExistentTagName;
         expectedCommand = new RemoveTagCommand(indexes, nonExistentTag);
         assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_tagSpecified_failure() throws Exception {
+        // no indexes
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        final String tagName = "friends";
+        Tag toRemove = new Tag(tagName);
+        String userInput = " " + PREFIX_TAG.toString() + tagName;
+
+        assertParseFailure(parser, userInput, MESSAGE_INVALID_INDEX);
+    }
+
+    @Test
+    public void parse_indexSpecified_failure() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
 
         // no tags
-        userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString();
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString();
         assertParseFailure(parser, userInput, MESSAGE_TAG_CONSTRAINTS);
     }
 
