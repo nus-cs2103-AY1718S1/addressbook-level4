9736ee02f606eed59d55cf1936f487d49ce8623a
Khor Shao Liang <stephenksl@gmail.com>
2017-10-16 09:50:29 +0800

    NameContainsKeywordsPredicate.java: update to roll back to previous implementation

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,44 +1,24 @@
 package seedu.address.model.person;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
 import java.util.List;
-import java.util.HashMap;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code ReadOnlyPerson}'s {@code Name} or {@code Tag} matches any of the keywords given.
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
  */
 public class NameContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
-    private final HashMap<String, List<String>> keywords;
+    private final List<String> keywords;
 
-    public NameContainsKeywordsPredicate(HashMap<String, List<String>> keywords) {
+    public NameContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(ReadOnlyPerson person) {
-        boolean result = false;
-        if (keywords.containsKey(PREFIX_NAME.toString()) && !keywords.get(PREFIX_NAME.toString()).isEmpty()) {
-            result = keywords.get(PREFIX_NAME.toString()).stream()
-                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
-            
-        }
-
-        if (keywords.containsKey(PREFIX_TAG.toString()) && !keywords.get(PREFIX_TAG.toString()).isEmpty()) {
-            result = result || person.containTags(keywords.get(PREFIX_TAG.toString()));
-        }
-
-        if (keywords.containsKey(PREFIX_EMAIL.toString()) && !keywords.get(PREFIX_EMAIL.toString()).isEmpty()) {
-            result = result || keywords.get(PREFIX_EMAIL.toString()).stream()
-                    .anyMatch(keyword -> person.getEmail().value.contains(keyword));
-        }
-        
-        return result;
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
     }
 
     @Override
