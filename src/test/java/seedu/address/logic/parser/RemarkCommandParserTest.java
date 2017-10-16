fb397195396fb959602ea72b1ec935412515ab24
deep4k <e0031591@u.nus.edu>
2017-10-17 01:02:59 +0800

    Refactor remark feature

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -23,12 +24,12 @@ public class RemarkCommandParserTest {
 
         // have remarks
         Index targetIndex = INDEX_FIRST_PERSON;
-        String userInput = targetIndex.getOneBased() + " " + CliSyntax.PREFIX_REMARK.toString() + " " + remark;
+        String userInput = targetIndex.getOneBased() + " " + PREFIX_REMARK.toString() + " " + remark;
         RemarkCommand expectedCommand = new RemarkCommand(INDEX_FIRST_PERSON, remark);
         assertParseSuccess(parser, userInput, expectedCommand);
 
         // no remarks
-        userInput = targetIndex.getOneBased() + " " + CliSyntax.PREFIX_REMARK.toString();
+        userInput = targetIndex.getOneBased() + " " + PREFIX_REMARK.toString();
         expectedCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(""));
         assertParseSuccess(parser, userInput, expectedCommand);
     }
