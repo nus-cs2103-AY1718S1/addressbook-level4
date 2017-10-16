81623b3fbafd9547926758b65ca891e3f56cd1cd
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-16 21:49:12 +0800

    Add ConfigCommand class and add its explanation

--- src/main/java/seedu/address/logic/commands/ConfigCommand.java
+++ src/main/java/seedu/address/logic/commands/ConfigCommand.java
@@ -1,11 +1,26 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONFIG_TYPE;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 
 /**
  * Customizes the configuration of the application.
  */
 public class ConfigCommand extends Command {
+
+    public static final String COMMAND_WORD = "config";
+    public static final String COMMAND_ALIAS = "cfg";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes the configuration of the application. "
+            + "Parameters: "
+            + PREFIX_CONFIG_TYPE + "CONFIG_TYPE "
+            + "NEW_CONFIG_VALUE\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_CONFIG_TYPE + "set-tag-color "
+            + "friends #9381a0";
+
+    public static final String MESSAGE_SUCCESS = "Configuration changed: %1$s";
 
     @Override
     public CommandResult execute() throws CommandException {
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -4,8 +4,7 @@ package seedu.address.logic.parser;
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
 public class CliSyntax {
-
-    /* Prefix definitions */
+    /* Prefix definitions for pre-loaded properties. */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_DATE_TIME = new Prefix("d/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
@@ -13,5 +12,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
-
+    /* Prefix definitions for configuration change type. */
+    public static final Prefix PREFIX_CONFIG_TYPE  = new Prefix("--");
 }
