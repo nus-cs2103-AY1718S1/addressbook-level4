52147d29247f6aa1e989b17254fccb77d324943e
Lenald <lenaldnwj@gmail.com>
2017-10-17 01:50:05 +0800

    Update test cases to reflect changes after resolving merge conflict

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -30,39 +30,40 @@ public class TypicalPersons {
 
     public static final ReadOnlyPerson ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("85355255").withFormClass("12S23").withPostalCode("279392").withTags("friends").build();
+            .withPhone("student/97272031 parent/97979797")
+            .withFormClass("12S23").withPostalCode("279392").withTags("friends").build();
     public static final ReadOnlyPerson BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25").withEmail("johnd@example.com")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withFormClass("12S23").withPostalCode("279392").withTags("owesMoney", "friends").build();
     public static final ReadOnlyPerson CARL = new PersonBuilder().withName("Carl Kurz")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("heinz@example.com").withAddress("wall street").withFormClass("12S23")
             .withPostalCode("279392").build();
     public static final ReadOnlyPerson DANIEL = new PersonBuilder().withName("Daniel Meier")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("cornelia@example.com").withAddress("10th street").withFormClass("12S23")
             .withPostalCode("279392").build();
     public static final ReadOnlyPerson ELLE = new PersonBuilder().withName("Elle Meyer")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("werner@example.com").withAddress("michegan ave").withFormClass("12S23").withPostalCode("279392")
             .build();
     public static final ReadOnlyPerson FIONA = new PersonBuilder().withName("Fiona Kunz")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("lydia@example.com").withAddress("little tokyo").withFormClass("12S23").withPostalCode("279392")
             .build();
     public static final ReadOnlyPerson GEORGE = new PersonBuilder().withName("George Best")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("anna@example.com").withAddress("4th street").withFormClass("12S23").withPostalCode("279392")
             .build();
 
     // Manually added
     public static final ReadOnlyPerson HOON = new PersonBuilder().withName("Hoon Meier")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("stefan@example.com").withAddress("little india").withFormClass("12S23").withPostalCode("987527")
             .build();
     public static final ReadOnlyPerson IDA = new PersonBuilder().withName("Ida Mueller")
-            .withPhone("student/97272031 parent/97979797 ")
+            .withPhone("student/97272031 parent/97979797")
             .withEmail("hans@example.com").withAddress("chicago ave").withFormClass("12S23").withPostalCode("123456")
             .build();
     // Manually added - Person's details found in {@code CommandTestUtil}
