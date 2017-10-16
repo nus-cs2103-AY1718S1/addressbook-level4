9c3c8def0d1cee49828736e8e11f8bddf0f27a14
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-17 01:04:56 +0800

    Fix ConfigParserTest

--- src/test/java/seedu/address/logic/parser/ConfigCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ConfigCommandParserTest.java
@@ -5,13 +5,12 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_CONFIG_NEW_PROP
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CONFIG_TAG_COLOR;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_COLOR;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.logic.commands.configCommands.ConfigCommand.ConfigType.ADD_PROPERTY;
-import static seedu.address.logic.commands.configCommands.ConfigCommand.ConfigType.TAG_COLOR;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.Test;
 
-import seedu.address.logic.commands.configCommands.ConfigCommand;
+import seedu.address.logic.commands.configCommands.AddPropertyCommand;
+import seedu.address.logic.commands.configCommands.ChangeTagColorCommand;
 
 public class ConfigCommandParserTest {
     private ConfigCommandParser parser = new ConfigCommandParser();
@@ -19,9 +18,9 @@ public class ConfigCommandParserTest {
     @Test
     public void parse_allFieldsPresent_success() {
         assertParseSuccess(parser, VALID_CONFIG_TAG_COLOR + VALID_TAG_HUSBAND + VALID_TAG_COLOR,
-                new ConfigCommand(TAG_COLOR, "husband #7db9a1"));
+                new ChangeTagColorCommand("husband #7db9a1", "husband", "#7db9a1"));
 
         assertParseSuccess(parser, VALID_CONFIG_ADD_PROPERTY + VALID_CONFIG_NEW_PROPERTY,
-                new ConfigCommand(ADD_PROPERTY, "1"));
+                new AddPropertyCommand("1"));
     }
 }
