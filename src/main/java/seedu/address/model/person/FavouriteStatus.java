2d5463dab54acd6db27532b504397eec1c55423a
Houjisan <hongjing10188@gmail.com>
2017-10-17 00:57:22 +0900

    Minor fix

--- src/main/java/seedu/address/model/person/FavouriteStatus.java
+++ src/main/java/seedu/address/model/person/FavouriteStatus.java
@@ -1,7 +1,5 @@
 package seedu.address.model.person;
 
-import static java.util.Objects.requireNonNull;
-
 /**
  * Represents a Person's favourite status in the address book.
  * Guarantees: immutable; is always valid
