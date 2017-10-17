a2f29552ade5421dd97895a6f60028651a21652b
Wong Zeng Feng <e0031529@u.nus.edu>
2017-10-17 19:02:46 +0800

    Model: Modify `ReadOnlyPerson` to support a `Birthday` field 	- Add three methods `setBirthday(Birthday)`, `getBirthday()` and `birthdayProperty()`. 	- Implement these newly created methods in `Person`,which implements the `ReadOnlyPerson` interface.

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -9,6 +9,7 @@ import javax.xml.bind.annotation.XmlElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Birthday;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -70,7 +71,8 @@ public class XmlAdaptedPerson {
         final Phone phone = new Phone(this.phone);
         final Email email = new Email(this.email);
         final Address address = new Address(this.address);
+        final Birthday birthday = new Birthday(""); //TODO: To be fixed in Storage commit
         final Set<Tag> tags = new HashSet<>(personTags);
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, birthday, tags);
     }
 }
