102bcf96156b0fd866c029c5c23fd17aac785bc8
FRACTOR-PC\Fractor <zyng28@gmail.com>
2017-10-18 01:56:16 +0800

    Attenpt at linking person information panel with person information

--- src/main/java/seedu/address/ui/PersonInformationPanel.java
+++ src/main/java/seedu/address/ui/PersonInformationPanel.java
@@ -1,10 +1,21 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
+import seedu.address.MainApp;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
 /**
@@ -13,6 +24,11 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class PersonInformationPanel extends UiPart<Region> {
 
     private static final String FXML = "PersonInformationPanel.fxml";
+    private static String[] colors = {"red", "yellow", "blue", "orange", "brown", "green", "pink", "black", "grey"};
+    private static HashMap<String, String> tagColors = new HashMap<String, String>();
+    private static Random random = new Random();
+
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
 
     @FXML
     private Label name;
@@ -27,7 +43,43 @@ public class PersonInformationPanel extends UiPart<Region> {
     @FXML
     private FlowPane tags;
 
+    private static String getColorForTag(String tagValue) {
+        if (!tagColors.containsKey(tagValue)) {
+            tagColors.put(tagValue, colors[random.nextInt(colors.length)]);
+        }
+
+        return tagColors.get(tagValue);
+    }
+
     public PersonInformationPanel() {
         super(FXML);
+        loadDefaultScreen();
+        registerAsAnEventHandler(this);
+    }
+
+    private void loadDefaultScreen() {}
+
+    private void loadPersonInformation(ReadOnlyPerson person) {
+        name.textProperty().bind(Bindings.convert(person.nameProperty()));
+        phone.textProperty().bind(Bindings.convert(person.phoneProperty()));
+        address.textProperty().bind(Bindings.convert(person.addressProperty()));
+        email.textProperty().bind(Bindings.convert(person.emailProperty()));
+        person.tagProperty().addListener((observable, oldValue, newValue) -> {
+            tags.getChildren().clear();
+            initTags(person);
+        });
+    }
+
+    private void initTags(ReadOnlyPerson person) {
+        person.getTags().forEach(tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            tagLabel.setStyle("-fx-background-color: " + getColorForTag(tag.tagName));
+            tags.getChildren().add(tagLabel);
+        });
+
+    @Subscribe
+    private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        loadPersonInformation(event.getNewSelection().person);
     }
 }
