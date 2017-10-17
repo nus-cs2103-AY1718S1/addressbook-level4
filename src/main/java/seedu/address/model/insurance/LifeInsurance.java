0c9ca0b80533aecfb9f216e8ae3159979ea9d03b
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-10-18 05:36:44 +0800

    Add Insurance and LifeInsurance

---
+++ src/main/java/seedu/address/model/insurance/LifeInsurance.java
@@ -0,0 +1,28 @@
+package seedu.address.model.insurance;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.io.File;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import seedu.address.model.person.Person;
+
+public class LifeInsurance extends Insurance {
+
+    private ObjectProperty<Person> owner;
+    private ObjectProperty<Person> insured;
+    private ObjectProperty<Person> beneficiary;
+    private ObjectProperty<Double> premium;
+    private ObjectProperty<File> contract;
+
+    public LifeInsurance(Person owner, Person insured, Person beneficiary, Double premium, File contract) {
+        requireAllNonNull(owner, insured, beneficiary, premium, contract);
+        this.owner = new SimpleObjectProperty<>(owner);
+        this.insured = new SimpleObjectProperty<>(insured);
+        this.beneficiary = new SimpleObjectProperty<>(beneficiary);
+        this.premium = new SimpleObjectProperty<>(premium);
+        this.contract = new SimpleObjectProperty<>(contract);
+    }
+}
