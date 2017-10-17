3f0802500a4623c25a8fb7bf4e308fa6573ff30b
FRACTOR-PC\Fractor <zyng28@gmail.com>
2017-10-17 14:43:40 +0800

    Change label names appropriately.

--- src/main/java/seedu/address/ui/PersonInformationPanel.java
+++ src/main/java/seedu/address/ui/PersonInformationPanel.java
@@ -1,15 +1,31 @@
 package seedu.address.ui;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
-import javafx.scene.shape.Rectangle;
+import seedu.address.model.person.ReadOnlyPerson;
 
+/**
+ * The person information panel of the app.
+ */
 public class PersonInformationPanel extends UiPart<Region> {
 
     private static final String FXML = "PersonInformationPanel.fxml";
 
     @FXML
-    private Rectangle testRectangle;
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
+    @FXML
+    private FlowPane tags;
 
     public PersonInformationPanel() {
         super(FXML);
