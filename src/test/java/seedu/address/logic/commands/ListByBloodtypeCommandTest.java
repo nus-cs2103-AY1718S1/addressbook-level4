480b632a471fe264aa355cf656dc406a62258892
DESKTOP-9RFF1NR\Ernest Ong YuHeng <E0031105@u.nus.edu>
2017-10-18 01:07:57 +0800

    Checkstyle change 2.

--- src/test/java/seedu/address/logic/commands/ListByBloodtypeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListByBloodtypeCommandTest.java
@@ -22,8 +22,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.BloodtypeContainsKeywordPredicate;
+import seedu.address.model.person.ReadOnlyPerson;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
