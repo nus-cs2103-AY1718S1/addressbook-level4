a0b97898304e25f2b9f9f9cc75e903ac31231301
Alex Foo <alexfoodw@gmail.com>
2017-10-17 14:27:43 +0800

    complete v1 of authfacebook command, with chromebrowser popup

--- src/main/java/seedu/address/logic/commands/ConnectFacebookCommand.java
+++ src/main/java/seedu/address/logic/commands/ConnectFacebookCommand.java
@@ -5,10 +5,15 @@ import facebook4j.FacebookFactory;
 import facebook4j.auth.AccessToken;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.ui.BrowserPanel;
 
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static sun.misc.Signal.raise;
 
 /**
  * Connects the addressbook to Facebook account.
@@ -19,21 +24,10 @@ public class ConnectFacebookCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Connected to your Facebook Account!";
 
-    private String domain = "https://www.facebook.com/";
-    private String appID = "1985095851775955";
+    private static String domain = "https://www.facebook.com/";
+    private static String appID = "1985095851775955";
     private String accessToken;
-    private String authURL = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=" + appID
-            + "&redirect_uri=" + domain + "&scope=user_about_me,email,publish_actions,user_birthday,"
-            + "user_education_history,user_friends,user_games_activity,user_hometown,user_likes,"
-            + "user_location,user_photos,user_posts,user_relationship_details,user_relationships,"
-            + "user_religion_politics,user_status,user_tagged_places,user_videos,user_website,user_work_history,"
-            + "ads_management,ads_read,business_management,manage_pages,pages_manage_cta,publish_pages,"
-            + "user_actions.books,user_actions.fitness,pages_messaging,read_custom_friendlists,"
-            + "pages_messaging_phone_number,pages_messaging_subscriptions,pages_show_list,user_actions.music,"
-            + "user_actions.news,read_page_mailboxes,rsvp_event,user_events,user_managed_groups,"
-            + "pages_manage_instant_articles,user_actions.video,instagram_basic,instagram_manage_comments,"
-            + "instagram_manage_insights,read_audience_network_insights,read_insights";
-    private String commaSeparetedPermissions = "user_about_me,email,publish_actions,user_birthday,"
+    private static String commaSeparetedPermissions = "user_about_me,email,publish_actions,user_birthday,"
             + "user_education_history,user_friends,user_games_activity,user_hometown,user_likes,"
             + "user_location,user_photos,user_posts,user_relationship_details,user_relationships,"
             + "user_religion_politics,user_status,user_tagged_places,user_videos,user_website,user_work_history,"
@@ -44,8 +38,17 @@ public class ConnectFacebookCommand extends Command {
             + "pages_manage_instant_articles,user_actions.video,instagram_basic,instagram_manage_comments,"
             + "instagram_manage_insights,read_audience_network_insights,read_insights";
 
+    private static String authURL = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=" + appID
+            + "&redirect_uri=" + domain + "&scope=" + commaSeparetedPermissions;
+
+
+    public static String getAuthURL() {
+        return authURL;
+    }
+
     @Override
     public CommandResult execute() throws CommandException {
+
         System.setProperty("webdriver.chrome.driver", "chromedriver");
 
         WebDriver driver = new ChromeDriver();
@@ -66,7 +69,6 @@ public class ConnectFacebookCommand extends Command {
                 break;
             }
         }
-
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
