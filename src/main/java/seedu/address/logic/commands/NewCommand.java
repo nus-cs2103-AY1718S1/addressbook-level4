02a75099349b0dbf83a192ad5f04921c903397e8
Alex Ang <E0032221@u.nus.edu>
2017-10-17 17:55:27 +0800

    Add Open and New Commands

---
+++ src/main/java/seedu/address/logic/commands/NewCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Creates a new Rolodex at a specified directory
+ */
+public class NewCommand extends Command {
+    @Override
+    public CommandResult execute() throws CommandException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/commands/OpenCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Opens an existing Rolodex in a different directory
+ */
+public class OpenCommand extends Command {
+    @Override
+    public CommandResult execute() throws CommandException {
+        return null;
+    }
+}
