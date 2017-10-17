eb015d153f77ad8079f4a2d90bbd816dbacf9e43
liuhang0213 <liuhang0213@gmail.com>
2017-10-17 10:03:32 +0800

    Prints out current preference value

--- src/main/java/seedu/address/logic/commands/PrefCommand.java
+++ src/main/java/seedu/address/logic/commands/PrefCommand.java
@@ -1,6 +1,12 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.UserPrefs;
 
 /**
  * Edits the details of an existing person in the address book.
@@ -16,6 +22,8 @@ public class PrefCommand extends UndoableCommand {
 
     public static final String MESSAGE_EDIT_PREF_SUCCESS = "Edited preference: %1$s";
     public static final String MESSAGE_NOT_EDITED = "Please enter the new value for the preference";
+    public static final String MESSAGE_PREF_KEY_NOT_FOUND = "Invalid preference key: %1$s";
+    public static final String MESSAGE_ACCESSING_PREF_ERROR = "Unable to read preference value for %1$s";
 
     private static String prefKey;
 
@@ -27,7 +35,22 @@ public class PrefCommand extends UndoableCommand {
 
     @Override
     public CommandResult executeUndoableCommand() throws CommandException {
-        return new CommandResult(prefKey);
+        requireNonNull(model);
+        String prefValue = "";
+        prefKey = prefKey.trim();
+        try {
+            UserPrefs prefs = model.getUserPrefs();
+            // Attempt to get pref value by accessing the correspnding get method
+            String getMethodName = "get" + prefKey;
+            Class userPrefsClass = prefs.getClass();
+            Method getMethod = userPrefsClass.getMethod(getMethodName, null);
+            prefValue = getMethod.invoke(prefs, null).toString();
+        } catch (NoSuchMethodException e) {
+            throw new CommandException(String.format(MESSAGE_PREF_KEY_NOT_FOUND, prefKey));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new CommandException(String.format(MESSAGE_ACCESSING_PREF_ERROR, prefKey));
+        }
+        return new CommandResult(prefValue);
     }
 
     @Override
