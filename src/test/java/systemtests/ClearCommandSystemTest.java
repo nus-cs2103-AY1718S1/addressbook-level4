24a13739b470865ef91fb132ec9d94214bdf6d59
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-10-18 00:02:37 +0800

    JUnit: Modify ClearCommandSystemTest to include command feature

--- src/test/java/systemtests/ClearCommandSystemTest.java
+++ src/test/java/systemtests/ClearCommandSystemTest.java
@@ -17,6 +17,7 @@ public class ClearCommandSystemTest extends AddressBookSystemTest {
     @Test
     public void clear() {
         final Model defaultModel = getModel();
+        executeCommand("password");
 
         /* Case: clear non-empty address book, command with leading spaces and trailing alphanumeric characters and
          * spaces -> cleared
