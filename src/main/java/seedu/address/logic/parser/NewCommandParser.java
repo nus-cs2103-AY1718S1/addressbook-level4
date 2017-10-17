46135998e4bcd9908ed1c874ab122196a3f88da0
Alex Ang <E0032221@u.nus.edu>
2017-10-18 03:10:59 +0800

    Standby initial NewCommandParser object

---
+++ src/main/java/seedu/address/logic/parser/NewCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.NewCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class NewCommandParser implements Parser<NewCommand> {
+    @Override
+    public NewCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
