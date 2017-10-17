ae4ed2a69857a97179f87af305c95eaceae4f913
Alex Ang <E0032221@u.nus.edu>
2017-10-17 17:56:46 +0800

    Add command words

--- src/main/java/seedu/address/logic/commands/NewCommand.java
+++ src/main/java/seedu/address/logic/commands/NewCommand.java
@@ -6,6 +6,9 @@ import seedu.address.logic.commands.exceptions.CommandException;
  * Creates a new Rolodex at a specified directory
  */
 public class NewCommand extends Command {
+
+    public static final String COMMAND_WORD = "new";
+    
     @Override
     public CommandResult execute() throws CommandException {
         return null;
--- src/main/java/seedu/address/logic/commands/OpenCommand.java
+++ src/main/java/seedu/address/logic/commands/OpenCommand.java
@@ -6,6 +6,9 @@ import seedu.address.logic.commands.exceptions.CommandException;
  * Opens an existing Rolodex in a different directory
  */
 public class OpenCommand extends Command {
+
+    public static final String COMMAND_WORD = "open";
+
     @Override
     public CommandResult execute() throws CommandException {
         return null;
