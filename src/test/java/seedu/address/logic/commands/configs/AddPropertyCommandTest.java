3d8052ff84fd64a07d37d86ac11d644930b7e601
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-17 13:26:59 +0800

    Finish test case for AddPropertyCommand::execute()

--- src/test/java/seedu/address/logic/commands/configs/AddPropertyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/configs/AddPropertyCommandTest.java
@@ -3,23 +3,31 @@ package seedu.address.logic.commands.configs;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CONFIG_NEW_PROPERTY;
 
+import java.util.regex.PatternSyntaxException;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.stub.ModelStub;
+import seedu.address.model.property.PropertyManager;
+import seedu.address.model.property.exceptions.DuplicatePropertyException;
+
 public class AddPropertyCommandTest {
-    @Test
-    public void createCommand_allFields_success() throws Exception {
-        ConfigCommand command = new AddPropertyCommand(VALID_CONFIG_NEW_PROPERTY, "b", "birthday",
-                "something", "[^\\s].*");
-        command.execute();
+    private ConfigCommand successCommand;
+
+    @Before
+    public void setUp() {
+        successCommand = new AddPropertyCommand(VALID_CONFIG_NEW_PROPERTY, "b",
+                "birthday", "something", "[^\\s].*");
+        successCommand.setData(new AddPropertyModelStub(), new CommandHistory(), new UndoRedoStack());
     }
 
     @Test
     public void execute_addNewProperty_success() throws Exception {
-        ConfigCommand command = new AddPropertyCommand(VALID_CONFIG_NEW_PROPERTY, "b", "birthday",
-                "something", "[^\\s].*");
-
         // It should not throw any exception here. The command should succeed.
-        command.execute();
+        successCommand.execute();
     }
 
     @Test
@@ -30,5 +38,13 @@ public class AddPropertyCommandTest {
                 "something", "[^\\s].*");
 
         assertEquals(command1, command2);
+    }
+
+    private class AddPropertyModelStub extends ModelStub {
+        @Override
+        public void addProperty(String shortName, String fullName, String message, String regex)
+                throws DuplicatePropertyException, PatternSyntaxException {
+            PropertyManager.addNewProperty(shortName, fullName, message, regex);
+        }
     }
 }
