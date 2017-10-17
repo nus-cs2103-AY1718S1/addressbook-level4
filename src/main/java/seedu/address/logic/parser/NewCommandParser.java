8cdfe7b4ac5865f4ecf03cf8c569c3e06f6e3197
Alex Ang <E0032221@u.nus.edu>
2017-10-18 03:29:27 +0800

    Add javadoc comment for NewCommandParser

--- src/main/java/seedu/address/logic/parser/NewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/NewCommandParser.java
@@ -3,6 +3,9 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.NewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new NewCommand object
+ */
 public class NewCommandParser implements Parser<NewCommand> {
     @Override
     public NewCommand parse(String userInput) throws ParseException {
