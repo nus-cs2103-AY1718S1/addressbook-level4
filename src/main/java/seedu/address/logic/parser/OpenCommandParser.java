e5ff3f4582e8f9c801c411cdc9f734799b761cf4
Alex Ang <E0032221@u.nus.edu>
2017-10-18 04:42:13 +0800

    Add functionality for blocking invalid file extensions
    Rolodex files without .rldx will not be saved.

--- src/main/java/seedu/address/logic/parser/OpenCommandParser.java
+++ src/main/java/seedu/address/logic/parser/OpenCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.storage.util.RolodexStorageUtil.isValidRolodexStorageFilepath;
 
 import seedu.address.logic.commands.OpenCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -10,21 +11,18 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class OpenCommandParser implements Parser<OpenCommand> {
 
-    public static final String FILEPATH_REGEX = "^(.+)/([^/]+)$";
-
     /**
      * Parses the given {@code String} of arguments in the context of the OpenCommand
      * and returns a OpenCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public OpenCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        trimmedArgs = trimmedArgs.replace("\\", "/");
-        if (trimmedArgs.isEmpty()
-                || !trimmedArgs.matches(FILEPATH_REGEX)) {
+        String trimmedAndFormattedArgs = args.trim().replace("\\", "/");
+        if (trimmedAndFormattedArgs.isEmpty()
+                || !isValidRolodexStorageFilepath(trimmedAndFormattedArgs)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, OpenCommand.MESSAGE_USAGE));
         }
 
-        return new OpenCommand(trimmedArgs);
+        return new OpenCommand(trimmedAndFormattedArgs);
     }
 }
--- src/main/java/seedu/address/storage/XmlRolodexStorage.java
+++ src/main/java/seedu/address/storage/XmlRolodexStorage.java
@@ -1,6 +1,9 @@
 package seedu.address.storage;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_EXTENSION_FORMAT;
+import static seedu.address.storage.util.RolodexStorageUtil.ROLODEX_FILE_EXTENSION;
+import static seedu.address.storage.util.RolodexStorageUtil.isValidRolodexStorageExtension;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,6 +13,7 @@ import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.commons.exceptions.InvalidExtensionException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.model.ReadOnlyRolodex;
 
@@ -68,6 +72,10 @@ public class XmlRolodexStorage implements RolodexStorage {
     public void saveRolodex(ReadOnlyRolodex rolodex, String filePath) throws IOException {
         requireNonNull(rolodex);
         requireNonNull(filePath);
+        if (!isValidRolodexStorageExtension(filePath)) {
+            throw new InvalidExtensionException(
+                    String.format(MESSAGE_INVALID_EXTENSION_FORMAT, ROLODEX_FILE_EXTENSION));
+        }
 
         File file = new File(filePath);
         FileUtil.createIfMissing(file);
