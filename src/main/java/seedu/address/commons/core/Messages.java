eba454b41cb60ef332114bd0dabbb87208198c7a
Eldon\eldonng <eldonng95@gmail.com>
2017-10-18 23:46:25 +0800

    add "sort name, sort email, sort phone" to autocomplete keywords

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,6 +10,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String[] MESSAGE_AUTOCOMPLETE_LIST = {"add", "clear", "delete", "exit", "find", "help",
-            "undo", "redo", "pin", "unpin", "sort", "edit", "history", "list", "select"};
+        "undo", "redo", "pin", "unpin", "sort", "edit", "history", "list", "select", "sort name", "sort phone",
+        "sort email"};
 
 }
