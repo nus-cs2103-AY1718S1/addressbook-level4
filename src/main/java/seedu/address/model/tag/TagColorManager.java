503a6cd58b940b54f17fbceb5ecf316a905634b2
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-18 18:11:08 +0800

    Add TagColorManager class

--- src/main/java/seedu/address/model/tag/TagColorManager.java
+++ src/main/java/seedu/address/model/tag/TagColorManager.java
@@ -1,8 +1,14 @@
 package seedu.address.model.tag;
 
+import java.util.HashMap;
+
 /**
  * Manages the displayed color of all tags.
  */
 public class TagColorManager {
+    private static HashMap<Tag, String> internalMap = new HashMap<>();
 
+    public static String getColor(Tag tag) {
+        return internalMap.get(tag);
+    }
 }
