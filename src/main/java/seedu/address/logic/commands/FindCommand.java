55a22f77246146b4b5f10aae987101d279b2a2ec
Boo Kai Hsien <christopherboo66@gmail.com>
2017-10-19 00:19:45 +0800

    done: issue 43

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
@@ -8,10 +9,22 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_WEBSITE;
 
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Birthday;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Website;
+import seedu.address.model.tag.Tag;
+
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all persons in address book whose fields matches all of the argument keywords.
  * Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
@@ -33,15 +46,19 @@ public class FindCommand extends Command {
         + PREFIX_PHONE + "91234567 "
         + PREFIX_EMAIL + "johndoe@example.com";
 
-    private final NameContainsKeywordsPredicate predicate;
+    public static final String MESSAGE_NO_FIELD_PROVIDED = "At least one field to find must be provided.";
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    private FindPersonDescriptor findPersonDescriptor;
+
+    public FindCommand(FindPersonDescriptor findPersonDescriptor) {
+        requireNonNull(findPersonDescriptor);
+
+        this.findPersonDescriptor = new FindPersonDescriptor(findPersonDescriptor);
     }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredPersonList(person -> findPersonDescriptor.equals(person));
         return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
     }
 
@@ -49,6 +66,123 @@ public class FindCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
             || (other instanceof FindCommand // instanceof handles nulls
-            && this.predicate.equals(((FindCommand) other).predicate)); // state check
+            && this.findPersonDescriptor.equals(((FindCommand) other).findPersonDescriptor)); // state check
+    }
+
+    /**
+     * Stores the details to find of the person.
+     */
+    public static class FindPersonDescriptor {
+        private Name name;
+        private Phone phone;
+        private Email email;
+        private Address address;
+        private Birthday birthday;
+        private Website website;
+        private Set<Tag> tags;
+
+        public FindPersonDescriptor() {
+        }
+
+        public FindPersonDescriptor(FindPersonDescriptor toCopy) {
+            this.name = toCopy.name;
+            this.phone = toCopy.phone;
+            this.email = toCopy.email;
+            this.address = toCopy.address;
+            this.birthday = toCopy.birthday;
+            this.website = toCopy.website;
+            this.tags = toCopy.tags;
+        }
+
+        /**
+         *
+         * @return false if no fields are provided
+         */
+        public boolean allNull() {
+            return this.name == null
+                && this.phone == null
+                && this.email == null
+                && this.address == null
+                && this.birthday == null
+                && this.website == null
+                && this.tags == null;
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setPhone(Phone phone) {
+            if (phone.value != null) {
+                this.phone = phone;
+            }
+        }
+
+        public Optional<Phone> getPhone() {
+            return Optional.ofNullable(phone);
+        }
+
+        public void setEmail(Email email) {
+            if (email.value != null) {
+                this.email = email;
+            }
+        }
+
+        public Optional<Email> getEmail() {
+            return Optional.ofNullable(email);
+        }
+
+        public void setAddress(Address address) {
+            if (address.value != null) {
+                this.address = address;
+            }
+        }
+
+        public Optional<Address> getAddress() {
+            return Optional.ofNullable(address);
+        }
+
+        public void setBirthday(Birthday birthday) {
+            if (birthday.value != null) {
+                this.birthday = birthday;
+            }
+        }
+
+        public Optional<Birthday> getBirthday() {
+            return Optional.ofNullable(birthday);
+        }
+
+        public void setWebsite(Website website) {
+            if (website.value != null) {
+                this.website = website;
+            }
+        }
+
+        public Optional<Website> getWebsite() {
+            return Optional.ofNullable(website);
+        }
+
+        public void setTags(Set<Tag> tags) {
+            this.tags = tags;
+        }
+
+        public Optional<Set<Tag>> getTags() {
+            return Optional.ofNullable(tags);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (!(other instanceof Person)) return false;
+            if (this.name != null && !this.name.equals(((Person) other).getName())) { return false; }
+            if (this.phone != null && !this.phone.equals(((Person) other).getPhone())) { return false; }
+            if (this.email != null && !this.email.equals(((Person) other).getEmail())) { return false; }
+            if (this.address != null && !this.address.equals(((Person) other).getAddress())) { return false; }
+
+            return true;
+        }
     }
 }
--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
+import seedu.address.logic.commands.FindCommand.FindPersonDescriptor;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
