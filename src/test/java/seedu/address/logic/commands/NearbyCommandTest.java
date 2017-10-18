6d0bc00b4b1988c4cfcc6ebea4c2edc7ebdbb7d3
khooroko <khoowp25@hotmail.com>
2017-10-18 13:57:52 +0800

    Add NearbyCommand tests

---
+++ src/test/java/seedu/address/logic/commands/NearbyCommandTest.java
@@ -0,0 +1,143 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.commons.events.ui.JumpToNearbyListRequestEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code NearbyCommand}.
+ */
+public class NearbyCommandTest {
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
+    private Model model;
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Index lastPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size());
+
+        assertExecutionSuccess(INDEX_FIRST_PERSON);
+        assertExecutionSuccess(INDEX_THIRD_PERSON);
+        assertExecutionSuccess(lastPersonIndex);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_failure() {
+        Index outOfBoundsIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+
+        assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showFirstPersonOnly(model);
+
+        assertExecutionSuccess(INDEX_FIRST_PERSON);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_failure() {
+        showFirstPersonOnly(model);
+
+        Index outOfBoundsIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundsIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        NearbyCommand nearbyFirstCommand = new NearbyCommand(INDEX_FIRST_PERSON);
+        NearbyCommand nearbySecondCommand = new NearbyCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(nearbyFirstCommand.equals(nearbyFirstCommand));
+
+        // same values -> returns true
+        NearbyCommand nearbyFirstCommandCopy = new NearbyCommand(INDEX_FIRST_PERSON);
+        assertTrue(nearbyFirstCommand.equals(nearbyFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(nearbyFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(nearbyFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(nearbyFirstCommand.equals(nearbySecondCommand));
+    }
+
+    /**
+     * Executes a {@code NearbyCommand} with the given {@code index}, and checks that {@code JumpToListRequestEvent}
+     * is raised with the correct index.
+     */
+    private void assertExecutionSuccess(Index index) {
+        NearbyCommand nearbyCommand = prepareCommand(index);
+
+        try {
+            CommandResult commandResult = nearbyCommand.execute();
+            assertEquals(String.format(NearbyCommand.MESSAGE_NEARBY_PERSON_SUCCESS, index.getOneBased()),
+                    commandResult.feedbackToUser);
+        } catch (CommandException ce) {
+            throw new IllegalArgumentException("Execution of command should not fail.", ce);
+        }
+
+        JumpToNearbyListRequestEvent lastEvent =
+                (JumpToNearbyListRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
+        assertEquals(index, Index.fromZeroBased(lastEvent.targetIndex));
+    }
+
+    /**
+     * Executes a {@code NearbyCommand} with the given {@code index}, and checks that a {@code CommandException}
+     * is thrown with the {@code expectedMessage}.
+     */
+    private void assertExecutionFailure(Index index, String expectedMessage) {
+        NearbyCommand nearbyCommand = prepareCommand(index);
+
+        try {
+            nearbyCommand.execute();
+            fail("The expected CommandException was not thrown.");
+        } catch (CommandException ce) {
+            assertEquals(expectedMessage, ce.getMessage());
+            assertTrue(eventsCollectorRule.eventsCollector.isEmpty());
+        }
+    }
+
+    /**
+     * Returns a {@code NearbyCommand} with parameters {@code index}.
+     */
+    private NearbyCommand prepareCommand(Index index) {
+        NearbyCommand nearbyCommand = new NearbyCommand(index);
+        nearbyCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return nearbyCommand;
+    }
+}
