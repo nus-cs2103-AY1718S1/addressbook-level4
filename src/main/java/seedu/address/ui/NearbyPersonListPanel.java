c1dc7cb08b9278549b1c319c70daa94dc55cd7fd
khooroko <khoowp25@hotmail.com>
2017-10-18 12:13:54 +0800

    Fix select command behaviour

--- src/main/java/seedu/address/ui/NearbyPersonListPanel.java
+++ src/main/java/seedu/address/ui/NearbyPersonListPanel.java
@@ -72,12 +72,6 @@ public class NearbyPersonListPanel extends UiPart<Region> {
         });
     }
 
-    @Subscribe
-    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        scrollTo(event.targetIndex);
-    }
-
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code PersonCard}.
      */
