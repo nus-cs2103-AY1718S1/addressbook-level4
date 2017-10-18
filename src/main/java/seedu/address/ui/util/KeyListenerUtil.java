7013a5e10d89ca28582d6fe90acb3e9f4dc2eb39
jo-lyn <e0007247@u.nus.edu>
2017-10-18 15:42:57 +0800

    Handle exception for invalid key combinations

--- src/main/java/seedu/address/ui/util/KeyListenerUtil.java
+++ src/main/java/seedu/address/ui/util/KeyListenerUtil.java
@@ -13,19 +13,23 @@ import seedu.address.logic.commands.UndoCommand;
  * A utility class for mapping key events.
  */
 public class KeyListenerUtil {
-    public static HashMap<String, KeyCombination> getKeys() {
 
+    public static HashMap<String, KeyCombination> getKeys() {
         HashMap<String, KeyCombination> keys = new HashMap<>();
 
-        keys.put("FOCUS_PERSON_LIST", KeyCombination.valueOf("Esc"));
-        keys.put("FOCUS_COMMAND_BOX", KeyCombination.valueOf("Enter"));
-        keys.put("DELETE_SELECTION", KeyCombination.valueOf("Ctrl+D"));
-        keys.put(ClearCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+Shift+D"));
-        keys.put(HistoryCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+H"));
-        keys.put(UndoCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+Z"));
-        keys.put(RedoCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+Y"));
-        keys.put(ListCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+L"));
+        try {
+            keys.put("FOCUS_PERSON_LIST", KeyCombination.valueOf("Esc"));
+            keys.put("FOCUS_COMMAND_BOX", KeyCombination.valueOf("Enter"));
+            keys.put("DELETE_SELECTION", KeyCombination.valueOf("Ctrl+D"));
+            keys.put(ClearCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+Shift+D"));
+            keys.put(HistoryCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+H"));
+            keys.put(UndoCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+Z"));
+            keys.put(RedoCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+Y"));
+            keys.put(ListCommand.COMMAND_WORD, KeyCombination.valueOf("Ctrl+L"));
 
+        } catch (IllegalArgumentException e) {
+            throw new AssertionError("Key combination cannot be invalid", e);
+        }
         return keys;
     }
 }
