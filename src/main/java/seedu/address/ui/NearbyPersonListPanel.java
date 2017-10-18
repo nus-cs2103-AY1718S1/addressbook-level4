513c6ebdb960cade04e8c91be1df5a5b16be8a38
khooroko <khoowp25@hotmail.com>
2017-10-18 12:19:47 +0800

    Fix imports

--- src/main/java/seedu/address/ui/NearbyPersonListPanel.java
+++ src/main/java/seedu/address/ui/NearbyPersonListPanel.java
@@ -6,8 +6,6 @@ import java.util.logging.Logger;
 
 import org.fxmisc.easybind.EasyBind;
 
-import com.google.common.eventbus.Subscribe;
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -16,7 +14,6 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.NearbyPersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
