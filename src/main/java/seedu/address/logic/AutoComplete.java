7058450f40de038dd3a76964f2d34d2fad4b4971
Shi Tian <shitian007@hotmail.com>
2017-10-18 17:02:40 +0800

    Add documentation to Autocomplete Class and Methods

--- src/main/java/seedu/address/logic/AutoComplete.java
+++ src/main/java/seedu/address/logic/AutoComplete.java
@@ -1,43 +1,52 @@
 package seedu.address.logic;
 
+import java.util.ArrayList;
 import seedu.address.model.Model;
 import seedu.address.model.person.ReadOnlyPerson;
 
-import java.util.ArrayList;
 
+/**
+ * AutoComplete class integrated into LogicManager to keep track of current set
+ * of autocomplete suggestions
+ */
 public class AutoComplete {
 
-    private final String[] BASECOMMANDS = { "add", "edit", "select", "delete", "clear",
+    private final String[] baseCommands = { "add", "edit", "select", "delete", "clear",
         "backup", "find", "list", "history", "exit", "help", "undo", "redo"
     };
     private ArrayList<String> personsStringArray;
     private String[] autoCompleteList;
-    Model model;
+    private Model model;
 
     public AutoComplete(Model model) {
         this.model = model;
-        autoCompleteList = BASECOMMANDS;
+        autoCompleteList = baseCommands;
         personsStringArray = new ArrayList<String>();
         this.updatePersonsArray();
     }
 
+    /**
+     * Updates AutoComplete suggestions according to user typed input
+     * @param userInput
+     */
     public void updateAutoCompleteList(String userInput) {
         switch (userInput) {
-            case "":
-                this.resetAutocompleteList();
-                break;
-            case "find":
-                this.autoCompleteList = getConcatPersonsArray("find");
-                break;
-            case "edit":
-                this.autoCompleteList = getConcatPersonsArray("edit");
-                break;
-            case "delete":
-                this.autoCompleteList = getConcatPersonsArray("delete");
-                break;
+        case "":
+            this.resetAutocompleteList();
+            break;
+        case "find":
+            this.autoCompleteList = getConcatPersonsArray("find");
+            break;
+        case "edit":
+            this.autoCompleteList = getConcatPersonsArray("edit");
+            break;
+        case "delete":
+            this.autoCompleteList = getConcatPersonsArray("delete");
+            break;
         }
     }
 
+    // Concatenate Persons to suggestions when command typed
     private String[] getConcatPersonsArray(String command) {
         String[] newAutoCompleteList = new String[personsStringArray.size()];
         for (int i = 0; i < personsStringArray.size(); i++) {
@@ -50,10 +59,12 @@ public class AutoComplete {
         return newAutoCompleteList;
     }
 
+    // Reset autocomplete suggestions
     public void resetAutocompleteList() {
-        this.autoCompleteList = BASECOMMANDS;
+        this.autoCompleteList = baseCommands;
     }
 
+    // Update array of persons suggestions when list modified
     public void updatePersonsArray() {
         personsStringArray.clear();
         for (ReadOnlyPerson p: model.getFilteredPersonList()) {
@@ -61,6 +72,7 @@ public class AutoComplete {
         }
     }
 
+    // Getter for autoCompleteList
     public String[] getAutoCompleteList() {
         return autoCompleteList;
     }
