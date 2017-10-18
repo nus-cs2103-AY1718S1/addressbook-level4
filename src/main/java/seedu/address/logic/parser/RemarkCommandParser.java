146d40a7692e11388d055efadb76ac61155622f0
nusjzx <nusjzx@gmail.com>
2017-10-18 17:47:17 +0800

    Add remark class

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -8,6 +8,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
 
 /**
  * Parses the given {@code String} of arguments in the context of the RemarkCommand
@@ -30,6 +31,6 @@ public class RemarkCommandParser implements Parser<RemarkCommand>{
 
         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
 
-        return new RemarkCommand(index, remark);
+        return new RemarkCommand(index, new Remark(remark));
     }
 }
