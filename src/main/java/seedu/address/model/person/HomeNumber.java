8148156c79bbbb7a7c13950404f244633c970812
chernghann <chernghann.chia@gmail.com>
2017-10-18 12:47:41 +0800

    Updated HomeNumber

---
+++ src/main/java/seedu/address/model/person/HomeNumber.java
@@ -0,0 +1,57 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents a Person's home number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidHomeNumber(String)}
+ */
+public class HomeNumber{
+
+
+    public static final String MESSAGE_HOME_NUMBER_CONSTRAINTS =
+            "Home numbers can only contain numbers, and should be at least 3 digits long";
+    public static final String HOME_NUMBER_VALIDATION_REGEX = "\\d{3,}";
+    public final String value;
+
+    /**
+     * Validates given phone number.
+     *
+     * @throws IllegalValueException if given phone string is invalid.
+     */
+    public HomeNumber(String homeNumber) throws IllegalValueException {
+        requireNonNull(homeNumber);
+        String trimmedHomeNumber = homeNumber.trim();
+        if (!isValidHomeNumber(trimmedHomeNumber)) {
+            throw new IllegalValueException(MESSAGE_HOME_NUMBER_CONSTRAINTS);
+        }
+        this.value = trimmedHomeNumber;
+    }
+
+    /**
+     * Returns true if a given string is a valid person phone number.
+     */
+    public static boolean isValidHomeNumber(String test) {
+        return test.matches(HOME_NUMBER_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof HomeNumber // instanceof handles nulls
+                && this.value.equals(((HomeNumber) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
---
+++ src/test/java/seedu/address/model/person/HomeNumberTest.java
@@ -0,0 +1,25 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HomeNumberTest {
+
+    @Test
+    public void isValidHomeNumber() {
+        // invalid phone numbers
+        assertFalse(HomeNumber.isValidHomeNumber("")); // empty string
+        assertFalse(HomeNumber.isValidHomeNumber(" ")); // spaces only
+        assertFalse(HomeNumber.isValidHomeNumber("91")); // less than 3 numbers
+        assertFalse(HomeNumber.isValidHomeNumber("phone")); // non-numeric
+        assertFalse(HomeNumber.isValidHomeNumber("9011p041")); // alphabets within digits
+        assertFalse(HomeNumber.isValidHomeNumber("9312 1534")); // spaces within digits
+
+        // valid phone numbers
+        assertTrue(HomeNumber.isValidHomeNumber("911")); // exactly 3 numbers
+        assertTrue(HomeNumber.isValidHomeNumber("93121534"));
+        assertTrue(HomeNumber.isValidHomeNumber("124293842033123")); // long phone numbers
+    }
+}
