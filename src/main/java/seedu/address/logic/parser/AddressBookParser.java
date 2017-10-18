3ce93887ec09843256bd5e9d4d3f616f10c49f0a
Alexander Lee <alexleegs@gmail.com>
2017-10-18 12:33:23 +0800

    Added addTag command (#27)
    * Added new addTag command
    
    * Added parser for addTag commands
    
    * Updated parser
    
    * Added test cases for addTag command
    
    * Updated use cases and user guide
    
    * Style
    
    * Style 2
    
    * Style 3
    
    * Edited variable name

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddTagCommand;
 import seedu.address.logic.commands.BackupCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -54,6 +55,9 @@ public class AddressBookParser {
 
         case AddCommand.COMMAND_WORD: case AddCommand.COMMAND_ALIAS:
             return new AddCommandParser().parse(arguments);
+
+        case AddTagCommand.COMMAND_WORD: case AddTagCommand.COMMAND_ALIAS:
+            return new AddTagCommandParser().parse(arguments);
 
         case EditCommand.COMMAND_WORD: case EditCommand.COMMAND_ALIAS:
             return new EditCommandParser().parse(arguments);
