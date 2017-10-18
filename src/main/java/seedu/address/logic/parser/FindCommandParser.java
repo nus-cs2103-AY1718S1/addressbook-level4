e4ae22b6906832ef24b0f789af472928c63fbf68
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-10-18 12:05:07 +0800

    spacing problem AGAIN!

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.function.Predicate;
