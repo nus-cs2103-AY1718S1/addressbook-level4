3ef3fb223fc2ceb19b733f32af6c9c97e0881c09
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2017-10-19 01:55:39 +0800

    Test: Update Text Util Test

--- src/test/java/seedu/address/commons/util/TextUtilTest.java
+++ src/test/java/seedu/address/commons/util/TextUtilTest.java
@@ -3,22 +3,51 @@ package seedu.address.commons.util;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.commons.util.TextUtil.computeTextWidth;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import guitests.AddressBookGuiTest;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 // we extend AddressBookGuitest to load (a stage) internal graphics for TextUtil to work
 public class TextUtilTest extends AddressBookGuiTest {
+
+    private Text helper;
+    @Before
+    public void setup() {
+        helper = new Text();
+    }
 
     @Test
     public void compute_text_width() {
         Font stubFont = new Font(1);
         double width = computeTextWidth(stubFont, "stub", 0.0D);
-        assertEquals(width, 2.0, 0);
+
+        helper.setText("stub");
+        helper.setFont(stubFont);
+
+        helper.setWrappingWidth(0.0D);
+        helper.setLineSpacing(0.0D);
+        double expectedWidth = Math.min(helper.prefWidth(-1.0D), 0.0D);
+        helper.setWrappingWidth((int) Math.ceil(expectedWidth));
+        expectedWidth = Math.ceil(helper.getLayoutBounds().getWidth());
+
+
+        assertEquals(expectedWidth, width, 0);
 
         Font stubFont2 = new Font(20);
+
+        helper.setText("stub");
+        helper.setFont(stubFont2);
+
+        helper.setWrappingWidth(0.0D);
+        helper.setLineSpacing(0.0D);
+        double expectedWidth2 = Math.min(helper.prefWidth(-1.0D), 0.0D);
+        helper.setWrappingWidth((int) Math.ceil(expectedWidth2));
+        expectedWidth2 = Math.ceil(helper.getLayoutBounds().getWidth());
+
         double width2 = computeTextWidth(stubFont2, "stub", 0.0D);
-        assertEquals(width2, 39.0, 0);
+        assertEquals(expectedWidth2, width2, 0);
     }
 }
