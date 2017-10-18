91dd983b5120fa0658f1999674d533597899781c
Haozhe <phaozhe@hotmail.com>
2017-10-18 13:08:55 +0800

    add a timestamp class under Person to enable recording of time created and expiry time

---
+++ src/main/java/seedu/address/model/person/Timestamp.java
@@ -0,0 +1,33 @@
+package seedu.address.model.person;
+
+import java.time.LocalDateTime;
+
+/**
+ Create a timestamp in each person to record the time created and time that this temporary person will expire
+ */
+public class Timestamp {
+
+    LocalDateTime creationTime = null;
+    LocalDateTime expiryTime = null; //after construction, a null expiryTime means this person will not expire
+
+    public Timestamp(long day) {
+        creationTime = LocalDateTime.now();
+        if (day > 0) {
+            expiryTime = creationTime.plusDays(day);
+        }
+    }
+
+    public LocalDateTime getCreationTime(){
+        return creationTime;
+    }
+
+
+    /**
+     * following method returns null if this person does not expiry
+     * @return time of expiry
+     */
+    public LocalDateTime getExpiryTime() {
+        return expiryTime;
+    }
+
+}
