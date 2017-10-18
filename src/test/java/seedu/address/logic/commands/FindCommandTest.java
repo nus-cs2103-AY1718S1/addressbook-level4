e3430d76dd076e4f728dbb9713a8e5c56c5360c8
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-10-18 11:49:25 +0800

    fixed test for new find command

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.ELLE;
 import static seedu.address.testutil.TypicalPersons.FIONA;
@@ -30,6 +32,7 @@ import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
+ * Cases only test for finding of name and tag (name has the same implementation as other fields)
  */
 public class FindCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -40,25 +43,40 @@ public class FindCommandTest {
                 new NameContainsKeywordsPredicate(Collections.singletonList("first"));
         NameContainsKeywordsPredicate secondPredicate =
                 new NameContainsKeywordsPredicate(Collections.singletonList("second"));
+        TagContainsKeywordsPredicate thirdPredicate =
+                new TagContainsKeywordsPredicate(Collections.singletonList("first"));
+        TagContainsKeywordsPredicate fourthPredicate =
+                new TagContainsKeywordsPredicate(Collections.singletonList("second"));
 
         FindCommand findFirstCommand = new FindCommand(firstPredicate);
         FindCommand findSecondCommand = new FindCommand(secondPredicate);
+        FindCommand findThirdCommand = new FindCommand(thirdPredicate);
+        FindCommand findFourthCommand = new FindCommand(fourthPredicate);
 
         // same object -> returns true
         assertTrue(findFirstCommand.equals(findFirstCommand));
+        assertTrue(findThirdCommand.equals(findThirdCommand));
 
         // same values -> returns true
         FindCommand findFirstCommandCopy = new FindCommand(firstPredicate);
+        FindCommand findThirdCommandCopy = new FindCommand(thirdPredicate);
         assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+        assertTrue(findThirdCommand.equals(findThirdCommandCopy));
 
         // different types -> returns false
         assertFalse(findFirstCommand.equals(1));
+        assertFalse(findThirdCommand.equals(1));
 
         // null -> returns false
         assertFalse(findFirstCommand.equals(null));
+        assertFalse(findThirdCommand.equals(null));
 
         // different person -> returns false
         assertFalse(findFirstCommand.equals(findSecondCommand));
+        assertFalse(findThirdCommand.equals(findFourthCommand));
+
+        // different field, same input -> returns false
+        assertFalse(findFirstCommand.equals(findThirdCommand));
     }
 
     @Test
@@ -71,8 +89,15 @@ public class FindCommandTest {
     @Test
     public void execute_multipleKeywords_multiplePersonsFound() {
         String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-        FindCommand command = prepareCommand("Kurz Elle Kunz");
+        FindCommand command = prepareCommand("n/ Kurz Elle Kunz");
         assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, ELLE, FIONA));
+    }
+    //TODO: find out why typical persons does not reflect accurately no of tags
+    @Test
+    public void execute_multipleTagKeywords_multiplePersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        FindCommand command = prepareCommand("t/ owesMoney");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(BENSON));
     }
 
     /**
@@ -80,16 +105,21 @@ public class FindCommandTest {
      */
     private FindCommand prepareCommand(String userInput) {
         ArrayList<String> keywords = new ArrayList<>();
-        Predicate<ReadOnlyPerson> predicate = null;
+        Predicate<ReadOnlyPerson> predicate;
         StringTokenizer st = new StringTokenizer(userInput, " ");
+        Boolean tagExist = false;
         if (st.hasMoreTokens()) {
+            String prefix = st.nextToken();
             while (st.hasMoreTokens()) {
                 keywords.add(st.nextToken());
             }
 
-            if (userInput.contains("t/")) {
-                predicate = new TagContainsKeywordsPredicate(keywords);
+            if (prefix.equals("t/")) {
+                tagExist = true;
             }
+        }
+        if (tagExist) {
+            predicate = new TagContainsKeywordsPredicate(keywords);
         } else {
             predicate = new NameContainsKeywordsPredicate(keywords);
         }
