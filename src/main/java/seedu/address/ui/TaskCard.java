1091b670597b8e4f6e66bf74d7c885a7a2ffca48
TBY\User <tby1994@gmail.com>
2017-10-19 19:52:48 +0800

    Added TaskCard test

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -52,7 +52,7 @@ public class TaskCard extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof PersonCard)) {
+        if (!(other instanceof TaskCard)) {
             return false;
         }
 
