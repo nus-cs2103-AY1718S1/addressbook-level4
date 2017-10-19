209d9d7f0a0fa536fa7bb9298bd77c83b36da1c7
Lau Yung <lauy1997@gmail.com>
2017-10-19 15:58:39 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/ExpireCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExpireCommandParserTest.java
@@ -1,15 +1,16 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EXPIRE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
 import org.junit.Test;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ExpireCommand;
-
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EXPIRE;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 public class ExpireCommandParserTest {
 
