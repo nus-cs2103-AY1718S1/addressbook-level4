cd4d9fb06e61c07631b413108a951a270612c77a
Vanessa Lim <31247542+vlimshimin@users.noreply.github.com>
2017-10-20 02:33:07 +0800

    add setData(Model, CommandHistory, UndoRedoStack, RecentlyDeletedQueue) into RecentlyDeletedCommand

--- src/main/java/seedu/address/logic/commands/RecentlyDeletedCommand.java
+++ src/main/java/seedu/address/logic/commands/RecentlyDeletedCommand.java
@@ -4,8 +4,11 @@ import java.util.*;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
 import seedu.address.logic.RecentlyDeletedQueue;
+import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
@@ -31,10 +34,16 @@ public class RecentlyDeletedCommand extends Command {
         Collections.reverse(previouslyDeleted);
         LinkedList<String> DeletedAsText = new LinkedList<>();
         Iterator list = previouslyDeleted.listIterator(0);
+        //System.out.println(previouslyDeleted.size());
         while(list.hasNext()) {
             String personAsText = (list.next()).toString();
             DeletedAsText.add(personAsText);
         }
+        //System.out.println(DeletedAsText.size());
         return new CommandResult(String.format(MESSAGE_SUCCESS, String.join("\n", DeletedAsText)));
+    }
+
+    public void setData(Model model, CommandHistory history, UndoRedoStack undoRedoStack, RecentlyDeletedQueue queue) {
+        this.queue = queue;
     }
 }
