d8575855e42a04750fcd13f3e9a977458a3b403e
fustilio <lfrancis373@gmail.com>
2017-10-20 15:22:45 +0800

    Fixed checkstyle issues

--- src/test/java/seedu/address/model/ModelStub.java
+++ src/test/java/seedu/address/model/ModelStub.java
@@ -22,7 +22,8 @@ public class ModelStub implements Model {
     }
 
     @Override
-    public void addAllParcels(List<ReadOnlyParcel> parcels, List<ReadOnlyParcel> parcelsAdded, List<ReadOnlyParcel> duplicateParcels) {
+    public void addAllParcels(List<ReadOnlyParcel> parcels, List<ReadOnlyParcel> parcelsAdded,
+                              List<ReadOnlyParcel> duplicateParcels) {
         fail("This method should not be called.");
     }
 
