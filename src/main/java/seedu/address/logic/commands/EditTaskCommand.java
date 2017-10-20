34bbbe3263f1874f8ddff80d9b587be98a187ff4
Qi Kongjia <31134840+yuzu1209@users.noreply.github.com>
2017-10-21 01:00:17 +0800

    debug

--- src/main/java/seedu/address/logic/commands/EditTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/EditTaskCommand.java
@@ -1,7 +1,11 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
 
 import java.util.List;
@@ -16,9 +20,9 @@ import seedu.address.model.task.ReadOnlyTask;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.exceptions.DuplicateTaskException;
 import seedu.address.model.task.exceptions.TaskNotFoundException;
-import seedu.address.model.task.UniqueTaskList;
+
 import seedu.address.model.tag.Tag;
-import seedu.address.model.tag.UniqueTagList;
+
 
 /**
  * Edits the details of an existing task in the address book.
