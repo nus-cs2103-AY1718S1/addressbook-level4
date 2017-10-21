47f50161e0f80d3dbbc380e99ae43f8e00b77b69
Qi Kongjia <31134840+yuzu1209@users.noreply.github.com>
2017-10-21 11:42:46 +0800

    gg

--- src/main/java/seedu/address/logic/commands/EditTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/EditTaskCommand.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
