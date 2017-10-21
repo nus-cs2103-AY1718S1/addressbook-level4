c530ce50822b09b56f9431675d75ab3c5094ecdc
Archana Pradeep <archieprad@gmail.com>
2017-10-21 16:06:14 +0800

    Created the dialog box using fxml (hopefully)

--- src/main/java/seedu/address/ui/BirthdaysDialogBox.java
+++ src/main/java/seedu/address/ui/BirthdaysDialogBox.java
@@ -2,27 +2,36 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import javax.swing.JOptionPane;
-
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
 
 import seedu.address.commons.core.LogsCenter;
 
 public class BirthdaysDialogBox extends UiPart<Region> {
 
     private static final Logger logger = LogsCenter.getLogger(BirthdaysDialogBox.class);
-    //private static String message_true = "The following people have birthdays today. Wish them a Happy Birthday!";
-    //private static String message_false = "No birthdays today.";
-    private static final String FXML = "BirthdayDialog.fxml";
+    private static String message_true = "Wish your friends a Happy Birthday!";
+    private static String message_false = "No birthdays today.";
+    private static final String FXML = "BirthdayDialogBox.fxml";
     private static final String TITLE = "Birthdays Today";
+
+    @FXML
+    private Label messageLabel;
 
     private final Stage dialogStage;
 
 
     public BirthdaysDialogBox (String[] birthdays){
         super(FXML);
+        if (birthdays.length>0){
+            messageLabel.setText(message_true);
+        }
+        else{
+            messageLabel.setText(message_false);
+        }
         Scene scene = new Scene(getRoot());
         dialogStage = createDialogStage(TITLE,null, scene);
         registerAsAnEventHandler(this);
@@ -31,6 +40,11 @@ public class BirthdaysDialogBox extends UiPart<Region> {
     public void show() {
         logger.fine("Showing birthday dialog box.");
         dialogStage.showAndWait();
+    }
+
+    @FXML
+    public void close() {
+        messageLabel.getScene().getWindow().hide();
     }
 
 }
