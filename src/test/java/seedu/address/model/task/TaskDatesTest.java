96398cedc7bf01882c5e7908c9cd9799870b81bc
Raisa Kabir <raisa.kabir201@gmail.com>
2017-10-21 16:04:24 +0800

    added missing taskdate tesks

--- src/test/java/seedu/address/model/task/TaskDatesTest.java
+++ src/test/java/seedu/address/model/task/TaskDatesTest.java
@@ -8,11 +8,11 @@ import org.junit.Test;
 public class TaskDatesTest {
     @Test
     public void isValid() throws Exception {
-        //blank start date
+        // blank start date
         assertFalse(TaskDates.isDateValid("")); //empty string
         assertFalse(TaskDates.isDateValid(" ")); //spaces only
 
-        //wrong date format
+        // wrong date format
         assertFalse(TaskDates.isDateValid("20/09/2020"));
         assertFalse(TaskDates.isDateValid("20.09.2020"));
         assertFalse(TaskDates.isDateValid("2-09-2020"));
@@ -24,8 +24,12 @@ public class TaskDatesTest {
         assertFalse(TaskDates.isDateValid("2.09.2020"));
         assertFalse(TaskDates.isDateValid("2.9.20"));
         assertFalse(TaskDates.isDateValid("2nd January 2020"));
+        
+        // wrong dates
+        assertFalse(TaskDates.isDateValid("30-02-2019"));
+        assertFalse(TaskDates.isDateValid("12-13-2019"));
 
-        //valid date
+        // valid date
         assertTrue(TaskDates.isDateValid("20-09-2020"));
         assertTrue(TaskDates.isDateValid("02-10-2020"));
     }
