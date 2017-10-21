f078ea0e24689bfdbf1129954cff4aec09b0a9af
Jacob Li Peng Cheng <JacobLI@207-11.priv23.nus.edu.sg>
2017-10-21 19:13:37 +0800

    argumentMultimap: add a new function for birthday to obtain the string value of prefix

--- src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
@@ -39,6 +39,11 @@ public class ArgumentMultimap {
         return values.isEmpty() ? Optional.empty() : Optional.of(values.get(values.size() - 1));
     }
 
+    public String getStringValue(Prefix prefix) {
+        List<String> values = getAllValues(prefix);
+        return values.isEmpty() ? "" : values.get(values.size() - 1);
+    }
+
     /**
      * Returns all values of {@code prefix}.
      * If the prefix does not exist or has no values, this will return an empty list.
