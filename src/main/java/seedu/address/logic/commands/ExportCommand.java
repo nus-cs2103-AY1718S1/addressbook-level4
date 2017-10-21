635a8e51d615f74224498f4dade8324d15ff7b6c
CuiXiaohan <cuixiaohan@me.com>
2017-10-21 14:28:05 +0800

    Edit export command class to make it work properly

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,32 +1,15 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.events.ui.JumpToListRequestEvent;
-import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.model.person.exceptions.PersonNotFoundException;
-import seedu.address.model.tag.Tag;
 
 /**
  * Export the information about a person as a line of code that can be
@@ -38,7 +21,7 @@ public class ExportCommand extends Command {
     public static final String COMMAND_ALIAS = "ep";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Export the details of the person identified "
-            + "by the index number used in the last person listing. "
+            + "by the index number used in the last person listing. \n"
             + "Output will be in an add command format, which can be "
             + "directly given to 3W to excute.\n"
             + "Parameters: INDEX (must be a positive integer) "
@@ -65,7 +48,22 @@ public class ExportCommand extends Command {
         }
 
         EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
-        return new CommandResult(MESSAGE_EXPORT_PERSON_SUCCESS);
+        ReadOnlyPerson personToExport = lastShownList.get(targetIndex.getZeroBased());
+
+        final StringBuilder builder = new StringBuilder();
+        builder.append("add n/")
+                .append(personToExport.getName())
+                .append(" p/")
+                .append(personToExport.getPhone())
+                .append(" e/")
+                .append(personToExport.getEmail())
+                .append(" a/")
+                .append(personToExport.getAddress())
+                .append(" r/")
+                .append(personToExport.getRemark())
+                .append(" t/");
+        personToExport.getTags().forEach(builder::append);
+        return new CommandResult(builder.toString());
 
     }
 
