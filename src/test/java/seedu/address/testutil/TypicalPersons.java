60527c17dec5f31bf1a84204fee37072a8d46988
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-10-21 13:31:22 +0800

    checkstyle correction 3.

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -41,8 +41,8 @@ public class TypicalPersons {
     public static final ReadOnlyPerson FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
             .withEmail("lydia@example.com").withAddress("little tokyo").withPhoto("/images/noPhoto.png").build();
     public static final ReadOnlyPerson GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").withRemark("Likes to swim").
-                    withPhoto("/images/noPhoto.png").build();
+            .withEmail("anna@example.com").withAddress("4th street").withRemark("Likes to swim")
+                    .withPhoto("/images/noPhoto.png").build();
 
     // Manually added
     public static final ReadOnlyPerson HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
