9c5c3f978fecd0f6e34e729b3b753f0ee96794b7
CT15 <calvin.tantio@yahoo.com>
2017-10-22 00:56:54 +0800

    Model

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,6 +1,9 @@
 package seedu.address.model.util;
 
+import static java.util.Arrays.asList;
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -23,25 +26,28 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         try {
-            return new Person[] {
+            return new Person[]{
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                    new Address("Blk 30 Geylang Street 29, #06-40"), new Schedule(new ScheduleDate("19-10-2017"),
-                        new Activity("Do homework together")), getTagSet("friends")),
+                        new Address("Blk 30 Geylang Street 29, #06-40"), getScheduleSet(asList("15-01-2017",
+                        "03-01-2018"), asList("Team meeting", "Interview")), getTagSet("friends")),
                 new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Schedule(new ScheduleDate(""),
-                        new Activity("")), getTagSet("colleagues", "friends")),
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Schedule(new ScheduleDate("03-03-2019"),
-                        new Activity("Swim")), getTagSet("neighbours")),
+                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getScheduleSet(asList(
+                        "15-01-2017", "04-04-2017"), asList("Team meeting", "Team bonding")),
+                        getTagSet("colleagues", "friends")),
+                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
+                        new Email("charlotte@example.com"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                        getScheduleSet(asList("15-01-2017", "04-04-2017"), asList("Team meeting", "Team bonding")),
+                        getTagSet("neighbours")),
                 new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Schedule(new ScheduleDate(""),
-                        new Activity("")), getTagSet("family")),
+                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), getScheduleSet(asList(
+                        "15-01-2017", "03-01-2018"), asList("Team meeting", "Interview")),
+                        getTagSet("family")),
                 new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"), new Schedule(new ScheduleDate("15-01-2017"),
-                        new Activity("Play basketball")), getTagSet("classmates")),
+                        new Address("Blk 47 Tampines Street 20, #17-35"), getScheduleSet(asList("15-01-2017",
+                        "08-09-2018"), asList("Team meeting", "Study Tour")), getTagSet("classmates")),
                 new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"), new Schedule(new ScheduleDate(""),
-                        new Activity("")), getTagSet("colleagues"))
+                        new Address("Blk 45 Aljunied Street 85, #11-31"), getScheduleSet(asList("15-01-2017",
+                        "25-12-2017"), asList("Team meeting", "Christmas dinner")), getTagSet("colleagues"))
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
@@ -58,6 +64,20 @@ public class SampleDataUtil {
         } catch (DuplicatePersonException e) {
             throw new AssertionError("sample data cannot contain duplicate persons", e);
         }
+    }
+
+    /**
+     * Returns a schedule set containing the list of schedule dates and activities given.
+     * pre-condition: the number of elements in scheduleDates must be the same as that of activities.
+     */
+    public static Set<Schedule> getScheduleSet(List<String> scheduleDates, List<String> activities)
+            throws IllegalValueException {
+        HashSet<Schedule> schedules = new HashSet<>();
+        for (int i = 0; i < scheduleDates.size(); i++) {
+            schedules.add(new Schedule(new ScheduleDate(scheduleDates.get(i)), new Activity(activities.get(i))));
+        }
+
+        return schedules;
     }
 
     /**
