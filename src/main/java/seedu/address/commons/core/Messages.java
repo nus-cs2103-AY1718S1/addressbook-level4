b9d0419ba673346eae79b8f576895fff856786fe
itsdickson <itsklein@hotmail.com>
2017-10-21 13:19:00 +0800

    Messages.java: add invalid switch message

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,5 +9,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_DISPLAYED_INDEX = "The index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_INVALID_SWITCH = "Switch is invalid! Already in theme provided.";
 
 }
