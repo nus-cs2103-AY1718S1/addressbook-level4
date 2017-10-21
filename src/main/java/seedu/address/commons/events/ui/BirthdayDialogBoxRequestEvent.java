4ecb68740bd5fab165e42a35c3235fa64591dc2e
Archana Pradeep <archieprad@gmail.com>
2017-10-21 15:09:35 +0800

    Added a birthday request event to show dialog box

---
+++ src/main/java/seedu/address/commons/events/ui/BirthdayDialogBoxRequestEvent.java
@@ -0,0 +1,12 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+
+public class BirthdayDialogBoxRequestEvent extends BaseEvent{
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+}
