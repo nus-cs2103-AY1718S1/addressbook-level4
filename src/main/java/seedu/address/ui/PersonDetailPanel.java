5aba543f624bcd32c05afec119048351e4f4b164
jo-lyn <e0007247@u.nus.edu>
2017-10-21 22:01:20 +0800

    Add other fields to person detail panel

--- src/main/java/seedu/address/ui/PersonDetailPanel.java
+++ src/main/java/seedu/address/ui/PersonDetailPanel.java
@@ -1,5 +1,7 @@
-package seedu.address.ui.util;
+package seedu.address.ui;
 
+import java.util.HashMap;
+import java.util.Random;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -15,14 +17,16 @@ import javafx.scene.layout.StackPane;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.ui.PersonListPanel;
-import seedu.address.ui.UiPart;
+import seedu.address.ui.util.ColorsUtil;
 
 /**
  * The Person Detail Panel of the App.
  */
 public class PersonDetailPanel extends UiPart<Region> {
     private static final String FXML = "PersonDetailPanel.fxml";
+    private static String[] colors = ColorsUtil.getTagColors();
+    private static HashMap<String, String> tagColors = new HashMap<> ();
+    private static Random random = new Random();
 
     private final Logger logger = LogsCenter.getLogger(this.getClass());
 
@@ -63,6 +67,9 @@ public class PersonDetailPanel extends UiPart<Region> {
      */
     private void initialise() {
         name.setText("");
+        phone.setText("");
+        email.setText("");
+        address.setText("");
         avatarImage = new Image(getClass().getResourceAsStream("/images/avatarGray.png"));
         avatar.fitWidthProperty().bind(personDetailPanel.widthProperty());
     }
@@ -74,10 +81,22 @@ public class PersonDetailPanel extends UiPart<Region> {
 
         avatar.setImage(avatarImage);
         name.setText(person.getName().toString());
-        phone.setText(person.getName().toString());
-        address.setText(person.getName().toString());
-        email.setText(person.getName().toString());
-        tags.getChildren();
+        phone.setText(person.getPhone().toString());
+        address.setText(person.getAddress().toString());
+        email.setText(person.getEmail().toString());
+        tags.getChildren().clear();
+        person.getTags().forEach(tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            tagLabel.setStyle("-fx-background-color: " + getTagColor(tag.tagName));
+            tags.getChildren().add(tagLabel);
+        });
+    }
+
+    private String getTagColor(String tagValue) {
+        if (!tagColors.containsKey(tagValue)) {
+            tagColors.put(tagValue, colors[random.nextInt(colors.length)]);
+        }
+        return tagColors.get(tagValue);
     }
 
     /**
