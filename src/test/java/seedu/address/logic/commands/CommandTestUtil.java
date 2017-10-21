0f585817ad2239c351f968a07a7928fba0f63def
deep4k <e0031591@u.nus.edu>
2017-10-21 14:02:49 +0800

    Add AliasComandParser tests Update main parser test to include alias command test

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -3,6 +3,8 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ALIAS_KEYWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ALIAS_REPRESENTATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -37,6 +39,8 @@ public class CommandTestUtil {
     public static final String VALID_TAG_FRIEND = "friend";
     public static final String VALID_REMARK_AMY = "Like skiing.";
     public static final String VALID_REMARK_BOB = "Favourite pastime: Food";
+    public static final String VALID_ALIAS_KEYWORD_MONDAY = "mon";
+    public static final String VALID_ALIAS_REPRESENTATION_MONDAY = "monday";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
@@ -48,12 +52,19 @@ public class CommandTestUtil {
     public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
+    public static final String ALIAS_KEYWORD_DESC_MONDAY = " " + PREFIX_ALIAS_KEYWORD + VALID_ALIAS_KEYWORD_MONDAY;
+    public static final String ALIAS_REPRESENTATION_DESC_MONDAY = " " + PREFIX_ALIAS_REPRESENTATION
+            + VALID_ALIAS_REPRESENTATION_MONDAY;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+    public static final String INVALID_ALIAS_KEYWORD_DESC = " "
+            + PREFIX_ALIAS_KEYWORD + "a"; // alias keywords not allowed to be less than 2 characters
+    public static final String INVALID_ALIAS_REPRESENTATION_DESC = " "
+            + PREFIX_ALIAS_REPRESENTATION + ""; // alias representation cannot be empty
 
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
---
+++ src/test/java/seedu/address/logic/parser/AliasCommandParserTest.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.ALIAS_KEYWORD_DESC_MONDAY;
+import static seedu.address.logic.commands.CommandTestUtil.ALIAS_REPRESENTATION_DESC_MONDAY;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_ALIAS_KEYWORD_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_ALIAS_REPRESENTATION_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ALIAS_KEYWORD_MONDAY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ALIAS_REPRESENTATION_MONDAY;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.AliasCommand;
+import seedu.address.model.alias.AliasToken;
+import seedu.address.model.alias.Keyword;
+import seedu.address.model.alias.Representation;
+import seedu.address.testutil.AliasTokenBuilder;
+
+public class AliasCommandParserTest {
+
+    private AliasCommandParser parser = new AliasCommandParser();
+
+    @Test
+    public void parse_keywordAndRepresentation_success() {
+        AliasToken expectedToken = new AliasTokenBuilder().withKeyword(VALID_ALIAS_KEYWORD_MONDAY).withRepresentation(
+                VALID_ALIAS_REPRESENTATION_MONDAY).build();
+
+        assertParseSuccess(parser, AliasCommand.COMMAND_WORD + ALIAS_KEYWORD_DESC_MONDAY
+                + ALIAS_REPRESENTATION_DESC_MONDAY, new AliasCommand(expectedToken));
+    }
+
+    @Test
+    public void parse_representationMissing_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AliasCommand.MESSAGE_USAGE);
+
+        assertParseFailure(parser, AliasCommand.COMMAND_WORD + ALIAS_KEYWORD_DESC_MONDAY, expectedMessage);
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        // invalid keyword
+        assertParseFailure(parser, AliasCommand.COMMAND_WORD + INVALID_ALIAS_KEYWORD_DESC
+                + ALIAS_REPRESENTATION_DESC_MONDAY, Keyword.MESSAGE_NAME_CONSTRAINTS);
+
+        // invalid representation
+        assertParseFailure(parser, AliasCommand.COMMAND_WORD + ALIAS_KEYWORD_DESC_MONDAY
+                + INVALID_ALIAS_REPRESENTATION_DESC, Representation.MESSAGE_NAME_CONSTRAINTS);
+    }
+}
