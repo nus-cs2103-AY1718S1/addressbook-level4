1fd38a8d8ca9751812dfa8bb5c5a88bd77cc1693
Unknown <celinejams2@gmail.com>
2017-10-22 11:27:10 +0800

    UI: Replace BrowserPanel with ExtendedPersonCard

---
+++ src/main/java/seedu/address/ui/ExtendedPersonCard.java
@@ -0,0 +1,83 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Extended Person Card Panel that displays the details of a Person
+ */
+public class ExtendedPersonCard extends UiPart<Region> {
+
+    private static final String FXML = "ExtendedPersonCard.fxml";
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
+
+    @FXML
+    private HBox cardpane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label formClass;
+    @FXML
+    private Label grades;
+    @FXML
+    private Label postalCode;
+    @FXML
+    private Label email;
+    @FXML
+    private Label remark;
+    @FXML
+    private FlowPane tags;
+
+    public ExtendedPersonCard() {
+        super(FXML);
+        registerAsAnEventHandler(this);
+    }
+
+    /**
+     * Updates person details as displayed on the extended person card panel
+     */
+    private void loadPersonDetails(ReadOnlyPerson person) {
+        name.setText(person.getName().fullName);
+        phone.setText(person.getPhone().toString());
+        address.setText(person.getAddress().toString());
+        formClass.setText(person.getFormClass().toString());
+        grades.setText(person.getGrades().toString());
+        postalCode.setText(person.getPostalCode().toString());
+        email.setText(person.getEmail().toString());
+        remark.setText(person.getRemark().toString());
+        tags.getChildren().clear();
+        initTags(person);
+    }
+
+    @Subscribe
+    private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        loadPersonDetails(event.getNewSelection().person);
+    }
+
+    /**
+     * Initializes and styles tags belonging to a person.
+     * @param person must be a valid.
+     */
+    private void initTags(ReadOnlyPerson person) {
+        person.getTags().forEach(tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            tagLabel.setStyle("-fx-font-size:" + "15px");
+            tags.getChildren().add(tagLabel);
+        });
+    }
+}
