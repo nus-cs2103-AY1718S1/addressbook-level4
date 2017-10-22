c63d35689348c669aea3c412967d14ea8a496f49
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-22 22:35:08 +0800

    Move css files

---
+++ src/main/java/seedu/address/ui/PersonDetailsPanel.java
@@ -0,0 +1,9 @@
+package seedu.address.ui;
+
+/**
+ * The panel on the right side of {@link PersonListPanel}. Used to show the details (including photo and all
+ * properties) of a specific person (selected on the {@link PersonListPanel}).
+ */
+public class PersonDetailsPanel {
+
+}
