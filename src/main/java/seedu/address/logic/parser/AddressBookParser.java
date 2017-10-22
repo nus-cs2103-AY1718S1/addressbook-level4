c96c310ee89f38b5bb527b051953912eec6b4f2b
LAPTOP-DBVHII6C\Leon <leonchowwenhao@gmail.com>
2017-10-22 23:15:44 +0800

    Added selectE command - Select all events by person. To fix - For events with more than 1 participant, can only select by the first person.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -22,6 +22,7 @@ import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListEventCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.SelectJoinedEventsCommand;
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -67,6 +68,9 @@ public class AddressBookParser {
 
         case SelectCommand.COMMAND_WORD:
             return new SelectCommandParser().parse(arguments);
+
+        case SelectJoinedEventsCommand.COMMAND_WORD:
+            return new SelectJoinedEventsCommandParser().parse(arguments);
 
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
---
+++ src/main/java/seedu/address/model/event/EventContainsKeywordPredicate.java
@@ -0,0 +1,34 @@
+package seedu.address.model.event;
+
+import seedu.address.commons.util.StringUtil;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code ReadOnlyEvent}'s {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ */
+public class EventContainsKeywordPredicate implements Predicate<ReadOnlyEvent> {
+    private final List<String> keywords;
+
+    public EventContainsKeywordPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(ReadOnlyEvent event) {
+        Boolean isSelected = false;
+        if(!event.getParticipants().isEmpty()) {
+            isSelected = keywords.stream().anyMatch(keyword ->
+            StringUtil.containsWordIgnoreCase(event.getParticipants().iterator().next().getName().fullName, keyword));
+        }
+
+        return isSelected;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof EventContainsKeywordPredicate // instanceof handles nulls
+                && this.keywords.equals(((EventContainsKeywordPredicate) other).keywords)); // state check
+    }
+}
