34facb6fea6c74de0e15ad503124762578f3b6c2
Archana Pradeep <archieprad@gmail.com>
2017-10-22 19:40:23 +0800

    Update Messages.java

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,5 +11,4 @@ public class Messages {
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_BIRTHDAYS_LISTED_OVERVIEW = "Wish these %1$d people a Happy Birthday!";
     public static final String MESSAGE_INVALID_SWITCH = "Switch is invalid! Already in theme provided.";
-    
 }
