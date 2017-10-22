df1b6c1cc6463d46ba3a7e9834c69195df51a6eb
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-22 23:29:15 +0800

    Fix grid system problem

--- src/main/java/seedu/address/ui/person/PersonDetailsPanel.java
+++ src/main/java/seedu/address/ui/person/PersonDetailsPanel.java
@@ -4,7 +4,6 @@ import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.ui.UiPart;
 
@@ -20,7 +19,7 @@ public class PersonDetailsPanel extends UiPart<Region> {
     @FXML
     private Label name;
 
-    public PersonDetailsPanel(Person person) {
+    public PersonDetailsPanel(ReadOnlyPerson person) {
         super(FXML);
         this.person = person;
         name.textProperty().bind(Bindings.convert(person.nameProperty()));
