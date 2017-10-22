e0c9736c620879f963fec3d7b98c6460e61513dd
LawMan <lawrenciok@gmail.com>
2017-10-22 21:48:17 +0800

    add more testing for incrementing interest

--- src/test/java/seedu/address/model/InterestTest.java
+++ src/test/java/seedu/address/model/InterestTest.java
@@ -2,12 +2,16 @@ package seedu.address.model;
 
 import static org.junit.Assert.assertEquals;
 import static seedu.address.model.util.DateUtil.generateOutdatedDebtDate;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Date;
 
 import org.junit.Test;
 
 import seedu.address.model.person.Person;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.util.DateUtil;
 import seedu.address.testutil.PersonBuilder;
 
@@ -29,19 +33,22 @@ public class InterestTest {
         Date lastAccruedDate = generateOutdatedDebtDate(generateDateFromString(sampleDateInput1));
         personToTest.setLastAccruedDate(lastAccruedDate);
         assertEquals(personToTest.checkUpdateDebt(generateDateFromString(sampleDateInput1)), 1);
+
         lastAccruedDate = generateOutdatedDebtDate(generateDateFromString(sampleDateInput2));
         personToTest.setLastAccruedDate(lastAccruedDate);
         assertEquals(personToTest.checkUpdateDebt(generateDateFromString(sampleDateInput2)), 1);
+
         lastAccruedDate = generateOutdatedDebtDate(generateDateFromString(sampleDateInput3));
         personToTest.setLastAccruedDate(lastAccruedDate);
         assertEquals(personToTest.checkUpdateDebt(generateDateFromString(sampleDateInput3)), 1);
+
         lastAccruedDate = generateOutdatedDebtDate(generateDateFromString(sampleDateInput4));
         personToTest.setLastAccruedDate(lastAccruedDate);
         assertEquals(personToTest.checkUpdateDebt(generateDateFromString(sampleDateInput4)), 1);
+
         lastAccruedDate = generateOutdatedDebtDate(generateDateFromString(sampleDateInput5));
         personToTest.setLastAccruedDate(lastAccruedDate);
         assertEquals(personToTest.checkUpdateDebt(generateDateFromString(sampleDateInput5)), 1);
-
 
         //assert should return false as it is not the first day of the month
         lastAccruedDate = generateOutdatedDebtDate(generateDateFromString(sampleDateInput6));
@@ -54,14 +61,39 @@ public class InterestTest {
         // personToTest has interest rate of 1% and debt of $10,000
         Person personToTest = new PersonBuilder().withDebt("10000").build();
         assertEquals(personToTest.calcAccruedAmount(1), "100.00");
+
         personToTest = new PersonBuilder().withDebt("200").withInterest("3").build();
         assertEquals(personToTest.calcAccruedAmount(1), "6.00");
+
         personToTest = new PersonBuilder().withDebt("50").withInterest("3").build();
         assertEquals(personToTest.calcAccruedAmount(1), "1.50");
+
         personToTest = new PersonBuilder().withDebt("3").withInterest("1").build();
         assertEquals(personToTest.calcAccruedAmount(1), "0.03");
+
         personToTest = new PersonBuilder().withDebt("11.10").withInterest("1").build();
         assertEquals(personToTest.calcAccruedAmount(1), "0.11");
+    }
+
+    @Test
+    public void updatePersonDebtTest() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Person personToTest = new PersonBuilder().withName("AA").withDebt("10000").withInterest("1").build();
+        try {
+            model.addPerson(personToTest);
+        } catch (DuplicatePersonException dpe) {
+            assert false : " Should not happen as personToAdd is unique to typical persons";
+        }
+        for (ReadOnlyPerson person : model.getFilteredPersonList()) {
+            if (person.equals(personToTest)) {
+                model.updateDebtFromInterest(person, 1);
+            }
+        }
+        for (ReadOnlyPerson person : model.getFilteredPersonList()) {
+            if (person.equals(personToTest)) {
+                assertEquals("10100.00", person.getDebt().toNumber());
+            }
+        }
     }
 
     /**
