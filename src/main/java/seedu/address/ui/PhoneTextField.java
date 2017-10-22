5f4f7296254a9788e8d74be167194a3525ebb19a
blaqkrow <joshnfsmw@hotmail.com>
2017-10-22 20:56:25 +0800

    added edit form, managed to fill in textfields with corresponding contact details, yet to add edit functionality

---
+++ src/main/java/seedu/address/ui/PhoneTextField.java
@@ -0,0 +1,47 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+
+/**
+ * The UI component that is responsible for saving and displaying the currently selected contact's name.
+ */
+public class PhoneTextField extends UiPart<Region> {
+
+    public static final String ERROR_STYLE_CLASS = "error";
+    private static final String FXML = "PhoneTextField.fxml";
+
+    private final Logger logger = LogsCenter.getLogger(CommandBox.class);
+    private int selectedIndex;
+
+    @FXML
+    private TextField phoneTextField;
+
+    public PhoneTextField() {
+        super(FXML);
+        registerAsAnEventHandler(this);
+    }
+
+    public String getPhoneTextField() {
+        return phoneTextField.getText();
+    }
+
+    public void setPhoneTextField(String text) {
+        phoneTextField.setText(text);
+    }
+
+    @Subscribe
+    private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        setPhoneTextField(event.getNewSelection().person.getPhone().toString());
+    }
+
+
+}
---
+++ src/main/java/seedu/address/ui/TagTextField.java
@@ -0,0 +1,57 @@
+package seedu.address.ui;
+
+import java.util.Set;
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+import seedu.address.model.tag.Tag;
+
+/**
+ * The UI component that is responsible for saving and displaying the currently selected contact's name.
+ */
+public class TagTextField extends UiPart<Region> {
+
+    public static final String ERROR_STYLE_CLASS = "error";
+    private static final String FXML = "TagTextField.fxml";
+
+    private final Logger logger = LogsCenter.getLogger(CommandBox.class);
+
+    @FXML
+    private TextArea tagTextArea;
+
+    public TagTextField() {
+        super(FXML);
+        registerAsAnEventHandler(this);
+    }
+
+    public String getTagTextArea() {
+        return tagTextArea.getText();
+    }
+
+    public void setTagTextArea(String text) {
+        tagTextArea.setText(text);
+    }
+
+    @Subscribe
+    private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        StringBuilder currTags = new StringBuilder();
+        Set<Tag> tagSet = event.getNewSelection().person.getTags();
+        for( Tag t : tagSet) {
+            if (currTags.length() != 0) {
+                currTags.append(",");
+            }
+            currTags.append(t.tagName);
+        }
+        setTagTextArea(currTags.toString());
+    }
+
+
+}
+
