e20855f5054cdbfcfea26c24afefb67a6131b8dc
Keith Ong <october_arthur@hotmail.com>
2017-10-22 17:47:44 +0800

    - Add new app icon - Update app name (to be displayed in program's Window title bar) - Changed default window size and minimum window size (all defaults to a ratio of 4:3) - Shift fav_icon to source directory instead of docs directory

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -29,10 +29,10 @@ import seedu.address.model.UserPrefs;
  */
 public class MainWindow extends UiPart<Region> {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/kaypoh_icon_32.png";
     private static final String FXML = "MainWindow.fxml";
     private static final int MIN_HEIGHT = 600;
-    private static final int MIN_WIDTH = 450;
+    private static final int MIN_WIDTH = 800;
 
     private final Logger logger = LogsCenter.getLogger(this.getClass());
 
