302bccc8a990733d2037dad6030fdffaf0ddcca3
adeeb <chilipadiboy@hotmail.com>
2017-10-23 00:06:28 +0800

    For some reason my birtday.java has more spaces?

--- src/main/java/seedu/address/model/person/Birthday.java
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -14,7 +14,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
  */
 public class Birthday {
     public static final String MESSAGE_BIRTHDAY_CONSTRAINTS =
-        "Person Birthday should be in the format of DD/MM/YYYY";
+            "Person Birthday should be in the format of DD/MM/YYYY";
 
     public final String value;
 
