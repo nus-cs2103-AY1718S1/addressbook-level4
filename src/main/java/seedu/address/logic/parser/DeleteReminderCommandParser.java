9ca441cba61567809a66a414872a44493d3a7db8
LJH <jia_hao_95@hotmail.com>
2017-10-22 15:20:07 +0800

    Logic: Added DeleteReminderCommand and DeleteReminderCommandParser

---
+++ src/main/java/seedu/address/logic/parser/DeleteReminderCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.DeleteReminderCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new DeleteReminderCommand object
+ */
+public class DeleteReminderCommandParser implements Parser<DeleteReminderCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteReminderCommand
+     * and returns an DeleteReminderCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public DeleteReminderCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new DeleteReminderCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteReminderCommand.MESSAGE_USAGE));
+        }
+    }
+
+}
+
