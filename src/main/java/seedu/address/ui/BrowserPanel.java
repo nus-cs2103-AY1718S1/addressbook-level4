12efcac1004e4a6144e399481e2b76258d987d54
Nelson Quek <nelsonquek@Nelsons-MacBook-Pro.local>
2017-10-23 23:41:36 +0800

    Removed unused imports causing checkstyle errors

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -1,6 +1,6 @@
 package seedu.address.ui;
 
-import java.net.URL;
+//import java.net.URL;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -10,7 +10,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
-import seedu.address.MainApp;
+//import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.MeetingPanelSelectionChangedEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
