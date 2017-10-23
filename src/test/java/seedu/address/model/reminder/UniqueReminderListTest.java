96cb09a1883f1ef52b53a1da09bd8804cd8179d3
user\Junyang <junyangooooo@gmail.com>
2017-10-23 17:20:47 +0800

    added uniquereminder list test

--- src/test/java/seedu/address/model/reminder/UniqueReminderListTest.java
+++ src/test/java/seedu/address/model/reminder/UniqueReminderListTest.java
@@ -4,7 +4,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.model.reminder.UniqueReminderList;
 
 public class UniqueReminderListTest {
     @Rule
