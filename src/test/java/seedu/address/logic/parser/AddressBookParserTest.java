e3fa3a9f8664019309ecd73157133ccf0d8de823
newalter <zeyong1123@gmail.com>
2017-10-23 23:21:56 +0800

    increase test coverage

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -32,6 +32,7 @@ import seedu.address.logic.commands.NoteCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.ResizeCommand;
 import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.SyncCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.ContainsTagsPredicate;
@@ -135,6 +136,13 @@ public class AddressBookParserTest {
         SelectCommand command = (SelectCommand) parser.parseCommand(
                 SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new SelectCommand(INDEX_FIRST_PERSON), command);
+    }
+
+    @Test
+    public void parseCommand_sync() throws Exception {
+        SyncCommand command = (SyncCommand) parser.parseCommand(
+                SyncCommand.COMMAND_WORD);
+        assertEquals(new SyncCommand(), command);
     }
 
     @Test
