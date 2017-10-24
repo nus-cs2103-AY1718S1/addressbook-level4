5caeabff0840a9881a3064fb16eac496c93ce241
ZANARKAND\Daniel <danielkwang@outlook.com>
2017-10-24 16:13:04 +0800

    v1.25c fixed some error with stylecode

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -53,7 +53,7 @@ public class AddCommand extends UndoableCommand {
         try {
             model.addPerson(toAdd);
             LoggingCommand loggingCommand = new LoggingCommand();
-            loggingCommand.keepLog(toAdd.toString(),"Add");
+            loggingCommand.keepLog(toAdd.toString(), "Add");
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (DuplicatePersonException e) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
