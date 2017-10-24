e35d84969cf11f118355d5452bf2bfdf205f2244
Houjisan <hongjing10188@gmail.com>
2017-10-25 00:49:16 +0900

    DeveloperGuide documentation for sort command

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -8,7 +8,6 @@ import static seedu.address.logic.parser.SortCommandParser.DATA_FIELD_NAME;
 import static seedu.address.logic.parser.SortCommandParser.DATA_FIELD_PHONE;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
