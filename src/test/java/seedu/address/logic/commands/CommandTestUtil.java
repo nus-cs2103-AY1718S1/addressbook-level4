df3a403204587f0fb8601979f966296f8b571f06
derrick <derrick.2a4@gmail.com>
2017-10-24 21:06:53 +0800

    Add test cases

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -55,6 +55,10 @@ public class CommandTestUtil {
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
+    public static final String NO_PHONE_SET = "No Phone Number";
+    public static final String NO_EMAIL_SET = "No Email";
+    public static final String NO_ADDRESS_SET = "No Address";
+
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
 
