c5b4db3d0a8f58800ca2bea1b4c6f259bcdfd9f4
fustilio <lfrancis373@gmail.com>
2017-10-24 11:00:23 +0800

    Unselects Parcel card when finding history

--- src/main/java/seedu/address/logic/commands/HistoryCommand.java
+++ src/main/java/seedu/address/logic/commands/HistoryCommand.java
@@ -27,6 +27,7 @@ public class HistoryCommand extends Command {
         }
 
         Collections.reverse(previousCommands);
+        model.unselect();
         return new CommandResult(String.format(MESSAGE_SUCCESS, String.join("\n", previousCommands)));
     }
 
