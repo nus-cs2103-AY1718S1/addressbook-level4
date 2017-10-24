606505b94b997871a8d964d9dd5a9859f56b58a9
Jackie Tan <free-soup@hotmail.com>
2017-10-24 08:37:57 +0800

    Include TestCases for SortCommandParser.

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -26,4 +26,11 @@ public class SortCommand extends Command {
         model.sortFilteredPersonList(comparator);
         return new CommandResult(MESSAGE_SUCCESS);
     }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SortCommand // instanceof handles nulls
+                && this.comparator.equals(((SortCommand) other).comparator)); // state check
+    }
 }
