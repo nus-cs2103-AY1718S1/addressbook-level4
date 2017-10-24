cfa3a04ceb8c9b8bf19427083561e4811ef2f1a8
Yuchen He <yuchenhe@201-124.priv23.nus.edu.sg>
2017-10-25 00:13:44 +0800

    add Day and Time

--- src/main/java/seedu/address/model/schedule/Day.java
+++ src/main/java/seedu/address/model/schedule/Day.java
@@ -1,4 +1,33 @@
 package seedu.address.model.schedule;
 
+import seedu.address.model.schedule.exceptions.NotValidTimeException;
+
 public class Day {
+
+    private final Integer day;
+
+    public Day(String dayToAdd) {
+        if(dayToAdd.equals("Monday")){
+            day = 1;
+        } else if(dayToAdd.equals("Tuesday")){
+            day = 2;
+        } else if(dayToAdd.equals("Wednesday")){
+            day = 3;
+        } else if(dayToAdd.equals("Thursday")){
+            day = 4;
+        } else if(dayToAdd.equals("Friday")){
+            day = 5;
+        } else if(dayToAdd.equals("Saturday")){
+            day = 6;
+        } else if(dayToAdd.equals("Sunday")){
+            day = 7;
+        } else {
+            day = -1;
+        }
+    }
+
+    public boolean isValid(){
+        return (day != -1);
+    }
+
 }
--- src/main/java/seedu/address/model/schedule/Time.java
+++ src/main/java/seedu/address/model/schedule/Time.java
@@ -1,4 +1,19 @@
 package seedu.address.model.schedule;
 
+import seedu.address.model.schedule.exceptions.NotValidTimeException;
+
 public class Time {
+
+    private final Integer EarliestTime = 0600;
+    private final Integer LatestTime = 2330;
+
+    private Integer time;
+
+    public Time(String time) throws NotValidTimeException {
+        this.time = Integer.parseInt(time);
+    }
+
+    public boolean isValid(){
+        return (this.time >= EarliestTime && this.time <= LatestTime);
+    }
 }
