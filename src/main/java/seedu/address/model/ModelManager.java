a6776f196cc55e83523c1a0807c327562ff7013f
dennaloh <dennaloh@gmail.com>
2017-10-24 12:56:25 +0800

    Update ModelManager.java

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -99,6 +99,7 @@ public class ModelManager extends ComponentManager implements Model {
     @Override
     public synchronized void addPerson(ReadOnlyPerson person) throws DuplicatePersonException {
         addressBook.addPerson(person);
+        sortPersonList();
         updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         indicateAddressBookChanged();
     }
