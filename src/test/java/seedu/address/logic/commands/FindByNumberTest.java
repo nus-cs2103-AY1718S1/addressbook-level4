1b0ce7f0168bb77d60456c6facdd9d331db3f3ba
ETERNAL\Travis <zytravis@gmail.com>
2017-10-24 18:04:46 +0800

    Correcting Line120 limit CI error.

--- src/test/java/seedu/address/logic/commands/FindByNumberTest.java
+++ src/test/java/seedu/address/logic/commands/FindByNumberTest.java
@@ -84,11 +84,11 @@ public class FindByNumberTest {
      *     - the {@code FilteredList<ReadOnlyPerson>} is equal to {@code expectedList}<br>
      *     - the {@code AddressBook} in model remains the same after executing the {@code command}
      */
-    private void assertCommandSuccess(FindNumberCommand command, String expectedMessage, List<ReadOnlyPerson> expectedList) {
+    private void assertCommandSuccess(FindNumberCommand command, String eMessage, List<ReadOnlyPerson> expectedList){
         AddressBook expectedAddressBook = new AddressBook(model.getAddressBook());
         CommandResult commandResult = command.execute();
 
-        assertEquals(expectedMessage, commandResult.feedbackToUser);
+        assertEquals(eMessage, commandResult.feedbackToUser);
         assertEquals(expectedList, model.getFilteredPersonList());
         assertEquals(expectedAddressBook, model.getAddressBook());
     }
