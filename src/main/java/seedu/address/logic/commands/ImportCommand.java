1885711399f01a9ed29caf947935b65d3b9650a7
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-25 01:59:18 +0800

    Create ImportCommand and ImportCommandParser classes

---
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Imports data from various format to the application.
+ */
+public class ImportCommand extends Command {
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        return null;
+    }
+}
--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -1,8 +1,14 @@
 package seedu.address.logic.parser;
 
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 /**
  * Parses input arguments and creates a new ImportCommand object.
  */
-public class ImportCommandParser {
-
+public class ImportCommandParser implements Parser<ImportCommand> {
+    @Override
+    public ImportCommand parse(String userInput) throws ParseException {
+        return null;
+    }
 }
