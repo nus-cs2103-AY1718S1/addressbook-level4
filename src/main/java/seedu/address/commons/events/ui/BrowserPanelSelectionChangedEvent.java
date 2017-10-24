270069eb0457ef6cde5806d82d62c2b35d8924cb
fongwz <schifx95@gmail.com>
2017-10-24 18:21:07 +0800

    fixing travis errors

--- src/main/java/seedu/address/commons/events/ui/BrowserPanelSelectionChangedEvent.java
+++ src/main/java/seedu/address/commons/events/ui/BrowserPanelSelectionChangedEvent.java
@@ -2,6 +2,9 @@ package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
 
+/**
+ * Represents a selection change in the Browser List Panel
+ */
 public class BrowserPanelSelectionChangedEvent extends BaseEvent {
 
     private final String browserSelection;
--- src/main/java/seedu/address/commons/events/ui/JumpToBrowserListRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/JumpToBrowserListRequestEvent.java
@@ -2,6 +2,9 @@ package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
 
+/**
+ * Indicates a request to jump to the list of browser panels
+ */
 public class JumpToBrowserListRequestEvent extends BaseEvent {
 
     public final String browserItem;
