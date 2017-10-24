c66aa172144d711c5327731533199ec2b02b6ca1
ykt33 <vikingpixie@gmail.com>
2017-10-24 17:13:22 +0800

    Fixing chkstyle

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -22,7 +22,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     public DeleteCommand parse(String args) throws ParseException {
         try {
             ArrayList<Index> index = ParserUtil.parseMultiIndex(args);
-            if(index.size() == 0) {
+            if (index.size() == 0) {
                 throw new ParseException(
                         String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
             }
