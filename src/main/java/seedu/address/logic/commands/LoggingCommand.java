6502d30124e8bb1f0619815f9c33cfa1d9d49a00
ZANARKAND\Daniel <danielkwang@outlook.com>
2017-10-24 16:06:28 +0800

    v1.25b What is wrong

--- src/main/java/seedu/address/logic/commands/LoggingCommand.java
+++ src/main/java/seedu/address/logic/commands/LoggingCommand.java
@@ -5,12 +5,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
+
+
 /**
  * Method to Keep Track of User Activity Log
  */
 public class LoggingCommand {
+    private final Logger logger = LogsCenter.getLogger(LoggingCommand.class);
     /**
-     *
+     *keepLog Method
      */
     public void keepLog(String logText, String functionType) {
         try (FileWriter fileWrite = new FileWriter("src/test/data/XMLUtilTest/ConnectUsLog.txt", true);
