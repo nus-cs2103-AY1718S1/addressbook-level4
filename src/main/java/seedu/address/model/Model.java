b1ba77d95225593559a037b24ba2e511ee00c4f6
Jackie Tan <free-soup@hotmail.com>
2017-10-24 09:36:36 +0800

    Reordered import

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -1,7 +1,7 @@
 package seedu.address.model;
 
-import java.util.List;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
