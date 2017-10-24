3e2e50dc3171ee892c60b8676460cf49d9b8ff2b
dalessr <dale.sunshengran@gmail.com>
2017-10-24 19:28:54 +0800

    Implement BrowserPanelFindRouteEvent class

--- src/main/java/seedu/address/commons/events/ui/BrowserPanelFindRouteEvent.java
+++ src/main/java/seedu/address/commons/events/ui/BrowserPanelFindRouteEvent.java
@@ -1,4 +1,31 @@
 package seedu.address.commons.events.ui;
 
-public class BrowserPanelFindRouteEvent {
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Represents a display of route from start location to end location in the Browser Panel
+ */
+public class BrowserPanelFindRouteEvent extends BaseEvent {
+
+    private final ReadOnlyPerson person;
+    private final String address;
+
+    public BrowserPanelFindRouteEvent(ReadOnlyPerson person, String address) {
+        this.person = person;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public ReadOnlyPerson getSelectedPerson() {
+        return person;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
