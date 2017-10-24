a0ff57a12f6e650b4f17ef278c407d6e626871fd
liuhang0213 <liuhang0213@gmail.com>
2017-10-24 15:45:10 +0800

    Fix tests

--- src/test/java/seedu/address/logic/commands/ListByMostSearchedCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListByMostSearchedCommandTest.java
@@ -10,6 +10,7 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UniqueMeetingList;
 import seedu.address.model.UserPrefs;
 
 /***
@@ -22,7 +23,7 @@ public class ListByMostSearchedCommandTest {
 
     @Before
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        model = new ModelManager(getTypicalAddressBook(), new UniqueMeetingList(), new UserPrefs());
 
         lmsCommand = new ListByMostSearchedCommand();
         lmsCommand.setData(model, new CommandHistory(), new UndoRedoStack());
