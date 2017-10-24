199532b169353e7f3effdbe0f19a5845256b2c44
Kennard Ng <Kennard_ng@outlook.com>
2017-10-24 19:56:38 +0800

    Revert "update Delivery Date"
    This reverts commit 792ede40079a50ec318ff5885c0223ee1963fb54.

--- src/main/java/seedu/address/model/parcel/DeliveryDate.java
+++ src/main/java/seedu/address/model/parcel/DeliveryDate.java
@@ -2,9 +2,6 @@ package seedu.address.model.parcel;
 
 import static java.util.Objects.requireNonNull;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
@@ -18,7 +15,7 @@ public class DeliveryDate {
             "Delivery dates should be in the format dd-mm-yyyy";
     public static final String DATE_VALIDATION_REGEX =
             "^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$";
-    public final LocalDate value;
+    public final String value;
 
     /**
      * Validates given delivery date.
@@ -28,13 +25,10 @@ public class DeliveryDate {
     public DeliveryDate(String deliveryDate) throws IllegalValueException {
         requireNonNull(deliveryDate);
         String trimmedDate = deliveryDate.trim();
-
         if (!isValidDate(trimmedDate)) {
             throw new IllegalValueException(MESSAGE_DELIVERYDATE_CONSTRAINTS);
         }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        value = LocalDate.parse(trimmedDate, formatter);
+        this.value = trimmedDate;
     }
 
     /**
@@ -46,7 +40,7 @@ public class DeliveryDate {
 
     @Override
     public String toString() {
-        return value.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        return value;
     }
 
     @Override
@@ -54,10 +48,6 @@ public class DeliveryDate {
         return other == this // short circuit if same object
                 || (other instanceof DeliveryDate // instanceof handles nulls
                 && this.value.equals(((DeliveryDate) other).value)); // state check
-    }
-
-    public int compareTo(DeliveryDate deliveryDate) {
-        return this.value.compareTo(deliveryDate.value);
     }
 
     @Override
