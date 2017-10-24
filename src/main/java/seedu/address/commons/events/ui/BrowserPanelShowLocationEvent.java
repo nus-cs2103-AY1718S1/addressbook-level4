6198037380e7a844805fe4b5dbbbc506926e2f10
dalessr <dale.sunshengran@gmail.com>
2017-10-24 17:49:13 +0800

    Implement BrowserPanelShowLocationEvent class

--- src/main/java/seedu/address/commons/events/ui/BrowserPanelShowLocationEvent.java
+++ src/main/java/seedu/address/commons/events/ui/BrowserPanelShowLocationEvent.java
@@ -1,4 +1,25 @@
 package seedu.address.commons.events.ui;
 
-public class BrowserPanelShowLocationEvent {
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Represents a display of location in the Browser Panel
+ */
+public class BrowserPanelShowLocationEvent extends BaseEvent {
+
+    private final ReadOnlyPerson person;
+
+    public BrowserPanelShowLocationEvent(ReadOnlyPerson person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public ReadOnlyPerson getNewSelection() {
+        return person;
+    }
 }
