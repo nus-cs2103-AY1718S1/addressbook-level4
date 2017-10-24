e4a78b8dbe18d6d15b7391b42a2eeada0b8f8b62
ETERNAL\Travis <zytravis@gmail.com>
2017-10-24 18:08:36 +0800

    Corrected CI error.

--- src/test/java/seedu/address/logic/commands/FindByNumberTest.java
+++ src/test/java/seedu/address/logic/commands/FindByNumberTest.java
@@ -84,7 +84,7 @@ public class FindByNumberTest {
      *     - the {@code FilteredList<ReadOnlyPerson>} is equal to {@code expectedList}<br>
      *     - the {@code AddressBook} in model remains the same after executing the {@code command}
      */
-    private void assertCommandSuccess(FindNumberCommand command, String eMessage, List<ReadOnlyPerson> expectedList){
+    private void assertCommandSuccess(FindNumberCommand command, String eMessage, List<ReadOnlyPerson> expectedList) {
         AddressBook expectedAddressBook = new AddressBook(model.getAddressBook());
         CommandResult commandResult = command.execute();
 
