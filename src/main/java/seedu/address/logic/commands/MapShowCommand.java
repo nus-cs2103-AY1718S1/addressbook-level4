867073add5820f447ae855934f9e530c0aa50970
dalessr <dale.sunshengran@gmail.com>
2017-10-24 15:58:13 +0800

    Add MapShowCommand class

---
+++ src/main/java/seedu/address/logic/commands/MapShowCommand.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.commands;
+
+import java.util.List;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Shows the location of a person on Google map identified using it's last displayed index from the address book.
+ */
+public class MapShowCommand extends Command {
+
+    public static final String COMMAND_WORD = "m-show";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Shows the location of the person identified by the index number used in the last person listing.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_Locate_PERSON_SUCCESS = "Selected Person: %1$s";
+
+    private final Index targetIndex;
+
+    public MapShowCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
+        return new CommandResult(String.format(MESSAGE_Locate_PERSON_SUCCESS, targetIndex.getOneBased()));
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof MapShowCommand // instanceof handles nulls
+                && this.targetIndex.equals(((MapShowCommand) other).targetIndex)); // state check
+    }
+}
