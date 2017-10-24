00a1e51752c3d9ca05b440143cbcc27bd8a72070
dalessr <dale.sunshengran@gmail.com>
2017-10-24 18:07:08 +0800

    Implement MapRouteCommandParser class

--- src/main/java/seedu/address/logic/parser/MapRouteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MapRouteCommandParser.java
@@ -1,4 +1,31 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.MapRouteCommand;
+import seedu.address.logic.commands.MapShowCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new MapRoute Command object
+ */
 public class MapRouteCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the MapRouteCommand
+     * and returns an MapRouteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public MapRouteCommand parse(String args) throws ParseException {
+        try {
+            String indexArg = args.trim().substring(0, args.trim().indexOf(" "));
+            String addressArg = args.trim().substring(args.trim().indexOf(" ") + 1);
+            Index index = ParserUtil.parseIndex(indexArg);
+            return new MapShowCommand(index, addressArg);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MapShowCommand.MESSAGE_USAGE));
+        }
+    }
 }
