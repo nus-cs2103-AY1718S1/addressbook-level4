db8536a28606fe94f18b0680e0680d22799f6ebc
ETERNAL\Travis <zytravis@gmail.com>
2017-10-25 22:22:54 +0800

    Added missing JavaDoc statement.

--- src/main/java/seedu/address/model/person/FindCommandPredicate.java
+++ src/main/java/seedu/address/model/person/FindCommandPredicate.java
@@ -5,6 +5,9 @@ import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code name, number, address, email} matches any of the keywords given.
+ */
 public class FindCommandPredicate implements Predicate<ReadOnlyPerson> {
     private final List<String> keywords;
 
