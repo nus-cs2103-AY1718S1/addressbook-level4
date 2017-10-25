bd46feeb800bcc2c98cf0ff08719dc30b83af078
newalter <zeyong1123@gmail.com>
2017-10-25 15:13:47 +0800

    included `?`

--- src/main/java/seedu/address/logic/parser/ArgumentWildcardMatcher.java
+++ src/main/java/seedu/address/logic/parser/ArgumentWildcardMatcher.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Convert a list of string of unprocessed keywords with wildcard symbol "*"
+ * Convert a list of string of unprocessed keywords with wildcard symbol "*" and "?"
  * into a list of lowercase regular expression matching the keywords.
  */
 public class ArgumentWildcardMatcher {
 
     /**
-     * Convert arguments string of keywords with wildcard symbol "*"
+     * Convert arguments string of keywords with wildcard symbol "*" and "?"
      * into a list of lowercase regular expression matching the keywords.
      * @param keywords List of String containing unprocessed keywords
      * @return A lists of string regular expression in lowercase matching the keywords.
@@ -18,7 +18,7 @@ public class ArgumentWildcardMatcher {
     public static List<String> processKeywords(List<String> keywords) {
         ArrayList<String> processedKeywords = new ArrayList<String>();
         for (int i = 0; i < keywords.size(); i++) {
-            processedKeywords.add(keywords.get(i).toLowerCase().replace("*", "\\S*"));
+            processedKeywords.add(keywords.get(i).toLowerCase().replace("*", "\\S*").replace("?", "\\S"));
         }
         return processedKeywords;
     }
