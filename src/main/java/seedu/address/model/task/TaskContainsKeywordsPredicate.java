26fa60192004a69fa4c9c54ef216cac00d82749d
peiquan <peiquan@hiei.local>
2017-10-26 03:05:50 +0800

    add birthday to findtask command

--- src/main/java/seedu/address/model/task/TaskContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/task/TaskContainsKeywordsPredicate.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code ReadOnlyTask}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code ReadOnlyTask}'s {@code Name} and/or {@code Deadline} matches any of the keywords given.
  */
 public class TaskContainsKeywordsPredicate implements Predicate<ReadOnlyTask> {
     private final List<String> keywords;
@@ -18,7 +18,8 @@ public class TaskContainsKeywordsPredicate implements Predicate<ReadOnlyTask> {
     @Override
     public boolean test(ReadOnlyTask task) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(task.getDescription().taskDescription, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(task.getDescription().taskDescription, keyword)
+                || StringUtil.containsWordIgnoreCase(task.getDeadline().date, keyword));
     }
 
     @Override
