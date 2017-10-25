9ca2510100d716f614ac0fc7b3b1de570d982b28
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-10-25 23:10:39 +0800

    Add system test

---
+++ src/test/java/systemtests/AddMultipleByTsvCommandSystemTest.java
@@ -0,0 +1,93 @@
+package systemtests;
+
+import static seedu.address.logic.commands.AddMultipleByTsvCommand.MESSAGE_DUPLICATE_PERSON;
+import static seedu.address.logic.commands.AddMultipleByTsvCommand.MESSAGE_NUMBER_OF_ENTRIES_FAILED;
+import static seedu.address.logic.commands.AddMultipleByTsvCommand.MESSAGE_SUCCESS;
+
+import java.util.ArrayList;
+import java.util.StringJoiner;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.AddMultipleByTsvCommand;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.testutil.TsvFileBuilder;
+import seedu.address.testutil.TypicalTsvFiles;
+
+
+public class AddMultipleByTsvCommandSystemTest extends AddressBookSystemTest {
+    @Test
+    public void addMultipleByTsv() {
+        Model model = getModel();
+        TsvFileBuilder validTsvFile = new TsvFileBuilder();
+        String command = AddMultipleByTsvCommand.COMMAND_WORD + " " + TypicalTsvFiles.PERFECT_TSV_FILE_PATH;
+        String expectedResultMessage = TypicalTsvFiles.PERFECT_TSV_FILE_MESSAGE_SUCCESS;
+        assertCommandSuccess(command, validTsvFile);
+    }
+
+    /**
+     * Performs the same verification as {@code assertCommandSuccess(String, TsvFileBuilder)}. Executes {@code command}
+     * instead.
+     * @see AddMultipleByTsvCommandSystemTest#assertCommandSuccess(String, TsvFileBuilder)
+     */
+    private void assertCommandSuccess(String command, TsvFileBuilder tsvFile) {
+        Model expectedModel = getModel();
+        boolean isFileFound = tsvFile.getIsFileFound();
+        ArrayList<ReadOnlyPerson> toAddPeople = tsvFile.getToAddPeople();
+        ArrayList<Integer> failedEntries = tsvFile.getFailedEntries();
+
+        if (!isFileFound) {
+            throw new IllegalArgumentException(AddMultipleByTsvCommand.MESSAGE_FILE_NOT_FOUND);
+        }
+        int numAdded = 0;
+        int numDuplicated = 0;
+        ArrayList<Person> toAdd = new ArrayList<Person>();
+
+        for (ReadOnlyPerson person: toAddPeople) {
+            toAdd.add(new Person(person));
+        }
+        for (Person person: toAdd) {
+            try {
+                expectedModel.addPerson(person);
+                numAdded++;
+            } catch (DuplicatePersonException e) {
+                numDuplicated++;
+            }
+        }
+        String expectedResultMessage = String.format(MESSAGE_SUCCESS, numAdded) + ", "
+                + String.format(MESSAGE_DUPLICATE_PERSON, numDuplicated) + ", "
+                + String.format(MESSAGE_NUMBER_OF_ENTRIES_FAILED, failedEntries.size())
+                + joinFailedEntries(failedEntries);
+        assertCommandSuccess(command, expectedModel, expectedResultMessage);
+    }
+
+    /**
+     * Performs the same verification as {@code assertCommandSuccess(String, Model, String)} except that the result
+     * display box displays {@code expectedResultMessage} and the model related components equal to
+     * {@code expectedModel}.
+     * @see AddMultipleByTsvCommandSystemTest#assertCommandSuccess(String, Model, String)
+     */
+    private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage) {
+        executeCommand(command);
+        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchangedExceptSyncStatus();
+    }
+
+    /**
+     * Join list of failed entries as integers to a single string
+     * @param failedEntries
+     * @return
+     */
+    private static String joinFailedEntries(ArrayList<Integer> failedEntries) {
+        StringJoiner joiner = new StringJoiner(", ");
+        for (Integer entry: failedEntries) {
+            joiner.add(entry.toString());
+        }
+        return joiner.toString();
+    }
+}
