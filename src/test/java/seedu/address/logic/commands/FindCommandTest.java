9da66b916a05e3142e9c707d238ab1d5f3063a62
ETERNAL\Travis <zytravis@gmail.com>
2017-10-25 23:00:04 +0800

    Removed unused import statement.

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -21,7 +21,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.FindCommandPredicate;
 import seedu.address.model.person.ReadOnlyPerson;
 
