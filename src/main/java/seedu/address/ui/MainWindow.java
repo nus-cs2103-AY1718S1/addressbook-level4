60ef0c67424bbbc12df033f661127dd6b285b110
ALPHEUS\Alpheus Lim <e0031928@u.nus.edu>
2017-10-26 07:56:56 +0800

    setup overlay

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
@@ -48,6 +49,15 @@ public class MainWindow extends UiPart<Region> {
 
     @FXML
     private StackPane browserPlaceholder;
+
+    @FXML
+    private ImageView helpOverlay;
+
+    @FXML
+    private MenuItem helpOverlayItem;
+
+    @FXML
+    private MenuItem helpOverlayExit;
 
     @FXML
     private StackPane pinListPlaceHolder;
@@ -97,6 +107,8 @@ public class MainWindow extends UiPart<Region> {
 
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
+        setAccelerator(helpOverlayItem, KeyCombination.valueOf("F2"));
+        setAccelerator(helpOverlayExit, KeyCombination.valueOf("ESC"));
     }
 
     /**
@@ -218,6 +230,22 @@ public class MainWindow extends UiPart<Region> {
     @FXML
     private void handleExit() {
         raise(new ExitAppRequestEvent());
+    }
+
+    /**
+     * Opens the help overlay
+     */
+    @FXML
+    private void handleOverlay() {
+        helpOverlay.setVisible(true);
+    }
+
+    /**
+     * Closes the help overlay
+     */
+    @FXML
+    private void handleOverlayExit() {
+        helpOverlay.setVisible(false);
     }
 
     public PersonListPanel getPersonListPanel() {
