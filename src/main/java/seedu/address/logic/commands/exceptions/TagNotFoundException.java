49a592d4a5badcc2d5b38f359f16f5932f415620
Shi Tian <shitian007@hotmail.com>
2017-10-25 18:45:39 +0800

    Add HighlightCommandParser and TagNotFoundException

---
+++ src/main/java/seedu/address/logic/commands/exceptions/TagNotFoundException.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.commands.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Person objects.
+ */
+public class TagNotFoundException extends IllegalArgumentException {
+    public TagNotFoundException(String message) { super(message) ; }
+}
+
