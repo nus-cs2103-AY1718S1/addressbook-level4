210844ed5f7ad872fe679f1959fe5e3d6ad48afb
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-10-26 00:13:59 +0800

    PhoneTest.java: add import for assertEquals PhoneTest.java: add method isPhoneFormmatingCorrect

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -21,5 +22,12 @@ public class PhoneTest {
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
         assertTrue(Phone.isValidPhone("93121534"));
         assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+    }
+    
+    @Test
+    public void isPhoneFormattingCorrect() {
+        assertEquals(Phone.formatPhone("911"), "911");
+        assertEquals(Phone.formatPhone("93121534"), "9312-1534");
+        assertEquals(Phone.formatPhone("124293842033123"), "124-2938-4203-3123");
     }
 }
