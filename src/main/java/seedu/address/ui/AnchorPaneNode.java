1f3b520e1226c776e3fce724156397b895e896fa
chernghann <chernghann.chia@gmail.com>
2017-10-25 19:44:45 +0800

    Update EventCentre and Logger.info for mouseOnClick AnchorPaneNode for Calendar

--- src/main/java/seedu/address/ui/AnchorPaneNode.java
+++ src/main/java/seedu/address/ui/AnchorPaneNode.java
@@ -1,9 +1,13 @@
 package seedu.address.ui;
 
 import java.time.LocalDate;
+import java.util.logging.Logger;
 
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.CalendarPanelSelectionEvent;
 
 /**
  * Create an anchor pane that can store additional data.
@@ -12,6 +16,7 @@ public class AnchorPaneNode extends AnchorPane {
 
     // Date associated with this pane
     private LocalDate date;
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
 
     /**
      * Create a anchor pane node. Date is not assigned in the constructor.
@@ -20,7 +25,8 @@ public class AnchorPaneNode extends AnchorPane {
     public AnchorPaneNode(Node... children) {
         super(children);
         // Add action handler for mouse clicked
-        this.setOnMouseClicked(e -> System.out.println("This pane's date is: " + date));
+        this.setOnMouseClicked(e ->
+                handleCalendarEvent(date.toString()));
     }
 
     public LocalDate getDate() {
@@ -29,5 +35,10 @@ public class AnchorPaneNode extends AnchorPane {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    private void handleCalendarEvent(String event){
+        EventsCenter.getInstance().post(new CalendarPanelSelectionEvent(date.toString()));
+        logger.info(LogsCenter.getEventHandlingLogMessage(new CalendarPanelSelectionEvent(event)));
     }
 }
