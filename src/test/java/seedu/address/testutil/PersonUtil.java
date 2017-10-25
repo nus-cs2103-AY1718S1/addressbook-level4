93f48c33a7159f1a359ee2894ea01568d856e27a
huiyiiih <e0053334@u.nus.edu>
2017-10-25 18:55:56 +0800

    Update PersonUtil to include relationship information

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -8,6 +8,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POSITION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RELATIONSHIP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
@@ -42,6 +43,9 @@ public class PersonUtil {
         sb.append(PREFIX_NOTE + person.getNote().value + " ");
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+        person.getRelation().stream().forEach(
+        s -> sb.append(PREFIX_RELATIONSHIP + s.relType + " ")
         );
         return sb.toString();
     }
