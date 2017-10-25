15a9f409c4d94eb2491b8d77b7b8a45e1cd75f18
dalessr <dale.sunshengran@gmail.com>
2017-10-26 02:41:22 +0800

    Create PersonDetailsPanel.fxml file

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -8,7 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
