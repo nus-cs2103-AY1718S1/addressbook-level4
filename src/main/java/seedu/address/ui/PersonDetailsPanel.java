873dfef49e22a532bb7111588065d5f2050ba912
dalessr <dale.sunshengran@gmail.com>
2017-10-26 02:48:35 +0800

    Implement PersonDetailsPanel.java

--- src/main/java/seedu/address/ui/PersonDetailsPanel.java
+++ src/main/java/seedu/address/ui/PersonDetailsPanel.java
@@ -1,4 +1,29 @@
 package seedu.address.ui;
 
-public class PersonDetailsPanel {
+import javafx.fxml.FXML;
+
+import javax.swing.plaf.synth.Region;
+import java.awt.*;
+
+/**
+ * The Contact Details Panel of the App.
+ */
+public class PersonDetailsPanel extends UiPart<Region> {
+    private static final String FXML = "PersonDetailsPanel.fxml";
+
+    @FXML
+    private Label nameLable;
+
+    @FXML
+    private Label phoneLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label addressLabel;
+
+    public PersonDetailsPanel() {
+        super(FXML);
+    }
 }
