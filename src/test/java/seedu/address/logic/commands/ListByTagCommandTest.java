979aa5bbd72bb0cc002af62a1e704e635d44ba3d
Jeremy Long Shi Wei <E0014824@u.nus.edu>
2017-10-26 01:42:11 +0800

    Removed Unused import

--- src/test/java/seedu/address/logic/commands/ListByTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListByTagCommandTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.ELLE;
 import static seedu.address.testutil.TypicalPersons.FIONA;
