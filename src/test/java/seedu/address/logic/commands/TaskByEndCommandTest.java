b67cd5f04631873440810db3fdcdb48463062c77
nusjzx <nusjzx@gmail.com>
2017-10-25 11:24:41 +0800

    update taskbyendcommand test

--- src/test/java/seedu/address/logic/commands/TaskByEndCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TaskByEndCommandTest.java
@@ -2,9 +2,8 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.fail;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalTasks.*;
+import static seedu.address.testutil.TypicalTasks.getTypicalTaskbook;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,9 +12,7 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.TaskBook;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.task.exceptions.DuplicateTaskException;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
@@ -35,15 +32,8 @@ public class TaskByEndCommandTest {
 
     @Test
     public void execute() {
-        TaskBook expectedTaskBook = new TaskBook();
-        try {
-            expectedTaskBook.addTask(MEETING);
-            expectedTaskBook.addTask(EXAM);
-            expectedTaskBook.addTask(PICNIC);
-        } catch (DuplicateTaskException e) {
-            fail("Impossible");
-        }
-        expectedModel = new ModelManager(getTypicalAddressBook(), expectedTaskBook, new UserPrefs());
+        expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTaskbook(), new UserPrefs());
+        expectedModel.taskByEnd();
         assertCommandSuccess(taskByEndCommand, model, taskByEndCommand.MESSAGE_SUCCESS, expectedModel);
     }
 }
