8dc55b046f1b5226ba0992e6d8ea98fdd359d69c
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-10-25 22:55:50 +0800

    changed name

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -54,7 +54,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing Contact List Pro ]===========================");
+        logger.info("=============================[ Initializing The Classroom ]===========================");
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
@@ -91,14 +91,14 @@ public class MainApp extends Application {
         try {
             addressBookOptional = storage.readAddressBook();
             if (!addressBookOptional.isPresent()) {
-                logger.info("Data file not found. Will be starting with a sample Contact List Pro");
+                logger.info("Data file not found. Will be starting with a sample The Classroom");
             }
             initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
         } catch (DataConversionException e) {
-            logger.warning("Data file not in the correct format. Will be starting with an empty Contact List Pro");
+            logger.warning("Data file not in the correct format. Will be starting with an empty The Classroom");
             initialData = new AddressBook();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty Contact List Pro");
+            logger.warning("Problem while reading from the file. Will be starting with an empty The Classroom");
             initialData = new AddressBook();
         }
 
@@ -163,7 +163,7 @@ public class MainApp extends Application {
                     + "Using default user prefs");
             initializedPrefs = new UserPrefs();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty Contact List Pro");
+            logger.warning("Problem while reading from the file. Will be starting with an empty The Classroom");
             initializedPrefs = new UserPrefs();
         }
 
@@ -183,13 +183,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting Contact List Pro " + MainApp.VERSION);
+        logger.info("Starting The Classroom " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Contact List Pro ] =============================");
+        logger.info("============================ [ Stopping The Classroom ] =============================");
         ui.stop();
         try {
             storage.saveUserPrefs(userPrefs);
