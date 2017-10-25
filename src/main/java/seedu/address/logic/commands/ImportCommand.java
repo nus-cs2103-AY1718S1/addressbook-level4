e83584da67e8255ba0346d625ab6f186c82767c6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-25 17:02:47 +0800

    Add ImportCommandParser to AddressBookParser

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -21,6 +21,8 @@ public class ImportCommand extends Command {
             + "file.\nYou need to specify with explicit parameter if you want to use BoNUS script format.";
     public static final String MESSAGE_NOT_BO_FILE = "According to the extension, the file is not a valid BoNUS"
             + "script file (should end with .bo).";
+    public static final String MESSAGE_PROBLEM_READING_FILE = "There is a problem when the application tried to"
+            + " read the given file. Please check the file permission.";
 
     @Override
     public CommandResult execute() throws CommandException {
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.ExportCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListEventCommand;
 import seedu.address.logic.commands.RedoCommand;
@@ -113,6 +114,10 @@ public class AddressBookParser {
         case ConfigCommand.COMMAND_WORD:
         case ConfigCommand.COMMAND_ALIAS:
             return new ConfigCommandParser().parse(arguments);
+
+        case ImportCommand.COMMAND_WORD:
+        case ImportCommand.COMMAND_ALIAS:
+            return new ImportCommandParser().parse(arguments);
 
         case ExportCommand.COMMAND_WORD:
         case ExportCommand.COMMAND_ALIAS:
