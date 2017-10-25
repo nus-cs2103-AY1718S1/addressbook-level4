661e80ee81996fefb750a3821811683834ee7ef5
Nam Vu <nam_vd2709@yahoo.com.vn>
2017-10-25 13:42:51 +0800

    resolve failing travis test

--- src/test/java/seedu/address/logic/parser/AppointCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AppointCommandParserTest.java
@@ -4,7 +4,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.commands.CommandTestUtil.APPOINTMENT_DESC;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import seedu.address.logic.commands.AppointCommand;
 
