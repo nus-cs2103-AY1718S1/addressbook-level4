93cad0bb8b616643ef05113d12d439cd60a91f05
Srivatsa <p.psrivatsa@gmail.com>
2017-10-26 02:15:56 +0800

    Fix style issues

--- src/test/java/guitests/StatusBarFooterTest.java
+++ src/test/java/guitests/StatusBarFooterTest.java
@@ -17,7 +17,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import seedu.address.logic.commands.ListCommand;
 import seedu.address.testutil.PersonUtil;
 import seedu.address.ui.StatusBarFooter;
 
