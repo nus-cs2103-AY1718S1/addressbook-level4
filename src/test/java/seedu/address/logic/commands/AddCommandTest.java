fcd4d53e542c6e695f28a649f9c4a69ee336c075
Yuchen He <yuchenhe@Yuchens-MBP.workgroup>
2017-10-27 07:49:58 +0800

    update Modelstub, override new method

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeSet;
 import java.util.function.Predicate;
 
 import org.junit.Rule;
@@ -144,6 +145,12 @@ public class AddCommandTest {
         @Override
         public void sort() {
             fail("This method should not be called.");
+        }
+
+
+        @Override
+        public void addScheduleToPerson(Integer index, TreeSet<Integer> span){
+            fail("The span is not valid");
         }
     }
 
