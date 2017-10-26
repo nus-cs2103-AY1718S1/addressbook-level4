fe86fb049bbe6580db6353947b3b4b858b344dab
Martyn <e00527972@u.nus.edu>
2017-10-27 06:41:59 +0800

    Removed unused import and listed imports

--- src/main/java/seedu/address/logic/parser/SearchCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchCommandParser.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 
 import seedu.address.logic.commands.SearchCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.PersonContainsKeywordsPredicate;
 
 /**
