98043967e6ab5925ad50aaf15502903653f08e38
blaqkrow <joshnfsmw@hotmail.com>
2017-10-26 18:34:22 +0800

    unused import

--- src/test/java/systemtests/EmailCommandSystemTest.java
+++ src/test/java/systemtests/EmailCommandSystemTest.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import static seedu.address.logic.commands.EmailCommand.MESSAGE_SUCCESS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalPersons.KEYWORD_MATCHING_MEIER;
 
 import org.junit.Test;
 
