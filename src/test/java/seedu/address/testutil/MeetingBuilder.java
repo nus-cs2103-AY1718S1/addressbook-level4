30897219d2c9cc3b95e68c7f50c2f9612517f61c
liuhang0213 <liuhang0213@gmail.com>
2017-10-26 13:42:47 +0800

    Removes unused imports

--- src/test/java/seedu/address/testutil/MeetingBuilder.java
+++ src/test/java/seedu/address/testutil/MeetingBuilder.java
@@ -2,20 +2,10 @@ package seedu.address.testutil;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.Meeting;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
 import seedu.address.model.person.InternalId;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.SearchData;
-import seedu.address.model.tag.Tag;
-import seedu.address.model.util.SampleDataUtil;
 
 /**
  * A utility class to help with building Meeting objects.
--- src/test/java/seedu/address/testutil/TypicalMeetings.java
+++ src/test/java/seedu/address/testutil/TypicalMeetings.java
@@ -1,29 +1,11 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Meeting;
-import seedu.address.model.ReadOnlyMeetingList;
 import seedu.address.model.UniqueMeetingList;
-import seedu.address.model.person.InternalId;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
@@ -37,7 +19,6 @@ public class TypicalMeetings {
 
     /**
      * Returns a {@code UniqueMeetingList} with all the typical meetings
-     * @return
      */
     public static UniqueMeetingList getTypicalMeetingList() {
         try {
