64f107c5a6e53a0ddd608b62c724e4b4cdceeda0
peiquan <peiquan@r-175-99-25-172.comp.nus.edu.sg>
2017-10-26 10:50:30 +0800

    update missing ;

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -84,8 +84,8 @@ public class AddressBookParserTest {
     public void parseCommand_find() throws Exception {
         List<String> keywords = Arrays.asList("foo", "bar", "baz");
         FindCommand command = (FindCommand) parser.parseCommand(
-                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" "))
-            MODE_ADDRESS_BOOK);
+                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")),
+                MODE_ADDRESS_BOOK);
         assertEquals(new FindCommand(new PersonContainsKeywordsPredicate(keywords)), command);
 
     }
