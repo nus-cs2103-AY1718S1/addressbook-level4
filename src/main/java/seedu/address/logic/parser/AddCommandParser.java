2893473e13a4235f607332d9b84af73581949a8d
jiasheng <jiashengan@gmail.com>
2017-10-26 23:37:20 +0800

    Style update

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-import static java.lang.Boolean.FALSE;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
