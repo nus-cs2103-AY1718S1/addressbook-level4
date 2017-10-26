78ce353b552757e03c38e54b3b5d873eaeb68dcd
fustilio <lfrancis373@gmail.com>
2017-10-26 13:59:00 +0800

    Changed exception thrown

--- src/main/java/seedu/address/model/parcel/DeliveryDate.java
+++ src/main/java/seedu/address/model/parcel/DeliveryDate.java
@@ -20,8 +20,6 @@ public class DeliveryDate {
 
     public static final String MESSAGE_DELIVERY_DATE_CONSTRAINTS =
             "Delivery dates should be in the format dd-mm-yyyy";
-    public static final String MESSAGE_DELIVERY_DATE_CONSTRAINTS_PARSE_ERROR =
-            "Formatting is close, please check again.";
     public static final String DATE_VALIDATION_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)"
            + "(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?"
            + ":1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?["
@@ -48,7 +46,7 @@ public class DeliveryDate {
                 this.date = formatDate(trimmedDate);
                 this.value = df.format(this.date);
             } catch (ParseException e) {
-                throw new IllegalValueException(e.toString());
+                throw new IllegalValueException(MESSAGE_DELIVERY_DATE_CONSTRAINTS);
             }
         } else {
             throw new IllegalValueException(MESSAGE_DELIVERY_DATE_CONSTRAINTS);
