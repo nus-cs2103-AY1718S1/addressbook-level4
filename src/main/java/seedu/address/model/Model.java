5bb31da75d385989ff4ca4fa7523ea2b5946b2cc
Choony93 <ngchoonheng@outlook.com>
2017-10-26 17:18:00 +0800

    Enhencement: Accepting names with gmap command

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -1,5 +1,6 @@
 package seedu.address.model;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
@@ -12,40 +13,60 @@ import seedu.address.model.tag.Tag;
  * The API of the Model component.
  */
 public interface Model {
-    /** {@code Predicate} that always evaluate to true */
+    /**
+     * {@code Predicate} that always evaluate to true
+     */
     Predicate<ReadOnlyPerson> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
 
-    /** Clears existing backing model and replaces with the provided new data. */
+    /**
+     * Clears existing backing model and replaces with the provided new data.
+     */
     void resetData(ReadOnlyAddressBook newData);
 
-    /** Returns the AddressBook */
+    /**
+     * Returns the AddressBook
+     */
     ReadOnlyAddressBook getAddressBook();
 
-    /** Deletes the given person. */
+    /**
+     * Deletes the given person.
+     */
     void deletePerson(ReadOnlyPerson target) throws PersonNotFoundException;
 
-    /** Adds the given person */
+    /**
+     * Adds the given person
+     */
     void addPerson(ReadOnlyPerson person) throws DuplicatePersonException;
 
     void deleteTag(Tag tag) throws PersonNotFoundException, DuplicatePersonException;
 
-    void sortImportantTag () throws PersonNotFoundException, DuplicatePersonException;
+    void sortImportantTag() throws PersonNotFoundException, DuplicatePersonException;
 
     /**
      * Replaces the given person {@code target} with {@code editedPerson}.
      *
      * @throws DuplicatePersonException if updating the person's details causes the person to be equivalent to
-     *      another existing person in the list.
-     * @throws PersonNotFoundException if {@code target} could not be found in the list.
+     *                                  another existing person in the list.
+     * @throws PersonNotFoundException  if {@code target} could not be found in the list.
      */
     void updatePerson(ReadOnlyPerson target, ReadOnlyPerson editedPerson)
             throws DuplicatePersonException, PersonNotFoundException;
 
-    /** Returns an unmodifiable view of the filtered person list */
+    /**
+     * Returns an unmodifiable view of the filtered person list
+     */
     ObservableList<ReadOnlyPerson> getFilteredPersonList();
 
     /**
+     * @param predicate
+     * @return the filter of the filtered person list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    List<ReadOnlyPerson> getPersonListByPredicate(Predicate<ReadOnlyPerson> predicate);
+
+    /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
+     *
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate);
