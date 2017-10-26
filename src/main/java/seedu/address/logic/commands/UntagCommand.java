44ffde8e8360f0363fe998e94c8adb18486f48c1
minato <dmc3hpcity@gmail.com>
2017-10-26 16:08:11 +0800

    Modify untag command execute logic

--- src/main/java/seedu/address/logic/commands/UntagCommand.java
+++ src/main/java/seedu/address/logic/commands/UntagCommand.java
@@ -31,15 +31,14 @@ public class UntagCommand extends UndoableCommand {
 
     public static final String MESSAGE_EMPTY_INDEX_LIST = "Please provide one or more indexes! \n%1$s";
     public static final String MESSAGE_INVALID_INDEXES = "One or more person indexes provided are invalid.";
-    public static final String MESSAGE_DUPLICATE_TAG = "One or more persons already have this tag.";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
 
-    private boolean toAll;
+    private boolean toAllPersonsInFilteredList;
     private List<Index> targetIndexes;
     private Tag tag;
 
     public UntagCommand() {
-        this.toAll = true;
+        this.toAllPersonsInFilteredList = true;
     }
 
     /**
@@ -48,7 +47,7 @@ public class UntagCommand extends UndoableCommand {
     public UntagCommand(Tag tag) {
         requireNonNull(tag);
 
-        this.toAll = true;
+        this.toAllPersonsInFilteredList = true;
         this.tag = tag;
     }
 
@@ -60,7 +59,7 @@ public class UntagCommand extends UndoableCommand {
         requireNonNull(targetIndexes);
         requireNonNull(tag);
 
-        this.toAll = false;
+        this.toAllPersonsInFilteredList = false;
         this.targetIndexes = targetIndexes;
         this.tag = tag;
     }
@@ -68,48 +67,25 @@ public class UntagCommand extends UndoableCommand {
     @Override
     protected CommandResult executeUndoableCommand() throws CommandException {
         List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
-        if (toAll && tag != null) {
+
+        if (toAllPersonsInFilteredList) {
             for (ReadOnlyPerson personToUntag : lastShownList) {
-                Person untaggedPerson = new Person(personToUntag);
-                UniqueTagList updatedTags = new UniqueTagList(personToUntag.getTags());
-                updatedTags.remove(tag);
-                untaggedPerson.setTags(updatedTags.toSet());
-                try {
-                    model.updatePerson(personToUntag, untaggedPerson);
-                } catch (DuplicatePersonException e) {
-                    throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-                } catch (PersonNotFoundException e) {
-                    throw new AssertionError("The target person cannot be missing");
-                }
+                removeTagFromPerson(personToUntag, tag);
             }
-        } else if (tag == null) {
-            for (ReadOnlyPerson personToUntag : lastShownList) {
-                Person untaggedPerson = new Person(personToUntag);
-                UniqueTagList updatedTags = new UniqueTagList();
-                untaggedPerson.setTags(updatedTags.toSet());
-                try {
-                    model.updatePerson(personToUntag, untaggedPerson);
-                } catch (DuplicatePersonException e) {
-                    throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-                } catch (PersonNotFoundException e) {
-                    throw new AssertionError("The target person cannot be missing");
-                }
+
+            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+            return new CommandResult("Persons succcessfully untagged.");
+        }
+
+        for (Index targetIndex : targetIndexes) {
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(MESSAGE_INVALID_INDEXES);
             }
         }
 
         for (Index targetIndex : targetIndexes) {
             ReadOnlyPerson personToUntag = lastShownList.get(targetIndex.getZeroBased());
-            Person untaggedPerson = new Person(personToUntag);
-            UniqueTagList updatedTags = new UniqueTagList(personToUntag.getTags());
-            updatedTags.remove(tag);
-            untaggedPerson.setTags(updatedTags.toSet());
-            try {
-                model.updatePerson(personToUntag, untaggedPerson);
-            } catch (DuplicatePersonException e) {
-                throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-            } catch (PersonNotFoundException e) {
-                throw new AssertionError("The target person cannot be missing");
-            }
+            removeTagFromPerson(personToUntag, tag);
         }
 
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
@@ -120,8 +96,32 @@ public class UntagCommand extends UndoableCommand {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof UntagCommand // instanceof handles nulls
-                && this.targetIndexes.equals(((UntagCommand) other).targetIndexes)) // state check
-                && this.tag.equals(((UntagCommand) other).tag); // state check
+                && ((targetIndexes == null) || this.targetIndexes.equals(((UntagCommand) other).targetIndexes)))
+                && ((tag == null) || this.tag.equals(((UntagCommand) other).tag)); // state check
+    }
+
+    /**
+     * Removes a tag from the person
+     * Removes all tags if tag is not specified
+     * @param person to be untagged
+     * @param tag to be removed
+     */
+    private void removeTagFromPerson(ReadOnlyPerson person, Tag tag) throws CommandException {
+        Person untaggedPerson = new Person(person);
+        UniqueTagList updatedTags = new UniqueTagList();
+        if (tag != null) {
+            updatedTags = new UniqueTagList(person.getTags());
+            updatedTags.remove(tag);
+        }
+        untaggedPerson.setTags(updatedTags.toSet());
+
+        try {
+            model.updatePerson(person, untaggedPerson);
+        } catch (DuplicatePersonException e) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        } catch (PersonNotFoundException e) {
+            throw new AssertionError("The target person cannot be missing");
+        }
     }
 
 }
