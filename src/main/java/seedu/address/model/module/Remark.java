aea7e121f753d3b2d0abb95d6000f53d04a81aca
Wang Junming <e0134140@u.nus.edu>
2017-10-27 23:28:47 +0800

    add class remark

---
+++ src/main/java/seedu/address/model/module/Remark.java
@@ -0,0 +1,50 @@
+package seedu.address.model.module;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents a Module's remark(if any) in the application.
+ */
+public class Remark {
+
+    private final int REMARK_LENGTH_LIMIT = 50;
+    private static final String MESSAGE_REMARK_CONSTRAINTS =
+            "Remark can only be no more than 50 characters";
+
+    public final String value;
+    public final Code moduleCode;
+
+    /**
+     * Validates given group number.
+     *
+     * @throws IllegalValueException if given group string is invalid.
+     */
+    public Remark(String remark, Code module) throws IllegalValueException {
+        requireNonNull(remark);
+        if (remark.length() >= REMARK_LENGTH_LIMIT) {
+            throw new IllegalValueException(MESSAGE_REMARK_CONSTRAINTS);
+        }
+        value = remark;
+        moduleCode = module;
+
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Group // instanceof handles nulls
+                && this.value.equals(((Group) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
