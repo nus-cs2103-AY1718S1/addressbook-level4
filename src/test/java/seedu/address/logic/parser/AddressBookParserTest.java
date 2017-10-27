7ce7c90df7f40fd7d0b98b599957fb8c2d8e1881
Sky Ang Tian Lan <tianlanwork44444@gmail.com>
2017-10-27 14:47:48 +0800

    uncommented parse command find

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -1,23 +1,14 @@
 package seedu.address.logic.parser;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.logic.commands.EditCommand.EditLessonDescriptor;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_LESSON;
-
 import org.junit.Rule;
 import org.junit.Test;
-
 import org.junit.rules.ExpectedException;
-
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
@@ -32,6 +23,17 @@ import seedu.address.testutil.EditLessonDescriptorBuilder;
 import seedu.address.testutil.LessonBuilder;
 import seedu.address.testutil.LessonUtil;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.commands.EditCommand.EditLessonDescriptor;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_LESSON;
 
 
 public class AddressBookParserTest {
@@ -76,14 +78,14 @@ public class AddressBookParserTest {
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
     }
 
-    /***
-     @Test public void parseCommand_find() throws Exception {
-     ListString keywords = Arrays.asList("foo", "bar", "baz");
-     FindCommand command = (FindCommand) parser.parseCommand(
-     FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
-     assertEquals(new FindCommand(keywords), command);
-     }
-     ***/
+    @Test
+    public void parseCommand_find() throws Exception {
+        List<String> keywords = Arrays.asList("MA1101R", "CS1010", "CS2100");
+        FindCommand command = (FindCommand) parser.parseCommand(
+                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindCommand(keywords), command);
+    }
+
     @Test
     public void parseCommand_help() throws Exception {
         assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
