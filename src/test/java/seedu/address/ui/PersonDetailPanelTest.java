4db7a496bf501171fa7df3e733fa9fdc47d2a2e1
jo-lyn <e0007247@u.nus.edu>
2017-10-27 17:14:19 +0800

    Add test for ListClearedEvent to PersonDetailPanelTest

--- src/test/java/seedu/address/ui/PersonDetailPanelTest.java
+++ src/test/java/seedu/address/ui/PersonDetailPanelTest.java
@@ -14,6 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import guitests.guihandles.PersonDetailPanelHandle;
+import seedu.address.commons.events.ui.ListClearedEvent;
 import seedu.address.commons.events.ui.PersonModifiedEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -48,21 +49,37 @@ public class PersonDetailPanelTest extends GuiUnitTest {
 
         postNow(new PersonModifiedEvent(BOB));
         assertPanelDisplaysPerson(BOB, personDetailPanelHandle);
+
+        // panel is empty when list is cleared
+        postNow(new ListClearedEvent());
+        assertPanelDisplaysNothing(personDetailPanelHandle);
     }
 
     /**
-     * Asserts that {@code actualPerson} displays the details of {@code expectedPerson}.
+     * Asserts that {@code panel} displays the details of {@code expectedPerson}.
      */
-    private void assertPanelDisplaysPerson(ReadOnlyPerson expectedPerson, PersonDetailPanelHandle actualPerson) {
+    private void assertPanelDisplaysPerson(ReadOnlyPerson expectedPerson, PersonDetailPanelHandle panel) {
         guiRobot.pauseForHuman();
 
-        assertEquals(expectedPerson.getName().toString(), actualPerson.getName());
-        assertEquals(PERSON_PHONE_ICON + expectedPerson.getPhone().toString(), actualPerson.getPhone());
-        assertEquals(PERSON_ADDRESS_ICON + expectedPerson.getAddress().toString(), actualPerson.getAddress());
-        assertEquals(PERSON_EMAIL_ICON + expectedPerson.getEmail().toString(), actualPerson.getEmail());
+        assertEquals(expectedPerson.getName().toString(), panel.getName());
+        assertEquals(PERSON_PHONE_ICON + expectedPerson.getPhone().toString(), panel.getPhone());
+        assertEquals(PERSON_ADDRESS_ICON + expectedPerson.getAddress().toString(), panel.getAddress());
+        assertEquals(PERSON_EMAIL_ICON + expectedPerson.getEmail().toString(), panel.getEmail());
 
-        actualPerson.updateTags();
+        panel.updateTags();
         assertEquals(expectedPerson.getTags().stream().map(tag -> tag.tagName).collect(Collectors.toList()),
-                actualPerson.getTags());
+                panel.getTags());
+    }
+
+    /**
+     * Asserts that panel displays nothing.
+     */
+    private void assertPanelDisplaysNothing(PersonDetailPanelHandle panel) {
+        assertEquals("", panel.getName());
+        assertEquals("", panel.getPhone());
+        assertEquals("", panel.getAddress());
+        assertEquals("", panel.getEmail());
+        panel.updateTags();
+        assertEquals(panel.getEmptyTagList(), panel.getTags());
     }
 }
