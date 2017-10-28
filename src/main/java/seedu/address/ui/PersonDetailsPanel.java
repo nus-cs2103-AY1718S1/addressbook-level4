61e5b9c6b94fa35dc3bc2025ba3ae507dc8094a4
dalessr <dale.sunshengran@gmail.com>
2017-10-28 11:01:58 +0800

    Add event list to person details panel

--- src/main/java/seedu/address/ui/PersonDetailsPanel.java
+++ src/main/java/seedu/address/ui/PersonDetailsPanel.java
@@ -4,15 +4,19 @@ import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.ClearPersonListEvent;
 import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+import seedu.address.model.event.Event;
 import seedu.address.model.person.ReadOnlyPerson;
 
 /**
@@ -43,6 +47,9 @@ public class PersonDetailsPanel extends UiPart<Region> {
     @FXML
     private Label addressLabel;
 
+    @FXML
+    private VBox eventsBox;
+
     public PersonDetailsPanel(ObservableList<ReadOnlyPerson> personList) {
         super(FXML);
         this.personList = personList;
@@ -72,6 +79,19 @@ public class PersonDetailsPanel extends UiPart<Region> {
         phoneLabel.setText(person.getPhone().toString());
         emailLabel.setText(person.getEmail().toString());
         addressLabel.setText(person.getAddress().toString());
+
+        eventsBox.getChildren().clear();
+        Label title = new Label();
+        title.setText("Events:");
+        title.setStyle("-fx-font-size: 32px");
+        eventsBox.getChildren().add(title);
+        ObservableList<String> events = FXCollections.observableArrayList();
+        for (Event e: person.getEvents()) {
+            Label label = new Label();
+            label.setText(e.getEventName().fullName + " -- " + e.getEventTime().toString());
+            label.setStyle("-fx-font-size: 20px");
+            eventsBox.getChildren().add(label);
+        }
     }
 
     @Subscribe
@@ -87,5 +107,18 @@ public class PersonDetailsPanel extends UiPart<Region> {
         phoneLabel.setText(person.getPhone().toString());
         emailLabel.setText(person.getEmail().toString());
         addressLabel.setText(person.getAddress().toString());
+
+        eventsBox.getChildren().clear();
+        Label title = new Label();
+        title.setText("Events:");
+        title.setStyle("-fx-font-size: 32px");
+        eventsBox.getChildren().add(title);
+        ObservableList<String> events = FXCollections.observableArrayList();
+        for (Event e: person.getEvents()) {
+            Label label = new Label();
+            label.setText(e.getEventName().fullName + " -- " + e.getEventTime().toString() + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            label.setStyle("-fx-font-size: 20px");
+            eventsBox.getChildren().add(label);
+        }
     }
 }
