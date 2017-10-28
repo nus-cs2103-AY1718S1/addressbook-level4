f5a688331d4db6997dd14a04bec15c1ec8c52d95
Eric Loh <ericlohyg@gmail.com>
2017-10-28 14:51:10 +0800

    Just in case i need this again

--- src/main/java/seedu/address/ui/CalendarView.java
+++ src/main/java/seedu/address/ui/CalendarView.java
@@ -6,7 +6,17 @@ import javafx.scene.layout.Region;
 
 import jfxtras.icalendarfx.VCalendar;
 import jfxtras.scene.control.agenda.icalendar.ICalendarAgenda;
+import seedu.address.model.person.Appointment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+\
 /**
  * The Browser Panel of the App.
  */
@@ -17,17 +27,40 @@ public class CalendarView extends UiPart<Region> {
     @FXML
     private BorderPane root;
 
+    private VCalendar vCalendar;
     public CalendarView() {
         super(FXML);
-        VCalendar vCalendar = new VCalendar();
+
+        CalendarView calendarView = new Ca
+        vCalendar = new VCalendar();
         ICalendarAgenda agenda = new ICalendarAgenda(vCalendar);
 
         root = new BorderPane();
         root.setCenter(agenda);
+
+        setAppointment(new Appointment("john", Calendar.getInstance()));
     }
 
     public BorderPane getRoot() {
         return this.root;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        final DateTimeFormatter DATE_FORMATTER = new DateTimeFormatter("yyyy-MM-dd'T'HH:mm:ss.SSS zzz");
+        LocalDate date = appointment.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        String publishMessage = "BEGIN:VCALENDAR" + System.lineSeparator()
+                + "METHOD:PUBLISH" + System.lineSeparator()
+                + "BEGIN:VEVENT" + System.lineSeparator()
+                + "ORGANIZER:mailto:a@example.com" + System.lineSeparator()
+                + "DTSTART;TZID=Asia/Singapore:"
+                + date.format() + System.lineSeparator()
+                + "DTEND;TZID=Asia/Singapore:20171027T220000Z" + System.lineSeparator()
+                + "SUMMARY:Friday meeting with Joe" + System.lineSeparator()
+                + "UID:0981234-1234234-23@example.com" + System.lineSeparator()
+                + "END:VEVENT" + System.lineSeparator()
+                + "END:VCALENDAR";
+        vCalendar.processITIPMessage(publishMessage);
+
     }
 
 }
