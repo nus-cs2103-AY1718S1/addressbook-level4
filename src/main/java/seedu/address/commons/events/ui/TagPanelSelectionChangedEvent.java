027bc9664f3fae98ad9aef81ac89a645ebc60334
Houjisan <hongjing10188@gmail.com>
2017-10-29 03:51:50 +0900

    Slight edits to class description

--- src/main/java/seedu/address/commons/events/ui/TagPanelSelectionChangedEvent.java
+++ src/main/java/seedu/address/commons/events/ui/TagPanelSelectionChangedEvent.java
@@ -4,7 +4,7 @@ import seedu.address.commons.events.BaseEvent;
 import seedu.address.ui.TagCard;
 
 /**
- * Represents a selection change in the Person List Panel
+ * Represents a selection change in the Tag List Panel
  */
 public class TagPanelSelectionChangedEvent extends BaseEvent {
 
