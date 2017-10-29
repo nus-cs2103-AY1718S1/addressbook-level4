59a587bb277a8302912e56a9c31b11a2759f7ad6
Chan Qing Hong <cqhchan@gmail.com>
2017-10-29 14:59:06 +0800

    Update LogoutCommand.java

--- src/main/java/seedu/address/logic/commands/LogoutCommand.java
+++ src/main/java/seedu/address/logic/commands/LogoutCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import seedu.address.MainApp;
 import seedu.address.ui.Ui;
 
+/**
+ * The LOgout Function
+ **/
 public class LogoutCommand extends Command {
 
     public static final String COMMAND_WORD = "logout";
