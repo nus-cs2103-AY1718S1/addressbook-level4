8d2b012df62c57299e36ec1f4e43f719772bd5fa
Jeremy Long Shi Wei <E0014824@u.nus.edu>
2017-10-30 02:19:08 +0800

    Check if user input is valid

--- src/main/java/seedu/address/logic/parser/ListByTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListByTagCommandParser.java
@@ -3,12 +3,14 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.List;
 
 import seedu.address.logic.commands.ListByTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 //@@author Jeremy
+
 /**
  * Parses input arguments and creates a new ListByTagCommand object
  */
@@ -22,15 +24,36 @@ public class ListByTagCommandParser implements Parser<ListByTagCommand> {
      */
     public ListByTagCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+
+        String[] tagKeyWords = trimmedArgs.split("\\s+");
+        List<String> evaluateList = Arrays.asList(tagKeyWords);
+
+        if (trimmedArgs.isEmpty() || invalidListTagArgs(evaluateList)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     ListByTagCommand.MESSAGE_USAGE));
         }
 
-        String[] tagKeyWords = trimmedArgs.split("\\s+");
 
-        return new ListByTagCommand(new TagContainsKeywordsPredicate(Arrays.asList(tagKeyWords)));
+        return new ListByTagCommand(new TagContainsKeywordsPredicate(evaluateList));
 
+    }
+
+    private boolean invalidListTagArgs(List<String> evaluateList) {
+        String previousString = "";
+        boolean multipleAndOrCluster = false;
+        boolean startWithAndOr;
+        boolean endWithAndOr;
+        for (String str : evaluateList) {
+            if (("and".equalsIgnoreCase(previousString) || "or".equalsIgnoreCase(previousString)) &&
+                    ("and".equalsIgnoreCase(str) || "or".equalsIgnoreCase(str))) {
+                multipleAndOrCluster = true;
+            }
+            previousString = str;
+        }
+        startWithAndOr = "and".equalsIgnoreCase(evaluateList.get(0)) || "or".equalsIgnoreCase(evaluateList.get(0));
+        endWithAndOr = "and".equalsIgnoreCase(evaluateList.get(evaluateList.size() - 1)) ||
+                "or".equalsIgnoreCase(evaluateList.get(evaluateList.size() - 1));
+        return multipleAndOrCluster || startWithAndOr || endWithAndOr;
     }
 
 }
