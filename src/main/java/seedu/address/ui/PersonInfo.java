7f34050c7d8ce029224eefe7db36690b36c9c72c
Eldon\eldonng <eldonng95@gmail.com>
2017-10-29 23:51:40 +0800

    set personInfo tags to be null to be consistent with others

--- src/main/java/seedu/address/ui/PersonInfo.java
+++ src/main/java/seedu/address/ui/PersonInfo.java
@@ -122,7 +122,7 @@ public class PersonInfo extends UiPart<Region> {
         if (colourMap.containsKey(tag.tagName)) {
             tagLabel.setStyle("-fx-background-color: " + colourMap.get(tag.tagName));
         } else {
-            tagLabel.setStyle("-fx-background-color: blue");
+            tagLabel.setStyle("-fx-background-color: null");
         }
     }
 
