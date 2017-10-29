bd9c3392a6f3776d68090a8661c8269960c0a96f
peiquan <peiquan@hiei.local>
2017-10-30 00:23:21 +0800

    add colour to taskcard depending on urgency

--- src/main/java/seedu/address/ui/TaskCard.java
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -3,8 +3,17 @@ package seedu.address.ui;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.task.ReadOnlyTask;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * An UI component that displays information of a {@code Task}.
@@ -12,8 +21,13 @@ import seedu.address.model.task.ReadOnlyTask;
 public class TaskCard extends UiPart<Region> {
 
     private static final String FXML = "TaskCard.fxml";
+    private static final int GREEN_RANGE = 7;
+    private static final int YELLOW_RANGE = 3;
 
     public final ReadOnlyTask task;
+
+    @FXML
+    private GridPane gridPane;
 
     @FXML
     private Label id;
@@ -32,6 +46,7 @@ public class TaskCard extends UiPart<Region> {
         this.task = task;
         id.setText("  " + displayedIndex + ". ");
         bindListeners(task);
+        setColour();
     }
 
     /**
@@ -42,6 +57,36 @@ public class TaskCard extends UiPart<Region> {
         description.textProperty().bind(Bindings.convert(task.descriptionProperty()));
         startDate.textProperty().bind(Bindings.convert(task.startDateProperty()));
         deadline.textProperty().bind(Bindings.convert(task.deadlineProperty()));
+    }
+
+    private void setColour() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String taskDate = "";
+        try {
+            Date deadline = ParserUtil.parseDate(task.getDeadline().date);
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            taskDate = dateFormat.format(deadline);
+        } catch (IllegalValueException e) {
+            e.printStackTrace();
+        }
+        LocalDate deaddate = LocalDate.parse(taskDate, formatter);
+        int range = deaddate.getDayOfYear() - date.getDayOfYear();
+        String bkgndColour;
+        if (range >= GREEN_RANGE) {
+            bkgndColour = "#00c300";
+        } else if (range >= YELLOW_RANGE) {
+            bkgndColour = "#d1d14f";
+        } else if (range == 0){
+            bkgndColour = "#ff444d";
+        } else {
+            bkgndColour = "#878787";
+        }
+        gridPane.setStyle("-fx-background-color: " + bkgndColour + ";"
+                + "-fx-border-style: solid inside;"
+                + "-fx-border-width: 2;"
+                + "-fx-border-height: 2;"
+                + "-fx-border-color: black;");
     }
 
     @Override
