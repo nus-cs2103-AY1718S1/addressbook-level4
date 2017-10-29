a5d4ac6f595beffca9c2c49b6d1a875c241a9ab6
itsdickson <itsklein@hotmail.com>
2017-10-29 09:46:55 +0800

    ResultDisplay.java: add style for text to error commands

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -7,10 +7,12 @@ import com.google.common.eventbus.Subscribe;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
 
 /**
@@ -18,6 +20,7 @@ import seedu.address.commons.events.ui.NewResultAvailableEvent;
  */
 public class ResultDisplay extends UiPart<Region> {
 
+    public static final String ERROR_STYLE_CLASS = "error";
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "ResultDisplay.fxml";
 
@@ -35,7 +38,33 @@ public class ResultDisplay extends UiPart<Region> {
     @Subscribe
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        if (event.message.equals(Messages.MESSAGE_UNKNOWN_COMMAND)
+                || event.message.contains(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ""))) {
+            setStyleToIndicateCommandFailure();
+        } else {
+            setStyleToDefault();
+        }
         Platform.runLater(() -> displayed.setValue(event.message));
+    }
+
+    /**
+     * Sets the result display style to use the default style.
+     */
+    private void setStyleToDefault() {
+        resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
+    }
+
+    /**
+     * Sets the result display style to indicate a failed command.
+     */
+    private void setStyleToIndicateCommandFailure() {
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if (styleClass.contains(ERROR_STYLE_CLASS)) {
+            return;
+        }
+
+        styleClass.add(ERROR_STYLE_CLASS);
     }
 
 }
