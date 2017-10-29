516f8f0b46f5f001ad531d8bf7fb76da45cae10e
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-29 13:02:15 +0800

    Add ImportXmlCommand class

---
+++ src/main/java/seedu/address/logic/commands/imports/ImportXmlCommand.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands.imports;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Imports data from a xml file (in BoNUS-specific format) to the application.
+ */
+public class ImportXmlCommand extends ImportCommand {
+    @Override
+    public CommandResult execute() throws CommandException {
+        return null;
+    }
+}
