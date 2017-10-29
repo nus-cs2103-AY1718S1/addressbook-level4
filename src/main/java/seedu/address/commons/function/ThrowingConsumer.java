2ae40418eb2d2f47450dd622d6c7644c3af5690c
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-10-29 08:57:12 +0800

    Create insurance-related exception and functional interface

---
+++ src/main/java/seedu/address/commons/function/ThrowingConsumer.java
@@ -0,0 +1,19 @@
+package seedu.address.commons.function;
+
+import java.util.function.Consumer;
+
+@FunctionalInterface
+public interface ThrowingConsumer<T> extends Consumer<T>{
+
+    @Override
+    default void accept(final T elem) {
+        try {
+            acceptThrows(elem);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void acceptThrows(T elem) throws Exception;
+
+}
