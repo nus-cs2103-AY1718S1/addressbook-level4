3934daef50d43bd452fbbe622f11137a7a4d2966
Kennard Ng <Kennard_ng@outlook.com>
2017-10-29 11:34:35 +0800

    add TabPaneHandle

--- src/test/java/guitests/guihandles/TabPaneHandle.java
+++ src/test/java/guitests/guihandles/TabPaneHandle.java
@@ -2,28 +2,30 @@ package guitests.guihandles;
 
 import static seedu.address.ui.ParcelListPanel.INDEX_FIRST_TAB;
 
-import java.util.List;
-import java.util.Optional;
-
-import javafx.scene.control.ListView;
 import javafx.scene.control.TabPane;
-import seedu.address.model.parcel.ReadOnlyParcel;
-import seedu.address.ui.ParcelCard;
 
 /**
  * Provides a handle for {@code ParcelListPanel} containing the list of {@code ParcelCard}.
  */
-public class TabPanelHandle extends NodeHandle<TabPane> {
+public class TabPaneHandle extends NodeHandle<TabPane> {
     public static final String TAB_PANE_ID = "#tabPanePlaceholder";
     private final ParcelListPanelHandle deliveredParcelListPanel;
     private final ParcelListPanelHandle undeliveredParcelListPanel;
 
-    public TabPanelHandle(TabPane tabPaneNode) {
+    public TabPaneHandle(TabPane tabPaneNode) {
         super(tabPaneNode);
         undeliveredParcelListPanel = new ParcelListPanelHandle(getChildNode(ParcelListPanelHandle
                 .UNDELIVERED_PARCEL_LIST_VIEW_ID));
         deliveredParcelListPanel = new ParcelListPanelHandle(getChildNode(ParcelListPanelHandle
                 .DELIVERED_PARCEL_LIST_VIEW_ID));
+    }
+
+    public TabHandle getTabHandle(int index) {
+        if (index == INDEX_FIRST_TAB.getZeroBased()) {
+            return new TabHandle(getChildNode(TabHandle.UNDELIVERED_PARCEL_TAB_ID));
+        } else {
+            return new TabHandle(getChildNode(TabHandle.DELIVERED_PARCEL_TAB_ID));
+        }
     }
 
     /**
@@ -41,17 +43,18 @@ public class TabPanelHandle extends NodeHandle<TabPane> {
         }
     }
 
+    public ParcelListPanelHandle getDeliveredParcelListPanel() {
+        return deliveredParcelListPanel;
+    }
+
+    public ParcelListPanelHandle getUndeliveredParcelListPanel() {
+        return undeliveredParcelListPanel;
+    }
+
     /**
      * Returns the index of the selected tab.
      */
     public int getSelectedTabIndex() {
          return getRootNode().getSelectionModel().getSelectedIndex();
-    }
-
-    /**
-     * Selects the {@code ParcelCard} at {@code index} in the list.
-     */
-    public void select(int index) {
-        getRootNode().getSelectionModel().select(index);
     }
 }
