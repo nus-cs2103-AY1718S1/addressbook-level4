4ba666e04ad4281f301073b5e37fbaa8e8295928
Hou Denghao <apple@ChuckHou-MacPro.local>
2017-10-30 13:36:17 +0800

    Update FindCommand

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -7,7 +7,7 @@ import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is not case sensitive.
  */
 public class FindCommand extends Command {
 
