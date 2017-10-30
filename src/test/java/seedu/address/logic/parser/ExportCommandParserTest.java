36d0c3c166e369f773b088ab99ca8a1e1c6d035d
Emil Koh <e0014667@u.nus.edu>
2017-10-30 14:47:38 +0800

    ExportParserTest

---
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.ExportCommand;
+
+public class ExportCommandParserTest {
+    private ExportCommandParser parser = new ExportCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsListCommand() {
+        assertParseSuccess(parser, "data\\circlesExportTest.xml", new ExportCommand("data\\circlesExportTest.xml"));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE));
+    }
+}
