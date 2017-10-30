3821329b1561d6297a403688a79f2f9537deb80d
Jelena <neohuilingjelena@gmail.com>
2017-10-30 23:25:28 +0800

    AddressBook.java: fix bug

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -290,9 +290,10 @@ public class AddressBook implements ReadOnlyAddressBook {
         Person editedPerson = new Person(target);
 
         try {
-            Debt newDebt = new Debt(target.getDebt().toNumber() + amount.toNumber());
-            editedPerson.setDebt(newDebt);
-            editedPerson.setTotalDebt(newDebt);
+            Debt newCurrDebt = new Debt(target.getDebt().toNumber() + amount.toNumber());
+            Debt newTotalDebt = new Debt(target.getTotalDebt().toNumber() + amount.toNumber());
+            editedPerson.setDebt(newCurrDebt);
+            editedPerson.setTotalDebt(newTotalDebt);
             persons.setPerson(target, editedPerson);
         } catch (DuplicatePersonException dpe) {
             assert false : "There should be no duplicate when updating the debt of a person";
