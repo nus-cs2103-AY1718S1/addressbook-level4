687f4fcccbcf5be42f5fe32ed859c5bfa1b431a2
Jacob Li Peng Cheng <JacobLI@197-94.priv23.nus.edu.sg>
2017-10-30 19:34:08 +0800

    Birthday.java: Stricter test case for inputs to ensure the correct birthday is entered

--- src/main/java/seedu/address/model/person/Birthday.java
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -6,13 +6,16 @@ import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
  * Represents a birthday field in the address book.
- * Guarantees: immutable; name is valid as declared in {@link #isValidBirthdayNumber(String)}
+ * Guarantees: immutable; name is valid as declared in {@link #isValidBirthdayFormat(String)}
  */
 public class Birthday {
 
-    public static final String MESSAGE_BIRTHDAY_CONSTRAINTS = "Birthdays should be in numeric, 6 numbers long "
-            + "in the format DDMMYY.";
-    public static final String BIRTHDAY_VALIDATION_REGEX = "[0-9]{4,8}";
+    public static final String MESSAGE_BIRTHDAY_CONSTRAINTS = "Birthdays should be numeric with 4 or 6 or 8 numbers "
+            + "long in the format DDMM or DDMMYY or DDMMYYYY.";
+    public static final String MESSAGE_INVALID_DAY_ENTERED = "The day should be within the range 1 to 31";
+    public static final String MESSAGE_INVALID_MONTH_ENTERED = "The month should be within the range 1 to 12";
+
+    public static final String BIRTHDAY_VALIDATION_REGEX = "^(\\d{4}|\\d{6}|\\d{8})";
 
     public static final String DEFAULT_BIRTHDAY = "No Birthday Added";
 
@@ -30,8 +33,12 @@ public class Birthday {
     public Birthday(String birthday) throws IllegalValueException {
         requireNonNull(birthday);
         String trimmedBirthday = birthday.trim();
-        if (!isValidBirthdayNumber(trimmedBirthday)) {
+        if (!isValidBirthdayFormat(trimmedBirthday)) {
             throw new IllegalValueException(MESSAGE_BIRTHDAY_CONSTRAINTS);
+        } else if (!isValidDayEntered(trimmedBirthday)) {
+            throw new IllegalValueException(MESSAGE_INVALID_DAY_ENTERED);
+        } else if (!isValidMonthEntered(trimmedBirthday)) {
+            throw new IllegalValueException(MESSAGE_INVALID_MONTH_ENTERED);
         }
         this.birthdayNumber = trimmedBirthday;
     }
@@ -44,9 +51,33 @@ public class Birthday {
     }
 
     /**
+     * Returns true if a given string has a valid birthday day number.
+     */
+    public static boolean isValidDayEntered(String test) {
+
+        if ((test.substring(0,2).compareTo("32") < 0 && test.substring(0,2).compareTo("00") > 0)
+                || test.equalsIgnoreCase(DEFAULT_BIRTHDAY)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if a given string has a valid birthday month number.
+     */
+    public static boolean isValidMonthEntered(String test) {
+
+        if ((test.substring(2,4).compareTo("13") < 0 && test.substring(2,4).compareTo("00") > 0)
+                || test.equalsIgnoreCase(DEFAULT_BIRTHDAY)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * Returns true if a given string is a valid birthday number.
      */
-    public static boolean isValidBirthdayNumber(String test) {
+    public static boolean isValidBirthdayFormat(String test) {
 
         if (test.matches(BIRTHDAY_VALIDATION_REGEX) || test.equalsIgnoreCase(DEFAULT_BIRTHDAY)) {
             return true;
