9d21d4c03a59fd411cc242cd5ba22fac39eac22d
marvinchin <chinkunsong.marvin@gmail.com>
2017-11-01 00:51:07 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -2,7 +2,6 @@ package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -158,7 +157,7 @@ public class AddressBook implements ReadOnlyAddressBook {
             throw new PersonNotFoundException();
         }
     }
-    
+
     //@@author keithsoc
     /**
      * Sets {@code personToFav} favorite field to true or false according to {@code type}.
