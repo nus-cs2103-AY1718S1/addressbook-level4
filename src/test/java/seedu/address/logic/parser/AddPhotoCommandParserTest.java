c02a6ce644783d902c2c8f6a3442071ae4b293d2
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-10-31 16:27:13 +0800

    checkstyle correction 2.

--- src/test/java/seedu/address/logic/parser/AddPhotoCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddPhotoCommandParserTest.java
@@ -1,21 +1,22 @@
 //@@author wishingmaid
 package seedu.address.logic.parser;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
+
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_FILEPATH;
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_FILEPATH;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+//import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.Test;
 
-import seedu.address.commons.core.index.Index;
+//import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddPhotoCommand;
-import seedu.address.model.person.Photo;
+//import seedu.address.model.person.Photo;
 
-    public class AddPhotoCommandParserTest {
-        private AddPhotoCommandParser parser = new AddPhotoCommandParser();
+public class AddPhotoCommandParserTest {
+    private AddPhotoCommandParser parser = new AddPhotoCommandParser();
 
         /*@Test
         public void parse_indexSpecified_failure() throws Exception {
@@ -23,7 +24,7 @@ import seedu.address.model.person.Photo;
             Index targetIndex = INDEX_FIRST_PERSON;
             String userInput = targetIndex.getOneBased() + " " + PREFIX_FILEPATH.toString();
             AddPhotoCommand expectedCommand = new AddPhotoCommand(INDEX_FIRST_PERSON, new Photo(""));
-            assertParseSuccess(parser, userInput, expectedCommand); 
+            assertParseSuccess(parser, userInput, expectedCommand);
             
             //Has a filepath that that is input from user
             Index newTargetIndex = INDEX_FIRST_PERSON;
@@ -36,13 +37,13 @@ import seedu.address.model.person.Photo;
 
 
         }*/
-       
-        @Test
-        public void parse_noFieldSpecified_failure() throws Exception {
-            String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPhotoCommand.MESSAGE_USAGE);
 
-            assertParseFailure(parser, AddPhotoCommand.COMMAND_WORD, expectedMessage);
-        }
+    @Test
+    public void parse_noFieldSpecified_failure() throws Exception {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPhotoCommand.MESSAGE_USAGE);
 
+        assertParseFailure(parser, AddPhotoCommand.COMMAND_WORD, expectedMessage);
     }
+
+}
 
