79f62b9e1475c5aed18d251867512d6cf520b197
Kyle Chu <ktnc.985@gmail.com>
2017-11-01 00:41:37 +0800

    add AutocompleteCommand Enum class to keep track of active command

---
+++ src/main/java/seedu/address/ui/AutocompleteCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.ui;
+
+public enum AutocompleteCommand {
+    ADD,
+    CLEAR,
+    DELETE,
+    DELETE_TAG,
+    EDIT,
+    EXIT,
+    FIND,
+    IMPORT,
+    HELP,
+    HISTORY,
+    LIST,
+    SELECT,
+    REDO,
+    UNDO,
+
+}
