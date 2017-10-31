00e738401912fb550a826c143b0eee53290af31b
Tao Jiashu <henrytjsh@gmail.com>
2017-10-31 20:25:06 +0800

    Revert "Add a confirmation stage in clear command"
    This reverts commit 891818cc240ec2f14a4e52fb1e7d7ea79b3bd50f.

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -9,31 +9,15 @@ import seedu.address.model.AddressBook;
  */
 public class ClearCommand extends UndoableCommand {
 
-    private boolean needToConfirm = true;
-
     public static final String COMMAND_WORDVAR_1 = "clear";
     public static final String COMMAND_WORDVAR_2 = "c";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-    public static final String MESSAGE_CONFIRMATION = "Type 'clear' again to confirm";
+
 
     @Override
     public CommandResult executeUndoableCommand() {
         requireNonNull(model);
-
-        if (needToConfirm) {
-            toggleConfirmationIndicator();
-            return new CommandResult(MESSAGE_CONFIRMATION);
-        } else {
-            toggleConfirmationIndicator();
-            model.resetData(new AddressBook());
-            return new CommandResult(MESSAGE_SUCCESS);
-        }
-    }
-
-    /**
-     * Flip the boolean value of needToConfirm
-     */
-    public void toggleConfirmationIndicator() {
-        needToConfirm = !needToConfirm;
+        model.resetData(new AddressBook());
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
