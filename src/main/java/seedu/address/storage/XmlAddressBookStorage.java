591d25d2d5fc36eef8a2c302d1db443cbcfbd360
lys <honestkiddy@gmail.com>
2017-11-01 02:59:20 +0800

    Checkstyle fixes

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -78,5 +78,5 @@ public class XmlAddressBookStorage implements AddressBookStorage {
     public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
         saveAddressBook(addressBook, filePath + "-backup.xml");
     }
-    //@@author 
+    //@@author
 }
