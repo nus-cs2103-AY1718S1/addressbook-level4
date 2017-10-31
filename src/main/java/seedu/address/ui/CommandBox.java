bed02270702431c7b684e06faf8a4c140c0101da
Rattanin Siripornpitak <e0008440@u.nus.edu>
2017-10-31 18:23:06 +0900

    First test with open pdf file on command

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,12 +1,18 @@
 package seedu.address.ui;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.logging.Logger;
 
+import javafx.application.HostServices;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
+import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
 import seedu.address.logic.ListElementPointer;
@@ -23,6 +29,7 @@ public class CommandBox extends UiPart<Region> {
 
     public static final String ERROR_STYLE_CLASS = "error";
     private static final String FXML = "CommandBox.fxml";
+    private static final String FILEPATH = "data/test.pdf";
 
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private final Logic logic;
@@ -102,6 +109,15 @@ public class CommandBox extends UiPart<Region> {
     @FXML
     private void handleCommandInputChanged() {
         try {
+            if (commandTextField.getText().equals("open")) {
+
+                    File file = new File(FILEPATH);
+                    Desktop.getDesktop().open(file);
+                    // HostServices hostServices = getRoot().getHostservices();
+                    // hostServices.showDocument(file.getAbsolutePath());
+
+
+            }
             CommandResult commandResult = logic.execute(commandTextField.getText());
             initHistory();
             historySnapshot.next();
@@ -124,6 +140,8 @@ public class CommandBox extends UiPart<Region> {
             setStyleToIndicateCommandFailure();
             logger.info("Invalid command: " + commandTextField.getText());
             raise(new NewResultAvailableEvent(e.getMessage(), true));
+        } catch ( IOException eee){
+            logger.info("Cannot open pdf file");
         }
     }
     //@@author
