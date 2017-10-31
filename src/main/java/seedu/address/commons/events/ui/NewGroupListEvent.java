9b3707061c2604095d5ecf6ba752efba8bfa7496
Eldon\eldonng <eldonng95@gmail.com>
2017-10-31 23:27:26 +0800

    fix a bug where group member size will not change when a person is deleted

---
+++ src/main/java/seedu/address/commons/events/ui/NewGroupListEvent.java
@@ -0,0 +1,34 @@
+package seedu.address.commons.events.ui;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.group.ReadOnlyGroup;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Indicates that the group list has been changed.
+ */
+public class NewGroupListEvent extends BaseEvent {
+
+    private ObservableList<ReadOnlyGroup> groups;
+    private ObservableList<ReadOnlyPerson> persons;
+
+    public NewGroupListEvent(ObservableList<ReadOnlyGroup> groups, ObservableList<ReadOnlyPerson> persons) {
+        this.groups = groups;
+        this.persons = persons;
+    }
+
+    public ObservableList<ReadOnlyPerson> getPersonsList() {
+        return persons;
+    }
+
+    public ObservableList<ReadOnlyGroup> getGroupsList() {
+        return groups;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+}
