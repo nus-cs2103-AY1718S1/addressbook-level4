b9c1587a5b70997781fda3dee0b924a8aff42831
Sky Ang Tian Lan <e0032176@u.nus.edu>
2017-10-31 11:41:04 +0800

    collate sort command

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -3,8 +3,10 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 
 /***
- * Sort list
+ * Sort list based on the current listing attribute
  */
+
+//@@author angtianlannus
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
