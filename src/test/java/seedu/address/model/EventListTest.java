28971001b557f991d9b2f78e994f722212d0a400
Junkai <reginleiff@gmail.com>
2017-10-31 23:41:35 +0800

    Test: Add test case for sublist method

--- src/test/java/seedu/address/model/EventListTest.java
+++ src/test/java/seedu/address/model/EventListTest.java
@@ -1,10 +1,20 @@
 package seedu.address.model;
 
+import static org.junit.Assert.assertEquals;
+import static seedu.address.testutil.TypicalEvents.ANNIVERSARY;
+import static seedu.address.testutil.TypicalEvents.BIRTHDAY;
+import static seedu.address.testutil.TypicalEvents.DEADLINE;
+import static seedu.address.testutil.TypicalEvents.EXAM;
+import static seedu.address.testutil.TypicalEvents.MOURN;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import javafx.collections.ObservableList;
 import seedu.address.model.event.EventList;
+import seedu.address.model.event.ReadOnlyEvent;
+import seedu.address.model.event.timeslot.Date;
 
 //@@author a0107442n
 
@@ -19,4 +29,29 @@ public class EventListTest {
         thrown.expect(UnsupportedOperationException.class);
         eventList.asObservableList().remove(0);
     }
+
+    //@@author A0162268B
+    @Test
+    public void getSubList_success() {
+        EventList eventList = new EventList();
+        EventList expectedSublist = new EventList();
+        try {
+            Date testDate = new Date("10/12/2017");
+
+            eventList.add(BIRTHDAY);
+            eventList.add(ANNIVERSARY);
+            eventList.add(EXAM);
+            eventList.add(MOURN);
+            eventList.add(DEADLINE);
+
+            expectedSublist.add(MOURN);
+            expectedSublist.add(DEADLINE);
+
+            ObservableList<ReadOnlyEvent> actualSublist = eventList.getObservableSubList(testDate);
+            assertEquals(actualSublist, expectedSublist.asObservableList());
+        } catch (Exception e) {
+
+        }
+    }
+    //@@author
 }
