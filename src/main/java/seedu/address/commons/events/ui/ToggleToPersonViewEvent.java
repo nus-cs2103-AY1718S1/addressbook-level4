b4bf35f9a0132ad65bf1745b58e69280a04a64b8
ALPHEUS\Alpheus Lim <e0031928@u.nus.edu>
2017-10-31 21:44:54 +0800

    changed comment for toggle event

--- src/main/java/seedu/address/commons/events/ui/ToggleToPersonViewEvent.java
+++ src/main/java/seedu/address/commons/events/ui/ToggleToPersonViewEvent.java
@@ -5,7 +5,7 @@ import seedu.address.commons.events.BaseEvent;
 //@@author Alim95
 
 /**
- * An event requesting to toggle the viewo to PersonPanel.
+ * An event requesting to toggle the view to PersonPanel.
  */
 public class ToggleToPersonViewEvent extends BaseEvent {
 
--- src/main/java/seedu/address/commons/events/ui/ToggleToTaskViewEvent.java
+++ src/main/java/seedu/address/commons/events/ui/ToggleToTaskViewEvent.java
@@ -5,7 +5,7 @@ import seedu.address.commons.events.BaseEvent;
 //@@author Alim95
 
 /**
- * An event requesting to toggle the viewo to TaskPanel.
+ * An event requesting to toggle the view to TaskPanel.
  */
 public class ToggleToTaskViewEvent extends BaseEvent {
 
