d8e2504a7edfe7143e7bd3958a8326fd8c704638
Low Weng Fei <lowwengfei@hotmail.com>
2017-10-31 13:27:36 +0800

    Update ImportCommandParser to return a ImportXmlCommand whenever appropriate

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.imports.ImportCommand;
 import seedu.address.logic.commands.imports.ImportCommand.ImportType;
+import seedu.address.logic.commands.imports.ImportXmlCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -75,8 +76,11 @@ public class ImportCommandParser implements Parser<ImportCommand> {
         }
     }
 
+    /**
+     * File path is being checked in {@code XmlAddressBookStorage#validateFilePath(String)}, similar to {@code ExportCommand}.
+     */
     private ImportCommand checkXmlImport(String path) {
-        return null;
+        return new ImportXmlCommand(path);
     }
 
     private ImportCommand checkScriptImport(String path) {
