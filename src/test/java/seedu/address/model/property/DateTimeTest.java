cd2424fac6837ee84a9c54fb022370b4a94e169f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-10-31 22:16:58 +0800

    Add test case for DateTime property constructors

--- src/test/java/seedu/address/model/property/DateTimeTest.java
+++ src/test/java/seedu/address/model/property/DateTimeTest.java
@@ -1,7 +1,12 @@
 package seedu.address.model.property;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_EVENT1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,6 +29,21 @@ public class DateTimeTest {
         assertTrue(DateTime.isValidTime("25122015 08:30"));
         assertTrue(DateTime.isValidTime("14122016 13:30"));
         assertTrue(DateTime.isValidTime("09121924 23:30"));
+    }
 
+    @Test
+    public void create_viaString_checkCorrectness() throws Exception {
+        DateTime dateTime = new DateTime(VALID_DATE_EVENT1);
+        assertEquals(VALID_DATE_EVENT1, dateTime.getValue());
+    }
+
+    @Test
+    public void create_viaDateObject_checkCorrectness() throws Exception {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("ddMMyyyy HH:mm");
+        Date date = dateFormatter.parse(VALID_DATE_EVENT1);
+
+        // Create a Datetime property via alternative constructor.
+        DateTime dateTime = new DateTime(date);
+        assertEquals(VALID_DATE_EVENT1, dateTime.getValue());
     }
 }
