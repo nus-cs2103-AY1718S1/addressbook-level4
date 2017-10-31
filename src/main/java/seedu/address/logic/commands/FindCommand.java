b2f04c64772100deabcfdbfd67e1bd495d51181b
JiangLingshuo <jianglingshuo0526@gmail.com>
2017-11-01 02:25:04 +0800

    add find address and phone number functionality

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.commands;
 
+import java.util.function.Predicate;
+
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.AddressContainsKeywordsPredicate;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -11,14 +14,19 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
     public static final String COMMAND_ALT = "f";
 
+    private static final String PREFIX_NAME_FIELD = "n/";
+    private static final String PREFIX_PHONE_FIELD = "p/";
+    private static final String PREFIX_EMAIL_FIELD = "e/";
+    private static final String PREFIX_ADDRESS_FIELD = "a/";
+
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final Predicate predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindCommand(Predicate predicate) {
         this.predicate = predicate;
     }
 
