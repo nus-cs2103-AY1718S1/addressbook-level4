611bede6487fce03fb724870a9b4382bbceded57
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-11-01 15:21:00 +0800

    Refactor ModuleInfoTest and add acknowledgement to NUSMods

--- src/test/java/seedu/address/logic/commands/imports/ModuleInfoTest.java
+++ src/test/java/seedu/address/logic/commands/imports/ModuleInfoTest.java
@@ -7,26 +7,44 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULE_INFO_JSO
 import java.net.URL;
 import java.util.Date;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.property.DateTime;
 
 public class ModuleInfoTest {
+    private static ModuleInfo info;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        info = JsonUtil.fromJsonUrl(new URL(VALID_MODULE_INFO_JSON_URL), ModuleInfo.class);
+        assertNotNull(info);
+    }
+
     @Test
     public void createModuleInfo_fromJsonUrl_checkCorrectness() throws Exception {
-        ModuleInfo info = JsonUtil.fromJsonUrl(new URL(VALID_MODULE_INFO_JSON_URL), ModuleInfo.class);
-
-        assertNotNull(info);
         assertEquals("CS1101S", info.getModuleCode());
 
         Date expectedDate = DateTime.formatDateTime("29112017 17:00");
         assertEquals(expectedDate, info.getExamDate());
 
+        String expected = "Module Code: CS1101S\n"
+                + "Module Title: Programming Methodology\n"
+                + "Module Credit: 5\n"
+                + "Examination Date: Wed Nov 29 17:00:00 SGT 2017";
+        assertEquals(expected, info.toString());
+    }
+
+    @Test
+    public void equals_returnTrue_checkCorrectness() throws Exception {
         ModuleInfo another = JsonUtil.fromJsonUrl(new URL(VALID_MODULE_INFO_JSON_URL), ModuleInfo.class);
         assertEquals(info, info);
         assertEquals(info, another);
+    }
 
+    @Test
+    public void toString_checkCorrectness() throws Exception {
         String expected = "Module Code: CS1101S\n"
                 + "Module Title: Programming Methodology\n"
                 + "Module Credit: 5\n"
