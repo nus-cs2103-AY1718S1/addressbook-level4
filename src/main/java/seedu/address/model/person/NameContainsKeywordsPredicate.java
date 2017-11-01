a044fcaa05838de62a36eba3a0fce9c17018e94a
jiasheng <jiashengan@gmail.com>
2017-11-02 01:32:25 +0800

    Update nameContainsKeywordpredicate to search for phone, address tag and weblink as well. Name should be changed to something more appropriate later

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -7,6 +7,7 @@ import seedu.address.commons.util.StringUtil;
 
 /**
  * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ * TODO: currently, NameContainsKeywordsPredicate will search under address, name and phone. It will be expanded.
  */
 public class NameContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
     private final List<String> keywords;
@@ -15,11 +16,42 @@ public class NameContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> 
         this.keywords = keywords;
     }
 
+    //@@Jiasheng
     @Override
     public boolean test(ReadOnlyPerson person) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+        if(containsKeyWordInName(person) || containsKeyWordInAddress(person)
+                || containsKeyWordInPhone(person) || containsKeyWordInTag(person)
+                || containsKeyWordInWebLink(person)){
+            return true;
+        }else{
+            return false;
+        }
     }
+    //@@author jiasheng
+    private boolean containsKeyWordInName(ReadOnlyPerson person) {
+        return keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+    }
+    //@@author jiasheng
+    private boolean containsKeyWordInPhone(ReadOnlyPerson person) {
+        return keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword));
+    }
+    //@@author jiasheng
+    private boolean containsKeyWordInAddress(ReadOnlyPerson person) {
+        return keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword));
+    }
+
+    //@@author jiasheng
+    private boolean containsKeyWordInTag(ReadOnlyPerson person) {
+        return person.getTags().stream().anyMatch(s -> keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(s.toStringFilter(), keyword)));
+    }
+
+    //@@author jiasheng
+    private boolean containsKeyWordInWebLink(ReadOnlyPerson person) {
+        return person.getWebLinks().stream().anyMatch(s -> keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(s.toStringWebLink(), keyword)));
+    }
+
 
     @Override
     public boolean equals(Object other) {
