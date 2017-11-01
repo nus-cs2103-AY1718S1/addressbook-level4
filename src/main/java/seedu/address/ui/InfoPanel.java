a4db1bfc18bcb77381864f501c3a3d16870e94eb
jaivigneshvenugopal <smugs1995@gmail.com>
2017-11-02 01:54:30 +0800

    Checkstyle correction

--- src/main/java/seedu/address/ui/InfoPanel.java
+++ src/main/java/seedu/address/ui/InfoPanel.java
@@ -9,7 +9,6 @@ import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
