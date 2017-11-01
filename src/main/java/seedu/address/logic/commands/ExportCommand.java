fc34ba9eab6fc2f80dee1545972eca1e5b509161
Jackie Tan <free-soup@hotmail.com>
2017-11-01 19:01:06 +0800

    Allow export of xml files

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,15 +1,13 @@
 package seedu.address.logic.commands;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Set;
 
-import javafx.collections.ObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.tag.Tag;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.storage.VcfExport;
+import seedu.address.storage.XmlFileStorage;
+import seedu.address.storage.XmlSerializableAddressBook;
 
 //@@author freesoup
 /**
@@ -20,47 +18,34 @@ public class ExportCommand extends Command {
     public static final String COMMAND_USAGE = COMMAND_WORD;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports contacts into a .vcf file.";
+    public static final String MESSAGE_WRONG_FILE_TYPE = "Export only exports .vcf and .xml file.";
 
     public static final String MESSAGE_SUCCESS = "Successfully exported contacts.";
+    public final String filePath;
+
+    public ExportCommand(String path) {
+        this.filePath = path;
+    }
 
     @Override
     public CommandResult execute() throws CommandException {
-        File export = new File(System.getProperty("user.home"), "Desktop/export.vcf");
-        BufferedWriter bw = null;
-        try {
-            bw = new BufferedWriter(new FileWriter(export));
-            ObservableList<ReadOnlyPerson> list = model.getFilteredPersonList();
-
-            for (int i = 0; i < list.size(); i++) {
-                ReadOnlyPerson person = list.get(i);
-                Set<Tag> tagList = person.getTags();
-                bw.write("BEGIN:VCARD\n");
-                bw.write("VERSON:2.1\n");
-                bw.write("FN:" + person.getName() + "\n");
-                bw.write("EMAIL:" + person.getEmail() + "\n");
-                bw.write("TEL:" + person.getPhone() + "\n");
-                bw.write("ADR:" + person.getAddress() + "\n");
-                if (!person.getRemark().isEmpty()) {
-                    bw.write("RM:" + person.getRemark() + "\n");
-                }
-                for (Tag tag : tagList) {
-                    bw.write("TAG:" + tag.getTagName() + "\n");
-                }
-                bw.write("END:VCARD\n");
-            }
-        } catch (IOException ioe) {
-            throw new CommandException("Problem writing to file");
-        } finally {
+        File export = new File(filePath);
+        ReadOnlyAddressBook addressBook = model.getAddressBook();
+        if (export.getName().endsWith(".xml")) {
+            XmlSerializableAddressBook xmlAddressBook = new XmlSerializableAddressBook(addressBook);
             try {
-                if (bw != null) {
-                    bw.close();
-                }
+                export.createNewFile();
+                XmlFileStorage.saveDataToFile(export, xmlAddressBook);
             } catch (IOException ioe) {
-                throw new CommandException("Problem closing file");
+                throw new CommandException("File not found.");
+            }
+        } else if (export.getName().endsWith(".vcf")) {
+            try {
+                VcfExport.saveDataToFile(export, addressBook.getPersonList());
+            } catch (IOException ioe) {
+                throw new CommandException("File not found.");
             }
         }
-
-
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
