c8af83a952d20eef01e0fa5baf5814f09f0ade88
Chan Qing Hong <cqhchan@gmail.com>
2017-11-02 00:00:03 +0800

    Update LoginCommand.java

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -1,14 +1,14 @@
 //@@author cqhchan
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+
 import java.util.logging.Logger;
 
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.ui.Ui;
-
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
 
 //@@author cqhchan
 /**
@@ -18,13 +18,18 @@ public class LoginCommand extends Command {
 
 
     public static final String COMMAND_WORD = "login";
+    
     private static String MESSAGE_LOGIN_ACKNOWLEDGEMENT;
+    
     public static String MESSAGE_SUCCESS = "Login Successful";
-    public static String MESSAGE_FAILURE = "Username or Password Incorrect";
+    
+    public static String MESSAGE_FAILURE = "Username or Password Incorrect"
+        ;
     public static String MESSAGE_USAGE = COMMAND_WORD + ": Login to private Database. "
             + "Parameters: "
             + PREFIX_USERNAME + "USERNAME "
             + PREFIX_PASSWORD + "PASSWORD";
+    
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
 
