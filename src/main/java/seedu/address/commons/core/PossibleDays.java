ce1b5b4dadfc5454314f66a2fe3503775ea1f1e3
Yuchen He <yuchenhe@r-63-103-25-172.comp.nus.edu.sg>
2017-11-01 12:15:32 +0800

    possible days

--- src/main/java/seedu/address/commons/core/PossibleDays.java
+++ src/main/java/seedu/address/commons/core/PossibleDays.java
@@ -1,4 +1,9 @@
 package seedu.address.commons.core;
 
 public class PossibleDays {
+
+    public final static int[] days = {
+           1, 2, 3, 4, 5, 6, 7
+    };
+    public final static int dayCoefficient = 10000;
 }
