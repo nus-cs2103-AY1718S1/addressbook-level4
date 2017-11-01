9e9eabee26d759c51741a5a94950df811fb7d198
Jiachao Lin <lin_jiachao@hotmail.com>
2017-11-01 17:18:07 +0800

    Add FilteredListChangedEvent and handlers
    Add FilteredListChangedEvent
    Add method to load the statistics into the Statistics Panel
    Add a subscribe method to handle the event
    Add fxid in StatisticsPanel.fxml

--- src/main/java/seedu/address/ui/StatisticsPanel.java
+++ src/main/java/seedu/address/ui/StatisticsPanel.java
@@ -2,12 +2,15 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.FilteredListChangedEvent;
 import seedu.address.logic.statistics.Statistics;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -42,7 +45,31 @@ public class StatisticsPanel extends UiPart<Region> {
 
     public StatisticsPanel(ObservableList<ReadOnlyPerson> currentList) {
         super(FXML);
+        registerAsAnEventHandler(this);
         statistics = new Statistics(currentList);
+
+    }
+
+    /**
+     * Updates list Statistics in the Statistics panel
+     */
+    protected void loadListStatistics() {
+        mean.setText(Statistics.round(statistics.getMean(),2));
+        median.setText(Statistics.round(statistics.getMedian(),2));
+        mode.setText(Statistics.round(statistics.getMode(),2));
+        variance.setText(Statistics.round(statistics.getVariance(),2));
+        standardDeviation.setText(Statistics.round(statistics.getStdDev(),2));
+        quartile1.setText(Statistics.round(statistics.getQuartile1(),2));
+        quartile3.setText(Statistics.round(statistics.getQuartile3(),2));
+        interquartileRange.setText(Statistics.round(statistics.getInterQuartileRange(),2));
+    }
+
+    @FXML
+    @Subscribe
+    private void handleFilteredListChangedEvent(FilteredListChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        statistics.initScore(event.getCurrentFilteredList()); // Update currentList data
+        loadListStatistics();
     }
 
 }
