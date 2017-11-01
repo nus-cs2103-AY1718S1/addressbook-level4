adb3866acd5783c6902f0800d3d043a53d213e92
denna <dennaloh@gmail.com>
2017-11-01 21:10:43 +0800

    styling issue

--- src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
@@ -77,7 +77,8 @@ public class FindTagCommandTest {
      *     - the {@code FilteredList<ReadOnlyPerson>} is equal to {@code expectedList}<br>
      *     - the {@code AddressBook} in model remains the same after executing the {@code command}
      */
-    private void assertCommandSuccess(FindTagCommand command, String expectedMessage, List<ReadOnlyPerson> expectedList) {
+    private void assertCommandSuccess(FindTagCommand command, String expectedMessage,
+                                      List<ReadOnlyPerson> expectedList) {
         AddressBook expectedAddressBook = new AddressBook(model.getAddressBook());
         CommandResult commandResult = command.execute();
 
