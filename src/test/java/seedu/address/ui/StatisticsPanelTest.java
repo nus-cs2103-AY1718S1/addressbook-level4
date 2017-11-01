b908d2b99745f87dd0309387189416b2640b08fc
Jiachao Lin <lin_jiachao@hotmail.com>
2017-11-02 02:53:04 +0800

    Remove wrong test cases and rearrange Asserts

--- src/test/java/seedu/address/ui/StatisticsPanelTest.java
+++ src/test/java/seedu/address/ui/StatisticsPanelTest.java
@@ -1,10 +1,8 @@
 package seedu.address.ui;
 
-import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.getTypicalPersons;
-
-import java.util.Arrays;
+import static seedu.address.ui.testutil.GuiTestAssert.assertPanelDisplaysStatistics;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,9 +11,7 @@ import guitests.guihandles.StatisticsPanelHandle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.commons.events.ui.FilteredListChangedEvent;
-import seedu.address.logic.statistics.Statistics;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.TagsContainsKeywordsPredicate;
 
 public class StatisticsPanelTest extends GuiUnitTest {
 
@@ -38,31 +34,11 @@ public class StatisticsPanelTest extends GuiUnitTest {
 
     @Test
     public void display() throws Exception {
-
-        // findtags friends
-        ObservableList<ReadOnlyPerson> actualList =
-                TYPICAL_PERSONS.filtered(new TagsContainsKeywordsPredicate(Arrays.asList("friends")));
-        postNow(new FilteredListChangedEvent(actualList));
-        assertStatisticsPanelDisplay(actualList, statisticsPanelHandle);
-    }
-
-    //======================== Helper methods ===============================
-
-    /**
-     * Asserts that {@code statisticsPanel} displays the statistics correctly
-     */
-    private void assertStatisticsPanelDisplay(ObservableList<ReadOnlyPerson> actualList,
-                                              StatisticsPanelHandle statisticsPanelHandle) {
-        Statistics expectedStatistics = new Statistics(actualList);
         guiRobot.pauseForHuman();
-        assertEquals(expectedStatistics.getMeanString(), statisticsPanelHandle.getMeanLabel());
-        assertEquals(expectedStatistics.getMedianString(), statisticsPanelHandle.getMedianLabel());
-        assertEquals(expectedStatistics.getModeString(), statisticsPanelHandle.getModeLabel());
-        assertEquals(expectedStatistics.getVarianceString(), statisticsPanelHandle.getVarianceLabel());
-        assertEquals(expectedStatistics.getStdDevString(), statisticsPanelHandle.getStandardDeviationLabel());
-        assertEquals(expectedStatistics.getQuartile3String(), statisticsPanelHandle.getQuartile3Label());
-        assertEquals(expectedStatistics.getQuartile1String(), statisticsPanelHandle.getQuartile1Label());
-        assertEquals(expectedStatistics.getInterquartileRangeString(), statisticsPanelHandle.getInterquartileLabel());
+        postNow(new FilteredListChangedEvent(TYPICAL_PERSONS));
+        assertPanelDisplaysStatistics(TYPICAL_PERSONS, statisticsPanelHandle);
     }
+
+    //TODO: Add GUI tests for all commands
 
 }
