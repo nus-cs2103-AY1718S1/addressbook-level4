e8a54e3daea072d53a8ccd2351aab975b2b4f62c
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-11-02 14:23:06 +0800

    Fix setter methods

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -52,11 +52,11 @@ public class UserPrefs {
     }
 
     public void setDefaultUsername(String username) {
-        this.username = "admin";
+        this.username = username;
     }
 
     public void setDefaultPassword(String password) {
-        this.password = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+        this.password = password;
     }
 
     @Override
