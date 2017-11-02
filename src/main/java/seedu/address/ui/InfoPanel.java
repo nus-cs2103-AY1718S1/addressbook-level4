50daf09b6104954358e6c6b578f9b2084cf2cd52
Jelena <neohuilingjelena@gmail.com>
2017-11-02 23:11:43 +0800

    InfoPanel.java: fix bug

--- src/main/java/seedu/address/ui/InfoPanel.java
+++ src/main/java/seedu/address/ui/InfoPanel.java
@@ -165,6 +165,7 @@ public class InfoPanel extends UiPart<Region> {
      */
     private void resetDebtRepaymentProgressBar(ReadOnlyPerson person) {
         debtRepaymentProgressBar = new DebtRepaymentProgressBar(person);
+        progressBarPlaceholder.getChildren().clear();
         progressBarPlaceholder.getChildren().add(debtRepaymentProgressBar.getRoot());
     }
     //@@author
