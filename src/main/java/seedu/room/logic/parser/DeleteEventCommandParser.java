0e340db8b7afc8bf9877e8f987ecac5a41fe159f
Suyash Shekhar <suyashshekhar@gmail.com>
2017-11-03 05:33:05 +0800

    Add DeleteEvent command

---
+++ src/main/java/seedu/room/logic/parser/DeleteEventCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.room.logic.parser;
+
+import seedu.room.commons.core.index.Index;
+import seedu.room.commons.exceptions.IllegalValueException;
+import seedu.room.logic.commands.DeleteEventCommand;
+import seedu.room.logic.parser.exceptions.ParseException;
+
+import static seedu.room.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+//@@author sushinoya
+/**
+ * Parses input arguments and creates a new DeleteEventCommand object
+ */
+public class DeleteEventCommandParser implements Parser<DeleteEventCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteEventCommand
+     * and returns an DeleteEventCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public DeleteEventCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new DeleteEventCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE));
+        }
+    }
+
+}
