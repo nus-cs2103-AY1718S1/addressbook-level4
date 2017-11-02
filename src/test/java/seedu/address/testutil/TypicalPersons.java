59af2a4849719782bf5f4da7d0740ad8c5c1a582
Lau Yung <lauy1997@gmail.com>
2017-11-02 11:30:43 +0800

    Fix addCommandSystemTest

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -62,10 +62,11 @@ public class TypicalPersons {
 
         // Manually added - Person's details found in {@code CommandTestUtil}
         AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY)
-                .withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).withGroup(DEFAULT_GROUP).build();
+                .withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).withGroup(DEFAULT_GROUP).withExpiryDate("")
+                .build();
         BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
                 .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
-                .withGroup(DEFAULT_GROUP).build();
+                .withGroup(DEFAULT_GROUP).withExpiryDate("").build();
     }
 
     private TypicalPersons() {} // prevents instantiation
