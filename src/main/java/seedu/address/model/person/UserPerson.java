6dd5a7269902dba820ff57494b0f52fe9718749b
Han Siang Phua <hansiang93@gmail.com>
2017-11-03 00:37:48 +0800

    Fixed init error

--- src/main/java/seedu/address/model/person/UserPerson.java
+++ src/main/java/seedu/address/model/person/UserPerson.java
@@ -124,7 +124,9 @@ public class UserPerson implements ReadOnlyPerson {
             builder.append(webLink.toStringWebLink());
             builder.append(", ");
         }
-        builder.delete(builder.length() - 2, builder.length());
+        if (builder.length() != 0) {
+            builder.delete(builder.length() - 2, builder.length());
+        }
         return builder.toString();
     }
 
