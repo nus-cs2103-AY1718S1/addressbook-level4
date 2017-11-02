0baab2149f03fa0ff28bb5f8780980cf4da8e595
Suyash Shekhar <suyashshekhar@gmail.com>
2017-11-03 05:38:41 +0800

    Modify storage to implement EventBook

--- src/main/java/seedu/room/storage/ResidentBookStorage.java
+++ src/main/java/seedu/room/storage/ResidentBookStorage.java
@@ -18,7 +18,7 @@ public interface ResidentBookStorage {
 
     /**
      * Returns ResidentBook data as a {@link ReadOnlyResidentBook}.
-     *   Returns {@code Optional.empty()} if storage file is not found.
+     * Returns {@code Optional.empty()} if storage file is not found.
      * @throws DataConversionException if the data in storage is not in the expected format.
      * @throws IOException if there was any problem when reading from the storage.
      */
--- src/main/java/seedu/room/storage/XmlFileStorage.java
+++ src/main/java/seedu/room/storage/XmlFileStorage.java
@@ -12,6 +12,9 @@ import seedu.room.commons.util.XmlUtil;
  * Stores residentbook data in an XML file
  */
 public class XmlFileStorage {
+
+    // Persons Storage Functions
+
     /**
      * Saves the given residentbook data to the specified file.
      */
@@ -31,6 +34,33 @@ public class XmlFileStorage {
                                                                             FileNotFoundException {
         try {
             return XmlUtil.getDataFromFile(file, XmlSerializableResidentBook.class);
+        } catch (JAXBException e) {
+            throw new DataConversionException(e);
+        }
+    }
+
+    // Events Storage Functions
+
+    /**
+     * Saves the given eventbook data to the specified file.
+     */
+    public static void saveDataToFile(File file, XmlSerializableEventBook eventBook)
+            throws FileNotFoundException {
+        try {
+            XmlUtil.saveDataToFile(file, eventBook);
+        } catch (JAXBException e) {
+            assert false : "Unexpected exception " + e.getMessage();
+        }
+    }
+
+
+    /**
+     * Returns eventBook in the file or an empty eventBook
+     */
+    public static XmlSerializableEventBook loadEventDataFromSaveFile(File file) throws DataConversionException,
+                                                                              FileNotFoundException {
+        try {
+            return XmlUtil.getDataFromFile(file, XmlSerializableEventBook.class);
         } catch (JAXBException e) {
             throw new DataConversionException(e);
         }
