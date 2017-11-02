8f51743f0624a3e39491099615f1afc0fc4293dd
Suyash Shekhar <suyashshekhar@gmail.com>
2017-11-03 06:56:09 +0800

    Add Sample Events Data

--- src/main/java/seedu/room/MainApp.java
+++ src/main/java/seedu/room/MainApp.java
@@ -20,12 +20,7 @@ import seedu.room.commons.util.ConfigUtil;
 import seedu.room.commons.util.StringUtil;
 import seedu.room.logic.Logic;
 import seedu.room.logic.LogicManager;
-import seedu.room.model.EventBook;
-import seedu.room.model.Model;
-import seedu.room.model.ModelManager;
-import seedu.room.model.ReadOnlyResidentBook;
-import seedu.room.model.ResidentBook;
-import seedu.room.model.UserPrefs;
+import seedu.room.model.*;
 import seedu.room.model.util.SampleDataUtil;
 import seedu.room.storage.EventBookStorage;
 import seedu.room.storage.JsonUserPrefsStorage;
@@ -93,22 +88,34 @@ public class MainApp extends Application {
      */
     private Model initModelManager(Storage storage, UserPrefs userPrefs) {
         Optional<ReadOnlyResidentBook> residentBookOptional;
+        Optional<ReadOnlyEventBook> eventBookOptional;
         ReadOnlyResidentBook initialData;
+        ReadOnlyEventBook initialEventsData;
         try {
             residentBookOptional = storage.readResidentBook();
+            eventBookOptional = storage.readEventBook();
             if (!residentBookOptional.isPresent()) {
                 logger.info("Data file not found. Will be starting with a sample ResidentBook");
             }
+
+            if (!eventBookOptional.isPresent()) {
+                logger.info("Data file not found. Will be starting with a sample EventBook");
+            }
+
+            initialEventsData = eventBookOptional.orElseGet(SampleDataUtil::getSampleEventBook);
             initialData = residentBookOptional.orElseGet(SampleDataUtil::getSampleResidentBook);
+
         } catch (DataConversionException e) {
             logger.warning("Data file not in the correct format. Will be starting with an empty ResidentBook");
             initialData = new ResidentBook();
+            initialEventsData = new EventBook();
         } catch (IOException e) {
             logger.warning("Problem while reading from the file. Will be starting with an empty ResidentBook");
             initialData = new ResidentBook();
+            initialEventsData = new EventBook();
         }
 
-        return new ModelManager(initialData, new EventBook(), userPrefs);
+        return new ModelManager(initialData, initialEventsData, userPrefs);
     }
 
     private void initLogging(Config config) {
