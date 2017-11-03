ed076570eb5a5919216529b3682d4f115081e875
grantcm <grantcomiller@gmail.com>
2017-11-03 14:14:39 +0800

    Updated tests for added export/import commands

--- src/test/java/seedu/address/logic/AutoCompleteTest.java
+++ src/test/java/seedu/address/logic/AutoCompleteTest.java
@@ -24,7 +24,7 @@ public class AutoCompleteTest {
         assert commandTrie.attemptAutoComplete("a").equals("add");
         assert commandTrie.attemptAutoComplete("e").equals("e");
         assert commandTrie.attemptAutoComplete("ed").equals("edit");
-        assert commandTrie.attemptAutoComplete("ex").equals("exit");
+        assert commandTrie.attemptAutoComplete("exi").equals("exit");
         assert commandTrie.attemptAutoComplete("H").equals("H");
         assert commandTrie.attemptAutoComplete("He").equals("help");
         assert commandTrie.attemptAutoComplete("Hi").equals("history");
