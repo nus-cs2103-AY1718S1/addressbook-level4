cf69a120aee0beba12464f4332a1a1ad15f01fbf
Daniel Chew <cctdaniel@outlook.com>
2017-11-03 19:29:30 +0800

    Changed `swt` command to `theme` to follow the coding standard

---
+++ src/main/java/seedu/address/logic/commands/ThemeCommand.java
@@ -0,0 +1,64 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.model.ThemeUnit.THEME_DARK_UNIT;
+import static seedu.address.model.ThemeUnit.THEME_LIGHT_UNIT;
+import static seedu.address.model.ThemeUnit.getCurrentThemeUnit;
+import static seedu.address.model.ThemeUnit.setCurrentThemeUnit;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.SwitchThemeRequestEvent;
+import seedu.address.logic.commands.exceptions.CommandException;
+
+//@@author cctdaniel
+/**
+ * Switch between light and dark theme
+ */
+public class ThemeCommand extends Command {
+
+    public static final String COMMAND_WORD = "theme";
+    public static final String LIGHT_THEME = "light";
+    public static final String DARK_THEME = "dark";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Switch to light/dark theme. "
+            + "Parameters: light/dark\n"
+            + "Example: " + COMMAND_WORD + " " + LIGHT_THEME;
+
+    public static final String SWITCH_THEME_SUCCESS_MESSAGE = "Switched theme to ";
+    public static final String SWITCH_THEME_FAILURE_MESSAGE = "Please choose a different theme from the current one. ";
+
+
+    private boolean isLight = true;
+
+    public ThemeCommand(String theme) {
+        if (theme.equalsIgnoreCase(LIGHT_THEME)) {
+            isLight = true;
+        } else {
+            isLight = false;
+        }
+    }
+
+    @Override
+    public CommandResult execute() {
+        switch (getCurrentThemeUnit()) {
+        case THEME_LIGHT_UNIT:
+            if (isLight) {
+                return new CommandResult(SWITCH_THEME_FAILURE_MESSAGE);
+            } else {
+                setCurrentThemeUnit(THEME_DARK_UNIT);
+                EventsCenter.getInstance().post(new SwitchThemeRequestEvent(isLight));
+                return new CommandResult(SWITCH_THEME_SUCCESS_MESSAGE + DARK_THEME + ".");
+            }
+        case THEME_DARK_UNIT:
+            if (!isLight) {
+                return new CommandResult(SWITCH_THEME_FAILURE_MESSAGE);
+            } else {
+                setCurrentThemeUnit(THEME_LIGHT_UNIT);
+                EventsCenter.getInstance().post(new SwitchThemeRequestEvent(isLight));
+                return new CommandResult(SWITCH_THEME_SUCCESS_MESSAGE + LIGHT_THEME + ".");
+            }
+        default:
+            break;
+        }
+        return new CommandResult("");
+    }
+}
