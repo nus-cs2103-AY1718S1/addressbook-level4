d06d99e2fa6c8b82150dfafc09662fb14b7abad6
Ru Shan Khor <rushan.khor@gmail.com>
2017-11-03 18:55:23 +0800

    Add author tag

--- src/test/java/seedu/address/logic/parser/BatchCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/BatchCommandParserTest.java
@@ -12,13 +12,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.BatchCommand;
 import seedu.address.model.tag.Tag;
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
+//@@author rushan-khor
 public class BatchCommandParserTest {
 
     private BatchCommandParser parser = new BatchCommandParser();
