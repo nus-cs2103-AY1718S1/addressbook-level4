83288d93c6bcb648dd34eed52eb74e1a80f7baad
dalessr <dale.sunshengran@gmail.com>
2017-11-04 09:16:54 +0800

    Implement event classes

--- src/main/java/seedu/address/commons/events/ui/OpenNusModsWebViewEvent.java
+++ src/main/java/seedu/address/commons/events/ui/OpenNusModsWebViewEvent.java
@@ -1,4 +1,25 @@
 package seedu.address.commons.events.ui;
 
-public class OpenNUSModsWebViewEvent {
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.ui.PersonCard;
+
+/**
+ * Indicates a request for opening nusmods webview
+ */
+public class OpenNusModsWebViewEvent extends BaseEvent {
+
+    private final PersonCard newSelection;
+
+    public OpenNusModsWebViewEvent(PersonCard newSelection) {
+        this.newSelection = newSelection;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public PersonCard getNewSelection() {
+        return newSelection;
+    }
 }
