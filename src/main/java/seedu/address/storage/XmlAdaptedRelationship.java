44b6ca7fcb5c5152e12a7f8ec87fae9de0233823
Tan Zhen Yong <Xenonym@users.noreply.github.com>
2017-11-05 02:24:26 +0800

    Add confidence estimate and name to relationships in storage

--- src/main/java/seedu/address/storage/XmlAdaptedRelationship.java
+++ src/main/java/seedu/address/storage/XmlAdaptedRelationship.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.relationship.ConfidenceEstimate;
 import seedu.address.model.relationship.Relationship;
 import seedu.address.model.relationship.RelationshipDirection;
 import seedu.address.model.relationship.exceptions.DuplicateRelationshipException;
@@ -25,6 +28,12 @@ public class XmlAdaptedRelationship {
     @XmlElement(required = true)
     private boolean isDirected;
 
+    @XmlElement(required = true)
+    private double confidenceEstimate;
+
+    @XmlElement(required = true)
+    private String name;
+
     /**
      * Constructs an XmlAdaptedRelationship.
      * This is the no-arg constructor that is required by JAXB.
@@ -34,10 +43,13 @@ public class XmlAdaptedRelationship {
     /**
      * Converts a given Relationship attributes into this class for JAXB use.
      */
-    public XmlAdaptedRelationship(int fromIndex, int toIndex, RelationshipDirection direction) {
+    public XmlAdaptedRelationship(int fromIndex, int toIndex, RelationshipDirection direction,
+                                  ConfidenceEstimate confidenceEstimate, Name name) {
         this.fromIndex = fromIndex;
         this.toIndex = toIndex;
         this.isDirected = direction.isDirected();
+        this.confidenceEstimate = confidenceEstimate.value;
+        this.name = name.fullName;
     }
 
     /**
@@ -50,11 +62,13 @@ public class XmlAdaptedRelationship {
         Person toPerson = (Person) persons.get(toIndex);
 
         try {
-            fromPerson.addRelationship(new Relationship(fromPerson, toPerson, getRelationshipDirection()));
+            fromPerson.addRelationship(new Relationship(fromPerson, toPerson, getRelationshipDirection(),
+                    new Name(name), new ConfidenceEstimate(confidenceEstimate)));
         } catch (DuplicateRelationshipException dre) {
             throw new AssertionError("impossible to have duplicate relationships in storage", dre);
+        } catch (IllegalValueException ive) {
+            throw new AssertionError("impossible to have invalid values in storage", ive);
         }
-
     }
 
     private RelationshipDirection getRelationshipDirection() {
@@ -78,14 +92,25 @@ public class XmlAdaptedRelationship {
         if (toIndex != that.toIndex) {
             return false;
         }
-        return isDirected == that.isDirected;
+        if (isDirected != that.isDirected) {
+            return false;
+        }
+        if (Double.compare(that.confidenceEstimate, confidenceEstimate) != 0) {
+            return false;
+        }
+        return name.equals(that.name);
     }
 
     @Override
     public int hashCode() {
-        int result = fromIndex;
+        int result;
+        long temp;
+        result = fromIndex;
         result = 31 * result + toIndex;
         result = 31 * result + (isDirected ? 1 : 0);
+        temp = Double.doubleToLongBits(confidenceEstimate);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + name.hashCode();
         return result;
     }
 }
