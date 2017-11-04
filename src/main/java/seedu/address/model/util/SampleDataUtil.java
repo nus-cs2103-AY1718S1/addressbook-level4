d4d475daec8e5d78db8d4a29dba675670ae96c42
Jacob Li Peng Cheng <JacobLI@221-219.priv23.nus.edu.sg>
2017-11-04 09:14:33 +0800

    Change birthday style for sample data files in address book

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -25,22 +25,22 @@ public class SampleDataUtil {
             //@@author taojiashu
             return new Person[] {
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                    new Address("Blk 30 Geylang Street 29, #06-40"), new Favourite(), new Birthday("191010"),
+                    new Address("Blk 30 Geylang Street 29, #06-40"), new Favourite(), new Birthday("19/10/10"),
                     getTagSet("friends")),
                 new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Favourite(true),
                         new Birthday(), getTagSet("colleagues", "friends")),
                 new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Favourite(),
-                        new Birthday("191010"), getTagSet("neighbours")),
+                        new Birthday("19/10/10"), getTagSet("neighbours")),
                 new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                     new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Favourite(true),
                         new Birthday(), getTagSet("family")),
                 new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                     new Address("Blk 47 Tampines Street 20, #17-35"), new Favourite(true),
-                        new Birthday("170398"), getTagSet("classmates")),
+                        new Birthday("17/03/98"), getTagSet("classmates")),
                 new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"), new Favourite(), new Birthday("110696"),
+                    new Address("Blk 45 Aljunied Street 85, #11-31"), new Favourite(), new Birthday("11/06/96"),
                     getTagSet("colleagues"))
             };
         } catch (IllegalValueException e) {
