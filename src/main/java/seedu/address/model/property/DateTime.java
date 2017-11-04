3591136c9932cf7c47c4986cc89a80d0a0c7ccb2
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-11-04 14:39:51 +0800

    Fully support natual language parsing of date time in DateTime property

--- src/main/java/seedu/address/model/property/DateTime.java
+++ src/main/java/seedu/address/model/property/DateTime.java
@@ -3,6 +3,7 @@ package seedu.address.model.property;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.parser.util.NaturalLanguageUtil;
@@ -15,8 +16,11 @@ import seedu.address.model.property.exceptions.PropertyNotFoundException;
  */
 public class DateTime extends Property {
     private static final String PROPERTY_SHORT_NAME = "dt";
-    // Change the regular expression in PropertyManager whenever you change this.
+
+    // The standard format for storing a date time in string format.
     private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("ddMMyyyy HH:mm");
+    private static final String STANDARD_FORMAT = "^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])[0-9]{4}"
+            + "(\\s((0[1-9]|1[0-9]|2[0-3]):([0-5][0-9]))?$)";
 
     public DateTime(String value) throws IllegalValueException, PropertyNotFoundException {
         super(PROPERTY_SHORT_NAME, prepareDateTimeValue(value));
@@ -26,27 +30,41 @@ public class DateTime extends Property {
         super(PROPERTY_SHORT_NAME, formatDateTime(value));
     }
 
+    //@@author yunpengn
     /**
      * Returns true if a given string is a valid phone number.
      */
     public static boolean isValidTime(String test) {
-        return test.matches(PropertyManager.getPropertyValidationRegex(PROPERTY_SHORT_NAME));
+        try {
+            prepareDateTimeValue(test);
+            return true;
+        } catch (IllegalValueException | PropertyNotFoundException e) {
+            return false;
+        }
     }
 
-    //@@author yunpengn
     /**
-     * Prepares the value by checking whether the input is in standard format: if it is, directly return its value;
-     * otherwise, tries to parse it using natural language parser.
+     * Prepares the value by checking whether the input can be interpreted by the natural language parser.
      */
     public static String prepareDateTimeValue(String value) throws IllegalValueException, PropertyNotFoundException {
-        // Return it directly if the input is already in standard format.
-        if (isValidTime(value)) {
+        // Returns the original value directly if it is already in standard format.
+        if (isInStandardFormat(value)) {
             return value;
         }
 
-        // Replace the value with standard format if the natural language parsing is successful; otherwise, return
-        // the original value.
-        return NaturalLanguageUtil.parseSingleDateTime(value).map(DateTime::formatDateTime).orElse(value);
+        Optional<Date> dateObject = NaturalLanguageUtil.parseSingleDateTime(value);
+        if (dateObject.isPresent()) {
+            return formatDateTime(dateObject.get());
+        } else {
+            throw new IllegalValueException(PropertyManager.getPropertyConstraintMessage(PROPERTY_SHORT_NAME));
+        }
+    }
+
+    /**
+     * Checks whether a string representation of datetime is in standard format.
+     */
+    public static boolean isInStandardFormat(String value) {
+        return value.matches(STANDARD_FORMAT);
     }
 
     public static Date parseDateTime(String value) throws ParseException {
