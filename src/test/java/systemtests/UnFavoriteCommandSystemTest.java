4647c58f38907100836bcaa4ac8618a4064374d3
Keith Ong <october_arthur@hotmail.com>
2017-11-05 00:36:40 +0800

    Fix a typo in UnFavoriteCommandSystemTest

--- src/test/java/systemtests/UnFavoriteCommandSystemTest.java
+++ src/test/java/systemtests/UnFavoriteCommandSystemTest.java
@@ -164,7 +164,7 @@ public class UnFavoriteCommandSystemTest extends AddressBookSystemTest {
      * Performs the same verification as {@code assertCommandSuccess(String, Model, String)} except that the
      * browser url and selected card are expected to update accordingly depending on the card
      * at {@code expectedSelectedCardIndex}.
-     * @see FavoriteCommandSystemTest#assertCommandSuccess(String, Model, String)
+     * @see UnFavoriteCommandSystemTest#assertCommandSuccess(String, Model, String)
      * @see AddressBookSystemTest#assertSelectedCardChanged(Index)
      */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage,
