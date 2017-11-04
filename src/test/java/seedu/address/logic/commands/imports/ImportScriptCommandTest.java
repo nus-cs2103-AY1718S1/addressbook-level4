92720f04ffd76bfc681554c43c31c53be59081dc
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-11-04 11:23:26 +0800

    Fix coding style due to unused import statements

--- src/test/java/seedu/address/logic/commands/imports/ImportScriptCommandTest.java
+++ src/test/java/seedu/address/logic/commands/imports/ImportScriptCommandTest.java
@@ -1,13 +1,8 @@
 package seedu.address.logic.commands.imports;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
-
-import seedu.address.logic.CommandHistory;
-import seedu.address.logic.UndoRedoStack;
-import seedu.address.logic.commands.stub.ModelStub;
 
 public class ImportScriptCommandTest {
     @Test
