80a0ec54569ae0055eeccbc4e25245f209cf8674
Lenald <lenaldnwj@gmail.com>
2017-11-05 20:11:30 +0800

    Edit add command msg

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -27,7 +27,7 @@ public class AddCommand extends UndoableCommand {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: "
             + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "student/(STUDENT_NUMBER) parent/(PARENT_NUMBER) "
+            + PREFIX_PHONE + "STUDENTPHONE "
             + PREFIX_PARENTPHONE + "PARENTPHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
