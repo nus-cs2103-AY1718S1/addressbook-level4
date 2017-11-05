cd1c531b9c595c15036abda78595099ed458af1f
newalter <zeyong1123@gmail.com>
2017-11-05 11:34:06 +0800

    updated commandlist

--- src/main/java/seedu/address/logic/commands/CommandWordList.java
+++ src/main/java/seedu/address/logic/commands/CommandWordList.java
@@ -11,9 +11,9 @@ import java.util.List;
 public class CommandWordList {
 
     public static final List<String> COMMAND_WORD_LIST = Arrays.asList(AddCommand.COMMAND_WORD,
-            AddTagCommand.COMMAND_WORD, BackupCommand.COMMAND_WORD, ClearCommand.COMMAND_WORD,
-            DeleteCommand.COMMAND_WORD, DeleteTagCommand.COMMAND_WORD, EditCommand.COMMAND_WORD,
-            ExitCommand.COMMAND_WORD, FindCommand.COMMAND_WORD,
+            AddTagCommand.COMMAND_WORD, AddMeetingCommand.COMMAND_WORD, BackupCommand.COMMAND_WORD,
+            ClearCommand.COMMAND_WORD, DeleteCommand.COMMAND_WORD, DeleteTagCommand.COMMAND_WORD,
+            EditCommand.COMMAND_WORD, ExitCommand.COMMAND_WORD, FindCommand.COMMAND_WORD,
             HelpCommand.COMMAND_WORD, HistoryCommand.COMMAND_WORD, ListCommand.COMMAND_WORD,
             LoginCommand.COMMAND_WORD, NoteCommand.COMMAND_WORD, RedoCommand.COMMAND_WORD,
             ResizeCommand.COMMAND_WORD, RestoreBackupCommand.COMMAND_WORD, SelectCommand.COMMAND_WORD,
