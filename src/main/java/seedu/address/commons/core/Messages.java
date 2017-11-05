c32d0154a14be2a3d90c55120560a5306929adb6
Wyn <wynkheng1@gmail.com>
2017-11-06 00:36:41 +0800

    Settled whitespace issue

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -6,12 +6,12 @@ package seedu.address.commons.core;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Chirp! Unknown command! Press F2 for a list of commands.";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Oh no! The format you typed is not correct. " +
-            "Scroll down for the correct format.\n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Oh no! This index provided seems to be " +
-            "invalid.";
-    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "Oh no! The task index provided seems to be " +
-            "invalid.";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Oh no! The format you typed is not correct. "
+            + "Scroll down for the correct format.\n%1$s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Oh no! This index provided seems to be "
+            + "invalid.";
+    public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "Oh no! The task index provided seems to be "
+            + "invalid.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_TASK_LISTED_OVERVIEW = "%1$d tasks listed!";
     public static final String MESSAGE_PERSON_ALREADY_PINNED = "Chirp! This person is already pinned.";
