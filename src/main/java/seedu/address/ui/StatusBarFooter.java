cbd5f65d6421cacbd7f6f5faa4408ad21586a923
deep4k <e0031591@u.nus.edu>
2017-11-05 17:22:19 +0800

    Add time clock for UI in statusbar

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -1,6 +1,8 @@
 package seedu.address.ui;
 
+import java.text.SimpleDateFormat;
 import java.time.Clock;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Logger;
 
@@ -8,11 +10,20 @@ import org.controlsfx.control.StatusBar;
 
 import com.google.common.eventbus.Subscribe;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.util.Duration;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
+import seedu.address.commons.util.FxViewUtil;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
@@ -45,6 +56,8 @@ public class StatusBarFooter extends UiPart<Region> {
     private StatusBar numberPersonStatus;
     @FXML
     private StatusBar taskPersonStatus;
+    @FXML
+    private StatusBar timeStatus;
 
     public StatusBarFooter(String saveLocation, int totalPerson, int totalTask) {
         super(FXML);
@@ -53,6 +66,7 @@ public class StatusBarFooter extends UiPart<Region> {
         registerAsAnEventHandler(this);
         numberPersonStatus.setText(String.format(SYNC_NUMBER_PERSON, totalPerson));
         taskPersonStatus.setText(String.format(SYNC_NUMBER_TASK, totalTask));
+        setTimeStatus();
     }
 
     /**
@@ -85,5 +99,37 @@ public class StatusBarFooter extends UiPart<Region> {
         setSyncStatus(String.format(SYNC_STATUS_UPDATED, lastUpdated));
         numberPersonStatus.setText(String.format(SYNC_NUMBER_PERSON, abce.data.getPersonList().size()));
         taskPersonStatus.setText(String.format(SYNC_NUMBER_TASK, abce.data.getTaskList().size()));
+    }
+
+    private void setTimeStatus() {
+        long now = clock.millis();
+        Label timeStatusLabel = new TimeClock();
+        FxViewUtil.applyAnchorBoundaryParameters(timeStatusLabel, 0.0, 0.0, 0.0, 0.0);
+        timeStatus.setGraphic(timeStatusLabel);
+        timeStatusLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 10));
+    }
+}
+
+/**
+ * Current clock and date display in statusfootbar
+ */
+class TimeClock extends Label {
+
+    private static final String DATE_TIME_PATTERN = "HH:mm, EEE d MMM yyyy";
+
+    public TimeClock() {
+        getTime();
+    }
+
+    private void getTime() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0), actionEvent -> {
+            Calendar time = Calendar.getInstance();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_TIME_PATTERN);
+            setText(simpleDateFormat.format(time.getTime()));
+            setTextFill(Color.web("#ffffff"));
+        }), new KeyFrame(Duration.seconds(1)));
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 }
