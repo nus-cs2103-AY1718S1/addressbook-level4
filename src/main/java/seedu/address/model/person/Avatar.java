3a2062a059c5b61387552c88d4ec6af8a0026be2
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-11-05 17:44:19 +0800

    Add empty Avatar class and update UserGuide to mention parameters with the same prefix

---
+++ src/main/java/seedu/address/model/person/Avatar.java
@@ -0,0 +1,16 @@
+package seedu.address.model.person;
+
+import seedu.address.model.property.Property;
+import seedu.address.ui.person.PersonDetailsPanel;
+
+//@@author yunpengn
+/**
+ * Represents the {@link Avatar} image of each {@link Person}.
+ *
+ * Notice {@link Avatar} is not a {@link Property}. This is because it is indeed different from other fields of
+ * {@link Person}. It is not shown as a row in the {@link PersonDetailsPanel}. Meanwhile, the input validation is
+ * done by separate methods rather than a single regular expression (the complexity is not at the same level).
+ */
+public class Avatar {
+
+}
