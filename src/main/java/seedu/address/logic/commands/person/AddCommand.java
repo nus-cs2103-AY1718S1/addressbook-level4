aea874c2ab222a276c782ff0ec38cdf98c5c2aa2
Wyn <wynkheng1@gmail.com>
2017-11-05 19:06:00 +0800

    Updated error messages and fixed formatting in AddCommand

--- src/main/java/seedu/address/logic/commands/person/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/person/AddCommand.java
@@ -22,14 +22,14 @@ public class AddCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book."
             + "\n n/NAME is a compulsory field \n" + "Parameters: "
             + PREFIX_NAME + "NAME "
             + "[" + PREFIX_PHONE + "PHONE] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_BIRTHDAY + "BIRTHDAY] "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "[" + PREFIX_TAG + "TAGS]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
