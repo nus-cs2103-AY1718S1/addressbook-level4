12dd2c4991dc6da038bf8ed726a37463ec8d954c
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-11-05 19:07:09 +0800

    Correcting Checkstyle 8.

--- src/test/java/seedu/address/logic/commands/AddPhotoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPhotoCommandTest.java
@@ -34,12 +34,10 @@ public class AddPhotoCommandTest {
     private static final String INVALID_FILETYPE = "docs/AboutUs.adoc";
     private static final String VALID_FILEPATH = "src/main/resources/images/noPhoto.png";
     private static final String VALID_ALT_FILEPATH = "src/main/resources/images/fail.png";
-    
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-    
     @Test
     public void equals() {
         AddPhotoCommand standardCommand = new AddPhotoCommand(INDEX_FIRST_PERSON, new Photo(""));
