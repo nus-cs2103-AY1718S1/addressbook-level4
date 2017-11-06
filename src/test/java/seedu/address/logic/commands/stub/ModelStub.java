676d9260f10424414264b29bbf60a09645e3e53d
user\Junyang <junyangooooo@gmail.com>
2017-11-06 18:36:01 +0800

    solved trailing white space issue

--- src/test/java/seedu/address/logic/commands/stub/ModelStub.java
+++ src/test/java/seedu/address/logic/commands/stub/ModelStub.java
@@ -35,8 +35,9 @@ public class ModelStub implements Model {
     public void deleteEvent(ReadOnlyEvent event) throws EventNotFoundException {
         fail("This method should not be called.");
     }
-    
+
     //@@author dennaloh
+
     @Override
     public boolean haveDuplicate(String name, ObservableList<ReadOnlyPerson> list) {
         return false;
