4be8735b1e0b0dfe33a6c90e5e1bc940ceb889a0
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-11-06 16:59:08 +0800

    Add system test for ChangeProfilePictureCommand

--- src/test/java/seedu/address/testutil/TypicalProfilePicture.java
+++ src/test/java/seedu/address/testutil/TypicalProfilePicture.java
@@ -8,4 +8,11 @@ public class TypicalProfilePicture {
             "src/test/resources/images/firstPerfectProfilePicture.jpg";
     public static final String SECOND_PERFECT_PROFILE_PICTURE_PATH =
             "src/test/resources/images/secondPerfectProfilePicture.jpg";
+    public static final String THIRD_PERFECT_PROFILE_PICTURE_PATH =
+            "src/test/resources/images/thirdPerfectProfilePicture.jpg";
+    public static final String FORTH_PERFECT_PROFILE_PICTURE_PATH =
+            "src/test/resources/images/forthPerfectProfilePicture.jpg";
+    public static final String FIFTH_PERFECT_PROFILE_PICTURE_PATH =
+            "src/test/resources/images/fifthPerfectProfilePicture.jpg";
+    public static final String FILE_NOT_FOUND_PATH = "D:/pleaseDontNameYourPictureLikeThis.jpg";
 }
