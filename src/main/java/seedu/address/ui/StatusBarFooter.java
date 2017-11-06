5dabdaa4d8fdeaf9563a1985a396a64454e62ba5
minato <dmc3hpcity@gmail.com>
2017-11-07 00:14:15 +0800

    Add css for displayDate, fix StatusBarFooter class

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -62,7 +62,7 @@ public class StatusBarFooter extends UiPart<Region> {
         setSaveLocation("./" + saveLocation);
         registerAsAnEventHandler(this);
 
-        startClock(footerClock);
+        startFooterClock(footerClock);
     }
 
     /**
@@ -99,21 +99,30 @@ public class StatusBarFooter extends UiPart<Region> {
     /**
      * Starts running the clock display.
      */
-    private void startClock(ClockDisplay footerClock) {
+    private void startFooterClock(ClockDisplay footerClock) {
         final Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                footerClock.getTime().setCurrentTime();
-                footerClock.setTime(requireNonNull(footerClock.getTime()));
-                Platform.runLater(() -> displayTime.setText(footerClock.getTimeAsText()));
-
-                footerClock.getDate().setCurrentDate();
-                footerClock.setDate(requireNonNull(footerClock.getDate()));
-                Platform.runLater(() -> displayDate.setText(footerClock.getDateAsText()));
+                setFooterClockTime(footerClock);
+                setFooterClockDate(footerClock);
             }
         };
         timer.scheduleAtFixedRate(task, 0, 1000);
+    }
+
+    private void setFooterClockTime(ClockDisplay footerClock) {
+        requireNonNull(footerClock.getTime());
+
+        footerClock.getTime().setCurrentTime();
+        Platform.runLater(() -> displayTime.setText(footerClock.getTimeAsText()));
+    }
+
+    private void setFooterClockDate(ClockDisplay footerClock) {
+        requireNonNull(footerClock.getDate());
+
+        footerClock.getDate().setCurrentDate();
+        Platform.runLater(() -> displayDate.setText(footerClock.getDateAsText()));
     }
     //@@author
 
