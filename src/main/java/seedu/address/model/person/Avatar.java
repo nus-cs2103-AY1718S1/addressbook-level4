f53bd7c9bde2c1e97fc91a4c6a956546d4b26785
DESKTOP-AUBBJO3\Phung Tuan Hoang <phungtuanhoang1996@gmail.com>
2017-11-07 00:43:52 +0800

    fix checkstyle again and again

--- src/main/java/seedu/address/model/person/Avatar.java
+++ src/main/java/seedu/address/model/person/Avatar.java
@@ -4,14 +4,9 @@ package seedu.address.model.person;
 
 import java.io.File;
 
-import com.sun.javafx.robot.FXRobot;
-import com.sun.javafx.robot.FXRobotImage;
-import javafx.application.Application;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import seedu.address.MainApp;
 import seedu.address.commons.exceptions.IllegalValueException;
 
@@ -25,68 +20,6 @@ public class Avatar {
 
     private ObjectProperty<Image> avatarImage;
     private String avatarFilePath;
-
-    private FXRobot robot = new FXRobot() {
-        @Override
-        public void waitForIdle() {
-
-        }
-
-        @Override
-        public void keyPress(KeyCode code) {
-
-        }
-
-        @Override
-        public void keyRelease(KeyCode code) {
-
-        }
-
-        @Override
-        public void keyType(KeyCode code, String keyChar) {
-
-        }
-
-        @Override
-        public void mouseMove(int x, int y) {
-
-        }
-
-        @Override
-        public void mousePress(MouseButton button, int clickCount) {
-
-        }
-
-        @Override
-        public void mouseRelease(MouseButton button, int clickCount) {
-
-        }
-
-        @Override
-        public void mouseClick(MouseButton button, int clickCount) {
-
-        }
-
-        @Override
-        public void mouseDrag(MouseButton button) {
-
-        }
-
-        @Override
-        public void mouseWheel(int wheelAmt) {
-
-        }
-
-        @Override
-        public int getPixelColor(int x, int y) {
-            return 0;
-        }
-
-        @Override
-        public FXRobotImage getSceneCapture(int x, int y, int width, int height) {
-            return null;
-        }
-    };
 
     public Avatar() {
         // Default object -> 'generic' avatar
