636f8765cf06aee9feb23fd64eefd091f7e2452f
minato <dmc3hpcity@gmail.com>
2017-11-07 00:04:31 +0800

    Using enums for day of week and month attributes of RunningDate

--- src/main/java/seedu/address/model/clock/RunningDate.java
+++ src/main/java/seedu/address/model/clock/RunningDate.java
@@ -1,8 +1,8 @@
 package seedu.address.model.clock;
 
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -12,7 +12,7 @@ public class RunningDate {
 
     private DayOfWeek dayOfWeek;
     private int dayOfMonth;
-    private Month month;
+    private MonthOfYear month;
     private int year;
 
     public RunningDate() {
@@ -27,7 +27,7 @@ public class RunningDate {
         return dayOfMonth;
     }
 
-    public Month getMonth() {
+    public MonthOfYear getMonth() {
         return month;
     }
 
@@ -36,15 +36,15 @@ public class RunningDate {
     }
 
     public void setCurrentDate() {
-        this.dayOfWeek = LocalDateTime.now().getDayOfWeek();
+        this.dayOfWeek = DayOfWeek.valueOf(LocalDateTime.now().getDayOfWeek().getValue());
         this.dayOfMonth = LocalDateTime.now().getDayOfMonth();
-        this.month = LocalDateTime.now().getMonth();
+        this.month = MonthOfYear.valueOf(LocalDateTime.now().getMonth().getValue());
         this.year = LocalDateTime.now().getYear();
     }
 
     @Override
     public String toString() {
-        return dayOfWeek.toString() + " " + dayOfMonth + " " + month.toString() + " " + year;
+        return dayOfWeek.toString() + ", " + dayOfMonth + " " + month.toString() + ", " + year;
     }
 
     @Override
@@ -60,6 +60,59 @@ public class RunningDate {
     @Override
     public int hashCode() {
         return Objects.hash(dayOfWeek, dayOfMonth, month, year);
+    }
+
+}
+
+/**
+ * Represents days of a week.
+ */
+enum DayOfWeek {
+
+    Monday(1), Tuesday(2), Wedsnesday(3), Thurday(4), Friday(5), Saturday(6), Sunday(7);
+
+    private static Map<Integer, DayOfWeek> map = new HashMap<>();
+    private int index;
+
+    DayOfWeek(int index) {
+        this.index = index;
+    }
+
+    static {
+        for (DayOfWeek item : DayOfWeek.values()) {
+            map.put(item.index, item);
+        }
+    }
+
+    public static DayOfWeek valueOf(int index) {
+        return map.get(index);
+    }
+
+}
+
+/**
+ * Represents months of a year.
+ */
+enum MonthOfYear {
+
+    January(1), February(2), March(3), April(4), May(5), June(6), July(7), August(8), September(9),
+    October(10), November(11), December(12);
+
+    private static Map<Integer, MonthOfYear> map = new HashMap<>();
+    private int index;
+
+    MonthOfYear(int index) {
+        this.index = index;
+    }
+
+    static {
+        for (MonthOfYear item : MonthOfYear.values()) {
+            map.put(item.index, item);
+        }
+    }
+
+    public static MonthOfYear valueOf(int index) {
+        return map.get(index);
     }
 
 }
