621dcc424850c314bbb9660c75583dd6af29b345
Kennard Ng <Kennard_ng@outlook.com>
2017-11-07 00:35:00 +0800

    make Ark enter list mode on start up.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -152,6 +152,9 @@ public class MainWindow extends UiPart<Region> {
 
         CommandBox commandBox = new CommandBox(logic);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+
+        // set splitPane divider position to start in list mode.
+        splitPanePlaceholder.setDividerPositions(0.0);
     }
 
     void hide() {
