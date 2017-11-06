397f97184d8e141b3e97788c0342889b3dcc8383
FRACTOR-PC\Fractor <zyng28@gmail.com>
2017-11-07 05:21:36 +0800

    Remove unused import

--- src/main/java/seedu/address/ui/PersonInformationPanel.java
+++ src/main/java/seedu/address/ui/PersonInformationPanel.java
@@ -16,7 +16,6 @@ import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
