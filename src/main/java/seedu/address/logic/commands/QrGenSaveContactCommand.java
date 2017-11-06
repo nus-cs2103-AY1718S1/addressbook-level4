aa509e8d0477b700a62a986eafcd3843c4f51c39
ZANARKAND\Daniel <danielkwang@outlook.com>
2017-11-07 00:39:22 +0800

    Fixed QR Error For Save Contact

--- src/main/java/seedu/address/logic/commands/QrGenSaveContactCommand.java
+++ src/main/java/seedu/address/logic/commands/QrGenSaveContactCommand.java
@@ -9,69 +9,38 @@ public class QrGenSaveContactCommand {
      * Method to Generate SaveContact for Phone QRCode
      */
     public String qrSaveContact(String phoneNum, String contactName, String contactEmail) {
-
-        String qrCodeA = "http://";
-        String qrCodeB = "api.qrserver.com/";
-        String qrCodeC = "v1/";
-        String qrCodeD = "create-qr-code/";
-        String qrCodeE = "?color=000000";
-        String qrCodeF = "&bgcolor=FFFFFF";
-        String qrCodeG = "&data";
-        String qrCodeH = "=BEGIN";
-        String qrCodeI = "%3A";
-        String qrCodeJ = "VCARD";
-        String qrCodeK = "%0";
-        String qrCodeL = "AVERSION";
-        String qrCodeM = "%3A2.1";
-        String qrCodeN = "%0AFN";
-        String qrCodeO = "%3A";
-        String qrLineA = qrCodeA + qrCodeB + qrCodeC + qrCodeD + qrCodeE + qrCodeF
-                + qrCodeG + qrCodeH + qrCodeI + qrCodeJ + qrCodeK + qrCodeL
-                + qrCodeM + qrCodeN + qrCodeO;
-
-        String qrpartbCodeA = "%0";
-        String qrpartbCodeB = "AN";
-        String qrpartbCodeC = "%3A";
-        String qrpartbCodeD = "%3B";
-        String qrLineB = qrpartbCodeA + qrpartbCodeB + qrpartbCodeC + qrpartbCodeD;
-        String qrpartcCodeA = "%0";
-        String qrpartcCodeB = "ATEL";
-        String qrpartcCodeC = "%3BWORK";
-        String qrpartcCodeD = "%3BVOICE";
-        String qrpartcCodeE = "%3A";
-        String qrLineC = qrpartcCodeA + qrpartcCodeB + qrpartcCodeC + qrpartcCodeD + qrpartcCodeE;
-        String qrpartdCodeA = "%0";
-        String qrpartdCodeB = "AEMAIL";
-        String qrpartdCodeC = "%3";
-        String qrpartdCodeD = "BWORK";
-        String qrpartdCodeE = "%3";
-        String qrpartdCodeF = "BINTERNET";
-        String qrpartdCodeG = "%3A";
-        String qrLineD = qrpartdCodeA + qrpartdCodeB + qrpartdCodeC + qrpartdCodeD + qrpartdCodeE + qrpartdCodeF
-                + qrpartdCodeG;
-        String qrparteCodeA = "%0";
-        String qrparteCodeB = "AORG";
-        String qrparteCodeC = "%3A";
-        String qrLineE = qrparteCodeA + qrparteCodeB + qrparteCodeC;
-        String qrpartfCodeA = "%0";
-        String qrpartfCodeB = "AEND";
-        String qrpartfCodeC = "%3";
-        String qrpartfCodeD = "AVCARD";
-        String qrpartfCodeE = "%0A";
-        String qrpartfCodeF = "&qzone";
-        String qrpartfCodeG = "=1";
-        String qrpartfCodeH = "&margin";
-        String qrpartfCodeI = "=0";
-        String qrpartfCodeJ = "&size";
-        String qrpartfCodeK = "500x500";
-        String qrpartfCodeL = "&ecc";
-        String qrpartfCodeM = "=L";
-        String qrLineF = qrpartfCodeA + qrpartfCodeB + qrpartfCodeC + qrpartfCodeD + qrpartfCodeE + qrpartfCodeF
-                + qrpartfCodeG + qrpartfCodeH + qrpartfCodeI + qrpartfCodeJ + qrpartfCodeK + qrpartfCodeL
-                + qrpartfCodeM;
-        //String that will generate the QR Code
-        String fullQr = qrLineA + contactName + qrLineB + contactName + qrLineC + phoneNum + qrLineD + contactEmail
-                + qrLineE + qrLineF;
+        String qrA = "http://";
+        String qrB = "api.qrserver.com/";
+        String qrC = "v1/";
+        String qrD = "create-qr-code/";
+        String qrE = "?color=000000";
+        String qrF = "&bgcolor=FFFFFF";
+        String qrG = "&data=BEGIN";
+        String qrH = "%3AVCARD";
+        String qrI = "%0AVERSION";
+        String qrJ = "%3A2.1%0";
+        String qrK = "AFN%3A";
+        String lineA = qrA + qrB + qrC + qrD + qrE + qrF + qrG + qrH + qrI
+                + qrJ + qrK;
+        String newName = contactName.replace(' ', '+');
+        String lineB = "%0AN%3A%3B";
+        String qrL = "%0ATEL";
+        String qrM = "%3BWORK";
+        String qrN = "%3BVOICE%3A";
+        String lineC = qrL + qrM + qrN;
+        String qrO = "%0AEMAIL";
+        String qrP = "%3BWORK";
+        String qrQ = "%3BINTERNET%3A";
+        String lineD = qrO + qrP + qrQ;
+        String qrCodeA = "%0AEND";
+        String qrCodeB = "%3AVCARD";
+        String qrCodeC = "%0A&qzone=1";
+        String qrCodeD = "&margin=0";
+        String qrCodeE = "&size=500x500";
+        String qrCodeF = "&ecc=L";
+        String lineE = qrCodeA + qrCodeB + qrCodeC + qrCodeD + qrCodeE + qrCodeF;
+        String fullQr = lineA + newName + lineB + newName + lineC + phoneNum + lineD + contactEmail + lineE;
+        System.out.println(fullQr);
         return fullQr;
     }
 }
