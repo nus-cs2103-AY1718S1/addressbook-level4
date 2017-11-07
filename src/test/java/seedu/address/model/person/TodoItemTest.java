95b7586885d2454aa3e876134bb1bdb6bab7a253
Xiang Hailin <895227442@qq.com>
2017-11-07 23:59:45 +0800

    Update code quality

--- src/test/java/seedu/address/model/person/TodoItemTest.java
+++ src/test/java/seedu/address/model/person/TodoItemTest.java
@@ -46,7 +46,7 @@ public class TodoItemTest {
     }
 
     @Test
-    public void test_returnsCorrectTimeString() {
+    public void ForgetTimeStringReturnsCorrect() {
         TodoItem todoItemWithEndTime = getTodoItemOne();
         Assert.assertEquals(todoItemWithEndTime.getTimeString(),
                 "From: " + convertTimeToString(EARLY_TIME_ONE) + "   To: " + convertTimeToString(LATE_TIME_ONE));
