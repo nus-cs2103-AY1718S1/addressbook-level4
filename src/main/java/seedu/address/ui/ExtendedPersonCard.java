cc9c489d3490ba1a63b55a3576afec06b5bc3e67
Lenald <lenaldnwj@gmail.com>
2017-11-07 22:58:42 +0800

    Icon text descriptors on startup of cherBook

--- src/main/java/seedu/address/ui/ExtendedPersonCard.java
+++ src/main/java/seedu/address/ui/ExtendedPersonCard.java
@@ -48,6 +48,7 @@ public class ExtendedPersonCard extends UiPart<Region> {
     public ExtendedPersonCard() {
         super(FXML);
         registerAsAnEventHandler(this);
+        loadIconDescriptionOnStartUp();
     }
 
     /**
@@ -65,6 +66,23 @@ public class ExtendedPersonCard extends UiPart<Region> {
         remark.setText(person.getRemark().toString());
     }
 
+    //@@author Lenaldnwj
+    /**
+     * Displays text description of the icons when cherBook starts up
+     */
+    protected void loadIconDescriptionOnStartUp() {
+        name.setText("Name of student");
+        phone.setText("Phone number of student");
+        parentPhone.setText("Phone number of parent");
+        address.setText("Address of student");
+        formClass.setText("Form class of student");
+        grades.setText("Grades of student");
+        postalCode.setText("Postal code of student");
+        email.setText("Email of student");
+        remark.setText("Remark of student");
+    }
+
+    //@@author limcel
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
