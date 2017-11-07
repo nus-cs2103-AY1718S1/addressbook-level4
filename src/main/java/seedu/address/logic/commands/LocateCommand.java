070c43fbc8a43e3bede9d8e627ada09c038479af
Tao Jiashu <henrytjsh@gmail.com>
2017-11-07 20:17:12 +0800

    Removed unused import in LocateCommand

--- src/main/java/seedu/address/logic/commands/LocateCommand.java
+++ src/main/java/seedu/address/logic/commands/LocateCommand.java
@@ -5,7 +5,6 @@ import java.util.List;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.ShowLocationRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
