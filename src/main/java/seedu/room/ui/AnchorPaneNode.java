720ea5c4d53325a9830cd70f644042292cd5f324
Haozhe <phaozhe@hotmail.com>
2017-11-07 20:18:10 +0800

    Add method to focus on calender grid on mouse-click

--- src/main/java/seedu/room/ui/AnchorPaneNode.java
+++ src/main/java/seedu/room/ui/AnchorPaneNode.java
@@ -2,8 +2,14 @@ package seedu.room.ui;
 
 import java.time.LocalDate;
 
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 //@@author Haozhe321-reused
 /**
@@ -13,6 +19,10 @@ public class AnchorPaneNode extends AnchorPane {
 
     // Date associated with this pane
     private LocalDate date;
+    private final Background focusBackground = new Background( new BackgroundFill(
+            Color.DARKGRAY, CornerRadii.EMPTY, Insets.EMPTY ) );
+    private final Background unfocusBackground = new Background( new BackgroundFill(
+            Color.TRANSPARENT, CornerRadii.EMPTY, Insets.EMPTY ) );
 
     /**
      * Create a anchor pane node. Date is not assigned in the constructor.
@@ -21,7 +31,15 @@ public class AnchorPaneNode extends AnchorPane {
     public AnchorPaneNode(Node... children) {
         super(children);
         // Add action handler for mouse clicked
-        this.setOnMouseClicked(e -> System.out.println("This pane's date is: " + date));
+
+
+        this.setOnMouseClicked( ( e ) ->
+        {
+            this.focusGrid();
+        } );
+
+
+
     }
 
     public LocalDate getDate() {
@@ -31,4 +49,17 @@ public class AnchorPaneNode extends AnchorPane {
     public void setDate(LocalDate date) {
         this.date = date;
     }
+
+    //this method focuses the Grid when the mouse clicks on it
+    public void focusGrid() {
+        this.requestFocus();
+        this.backgroundProperty().bind( Bindings
+                .when( this.focusedProperty() )
+                .then( focusBackground )
+                .otherwise(unfocusBackground)
+        );
+
+    }
+
+
 }
