769d4853487ff08b24aab1da3ede4433f74edfdf
Zhao Pengyu <973465134@qq.com>
2017-11-07 18:40:02 +0800

    style

--- src/main/java/seedu/address/logic/commands/BindeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/BindeleteCommand.java
@@ -7,7 +7,6 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 //@@author Pengyuz
