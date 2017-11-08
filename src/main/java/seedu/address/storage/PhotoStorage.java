19668faf3382cf6101b961da495fe56d077c5802
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-11-08 10:16:33 +0800

    added ExtensionCheckerUtil in util folder

--- src/main/java/seedu/address/storage/PhotoStorage.java
+++ src/main/java/seedu/address/storage/PhotoStorage.java
@@ -1,11 +1,15 @@
 //@@author wishingmaid
 package seedu.address.storage;
 
+import static java.util.Objects.requireNonNull;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+
+import seedu.address.commons.util.ExtensionCheckerUtil;
 
 /**
  * Represents the conversion of a local filepath inside the user's hard drive
@@ -13,8 +17,10 @@ import javax.imageio.ImageIO;
  * Guarantees : The newly written filepath must exist if ImageIO.write is successful
  */
 public class PhotoStorage {
-    public static final String WRITE_FAILURE_MESSAGE = "Unable to write to local resource folder: displaypictures. "
-            + "Make sure that the image type is supported. Supported types: JPEG, PNG.";
+    public static final String WRITE_FAILURE_MESSAGE = "Unable to write to local resource folder: displaypictures.\n" + 
+            "Make sure that the image type is supported and the image file cannot be hidden.\n" +  
+            "Supported types: JPEG, JPG, PNG.";
+    private String[] allowedExt = null;
     private File fileReader = null;
     private String filePath = "";
     private File fileWriter = null;
@@ -24,19 +30,23 @@ public class PhotoStorage {
         this.uniqueFileName = uniqueFileName;
         this.filePath = filePath;
         imageReader = new BufferedImage(300, 400, BufferedImage.TYPE_INT_ARGB);
+        allowedExt =  new String[]{"jpg", "png", "JPEG"};
     }
 
     public String setNewFilePath() throws IOException {
-        String newFilePath = "displaypictures/" + uniqueFileName + ".jpg";
+        requireNonNull(allowedExt);
+        String ext = ExtensionCheckerUtil.getExtension(filePath);
+        if(!ExtensionCheckerUtil.isOfType(ext, allowedExt)) {
+            throw new IOException(WRITE_FAILURE_MESSAGE);
+        }
+        String newFilePath = "displaypictures/" + uniqueFileName + "." + ext;
         try {
             fileReader = new File(filePath);
             imageReader = ImageIO.read(fileReader);
             fileWriter = new File(newFilePath);
-            ImageIO.write(imageReader, "jpg", fileWriter);
+            ImageIO.write(imageReader, ext, fileWriter);
             return newFilePath;
         } catch (IOException e) {
-            throw new IOException(WRITE_FAILURE_MESSAGE);
-        } catch (IllegalArgumentException f) {
             throw new IOException(WRITE_FAILURE_MESSAGE);
         }
     }
