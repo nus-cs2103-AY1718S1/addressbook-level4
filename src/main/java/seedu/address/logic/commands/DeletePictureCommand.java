d345db5919545a4ed32340dda2b5994861b5310b
jaivigneshvenugopal <smugs1995@gmail.com>
2017-11-09 00:14:35 +0800

    Logic: Add deletePicture command and command parser

--- src/main/java/seedu/address/logic/commands/DeletePictureCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePictureCommand.java
@@ -1,4 +1,45 @@
 package seedu.address.logic.commands;
 
-public class DeletePictureCommand {
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+
+public class DeletePictureCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "delpic";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Removes the profile picture of the currently selected person or the person"
+            + "identified by the index number used in the last "
+            + "person listing.\n"
+            + "Parameters: INDEX (optional, must be a positive integer if present)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+    public static final String MESSAGE_DELPIC_SUCCESS = "%1$s profile picture has been removed!";
+    public static final String MESSAGE_DELPIC_FAILURE = "Unable to remove %1$s profile picture!";
+
+    private final Index targetIndex;
+
+    public DeletePictureCommand() {
+        this.targetIndex = null;
+    }
+
+    public DeletePictureCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        String messageToDisplay = MESSAGE_DELPIC_SUCCESS;
+
+        ReadOnlyPerson personToUpdate = selectPerson(targetIndex);
+
+        model.removeProfilePicture(personToUpdate);
+
+        listObserver.updateCurrentFilteredList(PREDICATE_SHOW_ALL_PERSONS);
+
+        String currentList = listObserver.getCurrentListName();
+
+        return new CommandResult(currentList + String.format(messageToDisplay, personToUpdate.getName()));
+    }
 }
--- src/main/java/seedu/address/logic/parser/DeletePictureCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePictureCommandParser.java
@@ -1,4 +1,25 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.DeletePictureCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
 public class DeletePictureCommandParser {
+
+    public DeletePictureCommand parse(String args) throws ParseException {
+        try {
+            if (args.trim().equals("")) {
+                return new DeletePictureCommand();
+            } else {
+                Index index = ParserUtil.parseIndex(args);
+                return new DeletePictureCommand(index);
+            }
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePictureCommand.MESSAGE_USAGE));
+        }
+    }
 }
