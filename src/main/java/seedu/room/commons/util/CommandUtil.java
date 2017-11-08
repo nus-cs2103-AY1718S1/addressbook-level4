c2238a0f7984bcc7006ec4f72a6a344cad92259e
Suyash Shekhar <suyashshekhar@gmail.com>
2017-11-08 15:32:43 +0800

    Add command util

---
+++ src/main/java/seedu/room/commons/util/CommandUtil.java
@@ -0,0 +1,17 @@
+package seedu.room.commons.util;
+
+/**
+ * Utility methods related to Collections
+ */
+public class CommandUtil {
+
+    /**
+     * Returns true if the command parsed is an eventbook-related command
+     */
+    public static boolean isEventCommand(String command) {
+        String[] commandComponents = command.split(" ");
+        String commandKeyword = commandComponents[0];
+        return commandKeyword.equals("deleteEvent") || commandKeyword.equals("addevent") ||
+               commandKeyword.equals("de") || commandKeyword.equals("ae");
+    }
+}
