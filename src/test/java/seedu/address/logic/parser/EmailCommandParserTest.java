e1d7b0ea8256c38d2c0613d94badfe85cb62158e
jin-ting <e0104957@u.nus.edu>
2017-11-08 21:44:35 +0800

    Update EmailCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/EmailCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EmailCommandParserTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
@@ -41,47 +40,3 @@ public class EmailCommandParserTest {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, EmailCommand.MESSAGE_USAGE));
     }
 }
-=======
-package seedu.address.logic.parser;
-
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import org.junit.Test;
-
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.EmailCommand;
-
-
-
-//@@author jin-ting
-public class EmailCommandParserTest {
-
-    private EmailCommandParser parser = new EmailCommandParser();
-
-    @Test
-    public void parse_validArgs_returnsEmailCommand() {
-        Set<Index> indices = new HashSet<>();
-        indices.add(INDEX_FIRST_PERSON);
-        indices.add(INDEX_SECOND_PERSON);
-        assertParseSuccess(parser, "1 2", new EmailCommand(indices));
-    }
-
-    @Test
-    public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, EmailCommand.MESSAGE_USAGE));
-    }
-
-
-    @Test
-    public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, EmailCommand.MESSAGE_USAGE));
-    }
-}
->>>>>>> 38354595f0a5d5736d964aa07a0a6f55cbb277b5
