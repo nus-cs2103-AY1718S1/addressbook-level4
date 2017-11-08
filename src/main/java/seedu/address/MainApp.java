18aa7936386ae7bb2f785920affdf1e85cb556b2
user\Junyang <junyangooooo@gmail.com>
2017-11-08 16:11:42 +0800

    initialized theme according to preferences.json

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -60,7 +60,8 @@ public class MainApp extends Application {
 
         config = initConfig(getApplicationParameter("config"));
 
-        UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
+        UserPrefsStorage userPrefsStorage =
+                new JsonUserPrefsStorage(config.getUserPrefsFilePath(), config.getAddressBookTheme());
         userPrefs = initPrefs(userPrefsStorage);
         AddressBookStorage addressBookStorage = new XmlAddressBookStorage(userPrefs.getAddressBookFilePath());
         storage = new StorageManager(addressBookStorage, userPrefsStorage);
--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -56,6 +56,7 @@ public class UiManager extends ComponentManager implements Ui {
             mainWindow = new MainWindow(primaryStage, config, prefs, logic);
             mainWindow.show(); //This should be called before creating other UI parts
             mainWindow.fillInnerParts();
+            mainWindow.getRoot().getStylesheets().add(prefs.getAddressBookTheme()); //initialize themes from json prefs
 
         } catch (Throwable e) {
             logger.severe(StringUtil.getDetails(e));
