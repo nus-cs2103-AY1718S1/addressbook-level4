247d7aab2e8c811dc11d22374d8b2f64f8836a20
jo-lyn <e0007247@u.nus.edu>
2017-11-08 22:14:12 +0800

    Add color generator to Avatar

--- src/main/java/seedu/address/ui/util/Avatar.java
+++ src/main/java/seedu/address/ui/util/Avatar.java
@@ -1,5 +1,6 @@
 package seedu.address.ui.util;
 
+import java.awt.Color;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -11,16 +12,41 @@ public class Avatar {
     private static Random random = new Random();
     private static String[] colors = ColorsUtil.getColors();
 
+    private static final int COLOR_OFFSET_BOUND = 50;
+
     private Avatar() {}
 
     public static String getColor(String name) {
         if (!avatarColors.containsKey(name)) {
-            avatarColors.put(name, colors[random.nextInt(colors.length)]);
+            Color defaultColor = Color.decode(colors[random.nextInt(colors.length)]);
+            String newColor = generateNewColor(defaultColor);
+            avatarColors.put(name, newColor);
         }
         return avatarColors.get(name);
     }
 
     public static String getInitial(String name) {
         return name.substring(0, 1);
+    }
+
+    /**
+     * Generates a new color with a random offset from {@code defaultColor}.
+     */
+    private static String generateNewColor(Color defaultColor) {
+        int r = defaultColor.getRed();
+        int g = defaultColor.getGreen();
+        int b = defaultColor.getBlue();
+        int newR = Math.max(0, Math.min(255, (r + generateOffset())));
+        int newG = Math.max(0, Math.min(255, (g + generateOffset())));
+        int newB = Math.max(0, Math.min(255, (b + generateOffset())));
+
+        return String.format("#%02x%02x%02x", newR, newG, newB);
+    }
+
+    /**
+     * Generates an offset in the range [-1 * COLOR_OFFSET_BOUND, COLOR_OFFSET_BOUND].
+     */
+    private static int generateOffset() {
+        return random.nextInt(COLOR_OFFSET_BOUND * 2) - COLOR_OFFSET_BOUND;
     }
 }
