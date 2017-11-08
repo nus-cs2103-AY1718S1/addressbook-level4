bdf65e81a723fb2a33903107123135ff78d0caec
Haozhe <phaozhe@hotmail.com>
2017-11-09 00:22:04 +0800

    Add exception to be raised for invalid timestamp values

--- src/main/java/seedu/room/model/person/Timestamp.java
+++ src/main/java/seedu/room/model/person/Timestamp.java
@@ -2,18 +2,26 @@ package seedu.room.model.person;
 
 import java.time.LocalDateTime;
 
+import seedu.room.commons.exceptions.IllegalValueException;
+
 //@@author Haozhe321
 /**
  Create a timestamp in each person to record the time created and time that this temporary person will expire
  */
 public class Timestamp {
 
+    public static final String MESSAGE_TIMESTAMP_CONSTRAINTS =
+            "Timestamp cannot be negative";
+
     private LocalDateTime creationTime = null;
     private LocalDateTime expiryTime = null; //after construction, a null expiryTime means this person will not expire
     private long daysToLive;
 
-    public Timestamp(long day) {
+    public Timestamp(long day) throws IllegalValueException {
         creationTime = LocalDateTime.now().withNano(0).withSecond(0).withMinute(0);
+        if (!isValidTimestamp(day)) {
+            throw new IllegalValueException(MESSAGE_TIMESTAMP_CONSTRAINTS);
+        }
         if (day > 0) {
             expiryTime = creationTime.plusDays(day).withNano(0).withSecond(0).withMinute(0);
         }
@@ -52,6 +60,7 @@ public class Timestamp {
             return expiryTime.toString();
         }
     }
+
 
     /**
      * Returns true if a given long is a valid timestamp.
