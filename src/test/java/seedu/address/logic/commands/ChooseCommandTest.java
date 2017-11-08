30f483b83548111642a56cf2937a3f68eb2890f4
fongwz <schifx95@gmail.com>
2017-11-08 23:53:36 +0800

    fixing travis errors

--- src/test/java/seedu/address/logic/commands/ChooseCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ChooseCommandTest.java
@@ -8,6 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.events.ui.JumpToBrowserListRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
@@ -27,6 +28,10 @@ public class ChooseCommandTest {
         assertExecutionFailure("gibberish", Messages.MESSAGE_INVALID_BROWSER_INDEX);
     }
 
+    /**
+     * Executes a {@code ChooseCommand} with the given {@code arguments}, and checks that {@code JumpToBrowserListRequestEvent}
+     * is raised with the correct index.
+     */
     private void assertExecutionSuccess(String args) {
         ChooseCommand chooseCommand = new ChooseCommand(args);
 
@@ -37,8 +42,15 @@ public class ChooseCommandTest {
         } catch (CommandException ce) {
             throw new IllegalArgumentException("Assert Execution Failed: ", ce);
         }
+
+        JumpToBrowserListRequestEvent event = (JumpToBrowserListRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
+        assertEquals(args, event.browserItem);
     }
 
+    /**
+     *  Executes a {@code ChooseCommand} with the given {@code arguments}, and checks that a {@code CommandException}
+     * is thrown with the {@code expectedMessage}.
+     */
     private void assertExecutionFailure(String args, String expectedMessage) {
         ChooseCommand chooseCommand = new ChooseCommand(args);
 
