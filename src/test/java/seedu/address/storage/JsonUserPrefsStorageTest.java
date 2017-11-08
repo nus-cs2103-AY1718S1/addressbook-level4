5f573dacc768aa56d3c14491b73a71fdfcdc5cd7
user\Junyang <junyangooooo@gmail.com>
2017-11-08 16:50:17 +0800

    added more tests

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -90,6 +90,7 @@ public class JsonUserPrefsStorageTest {
         userPrefs.setGuiSettings(1000, 500, 300, 100);
         userPrefs.setAddressBookFilePath("addressbook.xml");
         userPrefs.setAddressBookName("TypicalAddressBookName");
+        userPrefs.setAddressBookTheme("/css/DarkTheme.css");
         return userPrefs;
     }
 
