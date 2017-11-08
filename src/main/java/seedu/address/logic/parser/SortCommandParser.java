b8af04722fd35ae0cb746a2051ab797f920d8518
Houjisan <hongjing10188@gmail.com>
2017-11-08 23:35:52 +0900

    Change method for finding ignorefav option

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -3,6 +3,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,10 +26,14 @@ public class SortCommandParser implements Parser<SortCommand> {
      * Used to get the datafield to sort by and check if ignore favourite is inputted
      */
     private static final Pattern SORT_ARGUMENT_FORMAT =
-            Pattern.compile("(?<dataField>\\S+)(?<option>\\s+-ignorefav)?");
+            Pattern.compile("(?<dataField>\\S+)(?<options>.*)");
 
+    private static final String IGNORE_FAV_INPUT = "-ignorefav";
     private static final boolean FAV_NOT_IGNORED = false;
     private static final boolean FAV_IGNORED = true;
+    private static final String REVERSE_ORDER_INPUT = "-reverse";
+    private static final boolean NOT_REVERSE_ORDER = false;
+    private static final boolean REVERSE_ORDER = true;
 
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
@@ -44,18 +50,32 @@ public class SortCommandParser implements Parser<SortCommand> {
         }
 
         String dataFieldToSortByFirst = matcher.group("dataField").toLowerCase();
-        String ignoreFavOption = matcher.group("option");
+        String options = matcher.group("options").trim();
 
+        boolean isFavIgnored = FAV_NOT_IGNORED;
+        boolean isReverseOrder = NOT_REVERSE_ORDER;
+
+        if (!options.isEmpty()) {
+            List<String> separateOptions = Arrays.asList(options.split("\\s+"));
+
+            for (String option : separateOptions) {
+                String trimmedOption = option.trim();
+                if (trimmedOption.equals(IGNORE_FAV_INPUT)) {
+                    isFavIgnored = FAV_IGNORED;
+                } else if (trimmedOption.equals(REVERSE_ORDER_INPUT)) {
+                    isReverseOrder = REVERSE_ORDER;
+                } else if (!trimmedOption.isEmpty()) {
+                    throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+                }
+            }
+        }
+        
         switch (dataFieldToSortByFirst) {
         case DATA_FIELD_NAME:
         case DATA_FIELD_PHONE:
         case DATA_FIELD_EMAIL:
         case DATA_FIELD_ADDRESS:
-            if (ignoreFavOption == null) {
-                return new SortCommand(dataFieldToSortByFirst, FAV_NOT_IGNORED);
-            } else {
-                return new SortCommand(dataFieldToSortByFirst, FAV_IGNORED);
-            }
+                return new SortCommand(dataFieldToSortByFirst, isFavIgnored);
         default:
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
