9e1bb93a633870fb60ab8c20cfab4e13ab7ef552
jin-ting <e0104957@u.nus.edu>
2017-11-08 21:17:54 +0800

    Create EmailCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/EmailCommandParser.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.EmailCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+//@@author jin-ting
+
+/**
+ * Parses input arguments and creates a new HelpCommand object
+ */
+public class EmailCommandParser implements Parser<EmailCommand> {
+}
