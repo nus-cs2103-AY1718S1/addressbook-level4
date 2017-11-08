1862dde4a9e9c19d4066e0fa9cf6df975f20e7ff
denna <dennaloh@gmail.com>
2017-11-08 23:43:34 +0800

    edited UG, DG, PPP and removed some unused import statements in ModelManager

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -2,10 +2,8 @@ package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.commons.util.StringUtil.containsWordIgnoreCase;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
