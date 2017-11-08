5726065ff368627da8ecc88e5833fbdc4d70ca5c
17navasaw <17navasaw@gmail.com>
2017-11-09 01:25:54 +0800

    Fixed checkstyle error

--- src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
