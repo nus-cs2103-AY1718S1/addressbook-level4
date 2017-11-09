c364a031f8f622cfb844cb6208ae71230ff9a874
peiquan <peiquan@hiei.local>
2017-11-09 08:57:22 +0800

    remove unused import

--- src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
@@ -27,7 +27,6 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.person.exceptions.TagNotFoundException;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.task.ReadOnlyTask;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.exceptions.DuplicateTaskException;
