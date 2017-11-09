7072d9bb8f320bec63270d7db990468f5548b0a1
DESKTOP-9RFF1NR\Ernest Ong YuHeng <E0031105@u.nus.edu>
2017-11-09 10:52:36 +0800

    Update consistency of tags to have first letter of word to be capitalized and the rest small letters. Update the order of result displayed in the display bar. Update collated code accordingly. Update User guide. Update portfolio.

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -36,7 +36,16 @@ public class Tag {
         if (!isValidTagName(trimmedName)) {
             throw new IllegalValueException(MESSAGE_TAG_CONSTRAINTS);
         }
-        this.tagName = trimmedName;
+
+        // To make the first letter of each word capital letter and the rest lower case
+        String[] arr = trimmedName.toLowerCase().split(" ");
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)))
+                    .append(arr[i].substring(1)).append(" ");
+        }
+        this.tagName = sb.toString().trim();
         this.tagColor = tagColor;
     }
 
