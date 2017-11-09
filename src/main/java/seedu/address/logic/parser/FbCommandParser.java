14955a1ada7df0c948e70ea2a3cb629fcc752e34
denna <dennaloh@gmail.com>
2017-11-09 16:02:37 +0800

    updated documentation and a few minor changes in some parser classes

--- src/main/java/seedu/address/logic/parser/FbCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FbCommandParser.java
@@ -21,12 +21,13 @@ public class FbCommandParser implements Parser<FbCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FbCommand parse(String args) throws ParseException {
+
         try {
             Index index = ParserUtil.parseIndex(args);
             return new FbCommand(index);
         } catch (IllegalValueException ive) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX));
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FbCommand.MESSAGE_USAGE));
         }
     }
 }
