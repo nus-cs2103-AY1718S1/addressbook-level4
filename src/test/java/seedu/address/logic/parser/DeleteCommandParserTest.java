1641fc565dcbe950ce60d12777ae89628317d39d
marvinchin <chinkunsong.marvin@gmail.com>
2017-11-09 23:25:24 +0800

    Refactor delete command parser and related tests

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -27,28 +27,43 @@ public class DeleteCommandParserTest {
 
     //@@author marvinchin
     @Test
-    public void parse_emptyTagArgs_throwsParseException() {
-        assertParseFailure(parser, "-tag    ",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+    public void parse_moreThanOneOption_throwsParseException() {
+        String input = "-tag -hello";
+        assertParseFailure(parser, input, DeleteCommandParser.INVALID_DELETE_COMMAND_FORMAT_MESSAGE);
+    }
+
+    @Test
+    public void parse_invalidOption_throwsParseException() {
+        String input = "-hello";
+        assertParseFailure(parser, input, DeleteCommandParser.INVALID_DELETE_COMMAND_FORMAT_MESSAGE);
+    }
+
+    @Test
+    public void parse_tagOptionNoArgs_throwsParseException() {
+        String input = "-tag    ";
+        assertParseFailure(parser, input, DeleteCommandParser.INVALID_DELETE_COMMAND_FORMAT_MESSAGE);
     }
 
     @Test
     public void parse_validTagArgs_returnsDeleteCommand() {
         HashSet<String> keys = new HashSet<>(Arrays.asList("friends", "colleagues"));
         DeleteCommand expectedDeleteCommand = new DeleteByTagCommand(keys);
-        assertParseSuccess(parser, "-tag colleagues friends", expectedDeleteCommand);
-        assertParseSuccess(parser, "-tag   \t friends \t\t\n colleagues", expectedDeleteCommand);
+        String input = "-tag colleagues friends";
+        String inputWithWhitespace = "-tag   \t friends \t\t\n colleagues";
+        assertParseSuccess(parser, input, expectedDeleteCommand);
+        assertParseSuccess(parser, inputWithWhitespace, expectedDeleteCommand);
     }
     //@@author
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteByIndexCommand(Arrays.asList(INDEX_FIRST_PERSON)));
+    public void parse_noOptionValidArgs_returnsDeleteCommand() {
+        String input = "1";
+        assertParseSuccess(parser, input, new DeleteByIndexCommand(Arrays.asList(INDEX_FIRST_PERSON)));
     }
 
     @Test
-    public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteByIndexCommand.MESSAGE_USAGE));
+    public void parse_noOptionInvalidArgs_throwsParseException() {
+        String input = "a";
+        assertParseFailure(parser, input, DeleteCommandParser.INVALID_DELETE_COMMAND_FORMAT_MESSAGE);
     }
 }
