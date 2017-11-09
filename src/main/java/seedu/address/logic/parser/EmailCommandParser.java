f5c31110e681b56ca32f9e9dcf5acd04edba01d8
Alex Ang <E0032221@u.nus.edu>
2017-11-09 09:23:36 +0800

    Add email command argument parser for suggestions

--- src/main/java/seedu/address/logic/parser/EmailCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EmailCommandParser.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static seedu.address.logic.parser.ParserUtil.isParsableInt;
+import static seedu.address.logic.parser.ParserUtil.parseFirstInt;
+import static seedu.address.logic.parser.ParserUtil.parseRemoveFirstInt;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -30,6 +33,25 @@ public class EmailCommandParser implements Parser<EmailCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, EmailCommand.MESSAGE_USAGE));
         }
+    }
+
+    /**
+     * Returns a formatted argument string given unformatted
+     * {@code commandWord} and {@code rawArgs}
+     * or a {@code null} {@code String} if not formattable.
+     */
+    public static String parseArguments(String commandWord, String rawArgs) {
+        // Check if index (number) exists, removes Remark prefix (if it exists) and re-adds it before returning.
+        if (isParsableInt(rawArgs)) {
+            String indexString = Integer.toString(parseFirstInt(rawArgs));
+            String subject = parseRemoveFirstInt(rawArgs).trim().replace(PREFIX_SUBJECT.toString(), "");
+            return " " + indexString + " " + PREFIX_SUBJECT + subject;
+        } else if (isParsableInt(commandWord)) {
+            String indexString = Integer.toString(parseFirstInt(commandWord));
+            String subject = rawArgs.trim().replace(PREFIX_SUBJECT.toString(), "");
+            return " " + indexString + " " + PREFIX_SUBJECT + subject;
+        }
+        return null;
     }
 
 }
