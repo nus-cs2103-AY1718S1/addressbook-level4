793dfa48d5cf605fc03ad939c746a4a8d2876142
Yuchen He <yuchenhe@206-104.priv23.nus.edu.sg>
2017-11-10 01:20:46 +0800

    visualize command

--- src/test/java/seedu/address/logic/commands/VisualizeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/VisualizeCommandTest.java
@@ -1,141 +1,49 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.ui.testutil.EventsCollectorRule;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.schedule.Day;
+import seedu.address.model.schedule.Slot;
+import seedu.address.model.schedule.Time;
 
 /**
- * Contains integration tests (interaction with the Model) for {@code VisualizeCommand}.
+ * Arrange a meeting test. As all the tests for date and time classes are added independently, this test only includes
+ * the command test with valid time and date form.
  */
 public class VisualizeCommandTest {
-    @Rule
-    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     private Model model;
+    private Model expectedModel;
+    private VisualizeCommand visualizeCommand;
 
     @Before
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    }
-
-    @Test
-    public void execute_validIndexUnfilteredList_success() {
-        Index lastPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size());
-
-        assertExecutionSuccess(INDEX_FIRST_PERSON);
-        assertExecutionSuccess(INDEX_THIRD_PERSON);
-        assertExecutionSuccess(lastPersonIndex);
-    }
-
-    @Test
-    public void execute_invalidIndexUnfilteredList_failure() {
-        Index outOfBoundsIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-
-        assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-    }
-
-    @Test
-    public void execute_validIndexFilteredList_success() {
-        showFirstPersonOnly(model);
-
-        assertExecutionSuccess(INDEX_FIRST_PERSON);
-    }
-
-    @Test
-    public void execute_invalidIndexFilteredList_failure() {
-        showFirstPersonOnly(model);
-
-        Index outOfBoundsIndex = INDEX_SECOND_PERSON;
-        // ensures that outOfBoundIndex is still in bounds of address book list
-        assertTrue(outOfBoundsIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
-
-        assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-    }
-
-    @Test
-    public void equals() {
-        SelectCommand selectFirstCommand = new SelectCommand(INDEX_FIRST_PERSON);
-        SelectCommand selectSecondCommand = new SelectCommand(INDEX_SECOND_PERSON);
-
-        // same object -> returns true
-        assertTrue(selectFirstCommand.equals(selectFirstCommand));
-
-        // same values -> returns true
-        SelectCommand selectFirstCommandCopy = new SelectCommand(INDEX_FIRST_PERSON);
-        assertTrue(selectFirstCommand.equals(selectFirstCommandCopy));
-
-        // different types -> returns false
-        assertFalse(selectFirstCommand.equals(1));
-
-        // null -> returns false
-        assertFalse(selectFirstCommand.equals(null));
-
-        // different person -> returns false
-        assertFalse(selectFirstCommand.equals(selectSecondCommand));
-    }
-
-    /**
-     * Executes a {@code SelectCommand} with the given {@code index}, and checks that {@code JumpToListRequestEvent}
-     * is raised with the correct index.
-     */
-    private void assertExecutionSuccess(Index index) {
-        VisualizeCommand visualizeCommand = prepareCommand(index);
-
-        try {
-            CommandResult commandResult = visualizeCommand.execute();
-            assertEquals(String.format(VisualizeCommand.MESSAGE_VISUALIZE_PERSON_SUCCESS, index.getOneBased()),
-                    commandResult.feedbackToUser);
-        } catch (CommandException ce) {
-            throw new IllegalArgumentException("Execution of command should not fail.", ce);
-        }
-
-        JumpToListRequestEvent lastEvent = (JumpToListRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
-        assertEquals(index, Index.fromZeroBased(lastEvent.targetIndex));
-    }
-
-    /**
-     * Executes a {@code VisualizeCommand} with the given {@code index}, and checks that a {@code CommandException}
-     * is thrown with the {@code expectedMessage}.
-     */
-    private void assertExecutionFailure(Index index, String expectedMessage) {
-        VisualizeCommand visualizeCommand = prepareCommand(index);
-
-        try {
-            visualizeCommand.execute();
-            fail("The expected CommandException was not thrown.");
-        } catch (CommandException ce) {
-            assertEquals(expectedMessage, ce.getMessage());
-            assertTrue(eventsCollectorRule.eventsCollector.isEmpty());
-        }
-    }
-
-    /**
-     * Returns a {@code VisualizeCommand} with parameters {@code index}.
-     */
-    private VisualizeCommand prepareCommand(Index index) {
-        VisualizeCommand visualizeCommand = new VisualizeCommand(index);
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        visualizeCommand = new VisualizeCommand(Index.fromOneBased(1));
         visualizeCommand.setData(model, new CommandHistory(), new UndoRedoStack());
-        return visualizeCommand;
+    }
+
+    @Test
+    public void findCommonSlotSuccess() throws IllegalValueException, PersonNotFoundException {
+        Slot FirstSlot = new Slot(new Day("Monday"), new Time("0700"), new Time("1000"));
+        Slot SecondSlot = new Slot(new Day("Tuesday"), new Time("0930"), new Time("1100"));
+        model.addScheduleToPerson(0, FirstSlot.getBusyTime());
+        model.addScheduleToPerson(0, SecondSlot.getBusyTime());
+        assertCommandSuccess(visualizeCommand, model, VisualizeCommand.MESSAGE_VISUALIZE_PERSON_SUCCESS
+                + "1" + visualizeCommand.scheduleInfo(), expectedModel);
     }
 }
+
