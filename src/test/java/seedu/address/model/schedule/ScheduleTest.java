2f09906597cd26cf318b9ab79a6ce6462dabba28
Yuchen He <yuchenhe@206-104.priv23.nus.edu.sg>
2017-11-10 00:37:46 +0800

    schedule test

--- src/test/java/seedu/address/model/schedule/ScheduleTest.java
+++ src/test/java/seedu/address/model/schedule/ScheduleTest.java
@@ -1,4 +1,30 @@
 package seedu.address.model.schedule;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import seedu.address.commons.exceptions.IllegalValueException;
+
+import java.util.TreeSet;
+
 public class ScheduleTest {
+
+    @Test
+    public void isValidSchedule() throws IllegalValueException {
+
+        TreeSet<Integer> timeSet = new TreeSet<>();
+        timeSet.add(10630);
+        timeSet.add(20700);
+        timeSet.add(30730);
+        timeSet.add(80730);
+        assertFalse(Schedule.isValidTimeSet(timeSet));
+        timeSet.remove(80730);
+        assertTrue(Schedule.isValidTimeSet(timeSet));
+        Schedule testSchedule = new Schedule(timeSet);
+        TreeSet[] tables = testSchedule.splitScheduleToDays();
+        assertTrue(tables[0].contains(630));
+        assertTrue(tables[1].contains(700));
+        assertTrue(tables[2].contains(730));
+    }
 }
