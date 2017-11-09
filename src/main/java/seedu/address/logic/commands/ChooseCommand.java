4d4cc7b9e177113c182a2f167422655a4adcd2f9
Srivatsa <p.psrivatsa@gmail.com>
2017-11-09 22:45:00 +0800

    Update ChooseCommand.java to take in 'google' as an argument

--- src/main/java/seedu/address/logic/commands/ChooseCommand.java
+++ src/main/java/seedu/address/logic/commands/ChooseCommand.java
@@ -37,7 +37,7 @@ public class ChooseCommand extends Command {
         if (targetDisplay.equals("meeting")) {
             EventsCenter.getInstance().post(new ShowMeetingEvent());
             EventsCenter.getInstance().post(new JumpToBrowserListRequestEvent(targetDisplay));
-        } else if (targetDisplay.equals("linkedin") || targetDisplay.equals("facebook")) {
+        } else if (targetDisplay.equals("linkedin") || targetDisplay.equals("google")) {
             EventsCenter.getInstance().post(new ShowBrowserEvent());
             EventsCenter.getInstance().post(new JumpToBrowserListRequestEvent(targetDisplay));
         } else {
