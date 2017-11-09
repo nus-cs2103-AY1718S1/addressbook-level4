d6a77e7a83d0954e0419037f26c675366ab8e117
MEL5469-LAPTOP\Mel54 <mel5469leo@hotmail.com>
2017-11-09 19:48:10 +0800

    Added meetin card with group meeting itself

--- src/main/java/seedu/address/logic/commands/SelectMeetingCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectMeetingCommand.java
@@ -6,6 +6,7 @@ import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.events.ui.JumpToMeetingListRequestEvent;
+import seedu.address.commons.events.ui.MeetingSelectionEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.meeting.ReadOnlyMeeting;
 
