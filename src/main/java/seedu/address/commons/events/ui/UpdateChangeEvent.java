b5ca32480a7a1ce0eebc4caaad2ec84f3497cd17
MEL5469-LAPTOP\Mel54 <mel5469leo@hotmail.com>
2017-11-09 23:38:46 +0800

    Edit meeting upgrade

--- src/main/java/seedu/address/commons/events/ui/UpdateChangeEvent.java
+++ src/main/java/seedu/address/commons/events/ui/UpdateChangeEvent.java
@@ -1,4 +1,14 @@
 package seedu.address.commons.events.ui;
 
-public class updateChangeEvent {
+import seedu.address.commons.events.BaseEvent;
+
+/**
+ * Create an event to handle the updates of meeting list when persons are handled
+ */
+public class UpdateChangeEvent extends BaseEvent {
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
 }
