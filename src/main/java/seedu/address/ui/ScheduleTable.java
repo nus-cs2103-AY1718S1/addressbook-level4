aec2be0c633bd25fabf854536a043a19d98f4e50
Yuchen He <yuchenhe@Yuchens-MBP.workgroup>
2017-11-10 07:18:25 +0800

    add comments

--- src/main/java/seedu/address/ui/ScheduleTable.java
+++ src/main/java/seedu/address/ui/ScheduleTable.java
@@ -1,9 +1,5 @@
 package seedu.address.ui;
 
-import seedu.address.commons.core.PossibleTimes;
-import seedu.address.model.schedule.Schedule;
-import seedu.address.model.schedule.Time;
-
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -11,8 +7,15 @@ import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
 import java.util.TreeSet;
 
+import seedu.address.commons.core.PossibleTimes;
+import seedu.address.model.schedule.Schedule;
+import seedu.address.model.schedule.Time;
 
+/**
+ * A ui used in ArrangeCommand and VisualizeCommand to show the schedule available.
+ */
 public class ScheduleTable extends JFrame {
+
     public ScheduleTable(Schedule schedule) {
         TreeSet[] timeSetArray = schedule.splitScheduleToDays();
         construct(timeSetArray);
@@ -22,6 +25,9 @@ public class ScheduleTable extends JFrame {
         construct(timeSetArray);
     }
 
+    /**
+     * Constructs the timetable when given the array of timeSet representing days from Monday to Sunday.
+     */
     private void construct(TreeSet[] timeSetArray) {
         String[] columnNames = {"", "Monday", "Tuesday" , "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
         Object[][] data = new Object[36][8];
@@ -36,7 +42,7 @@ public class ScheduleTable extends JFrame {
         }
         for (int j = 1; j <= timeSetArray.length; j++) {
             for (int k = 0; k < PossibleTimes.TIMES.length; k++) {
-                if (timeSetArray[j-1].contains(PossibleTimes.TIMES[k])) {
+                if (timeSetArray[j - 1].contains(PossibleTimes.TIMES[k])) {
                     data[k][j] = "AVAILABLE";
                 } else {
                     data[k][j] = "N";
@@ -45,7 +51,7 @@ public class ScheduleTable extends JFrame {
         }
 
         DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        JTable table = new JTable( model )
+        JTable table = new JTable(model)
         {
             //  Returning the Class of each column will allow different
             //  renderers to be used based on Class
@@ -57,37 +63,37 @@ public class ScheduleTable extends JFrame {
         table.setShowHorizontalLines(true);
         table.setPreferredScrollableViewportSize(table.getPreferredSize());
 
-        JScrollPane scrollPane = new JScrollPane( table );
-        getContentPane().add( scrollPane );
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane);
 
     }
 
+    /**
+     * Launch the ui based on schedule.
+     */
     public static void generates(Schedule schedule) {
-        try
-        {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch(Exception e)
-        {
+        } catch(Exception e) {
             e.printStackTrace();
         }
         ScheduleTable frame = new ScheduleTable(schedule);
-        frame.setDefaultCloseOperation( HIDE_ON_CLOSE);
+        frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
+    /**
+     * Launch the ui based on array of time set.
+     */
     public static void generates(TreeSet[] timeSetArray) {
-        try
-        {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch(Exception e)
-        {
+        } catch(Exception e) {
             e.printStackTrace();
         }
         ScheduleTable frame = new ScheduleTable(timeSetArray);
-        frame.setDefaultCloseOperation( HIDE_ON_CLOSE);
+        frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
