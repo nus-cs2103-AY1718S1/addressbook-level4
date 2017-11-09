1f24a76daa45a479ba8c97f2bfa97d7ed154da1d
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-11-10 04:03:38 +0800

    Transfer some BrowserPanel code to MapPanel

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -12,8 +12,6 @@ import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.LocateCommandEvent;
-import seedu.address.commons.events.ui.LocateMrtCommandEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -24,8 +22,6 @@ public class BrowserPanel extends UiPart<Region> {
 
     public static final String DEFAULT_PAGE = "default.html";
     public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
-    public static final String GOOGLE_MAP_URL_PREFIX = "https://www.google.com.sg/maps/search/";
-    public static final String GOOGLE_MRT_URL_SUFFIX = "+MRT";
     public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
 
     private static final String FXML = "BrowserPanel.fxml";
@@ -48,17 +44,6 @@ public class BrowserPanel extends UiPart<Region> {
     private void loadPersonPage(ReadOnlyPerson person) {
         loadPage(GOOGLE_SEARCH_URL_PREFIX + person.getName().fullName.replaceAll(" ", "+")
                 + GOOGLE_SEARCH_URL_SUFFIX);
-    }
-
-    //@@author YuchenHe98
-    private void loadLocationPage(ReadOnlyPerson person) {
-        loadPage(GOOGLE_MAP_URL_PREFIX + person.getAddress().value.replaceAll(" ", "+"));
-    }
-    //@@author
-
-    private void loadMrtPage(ReadOnlyPerson person) {
-        loadPage(GOOGLE_MAP_URL_PREFIX + person.getMrt().value.replaceAll("", "+")
-                + GOOGLE_MRT_URL_SUFFIX);
     }
 
     public void loadPage(String url) {
@@ -84,19 +69,5 @@ public class BrowserPanel extends UiPart<Region> {
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         loadPersonPage(event.getNewSelection().person);
-    }
-
-    //@@author YuchenHe98
-    @Subscribe
-    private void handleLocateCommandEvent(LocateCommandEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        loadLocationPage(event.getPerson());
-    }
-    //@@author
-
-    @Subscribe
-    private void handleLocateMrtCommandEvent(LocateMrtCommandEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        loadMrtPage(event.getPerson());
     }
 }
