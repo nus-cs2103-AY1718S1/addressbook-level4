9853a0146f703b359d2d5d87742e8fdb8cea8772
glenice <glenicetan@hotmail.com>
2017-11-09 23:10:54 +0800

    Update on picture backup test in StorageManagerTest

--- src/test/java/seedu/room/storage/StorageManagerTest.java
+++ src/test/java/seedu/room/storage/StorageManagerTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.room.testutil.TypicalPersons.getTypicalResidentBook;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,6 +20,7 @@ import seedu.room.model.ReadOnlyEventBook;
 import seedu.room.model.ReadOnlyResidentBook;
 import seedu.room.model.ResidentBook;
 import seedu.room.model.UserPrefs;
+import seedu.room.model.person.Picture;
 import seedu.room.ui.testutil.EventsCollectorRule;
 
 public class StorageManagerTest {
@@ -28,6 +31,7 @@ public class StorageManagerTest {
     public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     private StorageManager storageManager;
+    private File imageFolder;
 
     @Before
     public void setUp() {
@@ -41,7 +45,21 @@ public class StorageManagerTest {
         return testFolder.getRoot().getPath() + fileName;
     }
 
+    //@@author blackroxs
+    private void setupTempPictureFile() throws IOException {
+        File source = new File(storageManager.getDirAbsolutePath()
+                + File.separator + Picture.FOLDER_NAME);
+        File dest = new File(storageManager.getDirAbsolutePath()
+                + File.separator + Picture.FOLDER_NAME + "_backup");
 
+        source.mkdirs();
+        dest.mkdirs();
+        source.deleteOnExit();
+        dest.deleteOnExit();
+    }
+
+
+    //@@author
     @Test
     public void prefsReadSave() throws Exception {
         /*
@@ -84,19 +102,6 @@ public class StorageManagerTest {
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof DataSavingExceptionEvent);
     }
 
-    @Test
-    public void handleBackupResidentBook() throws Exception {
-        ResidentBook original = getTypicalResidentBook();
-        storageManager.saveResidentBook(original);
-        ReadOnlyResidentBook retrieved = storageManager.readResidentBook().get();
-
-        storageManager.backupResidentBook(retrieved);
-        ReadOnlyResidentBook backup = storageManager.readBackupResidentBook().get();
-
-        assertEquals(new ResidentBook(retrieved), new ResidentBook(backup));
-    }
-
-
     /**
      * A Stub class to throw an exception when the save method is called
      */
@@ -113,6 +118,7 @@ public class StorageManagerTest {
     }
 
     //@@author sushinoya
+
     /**
      * A Stub class to throw an exception when the save method is called
      */
@@ -126,6 +132,41 @@ public class StorageManagerTest {
         public void saveEventBook(ReadOnlyEventBook residentBook, String filePath) throws IOException {
             throw new IOException("dummy exception");
         }
+    }
+
+    //@@author blackroxs
+    @Test
+    public void handleBackupResidentBook() throws Exception {
+        ResidentBook original = getTypicalResidentBook();
+        storageManager.saveResidentBook(original);
+        ReadOnlyResidentBook retrieved = storageManager.readResidentBook().get();
+
+        storageManager.backupResidentBook(retrieved);
+        ReadOnlyResidentBook backup = storageManager.readBackupResidentBook().get();
+
+        assertEquals(new ResidentBook(retrieved), new ResidentBook(backup));
+    }
+
+    @Test
+    public void handleNoBackupImages() throws Exception {
+        storageManager.backupImages();
+
+        assertTrue(!new File(storageManager.getDirAbsolutePath()
+                + File.separator + Picture.FOLDER_NAME).exists());
+
+        assertTrue(!new File(storageManager.getDirAbsolutePath()
+                + File.separator + Picture.FOLDER_NAME + "_backup").exists());
+
+    }
+
+    @Test
+    public void handleBackupImagesValid() throws Exception {
+        setupTempPictureFile();
+
+        assertTrue(new File(storageManager.getDirAbsolutePath()
+                + File.separator + Picture.FOLDER_NAME).exists());
+        assertTrue(new File(storageManager.getDirAbsolutePath()
+                + File.separator + Picture.FOLDER_NAME + "_backup").exists());
     }
 
 
