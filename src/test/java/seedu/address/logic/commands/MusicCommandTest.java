fac9d71bf39906c72881b3d8a6a8e79cdd63dc4f
hanselblack <hanselblack@hotmail.com>
2017-11-09 22:20:24 +0800

    Logic: Update test for MusicCommandTest
    Logic: Update test for MusicCommandTest

--- src/test/java/seedu/address/logic/commands/MusicCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MusicCommandTest.java
@@ -3,20 +3,40 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.MusicCommand.MESSAGE_USAGE;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javafx.embed.swing.JFXPanel;
+import guitests.guihandles.MainWindowHandle;
+import seedu.address.TestApp;
+import systemtests.SystemTestSetupHelper;
 
 //@@author hanselblack
 public class MusicCommandTest {
 
+    private MainWindowHandle mainWindowHandle;
+    private TestApp testApp;
+    private SystemTestSetupHelper setupHelper;
+
+    @BeforeClass
+    public static void setupBeforeClass() {
+        SystemTestSetupHelper.initializeStage();
+    }
+
+    @Before
+    public void setUp() {
+        setupHelper = new SystemTestSetupHelper();
+        testApp = setupHelper.setupApplication();
+        mainWindowHandle = setupHelper.setupMainWindowHandle();
+    }
+
     @Test
-    public void execute_share_success() {
-        final JFXPanel fxPanel = new JFXPanel();
-        MusicCommand musicCommand = new MusicCommand("play");
+    public void execute_music_wrongGenre() {
+        MusicCommand musicCommand = new MusicCommand("play", "nonExistedGenre");
         CommandResult commandResult = musicCommand.execute();
-        assertEquals("POP Music Playing", commandResult.feedbackToUser);
+        assertEquals(MESSAGE_USAGE, commandResult.feedbackToUser);
     }
     @Test
     public void equals() {
