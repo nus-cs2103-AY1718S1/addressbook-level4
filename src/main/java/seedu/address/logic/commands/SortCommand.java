0ae18e9b58d66d6f946b84770ca64d1d6f7587ed
Hou Denghao <apple@ChuckHou-MacPro.local>
2017-11-10 11:37:50 +0800

    Fix SortCommad

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,8 +1,5 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
 // @@author HouDenghao
 /**
  * Sorts all persons in the address book to the user.
@@ -17,8 +14,6 @@ public class SortCommand extends Command {
     public CommandResult execute() {
         model.sortPersons();
         model.sortEvents();
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
