1dc959d35d5333dcef183d401a39c7e1792a2417
Jelena <neohuilingjelena@gmail.com>
2017-11-10 11:06:09 +0800

    LoginCommandParser.java: create overloaded parses method for login GUI

--- src/main/java/seedu/address/logic/parser/LoginCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoginCommandParser.java
@@ -16,10 +16,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class LoginCommandParser implements Parser<LoginCommand> {
 
     public static final String ONE_OR_MORE_SPACES_REGEX = "\\s+";
+    public static final String EMPTY_USERNAME_MESSAGE = "Username cannot be empty";
+    public static final String EMPTY_PASSWORD_MESSAGE = "Password cannot be empty";
 
     /**
      * Parses the given {@code String} of arguments in the context of the LoginCommand
-     * and returns an LoginCommand object for execution.
+     * and returns a LoginCommand object for execution.
+     * This method is used to parse arguments for CLI login.
      * @throws ParseException if the user input does not conform to the expected format
      */
     public LoginCommand parse(String args) throws ParseException {
@@ -40,6 +43,30 @@ public class LoginCommandParser implements Parser<LoginCommand> {
         } catch (ArrayIndexOutOfBoundsException aiofoe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
+        }
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the LoginCommand
+     * and returns an LoginCommand object for execution.
+     * This method is used to parse arguments for GUI login.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    public LoginCommand parse(String enteredUsername, String enteredPassword) throws ParseException {
+        requireNonNull(enteredUsername);
+        requireNonNull(enteredPassword);
+        if (enteredUsername.isEmpty()) {
+            throw new ParseException(EMPTY_USERNAME_MESSAGE);
+        }
+        if (enteredPassword.isEmpty()) {
+            throw new ParseException(EMPTY_PASSWORD_MESSAGE);
+        }
+        try {
+            Username username = new Username(enteredUsername);
+            Password password = new Password(enteredPassword);
+            return new LoginCommand(username, password);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(ive.getMessage());
         }
     }
 }
