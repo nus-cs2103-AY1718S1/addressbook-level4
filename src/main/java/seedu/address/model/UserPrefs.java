cee7645eab93dffa8bfd6d4a39f054b86595e505
khooroko <khoowp25@hotmail.com>
2017-11-10 12:24:41 +0800

    Fix tests

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -22,7 +22,7 @@ public class UserPrefs {
     private String passwordSaltInString = "KeAnoJrBdpqc0AnhiZDSPw==";
 
     public UserPrefs() {
-        setDefaultGuiSettings();
+        setGuiSettings(1700, 980, 0, 0);
     }
 
     public GuiSettings getGuiSettings() {
@@ -31,10 +31,6 @@ public class UserPrefs {
 
     public void updateLastUsedGuiSetting(GuiSettings guiSettings) {
         this.guiSettings = guiSettings;
-    }
-
-    public void setDefaultGuiSettings() {
-        guiSettings = new GuiSettings();
     }
 
     public void setGuiSettings(double width, double height, int x, int y) {
