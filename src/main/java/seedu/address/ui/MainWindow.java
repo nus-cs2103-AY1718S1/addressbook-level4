4f9cd696d42478e14625a538c2daee3a7ca6d12f
deep4k <e0031591@u.nus.edu>
2017-11-10 11:20:50 +0800

    Fix icon size by removing WindowMinSize()

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -130,7 +130,6 @@ public class MainWindow extends UiPart<Region> {
         // Configure the UI
         setTitle(config.getAppTitle());
         setIcon(ICON);
-        setWindowMinSize();
         setWindowDefaultSize(prefs);
         Scene scene = new Scene(getRoot());
         primaryStage.setScene(scene);
