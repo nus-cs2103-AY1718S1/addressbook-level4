3974d2bc1077f5231e5291f30f4e092ca1fd67b9
denna <dennaloh@gmail.com>
2017-11-10 23:38:51 +0800

    removed some test methods

--- src/test/java/seedu/address/logic/commands/FbCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FbCommandTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.fail;
 import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Before;
@@ -16,7 +15,6 @@ import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -41,26 +39,10 @@ public class FbCommandTest {
     }
 
     @Test
-    public void execute_validIndexUnfilteredList_success() {
-        Index lastPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size());
-
-        assertExecutionSuccess(INDEX_FIRST_PERSON);
-        assertExecutionSuccess(INDEX_THIRD_PERSON);
-        assertExecutionSuccess(lastPersonIndex);
-    }
-
-    @Test
     public void execute_invalidIndexUnfilteredList_failure() {
         Index outOfBoundsIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
 
         assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-    }
-
-    @Test
-    public void execute_validIndexFilteredList_success() {
-        showFirstPersonOnly(model);
-
-        assertExecutionSuccess(INDEX_FIRST_PERSON);
     }
 
     @Test
@@ -94,25 +76,6 @@ public class FbCommandTest {
 
         // different person -> returns false
         assertFalse(fbFirstCommand.equals(fbSecondCommand));
-    }
-
-    /**
-     * Executes a {@code FbCommand} with the given {@code index}, and checks that {@code JumpToListRequestEvent}
-     * is raised with the correct index.
-     */
-    private void assertExecutionSuccess(Index index) {
-        FbCommand fbCommand = prepareCommand(index);
-
-        try {
-            CommandResult commandResult = fbCommand.execute();
-            assertEquals(String.format(FbCommand.MESSAGE_SUCCESS, index.getOneBased()),
-                    commandResult.feedbackToUser);
-        } catch (CommandException ce) {
-            throw new IllegalArgumentException("Execution of command should not fail.", ce);
-        }
-
-        JumpToListRequestEvent lastEvent = (JumpToListRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
-        assertEquals(index, Index.fromZeroBased(lastEvent.targetIndex));
     }
 
     /**
--- src/test/java/seedu/address/logic/commands/person/GMapCommandTest.java
+++ src/test/java/seedu/address/logic/commands/person/GMapCommandTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.fail;
 import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Before;
@@ -16,10 +15,8 @@ import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -42,26 +39,10 @@ public class GMapCommandTest {
     }
 
     @Test
-    public void execute_validIndexUnfilteredList_success() {
-        Index lastPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size());
-
-        assertExecutionSuccess(INDEX_FIRST_PERSON);
-        assertExecutionSuccess(INDEX_THIRD_PERSON);
-        assertExecutionSuccess(lastPersonIndex);
-    }
-
-    @Test
     public void execute_invalidIndexUnfilteredList_failure() {
         Index outOfBoundsIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
 
         assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-    }
-
-    @Test
-    public void execute_validIndexFilteredList_success() {
-        showFirstPersonOnly(model);
-
-        assertExecutionSuccess(INDEX_FIRST_PERSON);
     }
 
     @Test
@@ -95,25 +76,6 @@ public class GMapCommandTest {
 
         // different person -> returns false
         assertFalse(gMapFirstCommand.equals(gmapSecondCommand));
-    }
-
-    /**
-     * Executes a {@code GMapCommand} with the given {@code index}, and checks that {@code JumpToListRequestEvent}
-     * is raised with the correct index.
-     */
-    private void assertExecutionSuccess(Index index) {
-        GMapCommand gMapCommand = prepareCommand(index);
-
-        try {
-            CommandResult commandResult = gMapCommand.execute();
-            assertEquals(String.format(GMapCommand.MESSAGE_SUCCESS, index.getOneBased()),
-                    commandResult.feedbackToUser);
-        } catch (CommandException ce) {
-            throw new IllegalArgumentException("Execution of command should not fail.", ce);
-        }
-
-        JumpToListRequestEvent lastEvent = (JumpToListRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
-        assertEquals(index, Index.fromZeroBased(lastEvent.targetIndex));
     }
 
     /**
