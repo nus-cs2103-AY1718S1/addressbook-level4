3f64f7a488f6f1a03f355b1244bb56a21e10a711
Jelena <neohuilingjelena@gmail.com>
2017-11-11 00:21:01 +0800

    LoginCommandParserTest.java: update tests for gui

--- src/test/java/seedu/address/logic/parser/LoginCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/LoginCommandParserTest.java
@@ -2,29 +2,29 @@ package seedu.address.logic.parser;
 
 import static guitests.guihandles.MainWindowHandle.TEST_PASSWORD;
 import static guitests.guihandles.MainWindowHandle.TEST_USERNAME;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Password.MESSAGE_PASSWORD_LENGTH_CONSTRAINTS;
 import static seedu.address.logic.Username.MESSAGE_USERNAME_CHARACTERS_CONSTRAINTS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.logic.parser.LoginCommandParser.EMPTY_PASSWORD_MESSAGE;
 import static seedu.address.logic.parser.LoginCommandParser.EMPTY_USERNAME_MESSAGE;
+import static seedu.address.ui.LoginView.setShowingLoginView;
 
 import org.junit.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.Password;
 import seedu.address.logic.Username;
-import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.LoginCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 
 //@@author jelneo
 public class LoginCommandParserTest {
     private static final String VALID_USERNAME = "User_1234";
     private static final String VALID_PASSWORD = "P@$$worD";
+    private static final String GUI_LOGIN_ARGS = " " + PREFIX_USERNAME + "%1$s" + " " + PREFIX_PASSWORD + "%2$s";;
 
     private LoginCommandParser parser = new LoginCommandParser();
 
@@ -45,7 +45,7 @@ public class LoginCommandParserTest {
     }
 
     @Test
-    public void cliParse_invalidArgs_returnsLoginCommand() {
+    public void cliParse_invalidArgs() {
         // empty input
         assertParseFailure(parser, "    ", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
@@ -59,52 +59,27 @@ public class LoginCommandParserTest {
 
     @Test
     public void guiParse_validArgs_returnsLoginCommand() {
+        setShowingLoginView(true);
+
         try {
             LoginCommand expectedLoginCommand =
                     new LoginCommand(new Username(VALID_USERNAME), new Password(VALID_PASSWORD));
 
-            assertParseSuccessForGui(parser, VALID_USERNAME, VALID_PASSWORD, expectedLoginCommand);
-
+            assertParseSuccess(parser,
+                    String.format(GUI_LOGIN_ARGS, VALID_USERNAME, VALID_PASSWORD), expectedLoginCommand);
         } catch (IllegalValueException ive) {
             ive.printStackTrace();
         }
     }
 
     @Test
-    public void guiParse_invalidArgs_returnsLoginCommand() {
+    public void guiParse_invalidArgs() {
+        setShowingLoginView(true);
+
         // no username
-        assertParseFailureForGui(parser, "", TEST_PASSWORD, EMPTY_USERNAME_MESSAGE);
+        assertParseFailure(parser, String.format(GUI_LOGIN_ARGS, "", TEST_PASSWORD), EMPTY_USERNAME_MESSAGE);
 
         // no password
-        assertParseFailureForGui(parser, TEST_USERNAME, "" , EMPTY_PASSWORD_MESSAGE);
-    }
-
-
-    /**
-     * Asserts that the parsing of {@code userInput1} and {@code userInput2} by {@code parser} is
-     * unsuccessful and the error message equals to {@code expectedMessage}.
-     */
-    public static void assertParseFailureForGui(LoginCommandParser parser, String userInput1, String userInput2,
-                                          String expectedMessage) {
-        try {
-            parser.parse(userInput1, userInput2);
-            fail("The expected ParseException was not thrown.");
-        } catch (ParseException pe) {
-            assertEquals(expectedMessage, pe.getMessage());
-        }
-    }
-
-    /**
-     * Asserts that the parsing of {@code userInput1} and {@code userInput2} by {@code parser} is successful
-     * and the command created equals to {@code expectedCommand}.
-     */
-    public static void assertParseSuccessForGui(LoginCommandParser parser, String userInput1, String userInput2,
-                                          Command expectedCommand) {
-        try {
-            Command command = parser.parse(userInput1, userInput2);
-            assertEquals(expectedCommand, command);
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        assertParseFailure(parser, String.format(GUI_LOGIN_ARGS, TEST_USERNAME, ""), EMPTY_PASSWORD_MESSAGE);
     }
 }
