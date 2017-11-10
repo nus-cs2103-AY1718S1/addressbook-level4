4ba5b5f1c9392ba72e50670c517a159da2a99df8
caoliangnus <liangcao1213@yahoo.com>
2017-11-11 01:07:02 +0800

    Fixed gibberish text in HelpWindow

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -22,14 +22,12 @@ public class HelpWindow extends UiPart<Region> {
     private static final String ICON = "/images/help_icon.png";
     private static final String FXML = "HelpWindow.fxml";
     private static final String TITLE = "Help";
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
 
     @FXML
     private WebView browser;
 
     private final Stage dialogStage;
-    public  String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
-
-
 
     public HelpWindow() {
         super(FXML);
