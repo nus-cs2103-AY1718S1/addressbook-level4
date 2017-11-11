dcde13ce1e30f020490cbcf80a48c61cd84d4b4d
minato <dmc3hpcity@gmail.com>
2017-11-11 21:05:25 +0800

    ReminderCardTest: Add test for reminder card display

--- src/test/java/seedu/address/ui/ReminderCardTest.java
+++ src/test/java/seedu/address/ui/ReminderCardTest.java
@@ -1,14 +1,44 @@
 package seedu.address.ui;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static seedu.address.testutil.TypicalReminders.BIRTHDAY;
+import static seedu.address.ui.testutil.GuiTestAssert.assertCardDisplaysReminder;
 
 import org.junit.Test;
 
+import guitests.guihandles.ReminderCardHandle;
+import seedu.address.model.reminder.ReadOnlyReminder;
 import seedu.address.model.reminder.Reminder;
 import seedu.address.testutil.ReminderBuilder;
 
 public class ReminderCardTest extends GuiUnitTest {
+
+    @Test
+    public void display() {
+        // no tags
+        Reminder reminderWithNoTags = new ReminderBuilder().withTags(new String[0]).build();
+        ReminderCard reminderCard = new ReminderCard(reminderWithNoTags, 1);
+        uiPartRule.setUiPart(reminderCard);
+        assertCardDisplay(reminderCard, reminderWithNoTags, 1);
+
+        // with tags
+        Reminder reminderWithTags = new ReminderBuilder().build();
+        reminderCard = new ReminderCard(reminderWithTags, 2);
+        uiPartRule.setUiPart(reminderCard);
+        assertCardDisplay(reminderCard, reminderWithTags, 2);
+
+        // changes made to Reminder reflects on card
+        guiRobot.interact(() -> {
+            reminderWithTags.setTask(BIRTHDAY.getTask());
+            reminderWithTags.setPriority(BIRTHDAY.getPriority());
+            reminderWithTags.setDate(BIRTHDAY.getDate());
+            reminderWithTags.setMessage(BIRTHDAY.getMessage());
+            reminderWithTags.setTags(BIRTHDAY.getTags());
+        });
+        assertCardDisplay(reminderCard, reminderWithTags, 2);
+    }
 
     @Test
     public void equals() {
@@ -34,5 +64,21 @@ public class ReminderCardTest extends GuiUnitTest {
 
         // same reminder, different index -> returns false
         assertFalse(reminderCard.equals(new ReminderCard(reminder, 1)));
+    }
+
+    /**
+     * Asserts that {@code reminderCard} displays the details of {@code expectedReminder} correctly and matches
+     * {@code expectedId}.
+     */
+    private void assertCardDisplay(ReminderCard reminderCard, ReadOnlyReminder expectedReminder, int expectedId) {
+        guiRobot.pauseForHuman();
+
+        ReminderCardHandle reminderCardHandle = new ReminderCardHandle(reminderCard.getRoot());
+
+        // verify id is displayed correctly
+        assertEquals(Integer.toString(expectedId) + ". ", reminderCardHandle.getId());
+
+        // verify reminder details are displayed correctly
+        assertCardDisplaysReminder(expectedReminder, reminderCardHandle);
     }
 }
