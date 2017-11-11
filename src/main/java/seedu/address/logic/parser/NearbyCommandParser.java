2482c2e5ec33b158b106c7336d52e30be82d34d5
khooroko <khoowp25@hotmail.com>
2017-11-11 22:10:54 +0800

    Fix non-index-based commands

--- src/main/java/seedu/address/logic/parser/NearbyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/NearbyCommandParser.java
@@ -20,12 +20,8 @@ public class NearbyCommandParser implements Parser<NearbyCommand> {
      */
     public NearbyCommand parse(String args) throws ParseException {
         try {
-            if (args.trim().equals("")) {
-                return new NearbyCommand();
-            } else {
-                Index index = ParserUtil.parseIndex(args);
-                return new NearbyCommand(index);
-            }
+            Index index = ParserUtil.parseIndex(args);
+            return new NearbyCommand(index);
         } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, NearbyCommand.MESSAGE_USAGE));
