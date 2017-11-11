1ed7312b455ee1b5fa162961e112c8ad4298b67b
Ahchuang <E0036072@u.nus.edu>
2017-11-11 20:40:35 +0800

    Update style

--- src/test/java/seedu/address/logic/parser/UploadPhotoCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/UploadPhotoCommandParserTest.java
@@ -26,4 +26,5 @@ public class UploadPhotoCommandParserTest {
         assertParseFailure(parser, "a", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, UploadPhotoCommand.MESSAGE_USAGE));
     }
+
 }
