203f7d1c69d64cf613359e9b422fbad758a3ba3b
namjjd <namjjd@gmail.com>
2017-11-11 12:11:22 +0800

    add statistics command test

---
+++ src/test/java/seedu/address/logic/commands/StatisticsCommandTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import org.junit.Rule;
+import org.junit.Test;
+import seedu.address.commons.events.ui.ToggleStatisticsPanelEvent;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.StatisticsCommand.MESSAGE_SUCCESS;
+
+public class StatisticsCommandTest {
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
+    @Test
+    public void execute_statistics_success() throws CommandException {
+        CommandResult result = new StatisticsCommand().execute();
+        assertEquals(MESSAGE_SUCCESS, result.feedbackToUser);
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ToggleStatisticsPanelEvent);
+        assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
+    }
+}
