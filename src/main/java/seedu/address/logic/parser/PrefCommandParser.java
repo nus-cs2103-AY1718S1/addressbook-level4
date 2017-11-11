3fbb80001e00c1e2accf49225b5d940a0f9ff901
liuhang0213 <liuhang0213@gmail.com>
2017-11-11 22:21:21 +0800

    Shortnames for preference keys

--- src/main/java/seedu/address/logic/parser/PrefCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PrefCommandParser.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 //@@author liuhang0213
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 import seedu.address.logic.commands.PrefCommand;
@@ -11,6 +12,15 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses input arguments and creates a new PrefCommand object
  */
 public class PrefCommandParser implements Parser<PrefCommand> {
+
+    private HashMap<String, String> prefShortforms;
+    {
+        prefShortforms = new HashMap<>();
+        prefShortforms.put("dp", "DefaultProfilePhoto");
+        prefShortforms.put("theme", "Theme");
+        prefShortforms.put("abpath", "AddressBookFilePath");
+        prefShortforms.put("abname", "AddressBookName");
+    }
 
     /**
      * Parses the given {@code String} of arguments in the context of the PrefCommand
@@ -31,8 +41,21 @@ public class PrefCommandParser implements Parser<PrefCommand> {
             newPrefValue = splitArgs[1].trim();
         }
 
-        prefKey = splitArgs[0].trim();
+        prefKey = parsePrefShortcut(splitArgs[0].trim());
 
         return new PrefCommand(prefKey, newPrefValue);
+    }
+
+    /**
+     * Checks whether the given key is a short form for a preference key
+     * @param prefKey User's input value for preference key
+     * @return the actual key name if the input was shortcut, otherwise returns the input itself
+     */
+    private String parsePrefShortcut (String prefKey) {
+        if (prefShortforms.containsKey(prefKey)) {
+            return prefShortforms.get(prefKey);
+        } else {
+            return prefKey;
+        }
     }
 }
