dc031d2a6ac5255ac2bdc697ac4300444a7a3902
Haozhe <phaozhe@hotmail.com>
2017-11-11 13:06:28 +0800

    Improve test on deleting temporary contact

--- src/test/java/seedu/room/model/ResidentBookTest.java
+++ src/test/java/seedu/room/model/ResidentBookTest.java
@@ -1,7 +1,11 @@
 package seedu.room.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static seedu.room.testutil.TypicalPersons.ALICE;
+import static seedu.room.testutil.TypicalPersons.TEMPORARY_DOE;
+import static seedu.room.testutil.TypicalPersons.TEMPORARY_JOE;
 import static seedu.room.testutil.TypicalPersons.getTypicalResidentBook;
 
 import java.util.ArrayList;
@@ -16,9 +20,12 @@ import org.junit.rules.ExpectedException;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.room.commons.exceptions.IllegalValueException;
 import seedu.room.model.person.Person;
 import seedu.room.model.person.ReadOnlyPerson;
+import seedu.room.model.person.exceptions.PersonNotFoundException;
 import seedu.room.model.tag.Tag;
+import seedu.room.testutil.ResidentBookBuilder;
 
 public class ResidentBookTest {
 
@@ -68,6 +75,25 @@ public class ResidentBookTest {
         thrown.expect(UnsupportedOperationException.class);
         residentBook.getTagList().remove(0);
     }
+
+    //@@author Haozhe321
+    @Test
+    public void deleteTemporaryTest() {
+        ResidentBook residentBookOne = new ResidentBookBuilder().withPerson(TEMPORARY_JOE).build();
+
+        residentBookOne.deleteTemporary();
+        //added temporary has argument of 0, so it stays permanently -> returns false
+        assertFalse(residentBookOne.getPersonList().size() == 0);
+
+        ResidentBook residentBookTwo = new ResidentBookBuilder().withPerson(TEMPORARY_JOE).
+                withPerson(TEMPORARY_DOE).build();
+
+        residentBookTwo.deleteTemporary();
+        //added new temporary contact has expiry day of 1000 years, so it won't be deleted
+        assertTrue(residentBookTwo.getPersonList().size() == 2);
+
+    }
+    //@@author
 
     /**
      * A stub ReadOnlyResidentBook whose persons and tags lists can violate interface constraints.
