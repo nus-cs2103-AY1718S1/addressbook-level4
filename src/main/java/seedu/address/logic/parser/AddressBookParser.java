940c9e3c0f5393eb1311e73eed451b0dd404d404
Boo Kai Hsien <christopherboo66@gmail.com>
2017-11-11 17:53:45 +0800

    add NewCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,6 +17,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.NewCommand;
 import seedu.address.logic.commands.OpenCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.RemarkCommand;
@@ -93,6 +94,10 @@ public class AddressBookParser {
         case OpenCommand.COMMAND_SHORT:
         case OpenCommand.COMMAND_WORD:
             return new OpenCommandParser().parse(arguments);
+
+        case NewCommand.COMMAND_SHORT:
+        case NewCommand.COMMAND_WORD:
+            return new NewCommandParser().parse(arguments);
         //@@author
 
         case ExitCommand.COMMAND_WORD:
