5ee8e4f27d21ff31eca28b888f59f624bb851d46
LAPTOP-DBVHII6C\Leon <leonchowwenhao@gmail.com>
2017-11-11 17:48:10 +0800

    Edited for stylecheck.

--- src/test/java/seedu/address/logic/parser/SelectJoinedEventsCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SelectJoinedEventsCommandParserTest.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.parser;
 
-import java.util.Arrays;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.getTypicalPersonIndexList;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
