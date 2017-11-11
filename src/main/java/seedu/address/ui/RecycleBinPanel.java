d3e606e2405a59d84e20e65dd1903798d9f9e1ef
dalessr <dale.sunshengran@gmail.com>
2017-11-11 09:44:27 +0800

    Fix the coding style

--- src/main/java/seedu/address/ui/RecycleBinPanel.java
+++ src/main/java/seedu/address/ui/RecycleBinPanel.java
@@ -2,13 +2,13 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import javafx.scene.control.TabPane;
 import org.fxmisc.easybind.EasyBind;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.ReadOnlyPerson;
