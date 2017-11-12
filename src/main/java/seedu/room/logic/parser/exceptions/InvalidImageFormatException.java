d0466d02d455db2e50e2757a1be498143d8ea72f
Shi Tian <shitian007@hotmail.com>
2017-11-12 17:42:46 +0800

    Add InvalidImageFormatException

---
+++ src/main/java/seedu/room/logic/parser/exceptions/InvalidImageFormatException.java
@@ -0,0 +1,8 @@
+package seedu.room.logic.parser.exceptions;
+
+public class InvalidImageFormatException extends ParseException {
+
+    public InvalidImageFormatException(String message) {
+        super(message);
+    }
+}
