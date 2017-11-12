34019a529f90f3be5a9827da8b6ff92020fee33a
chernghann <chernghann.chia@gmail.com>
2017-11-12 13:15:40 +0800

    Edit Addeventcommandsystemtest.java

--- src/test/java/systemtests/AddEventCommandSystemTest.java
+++ src/test/java/systemtests/AddEventCommandSystemTest.java
@@ -33,7 +33,6 @@ public class AddEventCommandSystemTest extends AddressBookSystemTest {
 
     @Test
     public void add() throws Exception {
-        Model model = getModel();
         /* Case: add a person without tags to a non-empty address book, command with leading spaces and trailing spaces
          * -> added
          */
