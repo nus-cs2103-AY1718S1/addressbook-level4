f3b2e70cf92f87b77f799c3437aaf81527bc9200
caoliangnus <liangcao1213@yahoo.com>
2017-11-12 22:56:59 +0800

    update DG

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -54,7 +54,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing ModU ]===========================");
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
@@ -184,13 +184,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting ModU " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Address Book ] =============================");
+        logger.info("============================ [ Stopping ModU ] =============================");
         ui.stop();
         try {
             storage.saveUserPrefs(userPrefs);
