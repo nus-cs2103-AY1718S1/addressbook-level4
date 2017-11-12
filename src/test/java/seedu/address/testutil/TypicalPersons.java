0e3ada90420f389834ba16380b1bb5c76907c142
Tan Zhen Yong <Xenonym@users.noreply.github.com>
2017-11-13 02:59:32 +0800

    Add typical persons with relationships for storage testing

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -18,9 +18,16 @@ import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.relationship.ConfidenceEstimate;
+import seedu.address.model.relationship.Relationship;
+import seedu.address.model.relationship.RelationshipDirection;
+import seedu.address.model.relationship.exceptions.DuplicateRelationshipException;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
@@ -66,7 +73,41 @@ public class TypicalPersons {
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withRemark(VALID_REMARK_BOB)
             .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
 
+    // These persons have relationships for the purpose of testing storage mechanisms.
+    // they are separate so as not to affect other tests that assume persons are independent eg. system tests
+    public static final ReadOnlyPerson JAMES = new PersonBuilder().withName("James King").withPhone("65637492")
+            .withEmail("jamesk@example.com").withAddress("123 Griffin Rd").build();
+    public static final ReadOnlyPerson KELVIN = new PersonBuilder().withName("Kelvin Klein").withPhone("98762456")
+            .withEmail("kklein@example.com").withAddress("23 Mobius Strip").build();
+    public static final ReadOnlyPerson LISA = new PersonBuilder().withName("Lisa McAvoy").withPhone("65243679")
+            .withEmail("lisamc@example.com").withAddress("43 Trance Ave").build();
+
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    /**
+     * Static initializer is for adding relationships to persons after they are all instantiated.
+     */
+    static {
+        try {
+            Relationship undirectedFrom = new Relationship(JAMES, KELVIN, RelationshipDirection.UNDIRECTED,
+                    new Name("undirected"), new ConfidenceEstimate(29));
+            Relationship undirectedTo = new Relationship(KELVIN, JAMES, RelationshipDirection.UNDIRECTED,
+                    new Name("undirected"), new ConfidenceEstimate(29));
+            Relationship directed = new Relationship(KELVIN, LISA, RelationshipDirection.DIRECTED,
+                    new Name("directed"), new ConfidenceEstimate(30));
+
+            Person james = (Person) JAMES;
+            Person kelvin = (Person) KELVIN;
+            Person lisa = (Person) LISA;
+
+            james.addRelationship(undirectedFrom);
+            kelvin.addRelationship(undirectedTo);
+            kelvin.addRelationship(directed);
+            lisa.addRelationship(directed);
+        } catch (DuplicateRelationshipException | IllegalValueException e) {
+            assert false : "impossible to have invalid data in test data";
+        }
+    }
 
     private TypicalPersons() {} // prevents instantiation
 
@@ -100,11 +141,30 @@ public class TypicalPersons {
         return ab;
     }
 
+    /**
+     * Returns an {@code AddressBook} with all the typical persons and with some relationships
+     */
+    public static AddressBook getTypicalAddressBookWithRelationships() {
+        AddressBook ab = new AddressBook();
+        for (ReadOnlyPerson person : getTypicalPersonsWithRelationships()) {
+            try {
+                ab.addPerson(person);
+            } catch (DuplicatePersonException e) {
+                assert false : "not possible";
+            }
+        }
+        return ab;
+    }
+
     public static List<ReadOnlyPerson> getTypicalPersons() {
         return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
     }
 
     public static List<ReadOnlyPerson> getUnsortedTypicalPersons() {
         return new ArrayList<>(Arrays.asList(DANIEL, BENSON, CARL, ALICE, ELLE, FIONA, GEORGE));
+    }
+
+    public static List<ReadOnlyPerson> getTypicalPersonsWithRelationships() {
+        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE, JAMES, KELVIN, LISA));
     }
 }
