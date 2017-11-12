d409dbae5b7ea9b4f87537d60615c1a2ddc7a7ba
LeeYingZheng <LeeYingZheng@users.noreply.github.com>
2017-11-12 23:10:09 +0800

    no message

--- src/test/java/systemtests/AddCommandSystemTest.java
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -217,7 +217,6 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         assertApplicationDisplaysExpected("" , expectedResultMessage, expectedModel);
         assertSelectedCardUnchanged();
         assertCommandBoxShowsDefaultStyle();
-        
     }
 
     /**
