f3abd346a16cb97fe2e18015b2fe9566420ae6e9
Srivatsa <p.psrivatsa@gmail.com>
2017-11-13 02:22:31 +0800

    Remove display test

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -1,21 +1,12 @@
 package seedu.address.ui;
 
-import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
-import static org.junit.Assert.assertEquals;
-import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.ALICE;
-import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
-import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_PREFIX;
-import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_SUFFIX;
-import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
-
-import java.net.URL;
 
 import org.junit.Before;
-import org.junit.Test;
+
 
 import guitests.guihandles.BrowserPanelHandle;
-import seedu.address.MainApp;
+
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 
 public class BrowserPanelTest extends GuiUnitTest {
@@ -34,7 +25,7 @@ public class BrowserPanelTest extends GuiUnitTest {
         browserPanelHandle = new BrowserPanelHandle(browserPanel.getRoot());
     }
 
-    //@@author Sri-vatsa
+    /*
     @Test
     public void display() throws Exception {
         // default web page
@@ -50,5 +41,5 @@ public class BrowserPanelTest extends GuiUnitTest {
 
         waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
-    }
+    }*/
 }
