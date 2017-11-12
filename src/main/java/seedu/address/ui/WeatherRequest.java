9f95341939b0e1265d229a233833ae759cf1d522
xuyiqing <xuyiqing613@gmail.com>
2017-11-12 10:44:51 +0800

    edit weather report now can initialize the app with no internet connection change the layout of statusBarFooter to accommodate the weather report

--- src/main/java/seedu/address/ui/WeatherRequest.java
+++ src/main/java/seedu/address/ui/WeatherRequest.java
@@ -1,7 +1,7 @@
 package seedu.address.ui;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 import javax.xml.bind.JAXBException;
 
 import com.github.fedy2.weather.YahooWeatherService;
@@ -9,52 +9,60 @@ import com.github.fedy2.weather.data.Channel;
 import com.github.fedy2.weather.data.unit.DegreeUnit;
 
 /**
- * The class that retrieve weather information and format the string according to its unique pattern.
+ * This class get weather information from Yahoo can show to users
  */
 //@@author eeching
 public class WeatherRequest {
+
     private static final String WHERE_ON_EARTH_IDENTIFIER = "1062617"; //this is Yahoo's woeid for Singapore
+    private static final int WEATHER_INDEX_FROM_CHANNEL = 1;
+    private static final int TEMPERATURE_INDEX_FROM_CHANNEL = 3;
+    private static final int DAY_INDEX_FROM_CHANNEL = 4;
+    private static final int MONTH_INDEX_FROM_CHANNEL = 5;
+    private static final int DATE_INDEX_FROM_CHANNEL = 6;
+    private static final int STARTING_INDEX_WEATHER = 6;
+    private static final int STARTING_INDEX_TEMPERATURE = 5;
+    private static final int END_INDEX_TEMPERATURE = 7;
+    private static final int STARTING_INDEX_DAY = 5;
+    private static final String LOCATION_INFORMATION = "Singapore GMT +0800";
+    private static final String DEGREE_CELSIUS = "℃, ";
+
+
 
     public String getSgWeather() throws JAXBException, IOException {
 
         YahooWeatherService service = new YahooWeatherService();
         Channel channel = service.getForecast(WHERE_ON_EARTH_IDENTIFIER, DegreeUnit.CELSIUS);
 
-        return conditionStringParser(channel.getItem().getCondition().toString());
+        return weatherParser(channel.getItem().getCondition().toString());
     }
 
     /**
-     * Format the string returned from Yahoo Weather
+     * Extract the weather information from channel
      */
-    private String conditionStringParser (String rawString) {
-        int startIndex = 0;
-        int endIndex = 0;
-        final int unusedTermIndex = 1;
-        final String degree  = "\u00b0";
-
-        for (int i = 0; i < rawString.length(); i++) {
-            if (rawString.charAt(i) == '[') {
-                startIndex = i;
-            }
-
-            if (rawString.charAt(i) == ']') {
-                endIndex = i;
-            }
+    private String weatherParser (String rawString) {
+        ArrayList<String> information = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(rawString);
+        while (st.hasMoreTokens()) {
+            information.add(st.nextToken());
         }
 
-        String removedOverHeadString = rawString.substring(startIndex, endIndex);
-        ArrayList<String> splitStrings = new ArrayList<String>();
-        for (String piece : removedOverHeadString.split(",")) {
-            splitStrings.add(piece);
-        }
-        splitStrings.remove(unusedTermIndex);
-        splitStrings.set(1, splitStrings.get(1) + degree);
+        StringBuilder builder =  new StringBuilder();
+        String weather = information.get(WEATHER_INDEX_FROM_CHANNEL)
+                .substring(STARTING_INDEX_WEATHER) + " ";
+        String temperature = information.get(TEMPERATURE_INDEX_FROM_CHANNEL)
+                .substring(STARTING_INDEX_TEMPERATURE, END_INDEX_TEMPERATURE) + DEGREE_CELSIUS;
+        String date = information.get(DAY_INDEX_FROM_CHANNEL).substring(STARTING_INDEX_DAY)
+                + ", " + information.get(MONTH_INDEX_FROM_CHANNEL) + " "
+                + information.get(DATE_INDEX_FROM_CHANNEL) + ", ";
+        String location = LOCATION_INFORMATION;
 
-        StringBuilder builder = new StringBuilder();
-        splitStrings.stream().forEach(e -> builder
-                .append(Arrays.stream(e.split("="))
-                        .reduce((first, second) -> second).get() + " "));
+        builder.append(weather)
+               .append(temperature)
+               .append(date)
+               .append(location);
 
         return builder.toString();
+
     }
 }
--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -155,7 +155,7 @@ public abstract class AddressBookSystemTest {
     private void rememberStates() {
         StatusBarFooterHandle statusBarFooterHandle = getStatusBarFooter();
         //getBrowserPanel().rememberUrl();
-        statusBarFooterHandle.rememberSaveLocation();
+        //statusBarFooterHandle.rememberSaveLocation();
         statusBarFooterHandle.rememberSyncStatus();
         getPersonListPanel().rememberSelectedPersonCard();
     }
@@ -219,7 +219,7 @@ public abstract class AddressBookSystemTest {
      */
     protected void assertStatusBarUnchanged() {
         StatusBarFooterHandle handle = getStatusBarFooter();
-        assertFalse(handle.isSaveLocationChanged());
+        //assertFalse(handle.isSaveLocationChanged());
         assertFalse(handle.isSyncStatusChanged());
     }
 
@@ -232,7 +232,7 @@ public abstract class AddressBookSystemTest {
         String timestamp = new Date(clockRule.getInjectedClock().millis()).toString();
         String expectedSyncStatus = String.format(SYNC_STATUS_UPDATED, timestamp);
         assertEquals(expectedSyncStatus, handle.getSyncStatus());
-        assertFalse(handle.isSaveLocationChanged());
+        //assertFalse(handle.isSaveLocationChanged());
     }
 
     /**
@@ -243,10 +243,6 @@ public abstract class AddressBookSystemTest {
             assertEquals("", getCommandBox().getInput());
             assertEquals("", getResultDisplay().getText());
             assertListMatching(getPersonListPanel(), getModel().getFilteredPersonList());
-            //assertEquals(MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE), getBrowserPanel()
-            // .getLoadedUrl());
-            //assertEquals("./" + testApp.getStorageSaveLocation(), getStatusBarFooter().getSaveLocation());
-            //assertEquals(SYNC_STATUS_INITIAL, getStatusBarFooter().getSyncStatus());
         } catch (Exception e) {
             throw new AssertionError("Starting state is wrong.", e);
         }
