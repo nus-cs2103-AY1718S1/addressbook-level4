0a0a72e975b9f9ff63772b12f871ba7f92f2cc31
Eldric <eldriclim93@gmail.com>
2017-11-12 16:04:00 +0800

    Fix check style issue

--- src/main/java/seedu/address/commons/events/ui/EventPanelSelectionChangedEvent.java
+++ src/main/java/seedu/address/commons/events/ui/EventPanelSelectionChangedEvent.java
@@ -1,8 +1,5 @@
 package seedu.address.commons.events.ui;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.commons.events.BaseEvent;
