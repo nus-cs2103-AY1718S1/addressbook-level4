d545ae47e6f3a3f11178ecaa8313cf686c1050fd
Raisa Kabir <raisa.kabir201@gmail.com>
2017-11-13 02:21:36 +0800

    checkstyle error fixed

--- src/main/java/seedu/address/logic/parser/EditTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditTaskCommandParser.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.tasks.AddTaskCommand;
 import seedu.address.logic.commands.tasks.EditTaskCommand;
 import seedu.address.logic.commands.tasks.EditTaskCommand.EditTaskDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
