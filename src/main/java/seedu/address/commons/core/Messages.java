62babe108bf893799087b08c58cfb215750edcf2
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-11-13 12:18:14 +0800

    Add more autocomplete suggestions

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,10 +10,11 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
+    //@@author derickjw
     public static final String[] AUTOCOMPLETE_SUGGESTIONS = {"createDefaultAcc", "removeLogin", "changepw",
-                                                             "changeuser", "add",
-                                                             "visualize", "arrange", "list", "edit", "find",
+                                                             "changeuser", "convenientStation",
+                                                             "visualize", "arrange", "list", "locateMrt", "edit", "find",
                                                              "findByAddress", "findByPhone", "findByEmail",
                                                              "findByTags", "delete", "locate", "select", "history",
-                                                             "sort", "undo", "clear", "exit" };
+                                                             "sort", "undo", "clear", "exit", "schedule", };
 }
