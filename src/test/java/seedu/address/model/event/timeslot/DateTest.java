ccb030dc24482fcb8ce259b22faa19a67cee8e8d
Junkai <reginleiff@gmail.com>
2017-11-13 14:56:45 +0800

    Checkstyle: Fix violations

--- src/test/java/seedu/address/model/event/timeslot/DateTest.java
+++ src/test/java/seedu/address/model/event/timeslot/DateTest.java
@@ -11,40 +11,40 @@ import seedu.address.commons.exceptions.IllegalValueException;
 //@@author reginleiff
 public class DateTest {
 
-    private Date TEST_DATE_ONE;
-    private Date TEST_DATE_TWO;
-    private Date TEST_DATE_THREE;
-    private Date TEST_DATE_FOUR;
-    private Date TEST_DATE_FIVE;
-    private Date TEST_DATE_SIX;
-    private Date TEST_DATE_SEVEN;
-    private Date TEST_DATE_EIGHT;
+    private Date testDateOne;
+    private Date testDateTwo;
+    private Date testDateThree;
+    private Date testDateFour;
+    private Date testDateFive;
+    private Date testDateSix;
+    private Date testDateSeven;
+    private Date testDateEight;
 
     public DateTest() {
         try {
-            TEST_DATE_ONE = new Date("22/10/2017");
-            TEST_DATE_TWO = new Date("21/11/2017");
-            TEST_DATE_THREE = new Date("22/12/2018");
-            TEST_DATE_FOUR = new Date("30/10/2017");
-            TEST_DATE_FIVE = new Date("06/11/2017");
-            TEST_DATE_SIX = new Date("07/11/2017");
-            TEST_DATE_SEVEN = new Date("31/12/2017");
-            TEST_DATE_EIGHT = new Date("01/01/2018");
+            testDateOne = new Date("22/10/2017");
+            testDateTwo = new Date("21/11/2017");
+            testDateThree = new Date("22/12/2018");
+            testDateFour = new Date("30/10/2017");
+            testDateFive = new Date("06/11/2017");
+            testDateSix = new Date("07/11/2017");
+            testDateSeven = new Date("31/12/2017");
+            testDateEight = new Date("01/01/2018");
         } catch (IllegalValueException e) {
-            assert false: "Not supposed to occur";
+            assert false : "Not supposed to occur";
         }
     }
 
     @Test
     public void addDays_success() throws Exception {
         // Within month
-        assertEquals(TEST_DATE_FIVE.addDays(1), TEST_DATE_SIX);
+        assertEquals(testDateFive.addDays(1), testDateSix);
 
         // Bypassing months
-        assertEquals(TEST_DATE_FOUR.addDays(7), TEST_DATE_FIVE);
+        assertEquals(testDateFour.addDays(7), testDateFive);
 
         // Bypassing years
-        assertEquals(TEST_DATE_SEVEN.addDays(1), TEST_DATE_EIGHT);
+        assertEquals(testDateSeven.addDays(1), testDateEight);
     }
 
     @Test
@@ -73,10 +73,10 @@ public class DateTest {
     public void compareTo() throws Exception {
 
 
-        assertTrue(TEST_DATE_ONE.compareTo(TEST_DATE_TWO) < 0);
-        assertTrue(TEST_DATE_TWO.compareTo(TEST_DATE_THREE) < 0);
-        assertTrue(TEST_DATE_THREE.compareTo(TEST_DATE_ONE) > 0);
-        assertTrue(TEST_DATE_ONE.compareTo(TEST_DATE_ONE) == 0);
+        assertTrue(testDateOne.compareTo(testDateTwo) < 0);
+        assertTrue(testDateTwo.compareTo(testDateThree) < 0);
+        assertTrue(testDateThree.compareTo(testDateOne) > 0);
+        assertTrue(testDateOne.compareTo(testDateOne) == 0);
     }
 
 }
