3a47e084a66559d5d12bee194c8d8b59b74b7a21
APPLETEA\Huijia <hjc2304@gmail.com>
2017-11-13 23:31:52 +0800

    updated collate files

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -184,7 +184,7 @@ public class UniquePersonList implements Iterable<Person> {
     public void clearSchedule(Integer toClear, TreeSet<Integer> timeSpan) {
         internalList.get(toClear).clearSpanForSchedule(timeSpan);
     }
-
+    //@@author hj2304
     public void addEventTag(Integer toAdd, Tag event) {
         internalList.get(toAdd).addEventTag(event);
     }
