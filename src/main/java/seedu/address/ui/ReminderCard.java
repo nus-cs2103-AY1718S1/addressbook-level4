18d592e891347656ce2d3b25389a5ab5beb42c8d
LJH <jia_hao_95@hotmail.com>
2017-11-13 12:29:09 +0800

    Update colors for PersonCard and ReminderCard

--- src/main/java/seedu/address/ui/ReminderCard.java
+++ src/main/java/seedu/address/ui/ReminderCard.java
@@ -32,7 +32,7 @@ public class ReminderCard extends UiPart<Region> {
 
     private static final String FXML = "ReminderListCard.fxml";
 
-    private static String[] colors = { "red", "gold", "blue", "purple", "orange", "brown",
+    private static String[] colors = { "red", "blue", "purple", "orange", "brown",
         "green", "magenta", "black", "grey" };
     private static HashMap<String, String> tagColors = new HashMap<String, String>();
     private static Random random = new Random();
