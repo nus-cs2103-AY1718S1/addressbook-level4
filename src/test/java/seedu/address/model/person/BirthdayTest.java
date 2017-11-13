4c727d80165682da5282e32bf96f1b35a5b0fa24
LJH <jia_hao_95@hotmail.com>
2017-11-13 14:26:55 +0800

    Added test cases for BirthdayCommandTest, BirthdayCommandParserTest and BirthdayTest

--- src/test/java/seedu/address/model/person/BirthdayTest.java
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -30,5 +30,23 @@ public class BirthdayTest {
         // different person -> returns false
         Birthday differentBirthday = new Birthday("02/02/1992");
         assertFalse(birthday.equals(differentBirthday));
+
+        // Non-existent birthday -> returns false
+        assertFalse(Birthday.isValidBirthday("99/99/9999"));
+    }
+
+    @Test
+    public void validBirthday() {
+        // invalid date
+        assertTrue(Birthday.isValidBirthday("")); // empty string
+        assertTrue(Birthday.isValidBirthday("01/01/1991"));  // valid date
+
+        assertFalse(Birthday.isValidBirthday("0d/fd/199g")); // contains non-numeric characters
+        assertFalse(Birthday.isValidBirthday("10/28/1999 ")); // wrong date format
+        assertFalse(Birthday.isValidBirthday("1/1/1994")); // Insufficient digits
+        assertFalse(Birthday.isValidBirthday("40/01/1994")); // invalid day
+        assertFalse(Birthday.isValidBirthday("01/16/1995")); // invalid month
+        assertFalse(Birthday.isValidBirthday("01/01/20000")); // invalid year
+
     }
 }
