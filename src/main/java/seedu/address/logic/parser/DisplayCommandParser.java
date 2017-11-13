609a1e7a8145eac466cf00c1afd502659ff03cde
江春辉 <jiangchunhui007@gmail.com>
2017-11-13 18:51:18 +0800

    fix a bug

--- src/main/java/seedu/address/logic/parser/DisplayCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DisplayCommandParser.java
@@ -7,11 +7,14 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DisplayCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-//@@author
+//@@author Adoby7
 /**
  * Parses input arguments and creates a new DisplayCommand object
  */
 public class DisplayCommandParser implements Parser<DisplayCommand> {
+    public static final int INDEX_POSITION = 0;
+    public static final int PARTICULAR_POSITION = 1;
+    public static final int REQUIRED_LENGTH = 2;
 
     /**
      * Parses the given {@code String} of arguments in the context of the DisplayCommand
@@ -21,12 +24,14 @@ public class DisplayCommandParser implements Parser<DisplayCommand> {
     public DisplayCommand parse(String args) throws ParseException {
         try {
             String[] splitArgs = args.trim().split(" ");
-            Index index = ParserUtil.parseIndex(splitArgs[0]);
-            String particular = splitArgs[1];
+            if (splitArgs.length < REQUIRED_LENGTH) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DisplayCommand.MESSAGE_USAGE));
+            }
+            Index index = ParserUtil.parseIndex(splitArgs[INDEX_POSITION]);
+            String particular = splitArgs[PARTICULAR_POSITION].trim();
             return new DisplayCommand(index, particular);
         } catch (IllegalValueException ive) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DisplayCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DisplayCommand.MESSAGE_USAGE));
         }
     }
 }
