f13d26c994572390e1ac87d5f4d2385c7e614bfb
Dan <dnl166@gmail.com>
2017-11-13 17:42:08 +0800

    Added more tests to GoogleUtilTest Updated SampleData

--- src/test/java/seedu/address/commons/util/GoogleUtilTest.java
+++ src/test/java/seedu/address/commons/util/GoogleUtilTest.java
@@ -14,7 +14,7 @@ import seedu.address.testutil.PersonBuilder;
 public class GoogleUtilTest {
 
     @Test
-    public void convertFromGooglePerson_success() throws IllegalValueException {
+    public void convertFromGooglePersonWithAllFields_success() throws IllegalValueException {
         Person typicalPerson = new GooglePersonBuilder().build();
         seedu.address.model.person.Person typicalAddressBookPerson = new PersonBuilder().withTags("Google")
                 .withTwitter("").withInstagram("").build();
@@ -42,6 +42,53 @@ public class GoogleUtilTest {
     public void convertFromGooglePersonWithInvalidName_returnsNull() throws IllegalValueException {
         Person typicalPerson = new GooglePersonBuilder().withName("").build();
         assertEquals(null, GoogleUtil.convertPerson(typicalPerson));
+
+    }
+    @Test
+    public void convertFromGooglePersonWithNoPhoneList_returnsNull() throws IllegalValueException {
+        Person typicalPerson = new GooglePersonBuilder().noPhone().build();
+        assertEquals(null, GoogleUtil.convertPerson(typicalPerson));
+
+    }
+
+    @Test
+    public void convertFromGooglePersonWithNoEmail_success() throws IllegalValueException {
+        Person typicalPerson = new GooglePersonBuilder().withEmail("").build();
+        seedu.address.model.person.Person typicalAddressBookPerson = new PersonBuilder().withTags("Google")
+                .withTwitter("").withInstagram("").withEmail("").build();
+        assertEquals(typicalAddressBookPerson, GoogleUtil.convertPerson(typicalPerson));
+
+    }
+    @Test
+    public void convertFromGooglePersonWithNoNameList_returnsNull() throws IllegalValueException {
+        Person typicalPerson = new GooglePersonBuilder().noName().build();
+        assertEquals(null, GoogleUtil.convertPerson(typicalPerson));
+
+    }
+    @Test
+    public void convertFromGooglePersonWithNoEmailList_success() throws IllegalValueException {
+        Person typicalPerson = new GooglePersonBuilder().noEmail().build();
+        seedu.address.model.person.Person typicalAddressBookPerson = new PersonBuilder().withTags("Google")
+                .withTwitter("").withInstagram("").withEmail("").build();
+        assertEquals(typicalAddressBookPerson, GoogleUtil.convertPerson(typicalPerson));
+
+    }
+
+    @Test
+    public void convertFromGooglePersonWithNoBirthdayList_success() throws IllegalValueException {
+        Person typicalPerson = new GooglePersonBuilder().noBirthday().build();
+        seedu.address.model.person.Person typicalAddressBookPerson = new PersonBuilder().withTags("Google")
+                .withTwitter("").withInstagram("").withBirthday("").build();
+        assertEquals(typicalAddressBookPerson, GoogleUtil.convertPerson(typicalPerson));
+
+    }
+
+    @Test
+    public void convertFromGooglePersonWithNoAddressList_success() throws IllegalValueException {
+        Person typicalPerson = new GooglePersonBuilder().noAddress().build();
+        seedu.address.model.person.Person typicalAddressBookPerson = new PersonBuilder().withTags("Google")
+                .withTwitter("").withInstagram("").withAddress("").build();
+        assertEquals(typicalAddressBookPerson, GoogleUtil.convertPerson(typicalPerson));
 
     }
 }
