3133558e32d3eeb6aa8819a71ca2082cc56e6557
Nelson Quek <nelsonquek@r-87-102-25-172.comp.nus.edu.sg>
2017-11-13 16:43:33 +0800

    Fixing travis still

--- src/test/java/systemtests/FindCommandSystemTest.java
+++ src/test/java/systemtests/FindCommandSystemTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+
 //import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
