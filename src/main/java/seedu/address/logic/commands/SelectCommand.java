7fc8a62f6fef90428eb90744ce0414e878a8b9bd
Sarah Ng <sarahnzx@gmail.com>
2017-11-13 21:04:09 +0800

    remove comments

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -57,10 +57,8 @@ public class SelectCommand extends Command {
 
         if (socialType != null && !checkPersonHasSocialType(selectedPerson, socialType)) {
             // check to see if the social type matches any of the selected person's social media accounts
-            // if (!checkPersonHasSocialType(selectedPerson, socialType)) {
             // if the selected person does not have the requested social type, throw a command exception
             throw new CommandException(MESSAGE_SOCIAL_TYPE_NOT_FOUND);
-            // }
         }
 
         try {
