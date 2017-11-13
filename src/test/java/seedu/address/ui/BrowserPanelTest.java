0908edbddb32f5ef97b94c6eaea8d7905e3990af
Huang Chi Ying <e0032341@u.nus.edu>
2017-11-13 11:41:31 +0800

    Fix imports in BrowserPanelTest.java

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -2,11 +2,8 @@ package seedu.address.ui;
 
 import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
 import static org.junit.Assert.assertEquals;
-import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
-import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_PREFIX;
-import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_SUFFIX;
 import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 
 import java.net.URL;
@@ -39,6 +36,7 @@ public class BrowserPanelTest extends GuiUnitTest {
     public void display() throws Exception {
         // default web page
         URL expectedDefaultPageUrl = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedDefaultPageUrl, browserPanelHandle.getLoadedUrl());
     }
 }
