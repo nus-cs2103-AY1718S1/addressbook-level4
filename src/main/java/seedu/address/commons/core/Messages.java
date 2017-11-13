50be350d31a39c89f3f72f81b63285bee773035f
Unknown <vivekscl5@gmail.com>
2017-11-13 18:26:07 +0800

    Update testscripts

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_NO_PERSON_FOUND = "No results for %1$s. Showing results for %2$s" + " instead";
+    public static final String MESSAGE_NO_PERSON_FOUND = "No results for %1$s. Showing results for %2$s" + " instead.";
     public static final String MESSAGE_NO_MATCHING_NAME_FOUND = "No matching name found for %1$s. "
             + "Showing all contacts instead.";
 
