4e46382093da75e9cd029bbab7c4fffdb8c0aee3
Kennard Ng <Kennard_ng@outlook.com>
2017-11-18 21:54:37 +0800

    update Model comments for setActiveList

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -29,11 +29,11 @@ public interface Model {
     /**
      * Sets the active list in the model.
      *
-     * @param event the {@link JumpToTabRequestEvent} contains the index of the selected Tab. The select tab will
+     * @param event the {@link JumpToTabRequestEvent} contains the index of the selected Tab. The selected tab will
      *              provide information on the active list to select. If the value of
-     *              {@link JumpToTabRequestEvent#targetIndex} is zero, then the uncompleted parcel list is set as the
-     *              new active list. Otherwise, the completed parcel list is set as the active list
-     *              {@link ModelManager#activeParcels}
+     *              {@link JumpToTabRequestEvent#targetIndex} is zero, then {@link ModelManager#completedParcels}
+     *              is set as the new active list. Otherwise, the {@link ModelManager#uncompletedParcels} is set as
+     *              the active list {@link ModelManager#activeParcels}
      */
     void setActiveList(JumpToTabRequestEvent event);
     //@@author
